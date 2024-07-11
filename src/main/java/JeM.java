f6011f107f808938d72be93b7923e07ebc1fbf78
damithc <damithc@github>
2024-07-11 16:50:14 +0800

    Updated the Duke.java file to JeM.java

---
+++ src/main/java/JeM.java
@@ -0,0 +1,12 @@
+public class ChernoAlpha {
+    public static void main(String[] args) {
+        String logo = "      _         __  __ \n"
+                + "     | |       |  \\/  |\n"
+                + "     | |  ___  | |\\/| |\n"
+                + " _   | | / _ \\ | |  | |\n"
+                + "| |__| ||  __/ | |  | |\n"
+                + " \\____/  \\___| |_|  |_|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
+
