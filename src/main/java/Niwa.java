115b487e12cc80d90c2a87ea7d4abe2164fc1d3e
LinhHuynhLeDan <22520759@ms.uit.edu.vn>
2024-08-16 19:29:26 +0800

    Add new method printGreet

--- src/main/java/Niwa.java
+++ src/main/java/Niwa.java
@@ -7,5 +7,19 @@ public class Niwa {
                 + "| |\\  | | | | |/  |// //_| |\n"
                 + "|_| \\_|_|_| |__/|__/ //  |_|";
 
+        printGreet(name, logo); //Print greeting lines
+    }
+    public static void printHorizontalLine (int numDash)
+    {
+        for (int i = 0; i<numDash; i++) {
+            System.out.print("_"); //Print "_" numDash times
+        }
+        System.out.print("\n");
+    }
+    public static void printGreet(String name, String logo) {
+        System.out.println(logo);
+        printHorizontalLine(40);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
     }
 }
