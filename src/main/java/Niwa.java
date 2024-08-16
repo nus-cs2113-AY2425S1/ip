ea521f3c4d96c0c6fe267343d66db6f01ebb76ff
LinhHuynhLeDan <22520759@ms.uit.edu.vn>
2024-08-16 19:23:22 +0800

    Rename chatbot name from Duke to Niwa

---
+++ src/main/java/Niwa.java
@@ -0,0 +1,11 @@
+public class Niwa {
+    public static void main(String[] args) {
+        String name = "Niwa";
+        String logo = " _   _\n"
+                + "| \\ | | (_)  _       _  ___\n"
+                + "|  \\| | | | | | __  // //| |\n"
+                + "| |\\  | | | | |/  |// //_| |\n"
+                + "|_| \\_|_|_| |__/|__/ //  |_|";
+
+    }
+}
