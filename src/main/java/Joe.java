0e5eb0d3620caf1fc491255bfa5eae28cafebc1f
kennethSty <kenneth.styppa@web.de>
2024-08-17 17:04:21 +0800

    Rename chatbot to Joe and add .class to .gitignore

---
+++ src/main/java/Joe.java
@@ -0,0 +1,13 @@
+public class Joe {
+    public static void main(String[] args) {
+        String logo = "     _            \n"
+            + "    (_)           \n"
+            + "     _  ___   ___ \n"
+            + "    | |/ _ \\ / _ \\\n"
+            + "    | | (_) |  __/\n"
+            + "    | |\\___/ \\___|\n"
+            + "   _/ |           \n"
+            + "  |__/            \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
