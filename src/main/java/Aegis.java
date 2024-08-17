661e16f4822189971e372d276664b44c9432044a
ThisisXXZ <xxz.7203@gmail.com>
2024-08-17 18:43:17 +0800

    feat: Implement skeleton version of Aegis

---
+++ src/main/java/Aegis.java
@@ -0,0 +1,36 @@
+public class Aegis {
+    public static void main(String[] args) {
+        String logo = """
+                                                                            \s
+                        **                                                  \s
+                     *****                                  *               \s
+                    *  ***                                 ***              \s
+                       ***                                  *               \s
+                      *  **                                          ****   \s
+                      *  **          ***        ****      ***       * **** *\s
+                     *    **        * ***      *  ***  *   ***     **  **** \s
+                     *    **       *   ***    *    ****     **    ****      \s
+                    *      **     **    ***  **     **      **      ***     \s
+                    *********     ********   **     **      **        ***   \s
+                   *        **    *******    **     **      **          *** \s
+                   *        **    **         **     **      **     ****  ** \s
+                  *****      **   ****    *  **     **      **    * **** *  \s
+                 *   ****    ** *  *******    ********      *** *    ****   \s
+                *     **      **    *****       *** ***      ***            \s
+                *                                    ***                    \s
+                 **                            ****   ***                   \s
+                                             *******  **                    \s
+                                            *     ****                      \s
+                                                                            \s""";
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Hello, this is\n" + logo);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Hello! This is Aegis, an Anti-Shadow Suppression Weapon and a chatbot.");
+        System.out.println("What can I do for you?");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("--------------------------------------------------");
+    }
+}
