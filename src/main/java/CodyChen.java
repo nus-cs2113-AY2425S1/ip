693cb7338c4a4a83c63d07492f24677bb620c9fc
lowjunchen <e1138420@u.nus.edu>
2024-08-17 11:55:35 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/CodyChen.java
@@ -0,0 +1,12 @@
+public class CodyChen {
+    public static void main(String[] args) {
+        String logo = "________________________\n" +
+                "Hello! I'm CodyChen\n" +
+                "What can I do for you?\n" +
+                "________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "________________________\n";
+
+        System.out.println(logo);
+    }
+}
