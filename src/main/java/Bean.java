dab45ed0c65c392bb236957088cd80517c78e6bf
raneeng <raneengyinyee@gmail.com>
2024-08-19 01:08:05 +0800

    Renamed chatbot. Added greeting and exit.

---
+++ src/main/java/Bean.java
@@ -0,0 +1,29 @@
+public class Bean {
+    final static String line = "____________________________________________________________________\n";
+
+    public static void greet() {
+        String logo = "  ┏━┓\n" +
+                "  ┃ ┃\n" +
+                "  ┃ ┗━━┳━━━┳━━━━┳━━━┓\n" +
+                "  ┃  ┏┓┃ ┃━┫ ┏┓ ┃ ┏┓ ┓\n" +
+                "  ┃  ┗┛┃ ┃━┫ ┏┓ ┃ ┃┃ ┃ ┏━━━┓\n" +
+                "  ┗━━ ━┻━━━┻━┛┗━┻━┛┗━┛ ┗━━━┛\n";
+
+        System.out.println(line +
+                logo +
+                "Howdy, mate! :) I am Bean, your personal assistant.\nLet me know how I can help you out!\n" +
+                line
+        );
+    }
+
+    public static void exit() {
+        System.out.println(line +
+                " Bye bye, see you again... :(\n" +
+                line);
+    }
+
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+}
