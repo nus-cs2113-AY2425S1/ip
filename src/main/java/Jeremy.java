59516a10bed0d8a82458e7c331f1b52b4faecb03
yakultbottle <e1121147@u.nus.edu>
2024-08-19 00:21:11 +0800

    Add ASCII art of Jeremy

--- src/main/java/Jeremy.java
+++ src/main/java/Jeremy.java
@@ -1,4 +1,29 @@
 public class Jeremy {
+    public static void printSus() {
+        String amongus = "                                        \n"
+                + "                .-++++++-.              \n"
+                + "              -##+------+##+.           \n"
+                + "             +#+      ....+##+          \n"
+                + "            .##.  -####+++++###.        \n"
+                + "          ..+#+   ##+..      .+#-       \n"
+                + "        +#####-  .##--.........##       \n"
+                + "       +#+  ##-.  +##+++-+++++##+       \n"
+                + "       +#-.-##-.   -########+##+        \n"
+                + "       +#-.-##-.     ......  .##        \n"
+                + "       ##-.-##--.            -##        \n"
+                + "       ##---##---.          .-##        \n"
+                + "       +#-..##----...........-#+        \n"
+                + "       .##+-##--------------.+#-        \n"
+                + "        .+++##----.+#####+---##         \n"
+                + "            +#-.--.+#+.#+...-#+         \n"
+                + "            -#+----##+-##+++##-         \n"
+                + "          ++####################-       \n"
+                + "            ....------------...         \n"
+                + "                                        \n";
+
+        System.out.print(amongus);
+    }
+
     public static void main(String[] args) {
         String name = "Jeremy";
 
@@ -8,7 +33,7 @@ public class Jeremy {
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm " + name + ".");
         System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        printSus();
 
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
