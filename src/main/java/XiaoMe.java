b4334fc754fbed3a9b161076037843b1469d506b
gskang <windstick26@gmail.com>
2024-08-19 00:59:26 +0800

    Implement an initial skeletal version
    Let's create a chatbot that simnply greets the user and exits

--- src/main/java/XiaoMe.java
+++ src/main/java/XiaoMe.java
@@ -1,10 +1,18 @@
 public class XiaoMe {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________ \n"
+                        + "Hello! I'm XiaoMe\n"
+                        + "What can I do for you?\n"
+                        + "____________________________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________");
+
     }
 }
