cf57534d8b7f7793a44e6d33b02a31cd0f72986d
jemehgoh <jeremygoheh@gmail.com>
2024-08-18 17:50:09 +0800

    Implement greet and exit

--- src/main/java/Nell.java
+++ src/main/java/Nell.java
@@ -1,6 +1,25 @@
 public class Nell {
+    /**
+     * Prints a 40-character long horizontal line
+     */
+    public static void printLine() {
+        for (int i = 0; i < 40; i++)
+        {
+            System.out.print('_');
+        }
+
+        System.out.println();
+    }
+
     public static void main(String[] args) {
+        // Greets user
+        printLine();
         System.out.println("Hello! I'm Nell!");
         System.out.println("What can I do for you?");
+        printLine();
+
+        // Exits
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
     }
 }
