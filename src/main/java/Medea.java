f1385ea864b9c2b5942e56a020dd1d9b947495d1
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-08-19 13:35:29 +0800

    Rename Duke to Medea

---
+++ src/main/java/Medea.java
@@ -0,0 +1,6 @@
+public class Medea {
+    public static void main(String[] args) {
+        System.out.printf("Hello! I'm Medea%nWhat can I do for you?%n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
