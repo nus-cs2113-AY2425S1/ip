7be35a525a9ca6c4143bce11d4ed766e03f2983a
Terence Chan Zun Mun <zunmun@gmail.com>
2024-08-19 11:42:56 +0800

    Rename, add greet and exit

---
+++ src/main/java/Cuboyd.java
@@ -0,0 +1,8 @@
+public class Cuboyd {
+    public static void main(String[] args) {
+        String name = "Cuboyd";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
