b9892710056a7dec141b9045771da544029c34fd
NCF3535 <e1122972@u.nus.edu>
2024-08-19 21:57:57 +0800

    Rename Duke to Aether and implement basic greeting and exit

---
+++ src/main/java/Aether.java
@@ -0,0 +1,17 @@
+public class Aether {
+    public static void main(String[] args) {
+        String logo = "     ___   ______  _______  _    _  ______  _____   \n"
+                + "    /   | |  ____||__   __|| |  | ||  ____||  __ \\  \n"
+                + "   / /| | | |__      | |   | |__| || |__   | |__) | \n"
+                + "  / /_| | |  __|     | |   |  __  ||  __|  |  _  /  \n"
+                + " /  __  | | |____    | |   | |  | || |____ | | \\ \\  \n"
+                + "/_/   |_| |______|   |_|   |_|  |_||______||_|  \\_\\ \n";
+        String startScreen = "____________________________________________________\n"
+                + "Hello! I'm Aether, your friendly assistant.\n"
+                + "How can I help you today?\n"
+                +"____________________________________________________\n";
+        String EndScreen = "Goodbye! Hope to see you again soon!\n"
+                +"____________________________________________________\n";
+        System.out.println(logo + startScreen + EndScreen);
+    }
+}
