d4d8fc697ab61973c3bf5505684b1e4649cd8928
tayjerom <jerometayfw@gmail.com>
2024-08-19 23:28:00 +0800

    Level-0

---
+++ src/main/java/JerChatBot.java
@@ -0,0 +1,21 @@
+//public class JerChatBot {
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//    }
+//}
+
+public class JerChatBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm JerChatBot");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
