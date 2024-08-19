b2b7d6d8b5789f00d533867fdd5700b057599031
KHsienKit <hsienkitkhor@gmail.com>
2024-08-19 22:12:14 +0800

    Update gitignore to include class files and modified Chatbot name to V

---
+++ src/main/java/V.java
@@ -0,0 +1,20 @@
+public class V {
+
+    public static void greet() {
+        String logo = " _       _ \n"
+                + "\\ \\     / / \n"
+                + " \\ \\   / / \n"
+                + "  \\ \\_/ / \n"
+                + "   \\___/ \n";
+        System.out.print("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm V");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        greet();
+    }
+}
