da2c576e71f9a2b1d4fb7abad546959e144b601a
GYN <gao2351624311@gmail.com>
2024-08-20 16:30:00 +0800

    deleted original Duke Class

---
+++ src/main/java/Wildpeace.java
@@ -0,0 +1,14 @@
+public class Wildpeace {
+    public static void main(String[] args) {
+
+        String logo = "__        ___ _     _                           \n" +
+                      "\\ \\      / (_) | __| |_ __   ___  __ _  ___ ___ \n" +
+                      " \\ \\ /\\ / /| | |/ _` | '_ \\ / _ \\/ _` |/ __/ _ \\\n" +
+                      "  \\ V  V / | | | (_| | |_) |  __/ (_| | (_|  __/\n" +
+                      "   \\_/\\_/  |_|_|\\__,_| .__/ \\___|\\__,_|\\___\\___|\n" +
+                      "                     |_|                        ";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you");
+        System.out.println("See you next time, bye!");
+    }
+}
