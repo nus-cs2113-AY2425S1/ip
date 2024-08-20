0facceb9a64b491c7f3793808cbc43a04cf75f01
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-08-20 14:28:15 +0800

    Add user input echo functionality to Medea

--- src/main/java/Medea.java
+++ src/main/java/Medea.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Medea {
-    public static void main(String[] args) {
-        System.out.printf("Hello! I'm Medea%nWhat can I do for you?%n");
+
+    private static void greet(){
+        System.out.println("Hello! I'm Medea.");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void leave(){
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void echo(String command){
+        System.out.println(command);
+    }
+
+    private static void handleInput(String command){
+        echo(command);
+    }
+
+    public static void main(String[] args) {
+        greet();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")){
+            handleInput(input);
+            input = sc.nextLine();
+        }
+        leave();
     }
 }
