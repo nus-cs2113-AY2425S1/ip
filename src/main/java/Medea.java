bd5203319a71c9f54b5108254e38e0544f1f5660
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-08-20 14:56:18 +0800

    Add Todo.java to provide create and list todo functionality

--- src/main/java/Medea.java
+++ src/main/java/Medea.java
@@ -11,20 +11,21 @@ public class Medea {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private static void echo(String command){
-        System.out.println(command);
-    }
-
-    private static void handleInput(String command){
-        echo(command);
+    private static void handleInput(String command, Todo todo){
+        if (command.equals("list")){
+            todo.listTodos();
+        }else{
+            todo.addTodo(command);
+        }
     }
 
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
+        Todo todo = new Todo();
         String input = sc.nextLine();
         while(!input.equals("bye")){
-            handleInput(input);
+            handleInput(input, todo);
             input = sc.nextLine();
         }
         leave();
---
+++ src/main/java/Todo.java
@@ -0,0 +1,20 @@
+public class Todo {
+    private static String[] todoList;
+    private static int currTodo;
+
+    public Todo(){
+        currTodo = 0;
+        todoList = new String[100];
+    }
+
+    public void listTodos(){
+        for (int i = 0; i < currTodo; i++){
+            System.out.printf("%d. %s%n", i + 1 , todoList[i]);
+        }
+    }
+
+    public void addTodo(String todo){
+        System.out.printf("Added: %s%n",todo);
+        todoList[currTodo++] = todo;
+    }
+}
