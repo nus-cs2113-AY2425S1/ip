2f097472258774697da4c061022096b2598f5f63
Littleme1032 <117549420+Littleme1032@users.noreply.github.com>
2024-08-21 02:47:22 +0800

    Create Bebe.java
    add Bebe.java

---
+++ src/main/java/Bebe.java
@@ -0,0 +1,8 @@
+public class Bebe {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bebe");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+}
