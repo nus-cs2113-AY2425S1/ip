c734b2b6c966cc5150295ddec0e79d2e0cad5520
Littleme1032 <117549420+Littleme1032@users.noreply.github.com>
2024-08-21 03:22:22 +0800

    Update Bebe.java
    added level 1, where it can echo and exit when given the  text bye or BYE

--- src/main/java/Bebe.java
+++ src/main/java/Bebe.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
+
 public class Bebe {
+
     public static void main(String[] args) {
+        // main function to run the chatbot
+        welcomeMessage();
+        runChatbot();
+        exitMessage();
+    }
+
+    private static void welcomeMessage(){
+        //Function is used to print out the welcome message
         System.out.println("Hello! I'm Bebe");
         System.out.println("What can I do for you?");
+    }
+
+    private static void exitMessage(){
+        //Function is used to print out exit message
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    private static void runChatbot() {
+        //Function is used to execute the chatbot functions
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine().trim();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            echoInput(userInput);
+        }
+
+        scanner.close();
+    }
+    
+    private static void echoInput(String input) {
+        // Function is used to echo the user input
+        System.out.println(input);
+    }
 }
