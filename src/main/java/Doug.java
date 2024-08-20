10d475b93740cae101f44f3d9ef37085e0c93d78
KSanjith <kodeeswaransanjith@gmail.com>
2024-08-20 17:55:39 +0800

    Rename chatbot to Doug Add greet and exit skeleton code

--- src/main/java/Doug.java
+++ src/main/java/Doug.java
@@ -1,10 +1,16 @@
-public class Duke {
+public class Doug {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+         */
+        String line = "_______________________________________\n";
+        System.out.println(line + "Hello! I'm Doug Dimmadome\n" + "What can I do for you?\n"
+                         + line + "Bye. Hope to see you again soon!\n" + line);
+
     }
 }
