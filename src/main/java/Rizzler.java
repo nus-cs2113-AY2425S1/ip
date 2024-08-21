2537f590273bd082f401c82ecfec445a541443e2
nirala-ts <tanishka.nirala@gmail.com>
2024-08-22 00:28:43 +0800

    update chat name

--- src/main/java/Rizzler.java
+++ src/main/java/Rizzler.java
@@ -1,13 +1,18 @@
-public class Brah {
+public class Rizzler {
     public static void main(String[] args) {
-        String logo = " ____             _     \n"
-                + "| __ )  __ _ _ __| | __ \n"
-                + "|  _ \\ / _` | '__| |/ / \n"
-                + "| |_) | (_| | |  |   <  \n"
-                + "|____/ \\__,_|_|  |_|\\_\\ \n";
+        String logo =
+                  " _____                          _ \n"
+                + "|     \\                        | | \n"
+                + "| |_)  |      ______   ______  | |   ____    _  __ \n"
+                + "|     /  (_) |__   /  |__   /  | | /      \\ | |/  \\ \n"
+                + "| |\\ \\   | |   /  /     /  /   | | |  ____/ |  _/\\_\\ \n"
+                + "| | \\ \\  | |  /  / __  /  / __ | | | |____  | |  \n"
+                + "|_|  \\_\\ |_| /______/ /______/ |_|  \\_____| |_| \n";
+
+
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello! I'm Brah.");
+        System.out.println("Hello! I'm Rizzler.");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
     }
