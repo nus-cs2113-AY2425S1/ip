a79531f9aa039db046f9c72f400601ae7a54bb61
Kenneth <63093454+Chinorea@users.noreply.github.com>
2024-08-22 00:01:19 +0800

    Level-0

---
+++ src/main/java/Ellio.java
@@ -0,0 +1,11 @@
+public class Ellio {
+    public static void main(String[] args) {
+        
+        String lineborder = "______________________________________\n";
+        String welcome = "Hello! I'm Ellio\nWhat can I do for you?";
+        String goodbye = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(lineborder + welcome);
+        System.out.println(lineborder + goodbye + lineborder);
+    }
+}
