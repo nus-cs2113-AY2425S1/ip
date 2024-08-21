0e589b094bf6291136e7cbf036e88e0cf127abc2
Metanyu <nguyenthu.vu2511@gmail.com>
2024-08-22 07:16:56 +0800

    Add greet and exit Amy bot

---
+++ src/main/java/Amy.java
@@ -0,0 +1,10 @@
+public class Amy {
+    public static void main(String[] args) {
+        String name = "Amy";
+        String greet = "Hello! I'm " + name + "\nWhat can I do for you? \n";
+        String filler = "____________________________________________________________" + "\n";
+        String bye = "Bye. Hope to see you again soon!\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println(filler + greet + filler + bye + filler);
+    }
+}
