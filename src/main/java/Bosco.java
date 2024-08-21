2771f649fe1f76fe0398aacc6a1713c3e0f0890d
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-08-21 23:18:37 +0800

    Rename chatbot and add greeting to Bosco.java

---
+++ src/main/java/Bosco.java
@@ -0,0 +1,8 @@
+public class Bosco {
+    public static void main(String[] args) {
+        String hr = "____________________________________________________________";
+        System.out.println(hr + "\nHello! I'm Bosco the All-Purpose Drone.");
+        System.out.println("What can I do for you?\n" + hr);
+        System.out.println("Bye. Hope to see you again soon!\n" + hr);
+    }
+}
