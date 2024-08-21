f02ef27d26b3ac688e611cf76d6aab2d5c55ce4e
ChunJia <chiew_chunjia@u.nus.edu>
2024-08-21 08:31:05 +0800

    Rename chatbot, Add greet and exit

---
+++ src/main/java/Appal.java
@@ -0,0 +1,8 @@
+public class Appal {
+    public static void main(String[] args) {
+        String chatbot = "Appal";
+        System.out.println("Hello! I'm " + chatbot + "!");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
