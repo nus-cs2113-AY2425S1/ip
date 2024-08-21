c80b3eae393eef00af3363c2948612ee12788a12
nirala-ts <tanishka.nirala@gmail.com>
2024-08-22 00:23:32 +0800

    update chat name

---
+++ src/main/java/Rizzler.java
@@ -0,0 +1,15 @@
+public class Brah {
+    public static void main(String[] args) {
+        String logo = " ____             _     \n"
+                + "| __ )  __ _ _ __| | __ \n"
+                + "|  _ \\ / _` | '__| |/ / \n"
+                + "| |_) | (_| | |  |   <  \n"
+                + "|____/ \\__,_|_|  |_|\\_\\ \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Brah.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
