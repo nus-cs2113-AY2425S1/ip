ec0b9384c7d0dd93247aadcfd1eec69b95f54807
yijiano <zhangyijian2002@gmail.com>
2024-08-21 12:18:00 +0800

    Add ASCII logo

--- src/main/java/Akshan.java
+++ src/main/java/Akshan.java
@@ -1,15 +1,28 @@
 public class Akshan {
 
+    private static final String name = "Akshan";
+    private static final String logo = """
+ 
+             ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░ \s
+            ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░\s
+            ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░\s
+            ░▒▓████████▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░\s
+            ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░\s
+            ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░\s
+            ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░\s
+            """;
+
+
     private static void printLine(){
-        System.out.println("\n___________________________________\n");
+        System.out.println("_______________________________________________________________");
     }
     public static void main(String[] args) {
-        String name = "Akshan";
+        System.out.println(logo);
         Akshan.printLine();
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         Akshan.printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println( "Bye. Hope to see you again soon!");
         
     }
 }
