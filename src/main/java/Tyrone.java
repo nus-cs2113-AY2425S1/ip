c1037daab0d828117e0cc3ae902d1d08c1f0d3bb
paulktham <134585908+paulktham@users.noreply.github.com>
2024-08-21 04:41:31 +0000

    Level-0-a

---
+++ src/main/java/Tyrone.java
@@ -0,0 +1,15 @@
+public class Tyrone {
+    public class Constants{
+        public static final String line = "___________________________________";
+        public static final String Whatcanido ="What can I do for you?";
+    }
+    public static void main(String[] args) {
+        String logo = "tyrone";
+        System.out.println(Constants.line);
+        System.out.println("Hello from\n" + logo);
+        System.out.println(Constants.Whatcanido);
+        System.out.println(Constants.line);
+        System.out.println("see you brother");
+        System.out.println(Constants.line);
+    }
+}
