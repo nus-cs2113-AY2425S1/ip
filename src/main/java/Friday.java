24f2b5dc782f4018bf5535b35cd80183a0a356f6
coraleaf0602 <zhangco@tcd.ie>
2024-08-21 10:57:08 +0800

    add Friday.java and greeting messages

---
+++ src/main/java/Friday.java
@@ -0,0 +1,20 @@
+public class Friday {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        String logo =
+                " ________ .-------.   .-./`)  ______        ____       ____     __  \n"
+                        + "|        ||  _ _   \\  \\ .-.')|    _ `''.  .'  __ `.    \\   \\   /  / \n"
+                        + "|   .----'| ( ' )  |  / `-' \\| _ | ) _  \\/   '  \\  \\    \\  _. /  '  \n"
+                        + "|  _|____ |(_ o _) /   `-'`\"`|( ''_'  ) ||___|  /  |     _( )_ .'   \n"
+                        + "|_( )_   || (_,_).' __ .---. | . (_) `. |   _.-`   | ___(_ o _)'    \n"
+                        + "(_ o._)__||  |\\ \\  |  ||   | |(_    ._) '.'   _    ||   |(_,_)'     \n"
+                        + "|(_,_)    |  | \\ `'   /|   | |  (_.\\.' / |  _( )_  ||   `-'  /      \n"
+                        + "|   |     |  |  \\    / |   | |       .'  \\ (_ o _) / \\      /       \n"
+                        + "'---'     ''-'   `'-'  '---' '-----'`     '.(_,_).'   `-..-'        \n";
+        System.out.println(logo + "\n" +"Hello, I'm Friday!");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
