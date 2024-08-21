69dd919a25aaaca28a03b34c6466a6277738bfec
yijiano <zhangyijian2002@gmail.com>
2024-08-21 12:15:00 +0800

    Rename, add greet and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Akshan User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Akshan.java
@@ -0,0 +1,15 @@
+public class Akshan {
+
+    private static void printLine(){
+        System.out.println("\n___________________________________\n");
+    }
+    public static void main(String[] args) {
+        String name = "Akshan";
+        Akshan.printLine();
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        Akshan.printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        
+    }
+}
