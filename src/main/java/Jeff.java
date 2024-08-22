c75c0b0bab2908125fdd87f25a9f765cd0d8b3f9
TheDinos <marcussvps2002@gmail.com>
2024-08-22 15:53:45 +0800

    Greets user and exits

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,12 @@
+public class Jeff {
+    public static void main(String[] args) {
+        String  text= "____________________________________________________________\n" +
+        "Hello! I'm JEFF\n" +
+        "What can I do for you?\n" +
+        "____________________________________________________________\n" +
+        "Bye. Hope to see you again soon!\n" +
+        "____________________________________________________________\n";
+
+        System.out.println(text);
+    }
+}
