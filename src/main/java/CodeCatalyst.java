30d6687196fd591f6a147c30afcea55927e2e9b3
CheeKiangg <ngchkiang@gmail.com>
2024-08-22 17:05:24 +0800

    Change Duke to CodeCatalyst

---
+++ src/main/java/CodeCatalyst.java
@@ -0,0 +1,7 @@
+public class CodeCatalyst {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm CodeCatalyst.");
+        System.out.println("What can I do for you?  \n");
+        System.out.println("Bye. Hope to see you again soon!  \n");
+    }
+}
