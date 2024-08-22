199242eab6daafa8f16b1c3abe939dc4a4d26792
hzxnancy <hzxnancy@gmail.com>
2024-08-22 18:51:01 +0800

    renamed chatbot to Cy

---
+++ src/main/java/Cy.java
@@ -0,0 +1,9 @@
+public class Cy {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm Cy");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
