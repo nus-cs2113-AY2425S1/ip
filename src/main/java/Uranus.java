75828bf5f3361352b28bb2833bdd634e7877b05e
C2linaung <c2linaung.main@gmail.com>
2024-08-22 17:16:20 +0800

    Complete Level-0

---
+++ src/main/java/Uranus.java
@@ -0,0 +1,10 @@
+public class Uranus {
+    public static void main(String[] args) {
+        System.out.println("_________________________________________________________");
+        System.out.println("Hello! I'm Uranus, the only Chatbot you'll ever need.");
+        System.out.println("How can I be of service?");
+        System.out.println("_________________________________________________________");
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println("_________________________________________________________");
+    }
+}
