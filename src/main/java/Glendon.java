a87e08d42012cd8195ed1f89903d2e3035728350
g13nd0n-admin <e0957797@u.nus.edu>
2024-08-22 11:41:39 +0800

    replaced Duke with Glendon

---
+++ src/main/java/Glendon.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics;
+
+public class Glendon {
+    public static void main(String[] args) {
+        String logo = " _______   _                     _\n"
+                + "| ______| | |                   | |\n"
+                + "| |  ___  | |  ___   _____   ___| |  _____   _____\n"
+                + "| | |__ | | | / _ \\ |  _  | |  _  | |  _  | |  _  |\n"
+                + "| |___| | | | | __/ | | | | | |_| | | |_| | | | | |\n"
+                + "|______ | |_| \\___| |_| |_| |_____| |_____| |_| |_|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Glendon.\n" + "What can I do for you?\n" );
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
