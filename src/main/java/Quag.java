97d3e2efb1c66edfb7ea62663590eafe692d23ea
Jaden Lim <jadenlimjc@gmail.com>
2024-08-22 17:40:25 +0800

    renamed files

---
+++ src/main/java/Quag.java
@@ -0,0 +1,21 @@
+public class Quag {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+
+//        String logo = " ____        _        \n"
+//                + "/  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "\\____ \\__,_|_|\\_\\___|\n";
+        System.out.println("_______________________________________");
+        System.out.println("Hello! I'm QUAG!");
+        System.out.println("What can I do for you today? quag");
+        System.out.println("_______________________________________");
+        System.out.println("See you soon! quag quag");
+        System.out.println("_______________________________________");
+    }
+}
