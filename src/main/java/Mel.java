0e4f6ac73da0f3f6028ce13cf36adbf9c106be3d
yeekian <53704991+yeekian@users.noreply.github.com>
2024-08-22 22:51:42 +0800

    Chatbot: rename Duke to Mel
    Remove all traces of Duke from the source code.
    
    Rename Duke.java to Mel.java.
    Replace Duke with Mel in Mel.java and README.md.
    
    Ensure chatbot name consistency.

---
+++ src/main/java/Mel.java
@@ -0,0 +1,11 @@
+public class Mel {
+    public static void main(String[] args) {
+        System.out.println("________________________________________");
+        System.out.println("Hello! I'm Mel");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________");
+
+    }
+}
