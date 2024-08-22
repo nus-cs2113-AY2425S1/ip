24dd1c834a1b8b3fdb67dba59351ee08df07b51b
Ajay Shanker <ajayshanker1998@gmail.com>
2024-08-22 15:56:21 +0800

    Add method to print breakline and exit

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,4 +1,14 @@
 public class Jarvis {
+    static String chatBotName = "Jarvis"; // Name of the chatbot
+
+    public static void printBreakLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void exit(int status) {
+        System.exit(status);
+    }
+
     public static void main(String[] args) {
         String logo = """
                      ██╗    █████╗    ██████╗   ██╗   ██╗  ██╗   ███████╗
