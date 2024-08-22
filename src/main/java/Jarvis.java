07de1ac22482791088b0ffcd461778ed11236941
Ajay Shanker <ajayshanker1998@gmail.com>
2024-08-22 16:14:51 +0800

    :tada: Level-0
    - Completed Level-0 Assignment
    - Rename README info

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -2,11 +2,23 @@ public class Jarvis {
     static String chatBotName = "Jarvis"; // Name of the chatbot
 
     public static void printBreakLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("────────────────────────────────────────────────────────────");
     }
 
     public static void exit(int status) {
         System.exit(status);
+    }
+
+    public static void printGreetingMsgs() {
+        String[] greetings = {"Hello! I'm " + chatBotName + "\nWhat can I do for you?",
+                "Bye. Hope to see you again soon!",};
+
+        for (String greeting : greetings) {
+            printBreakLine();
+            System.out.println(greeting);
+        }
+        printBreakLine();
+        exit(0);
     }
 
     public static void main(String[] args) {
@@ -19,5 +31,6 @@ public class Jarvis {
                  ╚════╝ ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝ ╚═══╝ ╚═╝╚═╝╚═╝╚══════╝╚═╝
                 """;
         System.out.println("Hello from\n" + logo);
+        printGreetingMsgs();
     }
 }
