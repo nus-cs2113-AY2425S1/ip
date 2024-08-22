572029aec4d8d1042002890161ee7e53698c18a6
ZiliaAJY <141621081+ZiliaAJY@users.noreply.github.com>
2024-08-22 13:50:23 +0800

    Add rename, greet, exit features

---
+++ src/main/java/Conglo.java
@@ -0,0 +1,9 @@
+public class Conglo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Conglo.");
+        System.out.println("What brings you here today?");
+        System.out.println("===========================");
+        System.out.println("Goodbye. See you next time!");
+    }
+}
