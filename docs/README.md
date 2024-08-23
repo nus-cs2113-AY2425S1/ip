9bf86d291936d82bda87580aaae1fccdb4c69824
naveen42x <naveensuresh101@gmail.com>
2024-08-23 15:15:44 +0800

    Rename Duke to Grok and implement initial skeletal version

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Grok User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Grok.java
@@ -0,0 +1,7 @@
+public class Grok {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Grok");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
