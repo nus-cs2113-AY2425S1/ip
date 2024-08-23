dc1e6fb3265d7484314fce0a019555d3ce1335f7
Sukkaito <40479420+Sukkaito@users.noreply.github.com>
2024-08-23 21:00:06 +0800

    Rename to SuBOT, add basic greets and exits

---
+++ src/main/java/SuBOT.java
@@ -0,0 +1,17 @@
+public class SuBOT {
+    public static void main(String[] args) {
+        String logo = """
+                       ____  ____  ______
+           _______  __/ __ )/ __ \\/_  __/
+          / ___/ / / / __  / / / / / /
+         (__  ) /_/ / /_/ / /_/ / / /
+        /____/\\__,_/_____/\\____/ /_/
+        """;
+
+        System.out.println(logo);
+        System.out.println("How can I help you today?");
+        System.out.println("-------------------------------");
+        System.out.println("System shutting down...");
+        System.out.println("Goodbye world!");
+    }
+}
