aceef5469ca82a0045e52ceb402ae0571b62802b
Ryan Harwood <ryan.harwood3643@gmail.com>
2024-08-23 07:31:24 +0000

    Level-0

---
+++ src/main/java/Doot.java
@@ -0,0 +1,6 @@
+public class Doot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm  Doot\n What can I do for you?\n")
+        System.out.println("Bye. Hope to see you again soon!")
+    }
+}
