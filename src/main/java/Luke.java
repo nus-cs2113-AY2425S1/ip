05566c038bf102da72c3b4e3fa4764343b48487e
wx-03 <tngwenxi@gmail.com>
2024-08-23 23:01:07 +0800

    Rename and implement greeting

---
+++ src/main/java/Luke.java
@@ -0,0 +1,11 @@
+public class Luke {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Luke\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+}
