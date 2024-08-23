beabdec84efdcb4b894f8a58ab00e95ef2fe61e8
kestryix <verilim3802@gmail.com>
2024-08-23 15:15:06 +0800

    Rename, greet, exit

---
+++ src/main/java/TulipTask.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class TulipTask {
+    public static void main(String[] args) {
+        String logo = "                                         \n" +
+                "--.--     |    o     --.--          |    \n" +
+                "  |  .   .|    .,---.  |  ,---.,---.|__/ \n" +
+                "  |  |   ||    ||   |  |  ,---|`---.|  \\ \n" +
+                "  `  `---'`---'`|---'  `  `---^`---'`   `\n" +
+                "                |";
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        System.out.println(logo);
+
+        System.out.println("--------------------------------------------");
+        System.out.println("Hello, I'm TulipTask");
+        System.out.println("What can I do for you today?");
+        System.out.println("--------------------------------------------");
+
+        System.out.println("Bye! Hope to see you again soon :)");
+        System.out.println("--------------------------------------------");
+
+    }
+}
