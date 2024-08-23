c60357419632779eb10228aa7682287f866903b5
Holy-An <anderson96728106@gmail.com>
2024-08-23 15:24:17 +0800

    Refactor Duke greeting to personal Ronaldo greeting

---
+++ src/main/java/Ronaldo.java
@@ -0,0 +1,6 @@
+public class Ronaldo {
+    public static void main(String[] args) {
+        String indent = "___________________________________________ \n";
+        System.out.println( indent + "Hello! I'm Ronaldo, Cristiano Ronaldo! The greatest footballer of all time. SIUUUUUUU! \n"+ "Hehehe what can I do for you? \n"  + indent + "Bye. Hope to see you soon! SIUUUUUUU!\n" + indent);
+    }
+}
