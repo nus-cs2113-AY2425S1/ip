e392379a6f4b317928fe21aba9aa0a9f32b61e88
parasytezz <gvnchua@gmail.com>
2024-08-23 12:48:08 +0800

    Renamed, added greeting and exit

---
+++ src/main/java/Sleepy.java
@@ -0,0 +1,18 @@
+public class Sleepy {
+    public static void main(String[] args) {
+        String logo = "  ____   _      ____   ____  ____   __    __\n"
+                + " / ___| | |    | ___| | ___| |    \\ \\ \\  / /\n"
+                + " \\___ \\ | |    | ===| | ===| | D  |  \\ V  /\n"
+                + "  ___) || |___ | |__  | |__  |   _/   |  |\n"
+                + " |____/ |____| |____| |____| |__|     |__|\n";
+
+        System.out.println("____________________________________________________________\n" + logo);
+        String greeting = "Hello! I'm Sleepy\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(greeting);
+        String goodbye = "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(goodbye);
+    }
+}
