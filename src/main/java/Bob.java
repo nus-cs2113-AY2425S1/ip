d49fe59d36e9d7599def7517e7da3cdacd7b1adc
e0968962@u.nus.edu <e0968962@u.nus.edu>
2024-08-23 14:19:29 +0800

    Level 0: Rename, greet and exit

---
+++ src/main/java/Bob.java
@@ -0,0 +1,13 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo = "  ____        _     \n"
+                + " | |_) \\ ___ | |___  \n"
+                + " |  _ //  _  \\   _ \\ \n"
+                + " | |_)\\\\ (_) /  |_) |\n"
+                + " |____/ \\___/|_|___/ \n";
+        System.out.println(logo);
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
