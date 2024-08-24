30f79e20972061871212be621a7905680d686d2b
ThisisXXZ <xxz.7203@gmail.com>
2024-08-24 23:20:12 +0800

    Add mark and unmark functions for Aegis skeletal version

--- src/main/java/Aegis.java
+++ src/main/java/Aegis.java
@@ -36,18 +36,37 @@ public class Aegis {
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------------------------");
 
-        ArrayList<String> promptHistory = new ArrayList<String>();
+        ArrayList<Task> taskList = new ArrayList<>();
+
         while (true) {
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
-                for (int i = 0; i < promptHistory.size(); ++i) {
-                    System.out.printf(" %d: %s%n", i + 1, promptHistory.get(i));
+                System.out.println(" I could do much more than checking your list, but, if you wish:");
+                for (int i = 0; i < taskList.size(); ++i) {
+                    System.out.printf(" %d.%s%n", i + 1, taskList.get(i));
+                }
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskList.size()) {
+                    taskList.get(taskIndex).markAsDone();
+                    System.out.printf(" I've marked this task as done:%n   %s%n", taskList.get(taskIndex));
+                } else {
+                    System.out.println(" Invalid task number.");
+                }
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskList.size()) {
+                    taskList.get(taskIndex).unmarkAsDone();
+                    System.out.printf(" I've marked this task as not done yet:%n   %s%n", taskList.get(taskIndex));
+                } else {
+                    System.out.println(" Invalid task number.");
                 }
             } else {
-                promptHistory.add(input);
+                Task newTask = new Task(input);
+                taskList.add(newTask);
                 System.out.println(" added: " + input);
             }
 
