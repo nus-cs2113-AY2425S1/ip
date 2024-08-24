2597d8b86515ab6da1a94ce317bd0c4e76774b12
isaacsaw25 <isaacsaw25@gmail.com>
2024-08-24 16:01:56 +0800

    Refactor: Rename chatbot to Cheonsa Bot

---
+++ src/main/java/CheonsaBot.java
@@ -0,0 +1,21 @@
+public class CheonsaBot {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        String logo = """
+                       (\\ -=- /)
+                       ( \\( )/ )
+                       (       )
+                        `>   <'
+                        /     \\
+                        `-._.-'
+                       """;
+        System.out.println(horizontalLine);
+        System.out.println("Hello, I'm 천사봇! (AngelBot)");
+        System.out.print(logo);
+        System.out.println("How may I assist you today?");
+        System.out.println(horizontalLine);
+
+        System.out.println("Bye, see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
