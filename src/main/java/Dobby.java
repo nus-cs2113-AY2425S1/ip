1a9a5dce88e5b2ade360af4792f08f2fc23c71a6
seanngja <ngjooansean@gmail.com>
2024-08-24 22:52:19 +0800

    Add ability to store and display user text

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -1,28 +1,58 @@
 import java.util.Scanner;
 
 public class Dobby {
+    private static final String DASH_LINE = "____________________________________________________________";
+    private static final int MAX_LIST_SIZE = 100;
+    private static final String[] list = new String[MAX_LIST_SIZE];
+    private static int listSize = 0;
+
     public static void main(String[] args) {
-        String lineBreak = "____________________________________________________________";
         Scanner in = new Scanner(System.in);
-        String line;
+        boolean saidBye = false;
 
-        System.out.println(lineBreak);
-        System.out.println("Hello! I'm Dobby!");
-        System.out.println("What can I do for you?");
-        System.out.println(lineBreak);
+        printWelcomeMessage();
 
-        line = in.nextLine();
-
-        while (!line.equalsIgnoreCase("bye")){
-            System.out.println("  " + lineBreak);
-            System.out.println("    " + line);
-            System.out.println("  " + lineBreak);
-            line = in.nextLine();
+        while (!saidBye){
+            String line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")){
+                saidBye = true;
+            } else if (line.equals("list")) {
+                printList();
+            } else{
+                addItem(line);
+            }
         }
 
-        System.out.println("  " + lineBreak);
-        System.out.println("    " + "Bye. Hope to see you again soon!");
-        System.out.println("  " + lineBreak);
-
+        printGoodbyeMessage();
     }
+
+    private static void printWelcomeMessage(){
+        System.out.println("  " + DASH_LINE);
+        System.out.println("    " + "Hello! I'm Dobby!");
+        System.out.println("    " + "What can I do for you, wizard?");
+        System.out.println("  " + DASH_LINE);
+    }
+
+    private static void printList(){
+        System.out.println("  " + DASH_LINE);
+        for (int i = 1; i <= listSize; i++) {
+            System.out.println("    " + i + ". " + list[i - 1]);
+        }
+        System.out.println("  " + DASH_LINE);
+    }
+
+    private static void addItem(String line){
+        list[listSize] = line;
+        listSize++;
+        System.out.println("  " + DASH_LINE);
+        System.out.println("    added: " + line);
+        System.out.println("  " + DASH_LINE);
+    }
+
+    private static void printGoodbyeMessage(){
+        System.out.println("  " + DASH_LINE);
+        System.out.println("    " + "Hohoho. Hope to see you again soon!");
+        System.out.println("  " + DASH_LINE);
+    }
+
 }
