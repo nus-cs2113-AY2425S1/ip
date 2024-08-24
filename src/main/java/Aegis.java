95b9552a57b2f5fa549ab889d9ef919067d2b66c
ThisisXXZ <xxz.7203@gmail.com>
2024-08-24 22:09:05 +0800

    Add echoing function for Aegis skeletal version

--- src/main/java/Aegis.java
+++ src/main/java/Aegis.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Aegis {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = """
                                                                             \s
                         **                                                  \s
@@ -30,7 +33,20 @@ public class Aegis {
         System.out.println("Hello! This is Aegis, an Anti-Shadow Suppression Weapon and a chatbot.");
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------------------------");
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(" " + input);
+            System.out.println("--------------------------------------------------");
+
+            if (input.equals("bye")) {
+                break;
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("--------------------------------------------------");
+
+        scanner.close();
     }
 }
