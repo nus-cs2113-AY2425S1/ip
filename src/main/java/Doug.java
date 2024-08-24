e14b7ff979925ee7165c82ce63ff07970e3c1eee
KSanjith <kodeeswaransanjith@gmail.com>
2024-08-24 10:48:26 +0800

    Let users add text and list added text Modify personality of chatbot

--- src/main/java/Doug.java
+++ src/main/java/Doug.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Doug {
     public static void main(String[] args) {
         /*
@@ -8,9 +10,38 @@ public class Doug {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
          */
-        String line = "_______________________________________\n";
-        System.out.println(line + "Hello! I'm Doug Dimmadome\n" + "What can I do for you?\n"
-                         + line + "Bye. Hope to see you again soon!\n" + line);
+        String dashedLine = "______________________________________________\n";
+        System.out.println(dashedLine + "Howdy partner! My name is Doug Dimmadome\n"
+                            + "Now what can I do for ya?\n" + dashedLine);
 
+        String[] commands = new String[100];
+        int counter = 0;
+
+        boolean saidBye = false;
+
+        while (!saidBye) {
+            Scanner input = new Scanner(System.in);
+            String command = input.nextLine();
+            if (command.equals("bye")) {
+                saidBye = true;
+            } else if (command.equals("list")){
+                System.out.print(dashedLine);
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i+1) + ". " + commands[i]);
+                }
+                System.out.print(dashedLine);
+            } else {
+                if (counter == 100) {
+                    System.out.println("Can't add no more commands partner, the list is full!");
+                    continue;
+                }
+                commands[counter] = command;
+                counter++;
+                System.out.println(dashedLine + "added: " + command + "\n" + dashedLine);
+            }
+        }
+
+        System.out.println(dashedLine + "It's been a pleasure partner.\n" +
+                "Hope to see you around these parts again soon!\n" + dashedLine);
     }
 }
