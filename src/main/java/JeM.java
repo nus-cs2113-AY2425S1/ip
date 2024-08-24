0250b0c89ded83edec32f2eb94ff0045c76e60ea
Atulteja <atulteja@hotmail.com>
2024-08-24 15:56:39 +0800

    Edited Duke.java file into JeM.java

--- src/main/java/JeM.java
+++ src/main/java/JeM.java
@@ -1,4 +1,4 @@
-public class ChernoAlpha {
+public class JeM {
     public static void main(String[] args) {
         String logo = "      _         __  __ \n"
                 + "     | |       |  \\/  |\n"
@@ -7,6 +7,17 @@ public class ChernoAlpha {
                 + "| |__| ||  __/ | |  | |\n"
                 + " \\____/  \\___| |_|  |_|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Hello! I'm JeM, Your e-Assistant");
+        System.out.println(" What can I do for you?");
+
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+
+        System.out.println("____________________________________________________________");
     }
 }
 
