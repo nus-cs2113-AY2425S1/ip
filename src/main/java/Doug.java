3d1f2b166ad8eae476325fbe507f7b740658d847
KSanjith <kodeeswaransanjith@gmail.com>
2024-08-24 11:29:44 +0800

    Add ability to mark/unmark tasks as done Create new Task class to store tasks

--- src/main/java/Doug.java
+++ src/main/java/Doug.java
@@ -11,10 +11,10 @@ public class Doug {
         System.out.println("Hello from\n" + logo);
          */
         String dashedLine = "______________________________________________\n";
-        System.out.println(dashedLine + "Howdy partner! My name is Doug Dimmadome\n"
+        System.out.println(dashedLine + "Howdy partner! My name is Doug Dimmadome.\n"
                             + "Now what can I do for ya?\n" + dashedLine);
 
-        String[] commands = new String[100];
+        Task[] commands = new Task[100];
         int counter = 0;
 
         boolean saidBye = false;
@@ -22,20 +22,44 @@ public class Doug {
         while (!saidBye) {
             Scanner input = new Scanner(System.in);
             String command = input.nextLine();
+
             if (command.equals("bye")) {
                 saidBye = true;
             } else if (command.equals("list")){
-                System.out.print(dashedLine);
+                System.out.println(dashedLine + "Here, let me lay out your tasks for you.");
                 for (int i = 0; i < counter; i++) {
-                    System.out.println((i+1) + ". " + commands[i]);
+                    System.out.println((i+1) + commands[i].listTask());
                 }
                 System.out.print(dashedLine);
-            } else {
-                if (counter == 100) {
-                    System.out.println("Can't add no more commands partner, the list is full!");
+            } else if (command.startsWith("mark")) {
+                int listIndex = Integer.parseInt(command.substring(5));
+
+                if (listIndex >= counter) {
+                    System.out.println(dashedLine + "No can do bud, your entry is outside the list size!\n" + dashedLine);
                     continue;
                 }
-                commands[counter] = command;
+
+                commands[listIndex - 1].markAsDone();
+                System.out.println(dashedLine + "Sure thing partner, I'll mark it as done");
+                System.out.print(commands[listIndex - 1].listTask() + "\n" + dashedLine);
+            } else if (command.startsWith("unmark")) {
+                int listIndex = Integer.parseInt(command.substring(7));
+
+                if (listIndex >= counter) {
+                    System.out.println(dashedLine + "No can do bud, your entry is outside the list size!\n" + dashedLine);
+                    continue;
+                }
+
+                commands[listIndex - 1].markAsNotDone();
+                System.out.println(dashedLine + "Sure thing partner, I'll mark it as not done");
+                System.out.print(commands[listIndex - 1].listTask() + "\n" + dashedLine);
+            } else {
+                if (counter >= 100) {
+                    System.out.println(dashedLine + "Sorry partner, the list is full!" + dashedLine);
+                    continue;
+                }
+                Task t = new Task(command);
+                commands[counter] = t;
                 counter++;
                 System.out.println(dashedLine + "added: " + command + "\n" + dashedLine);
             }
