fafcadb20de47b8290139dd19a011eecf4ad0543
LinhHuynhLeDan <22520759@ms.uit.edu.vn>
2024-08-24 22:17:13 +0800

    Modify to ensure code conventions.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
 public class Main {
+    /**
+     * The entry point of the program.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
+        // Create an instance of the Niwa chatbot
         Niwa chatBot = new Niwa();
-        while (chatBot.isIsRunning()) {
+
+        // Continue running the chatbot as long as it is active
+        while (chatBot.getRunning()) {
+            // Retrieve and process the next command from the user
             String command = chatBot.getCommand();
             chatBot.processCommand(command);
         }
