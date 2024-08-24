ce82645dfe4a348cc40ae6b255074b31243f59c5
louisjoety <kolouis002@gmail.com>
2024-08-25 00:28:06 +0800

    Import Scanner class and create an object to handle user input

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Echo {
     public static void main(String[] args) {
+        // Utilise Scanner object to allow user input
+        Scanner scanner = new Scanner(System.in);
+
         // Define the greeting message and the chatbot name as variables
         String chatbotName = "Echo";
         String greetingMessage = "Hello! I'm " + chatbotName + "\nWhat can I do for you?\n";
