65da08d2e182a760abbcdec5c01087422659ef3d
seanngja <ngjooansean@gmail.com>
2024-08-24 21:48:37 +0800

    Add echo functionality and exit command to Dobby

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Dobby {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        System.out.println(line);
+        String lineBreak = "____________________________________________________________";
+        Scanner in = new Scanner(System.in);
+        String line;
+
+        System.out.println(lineBreak);
         System.out.println("Hello! I'm Dobby!");
         System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(lineBreak);
+
+        line = in.nextLine();
+
+        while (!line.equalsIgnoreCase("bye")){
+            System.out.println("  " + lineBreak);
+            System.out.println("    " + line);
+            System.out.println("  " + lineBreak);
+            line = in.nextLine();
+        }
+
+        System.out.println("  " + lineBreak);
+        System.out.println("    " + "Bye. Hope to see you again soon!");
+        System.out.println("  " + lineBreak);
+
     }
 }
