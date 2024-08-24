9f79a460ba6c1810a24594a519b98eb93cea257f
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-08-24 22:00:40 +0800

    Added list function
    Stores the tasks that user inputs and outputs the list when user enter
    command "list"

--- src/main/java/Legin.java
+++ src/main/java/Legin.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Legin {
+    private static String[] tasks = new String[100];
+    private static int currentTaskCount = 0;
+
     public static void horizontalLine() {
         System.out.println("______________________" +
                 "______________________________________");
@@ -21,8 +24,18 @@ public class Legin {
     }
 
     public static void echo(String input) {
+        tasks[currentTaskCount] = input;
+        currentTaskCount++;
         horizontalLine();
-        System.out.println(input);
+        System.out.println("added: " + input);
+        horizontalLine();
+    }
+
+    public static void list() {
+        horizontalLine();
+        for (int i = 0; i < currentTaskCount; i++ ) {
+            System.out.println(i+1 + ". " + tasks[i]);
+        }
         horizontalLine();
     }
     public static void main(String[] args) {
@@ -33,8 +46,11 @@ public class Legin {
             command = in.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                list();
+            } else {
+                echo(command);
             }
-            echo(command);
         }
         bye();
     }
