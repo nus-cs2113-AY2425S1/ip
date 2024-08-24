83d77ef354ad51f7f5eab99f27894ae8267420b1
ThisisXXZ <xxz.7203@gmail.com>
2024-08-24 22:37:36 +0800

    Add store and list functions for Aegis skeletal version

--- src/main/java/Aegis.java
+++ src/main/java/Aegis.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Aegis {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         String logo = """
                                                                             \s
                         **                                                  \s
@@ -34,17 +36,25 @@ public class Aegis {
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------------------------");
 
+        ArrayList<String> promptHistory = new ArrayList<String>();
         while (true) {
             String input = scanner.nextLine();
-            System.out.println(" " + input);
-            System.out.println("--------------------------------------------------");
 
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < promptHistory.size(); ++i) {
+                    System.out.printf(" %d: %s%n", i + 1, promptHistory.get(i));
+                }
+            } else {
+                promptHistory.add(input);
+                System.out.println(" added: " + input);
             }
+
+            System.out.println("--------------------------------------------------");
         }
 
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("--------------------------------------------------");
 
         scanner.close();
