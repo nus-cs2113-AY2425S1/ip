19f55a36276503fa87f2cf1da652f77323c2faca
telferang <telferang@gmail.com>
2024-08-25 13:41:42 +0800

    Add Add and List feature
    For level-2

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Nova {
     public static void print(String info) {
@@ -10,14 +9,28 @@ public class Nova {
 
     public static void main(String[] args) {
         print(" Hello! I'm Nova\n What can I do for you?");
+        String[] tasks = new String[100];
+        int numberOfTasks = 0;
         while (true) {
             Scanner sc = new Scanner(System.in);
             String info = sc.nextLine();
-            if (info.equals("bye")) {
+            switch (info) {
+            case "bye":
                 print("Bye. Hope to see you again soon!");
+                return;
+            case "list":
+                System.out.println("____________________________________________________________");
+                for (int i = 1; i <= numberOfTasks; i++) {
+                    System.out.println(i + ": " + tasks[i - 1]);
+                }
+                System.out.println("____________________________________________________________");
+                break;
+            default:
+                print(info);
+                tasks[numberOfTasks] = info;
+                numberOfTasks++;
                 break;
             }
-            print(info);
         }
     }
 }
