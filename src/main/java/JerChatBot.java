08fe765f93b61ed79462e2f83c740507cb5e15f7
tayjerom <jerometayfw@gmail.com>
2024-08-26 01:34:07 +0800

    Level-2: Add, List

--- src/main/java/JerChatBot.java
+++ src/main/java/JerChatBot.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class JerChatBot {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int numOfTasks = 0;
 
         System.out.println("~~~~~~~~ Welcome to Jer Chat Bot ~~~~~~~~");
         System.out.println(" What you want?");
@@ -15,9 +17,18 @@ public class JerChatBot {
                 System.out.println(" Finally done, k bye...");
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 break;
-            } else {
+            } else if (input.equals("list")) {
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                System.out.println(input);
+                for (int i = 0; i < numOfTasks; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        } else {
+                tasks[numOfTasks] = input;
+                numOfTasks++;
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("added: " + input);
+                System.out.println(" And then what??");
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             }
         }
