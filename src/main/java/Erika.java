706942e99196480ceec73788147a037df72f73c6
RCPilot1604 <ethan.chua1604@gmail.com>
2024-08-25 14:43:17 +0800

    Add level-3 modifications
    Modified Erika bot to do the following:
    * Mark/Unmark list elements
    
    Added Task.java for OOP implementation

--- src/main/java/Erika.java
+++ src/main/java/Erika.java
@@ -5,8 +5,8 @@ public class Erika {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
-        int taskSize = 0;
+        ArrayList<Task> tasks = new ArrayList<>();h
+
         String logo =
                 " _______   ________  ___  ___  __    ________     \n" +
                 "|\\  ___ \\ |\\   __  \\|\\  \\|\\  \\|\\  \\ |\\   __  \\    \n" +
@@ -20,25 +20,56 @@ public class Erika {
         System.out.println(" Hello! I'm Erika\n What can I do for you?");
         System.out.println("____________________________________________________________");
         line = in.nextLine();
+
         while (!line.equals("bye")) {
             line = line.trim();
             if (line.equals("list")) {
                 System.out.println("____________________________________________________________");
-                for (int i = 0; i < taskSize; i++) {
-                    System.out.println(Integer.toString(i+1) + ". " + tasks.get(i));
+                if (Task.getTaskSize() == 0) {
+                    System.out.println("It seems that there are no tasks! Please consider adding some!");
+                }
+                for (int i = 0; i < Task.getTaskSize(); i++) {
+                    System.out.println(i+1 + ".[" + (tasks.get(i).getMark()?"x":" ") + "] "
+                            + tasks.get(i).getDescription());
                 }
                 System.out.println("____________________________________________________________");
             } else {
-                tasks.add(line);
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + line);
-                System.out.println("____________________________________________________________");
-                taskSize++;
+                int markIndex = -1;
+                String digitString = line.replaceAll("[^0-9]", "");
+                if (!digitString.isEmpty()) {
+                    markIndex = Integer.parseInt(digitString);
+                }
+                if(markIndex > Task.getTaskSize()) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Error! Task " + markIndex + " is out of bounds!");
+                    System.out.println("____________________________________________________________");
+                } else {
+                     if(line.contains("unmark")) {
+                        tasks.get(markIndex-1).setMark(false);
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as not done yet:");
+                        System.out.println("[ ] " + tasks.get(markIndex-1).getDescription());
+                        System.out.println("____________________________________________________________");
+                    } else if(line.contains("mark")) {
+                        tasks.get(markIndex-1).setMark(true);
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("[X] " + tasks.get(markIndex-1).getDescription());
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        Task newTask = new Task(line);
+                        tasks.add(newTask);
+                        System.out.println("____________________________________________________________");
+                        System.out.println("added: " + newTask.getDescription());
+                        System.out.println("____________________________________________________________");
+                        Task.incrementTaskSize();
+                    }
+                }
             }
             line = in.nextLine();
         }
         System.out.println("____________________________________________________________");
-        System.out.println("Auf Wiedersehen! Hope to see you again soon!");
+        System.out.println("Bye! Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,41 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+    private static int taskSize = 0;
+    public Task (String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setMark(boolean mark) {
+        this.isDone = mark;
+    }
+
+    public boolean getMark() {
+        return this.isDone;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static int getTaskSize() {
+        return taskSize;
+    }
+
+    public static void setTaskSize(int size) {
+        taskSize = size;
+    }
+
+    public static void incrementTaskSize() {
+        taskSize++;
+    }
+
+    public static void decrementTaskSize() {
+        taskSize--;
+    }
+}
