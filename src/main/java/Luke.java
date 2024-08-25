12e2a5f949ce6231e0c39af5f93d779d70dd6001
wx-03 <tngwenxi@gmail.com>
2024-08-25 17:00:54 +0800

    Implement echo

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Luke {
     public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        Scanner in = new Scanner(System.in);
+        String line;
+        boolean exit = false;
         String greeting = "____________________________________________________________\n" +
                 " Hello! I'm Luke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(greeting);
+
+        while (!exit) {
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("Bye")) {
+                System.out.println(horizontalLine);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                exit = true;
+                continue;
+            }
+            System.out.println(horizontalLine);
+            System.out.println(line);
+            System.out.println(horizontalLine);
+        }
     }
 }
