9236b18d9d6b99df5efaa1b4b2476cd7c5929142
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-08-26 00:43:49 +0800

    Add Level-0 increment: Rename, Greet, Exit

---
+++ src/main/java/Atom.java
@@ -0,0 +1,25 @@
+public class Atom {
+    public static void main(String[] args) {
+
+        System.out.println("__________________________________________________\n");
+
+        String logo = "    ____   __________  ________  __       __\n"
+                    + "   / __ \\ |___    ___||  ____  ||  \\     /  |\n"
+                    + "  / /__\\ \\    |  |    | |    | || |\\\\   //| |\n"
+                    + " / /    \\ \\   |  |    | |____| || | \\\\_// | |\n"
+                    + "/_/      \\_\\  |__|    |________||_|  \\_/  |_|\n";
+
+        System.out.println(logo);
+
+        System.out.println("__________________________________________________");
+
+        System.out.println("Hey there! I'm your friendly chatbot, ATOM!");
+        System.out.println("How can i assist you today?");
+
+        System.out.println("__________________________________________________");
+
+        System.out.println("Bye Bye. See ya soon!");
+
+        System.out.println("__________________________________________________");
+    }
+}
