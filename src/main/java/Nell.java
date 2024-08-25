570710e27b273e2bfce99cfa67f65bb2dcd64038
jemehgoh <jeremygoheh@gmail.com>
2024-08-25 15:55:18 +0800

    Change display format to arrow

--- src/main/java/Nell.java
+++ src/main/java/Nell.java
@@ -1,27 +1,13 @@
 import java.util.Scanner;
 
 public class Nell {
-    /**
-     * Prints a 40-character long horizontal line
-     */
-    public static void printLine() {
-        for (int i = 0; i < 40; i++)
-        {
-            System.out.print('_');
-        }
-
-        System.out.println();
-    }
-
     public static void main(String[] args) {
         // Initialises scanner to take in user input
         Scanner input = new Scanner(System.in);
 
         // Greets user
-        printLine();
         System.out.println("Hello! I'm Nell!");
         System.out.println("What can I do for you?");
-        printLine();
 
         boolean isGettingCommands = true;
 
@@ -31,17 +17,13 @@ public class Nell {
             switch (command) {
             case "bye":
                 // Exits
-                printLine();
-                System.out.println("Bye. Hope to see you again soon!");
-                printLine();
+                System.out.println("-> Bye. Hope to see you again soon!" + System.lineSeparator());
                 isGettingCommands = false;
                 break;
 
             default:
                 // Echoes command otherwise
-                printLine();
-                System.out.println(command);
-                printLine();
+                System.out.println("-> " + command);
                 break;
             }
         }
