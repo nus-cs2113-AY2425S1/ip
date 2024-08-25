08577f7bb4f490aa8c9defe6490dfa2f5f0023c1
lowjunchen <e1138420@u.nus.edu>
2024-08-25 16:49:32 +0800

    Improve skeletal system

--- src/main/java/CodyChen.java
+++ src/main/java/CodyChen.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
 public class CodyChen {
     public static void main(String[] args) {
-        String logo = "________________________\n" +
+        String hi = "________________________\n" +
                 "Hello! I'm CodyChen\n" +
                 "What can I do for you?\n" +
-                "________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
                 "________________________\n";
+        System.out.println(hi);
+        Scanner in = new Scanner(System.in);
+        String line = "";
+        int flag = 0;
+        while(!line.equals("bye")){
+            line = in.nextLine();
+            if(flag == 0){
+                System.out.print("Thank You for Responding" + "\n" +
+                        "__________________________________\nYou said:    " +
+                        line +  "\n__________________________________\n");
+                flag = 1;
+            } else {
+                System.out.println(
+                        "__________________________________\nYou said:     " +
+                        line +
+                        "\n__________________________________\n");
+            }
 
-        System.out.println(logo);
+        }
+
+        String bye = "Bye. Hope to see you again soon!\n" +
+                "________________________\n";
+        System.out.println(bye);
+
+
     }
 }
