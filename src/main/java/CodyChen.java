ee119b1b56d476d226655ced53270ac021913f72
lowjunchen <e1138420@u.nus.edu>
2024-08-25 17:07:27 +0800

    Add the ability to store whatever text user enters and display them back when requested

--- src/main/java/CodyChen.java
+++ src/main/java/CodyChen.java
@@ -9,25 +9,43 @@ public class CodyChen {
         Scanner in = new Scanner(System.in);
         String line = "";
         int flag = 0;
+        String [] arr = new String[100];
+        int count = 0;
         while(!line.equals("bye")){
             line = in.nextLine();
-            if(flag == 0){
-                System.out.print("Thank You for Responding" + "\n" +
-                        "__________________________________\nYou said:    " +
-                        line +  "\n__________________________________\n");
-                flag = 1;
-            } else {
-                System.out.println(
-                        "__________________________________\nYou said:     " +
-                        line +
-                        "\n__________________________________\n");
-            }
+            if(!line.equals("list")){
+                if(flag == 0){
+                    System.out.print("Thank You for Responding" + "\n" +
+                            "__________________________________\nAddd:    "
+                            + line +
+                            "\n__________________________________\n");
+                    flag = 1;
+                    arr[count] = line;
+                    count += 1;
+                }
+                else {
+                    System.out.println("__________________________________\nAdded:     " +
+                            line +
+                            "\n__________________________________\n");
+                    arr[count] = line;
+                    count += 1;
+                }
 
+            } else {
+                System.out.print("___________________________________\n" +
+                        "Yor List as follows: \n");
+                for(int i = 0; i < arr.length; i++){
+                    if(arr[i]!=null){
+                        System.out.print(i + 1 + ". " + arr[i] + "\n");
+                    }
+                }
+                System.out.print("___________________________________\n");
+            }
         }
 
         String bye = "Bye. Hope to see you again soon!\n" +
                 "________________________\n";
-        System.out.println(bye);
+        System.out.print(bye);
 
 
     }
