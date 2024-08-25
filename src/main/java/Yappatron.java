1f90742fd74e63da19f8be071a513e96e46bdd0d
ChuaCleon <chuaweiqian.cleon@gmail.com>
2024-08-25 22:38:50 +0800

    Remove Duke from existence Introduce brand-new improved Number 1 Yapper Chatbot aka Yappatron

---
+++ src/main/java/Yappatron.java
@@ -0,0 +1,7 @@
+public class Yappatron {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Yappatron");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
