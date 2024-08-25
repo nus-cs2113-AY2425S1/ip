a299844d1f3c9ef8340672ef4ceec564881de95d
ehz0ah <haozhe4547@gmail.com>
2024-08-25 17:44:02 +0800

    Add echo feature

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,8 +1,22 @@
 import esme.Esme;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         Esme esme = new Esme();
         esme.greet();
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                esme.farewell();
+                break;
+            } else {
+                esme.echo(line);
+            }
+        }
     }
 }
--- src/main/java/esme/Esme.java
+++ src/main/java/esme/Esme.java
@@ -5,18 +5,34 @@ public class Esme {
             "| ____|___ _ __ ___   ___ \n" +
             "|  _| / __| '_ ` _ \\ / _ \\ \n" +
             "| |___\\__ \\ | | | | |  __/ \n" +
-            "|_____|___/_| |_| |_|\\___| \n";
+            "|_____|___/_| |_| |_|\\___| ";
 
-    public void displayLine () {
-        System.out.println("----------------------------------------------");
+    public void displayLine (boolean hasIndent) {
+        if (hasIndent) {
+            System.out.println("\t──────────────────────────────────────────────────");
+        } else {
+            System.out.println("──────────────────────────────────────────────────");
+        }
     }
     public void greet () {
-        displayLine();
+        displayLine(false);
         System.out.println("Hello! I'm");
-        System.out.println(esmeLogo);
-        System.out.println("What can I do for you?");
-        displayLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        displayLine();
+        System.out.print(esmeLogo);
+        System.out.println(", the Astrologer. The cosmos whispers its secrets to me.");
+        displayLine(false);
+        System.out.println("How may I assist you today? The stars and I are at your service.");
+        displayLine(false);
+    }
+    public void farewell () {
+        displayLine(true);
+        System.out.println("\tAu revoir, mon ami! May the cosmos continue to weave a tapestry of fortune " +
+                "in your favor, and until our paths cross again under a lucky star!");
+        displayLine(true);
+    }
+
+    public void echo (String message) {
+        displayLine(true);
+        System.out.println("\t" + message);
+        displayLine(true);
     }
 }
