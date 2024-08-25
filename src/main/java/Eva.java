e5e049812fb68eccea6211b4c2fb6d8786c382d7
Chiang-HuiXin <huixinchiang@gmail.com>
2024-08-25 18:09:45 +0800

    Added Eva.java file for level 0
    I made a new file called Eva.java for my chatbot named Eva and coded it to greet and exit

---
+++ src/main/java/Eva.java
@@ -0,0 +1,8 @@
+public class Eva {
+    public static void main(String[] args) {
+       System.out.println("Hello! I'm Eva!");
+       System.out.println("What can I do for you?");
+       System.out.println("Bye. Hope to see you again soon!");
+    }
+
+}
