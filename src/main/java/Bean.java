52b70844829caa809809e00e05ec0f36f4139316
raneeng <raneengyinyee@gmail.com>
2024-08-25 11:21:39 +0800

    Level 2 increment: Add, List done

--- src/main/java/Bean.java
+++ src/main/java/Bean.java
@@ -1,40 +1,81 @@
 import java.util.Scanner;
 
 public class Bean {
-    final static String line = "\n____________________________________________________________________\n";
+    // Constants
+    final static String SEPARATOR_LINE = "____________________________________________________________________\n";
+    final static String INDENT = "  ";
+    final static int MAX_LIST_COUNT = 100;
+    final static String LOGO = "  ┏━┓\n" +
+            "  ┃ ┃\n" +
+            "  ┃ ┗━━┳━━━┳━━━━┳━━━┓\n" +
+            "  ┃  ┏┓┃ ┃━┫ ┏┓ ┃ ┏┓ ┓ ┏━━━━━━┓\n" +
+            "  ┃  ┗┛┃ ┃━┫ ┏┓ ┃ ┃┃ ┃ ┃• ᴗ • ┫\n" +
+            "  ┗━━ ━┻━━━┻━┛┗━┻━┛┗━┛ ┗━━━━━━┛\n";;
 
     public static void greet() {
-        String logo = "  ┏━┓\n" +
-                "  ┃ ┃\n" +
-                "  ┃ ┗━━┳━━━┳━━━━┳━━━┓\n" +
-                "  ┃  ┏┓┃ ┃━┫ ┏┓ ┃ ┏┓ ┓ ┏━━━━━━┓\n" +
-                "  ┃  ┗┛┃ ┃━┫ ┏┓ ┃ ┃┃ ┃ ┃• ᴗ • ┫\n" +
-                "  ┗━━ ━┻━━━┻━┛┗━┻━┛┗━┛ ┗━━━━━━┛\n";
-
-        System.out.println(line +
-                logo +
-                "  Howdy, mate! :) I am Bean, your personal assistant.\n  Let me know how I can help you out!\n" +
-                line
+        System.out.println(SEPARATOR_LINE +
+                LOGO +
+                INDENT + "Howdy, mate! :) I am Bean, your personal assistant.\n" +
+                INDENT + "Let me know how I can help you out!\n" +
+                SEPARATOR_LINE
         );
     }
 
     public static void exit() {
-        System.out.println("  Bye bye, glad I could help! See you soon? :'(\n" +
-                line);
+        System.out.println(SEPARATOR_LINE +
+                LOGO +
+                INDENT + "Bye bye, glad I could help! See you soon? :'(\n" +
+                SEPARATOR_LINE);
     }
 
     public static void echo(String string) {
-        System.out.println("  " + string + line);
+        System.out.println(INDENT + string + SEPARATOR_LINE);
+    }
+
+    public static void printList(String[] toDoList) {
+        if (toDoList[0] == null) {
+            System.out.println(SEPARATOR_LINE +
+                    INDENT + "Nothing in your to do list!" + SEPARATOR_LINE);
+            return;
+        }
+
+        System.out.println(SEPARATOR_LINE +
+                INDENT + "TO DO LIST:\n");
+
+        for (int i = 0; i < toDoList.length; i++) {
+            if (toDoList[i] == null) {
+                break;
+            }
+            System.out.println(INDENT + (i + 1) + ". " + toDoList[i]);
+        }
+        System.out.println(SEPARATOR_LINE);
     }
 
     public static void main(String[] args) {
-        String line = "";
+        String userInput;
         Scanner in = new Scanner(System.in);
 
+        String[] toDoList = new String[MAX_LIST_COUNT];
+        int count = 0;
+
         greet();
-        while (!line.equals("bye")) {
-            line = in.nextLine();
-            echo(line);
+
+        while (count < MAX_LIST_COUNT) {
+            userInput = in.nextLine();
+
+            if (userInput.equals("list")) {
+                printList(toDoList);
+            } else if (userInput.equals("bye")) {
+                break;
+            } else {
+                // Add task
+                toDoList[count] = userInput;
+
+                System.out.println(SEPARATOR_LINE +
+                        INDENT + "Task '" + userInput + "' has been added!\n" +
+                        SEPARATOR_LINE);
+                count++;
+            }
         }
         exit();
     }
