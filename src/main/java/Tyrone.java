43b428ae7e9191d2210e58c0e44e07bd5c8b9787
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-08-25 18:21:54 +0800

    Add echo function

--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public class Tyrone {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Tyrone.\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
-        String dummyInput = scanner.nextLine();
+        System.out.println("Hello! I'm Tyrone.\nWhat can I do for you?");
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
