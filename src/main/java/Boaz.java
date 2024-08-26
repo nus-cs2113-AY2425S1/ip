905436faaf721871a44f9e72c1e8eeb9d4618c43
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-08-26 15:01:13 +0800

    Level-1
    Echo user input
    Exits when command "bye"

--- src/main/java/Boaz.java
+++ src/main/java/Boaz.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Boaz {
+
+    public static boolean isDone = false;
+
+    public static void printHorizontalLine() {
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public static void greet() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Boaz.");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Boaz.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        greet();
+        Scanner in = new Scanner(System.in);
+        while (!isDone) {
+            String command = in.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                isDone = true;
+            } else {
+                System.out.println("\t"+command);
+                System.out.println("\t____________________________________________________________");
+            }
+        }
+        exit();
     }
 }
