f19f2d1e71d727d6aae31359e1f6fed8163fc6a0
ChuaCleon <chuaweiqian.cleon@gmail.com>
2024-08-26 16:23:45 +0800

    Add input actions to array Echo back list when user wants it

--- src/main/java/Yappatron.java
+++ src/main/java/Yappatron.java
@@ -6,14 +6,21 @@ public class Yappatron {
         System.out.println("What can I do for you?");
         Scanner in = new Scanner(System.in);
         String input;
-        int flag = 0;
+        String[] arrayInput = new String[100];
+        int flag = 0, position = 0, i;
         do {
             input = in.nextLine();
             if (input.equalsIgnoreCase("bye")){
                 flag = 1;
             }
+            else if (!input.equalsIgnoreCase("list")){
+                System.out.println("added: " + input);
+                arrayInput[position++] = input;
+            }
             else{
-                System.out.println(input);
+                for (i=0; i<position; i++){
+                    System.out.println(i+1 + ". " + arrayInput[i]);
+                }
             }
         }while (flag==0);
         System.out.println("Bye. Hope to see you again soon!");
