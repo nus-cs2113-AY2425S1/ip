3b9f0318a5dfb0822aa0c2f2a997f2b8fcdd7a12
averageandyyy <averageandyyy@gmail.com>
2024-08-26 15:48:37 +0800

    Implement echo behaviour and exit behaviour on bye input

--- src/main/java/Bento.java
+++ src/main/java/Bento.java
@@ -1,32 +1,61 @@
+import java.util.Scanner;
+
 public class Bento {
-    public static void printLogo() {
-        String logo = "  ____             _        \n"
-                + " | __ )  ___ _ __ | |_ ___  \n"
-                + " |  _ \\ / _ \\ '_ \\| __/ _ \\ \n"
-                + " | |_) |  __/ | | | || (_) |\n"
-                + " |____/ \\___|_| |_|\\__\\___/ \n"
-                + "                            \n";
+    private Scanner in = new Scanner(System.in);
+    private boolean isExit = false;
+
+    public void printLogo() {
+        String logo = "\t  ____             _        \n"
+                + "\t | __ )  ___ _ __ | |_ ___  \n"
+                + "\t |  _ \\ / _ \\ '_ \\| __/ _ \\ \n"
+                + "\t | |_) |  __/ | | | || (_) |\n"
+                + "\t |____/ \\___|_| |_|\\__\\___/ \n"
+                + "\t                            \n";
         System.out.print(logo);
     }
 
-    public static void sayKonichiwa() {
-        System.out.println("Konichiwa! I am Bento, your personal assistant!\nHow can I help you with your tasks today?");
-    }
 
-    public static void printLine() {
-        System.out.println("____________________________________________________________");
-    }
-
-    public static void saySayonara() {
-        System.out.println("Thank you for working with me today! See you next time! Sayonara~");
-    }
-
-    public static void main(String[] args) {
+    public void sayKonichiwa() {
         printLine();
         printLogo();
+        System.out.println("\tKonichiwa! I am Bento, your personal assistant!\n\tHow can I help you with your tasks today?");
+        printLine();
+    }
+
+    public void printLine() {
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public void saySayonara() {
+        isExit = true;
+        printLine();
+        System.out.println("\tThank you for working with me today! See you next time! Sayonara~");
+        printLine();
+    }
+
+    public String getUserInput() {
+        return in.nextLine();
+    }
+
+    public void echoInput(String input) {
+        printLine();
+        System.out.println("\t" + input);
+        printLine();
+    }
+
+    public void handleUserInput(String input) {
+        if (input.equals("bye")) {
+            saySayonara();
+            return;
+        }
+        echoInput(input);
+    }
+
+    public void run() {
         sayKonichiwa();
-        printLine();
-        saySayonara();
-        printLine();
+        while (!isExit) {
+            String input = getUserInput();
+            handleUserInput(input);
+        }
     }
 }
