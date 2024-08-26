601ae49767bfa0d9b20915c932454e24fa95b8a8
Dri_water <jonasseet@gmail.com>
2024-08-26 18:05:12 +0800

    Add text storage and list display

--- src/main/java/Archibald.java
+++ src/main/java/Archibald.java
@@ -2,9 +2,30 @@ import java.util.Scanner;
 
 public class Archibald {
 
+    private static String[] tasks = new String[100];
+    private static int taskCount = 0;
+
     public static void printArchibaldResponse(String message) {
         System.out.println("____________________________________________________________");
         System.out.println(message);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void addTask(String task) {
+        if (taskCount < tasks.length) {
+            tasks[taskCount] = task;
+            taskCount++;
+            printArchibaldResponse("added: " + task);
+        } else {
+            printArchibaldResponse("Error: Task list is full!");
+        }
+    }
+
+    public static void printList() {
+        printArchibaldResponse("Tasks:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
         System.out.println("____________________________________________________________");
     }
     public static void main(String[] args) {
@@ -13,14 +34,16 @@ public class Archibald {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String userInput = scanner.nextLine();
-
-            if (userInput.equals("bye")) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("bye")) {
                 printArchibaldResponse("I bid thee farewell! May our paths cross again!");
                 break;
+            } else if (input.equals("list")) {
+                printList();
+            } else {
+                addTask(input);
             }
-
-            printArchibaldResponse(userInput);
         }
 
         scanner.close();
