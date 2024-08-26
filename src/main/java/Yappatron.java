1950488b5b7504117e9c898dbfd66697569ea591
ChuaCleon <chuaweiqian.cleon@gmail.com>
2024-08-26 16:46:01 +0800

    Add task class for task object as each task has description and status Mark or unmark tasks when done/undone respectively

--- src/main/java/Yappatron.java
+++ src/main/java/Yappatron.java
@@ -6,20 +6,33 @@ public class Yappatron {
         System.out.println("What can I do for you?");
         Scanner in = new Scanner(System.in);
         String input;
-        String[] arrayInput = new String[100];
-        int flag = 0, position = 0, i;
+        String[] splitInput = new String[100];
+        Task[] arrayInput = new Task[100];
+        int flag = 0, position = 0, i, index;
         do {
             input = in.nextLine();
             if (input.equalsIgnoreCase("bye")){
                 flag = 1;
             }
+            else if (input.startsWith("mark")){
+                splitInput = input.split(" ");
+                index = Integer.parseInt(splitInput[1]) - 1;
+                arrayInput[index].markAsDone();
+            }
+            else if (input.startsWith("unmark")){
+                splitInput = input.split(" ");
+                index = Integer.parseInt(splitInput[1]) - 1;
+                arrayInput[index].markAsUndone();
+            }
             else if (!input.equalsIgnoreCase("list")){
                 System.out.println("added: " + input);
-                arrayInput[position++] = input;
+                arrayInput[position++] = new Task(input);
             }
+
             else{
+                System.out.println("Here are the tasks in your list:");
                 for (i=0; i<position; i++){
-                    System.out.println(i+1 + ". " + arrayInput[i]);
+                    System.out.println(i+1 + "." + "[" + arrayInput[i].getStatusIcon() + "] " + arrayInput[i].getDescription());
                 }
             }
         }while (flag==0);
