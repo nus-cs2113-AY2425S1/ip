29d42e7288412a3c9cd96d15b275e1a372d700f3
Dri_water <jonasseet@gmail.com>
2024-08-26 17:58:21 +0800

    Implement command echoing and exit condition

--- src/main/java/Archibald.java
+++ src/main/java/Archibald.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Archibald {
+
+    public static void printArchibaldResponse(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
         String name = "Archibald";
-        System.out.println("Hello, I am known as " + name);
-        System.out.println("Farewell for now!");
+        printArchibaldResponse("Hello, I am known as " + name + ",\nhow may I be of assistance!");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                printArchibaldResponse("I bid thee farewell! May our paths cross again!");
+                break;
+            }
+
+            printArchibaldResponse(userInput);
+        }
+
+        scanner.close();
     }
 }
-
