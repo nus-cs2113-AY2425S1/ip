076a0147053c5d365f6d67d0ee53f73b70ee6f7d
wx-03 <tngwenxi@gmail.com>
2024-08-26 13:25:06 +0800

    Implement mark and unmark

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -5,6 +5,7 @@ public class Luke {
     private static String horizontalLine = "____________________________________________________________";
 
     private static String[] storage = new String[100];
+    private static boolean[] isDone = new boolean[100];
     private static int size = 0;
 
     private static void printReply(String reply) {
@@ -16,12 +17,18 @@ public class Luke {
     private static void list() {
         System.out.println(horizontalLine);
         for (int i = 0; i < size; i++) {
-            System.out.print(String.format("%d. %s\n", i+1, storage[i]));
+            String status = (isDone[i] ? "X" : " ");
+            System.out.print(String.format("%d.[%s] %s\n", i+1, status, storage[i]));
         }
         System.out.println(horizontalLine);
     }
 
     public static void main(String[] args) {
+         // Initialize array values
+        for (int i = 0; i < 100; i++) {
+            isDone[i] = false;
+        }
+
         printReply("Hello! I'm Luke\nWhat can I do for you?");
         Scanner in = new Scanner(System.in);
         String line;
@@ -34,8 +41,19 @@ public class Luke {
                 exit = true;
                 continue;
             }
-            if (line.equals("list")) {
+            if (line.equalsIgnoreCase("list")) {
                 list();
+                continue;
+            }
+            String[] lineArr = line.split(" ");
+            if (lineArr[0].equalsIgnoreCase("mark")) {
+                int idx = Integer.parseInt(lineArr[1]) - 1;
+                isDone[idx] = true;
+                printReply(String.format("Marked as done:\n  [X] %s", storage[idx]));
+            } else if (lineArr[0].equalsIgnoreCase("unmark")) {
+                int idx = Integer.parseInt(lineArr[1]) - 1;
+                isDone[idx] = false;
+                printReply(String.format("Unmarked:\n  [ ] %s", storage[idx]));
             } else {
                 printReply(String.format("Added task: %s", line));
                 storage[size] = line;
