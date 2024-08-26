291cdc8c9102ce307870531ae14a726c85da346f
wx-03 <tngwenxi@gmail.com>
2024-08-26 12:59:14 +0800

    Implement add and list

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,29 +1,46 @@
 import java.util.Scanner;
 
 public class Luke {
+
+    private static String horizontalLine = "____________________________________________________________";
+
+    private static String[] storage = new String[100];
+    private static int size = 0;
+
+    private static void printReply(String reply) {
+        System.out.println(horizontalLine);
+        System.out.println(reply);
+        System.out.println(horizontalLine);
+    }
+
+    private static void list() {
+        System.out.println(horizontalLine);
+        for (int i = 0; i < size; i++) {
+            System.out.print(String.format("%d. %s\n", i+1, storage[i]));
+        }
+        System.out.println(horizontalLine);
+    }
+
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________";
+        printReply("Hello! I'm Luke\nWhat can I do for you?");
         Scanner in = new Scanner(System.in);
         String line;
         boolean exit = false;
-        String greeting = "____________________________________________________________\n" +
-                " Hello! I'm Luke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n";
-        System.out.println(greeting);
 
         while (!exit) {
             line = in.nextLine();
             if (line.equalsIgnoreCase("Bye")) {
-                System.out.println(horizontalLine);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(horizontalLine);
+                printReply("Bye. Hope to see you again soon!");
                 exit = true;
                 continue;
             }
-            System.out.println(horizontalLine);
-            System.out.println(line);
-            System.out.println(horizontalLine);
+            if (line.equals("list")) {
+                list();
+            } else {
+                printReply(String.format("Added task: %s", line));
+                storage[size] = line;
+                size++;
+            }
         }
     }
 }
