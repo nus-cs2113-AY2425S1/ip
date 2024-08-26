b7c903c972e05d45d822f6cb35a455d6072b923c
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-08-26 15:33:06 +0800

    List function
    Abstract some lines into methods
    Takes input into a list
    "list" command displays the full list

--- src/main/java/Boaz.java
+++ src/main/java/Boaz.java
@@ -3,33 +3,56 @@ import java.util.Scanner;
 public class Boaz {
 
     public static boolean isDone = false;
+    public static String[] userList = new String[100];
+    public static int listCount = 0;
 
     public static void printHorizontalLine() {
         System.out.println("\t____________________________________________________________");
     }
 
     public static void greet() {
-        System.out.println("\t____________________________________________________________");
+        printHorizontalLine();
         System.out.println("\tHello! I'm Boaz.");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHow can I assist you?");
+        printHorizontalLine();
     }
 
     public static void exit() {
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
+        System.out.println("\tGoodbye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
+    public static void echo(String input) {
+        System.out.println("\t" + input);
+        printHorizontalLine();
+    }
+
+    public static void addToUserList(String input) {
+        userList[listCount] = input;
+        listCount++;
+        System.out.println("\tadded: " + input);
+        printHorizontalLine();
+    }
+
+    public static void printUserList() {
+        for (int i = 0; i < listCount; i++) {
+            System.out.println("\t" + (i + 1) + ". " + userList[i]);
+        }
+        printHorizontalLine();
     }
 
     public static void main(String[] args) {
         greet();
         Scanner in = new Scanner(System.in);
         while (!isDone) {
-            String command = in.nextLine();
-            if (command.equalsIgnoreCase("bye")) {
+            String input = in.nextLine();
+            printHorizontalLine();
+            if (input.equalsIgnoreCase("bye")) {
                 isDone = true;
+            } else if (input.equalsIgnoreCase("list")) {
+                printUserList();
             } else {
-                System.out.println("\t"+command);
-                System.out.println("\t____________________________________________________________");
+                addToUserList(input);
             }
         }
         exit();
