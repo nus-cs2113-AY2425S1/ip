8a4ec851d5a193ce79936f6c219fc2c196bf9aa9
kennethszj <kennethszj.0123@gmail.com>
2024-08-26 19:57:39 +0800

    Renamed Duke.java to Buddy.java and added Level-0 functionality

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,13 @@
+public class Buddy {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Buddy");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+
+        System.out.println(" Bye, see you again !");
+        System.out.println("____________________________________________________________");
+
+    }
+}
