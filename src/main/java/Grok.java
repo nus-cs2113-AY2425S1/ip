3fc2bb3fe911903f60b2966133941ec737486227
naveen42x <naveensuresh101@gmail.com>
2024-08-26 12:38:02 +0800

    Implement Level 1: Echo feature for Grok chatbot

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Grok {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Grok");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon! Keep Grokking :)");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        scanner.close();
     }
 }
