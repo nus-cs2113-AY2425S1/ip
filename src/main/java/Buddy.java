9018f7e7054937f33623aaed4b87cf75db4b7fd8
kennethszj <kennethszj.0123@gmail.com>
2024-08-26 21:28:29 +0800

    Implement level 2 add,list functionality

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -7,19 +7,37 @@ public class Buddy {
         System.out.println(" What can I do for you? ⸜(｡˃ ᵕ ˂ )⸝♡");
         System.out.println("____________________________________________________________");
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Scanner object named "scanner"
+        String[] tasks = new String[100]; //Array to store tasks
+        int task_count = 0; //Counter for the number of tasks
         String input;
 
         while (true)
         {
             input = scanner.nextLine();
-            if (input.equals("bye"))
+
+            if (input.equals("bye")) //end loop when user inputs "bye"
             {
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println("     " + input);
-            System.out.println("____________________________________________________________");
+            else if (input.equals("list")) //display tasks when user inputs "list"
+            {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Here are your tasks Daddy:");
+                for (int i = 0; i < task_count; i++)
+                {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else //store whatever user input in "tasks" array
+            {
+                tasks[task_count] = input;
+                task_count++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
+            }
         }
 
         System.out.println("____________________________________________________________"); //first line after "bye"
