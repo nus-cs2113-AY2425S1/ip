7a1482f9df27ea47955d2fc75581924f8ee55b1b
Dri_water <jonasseet@gmail.com>
2024-08-26 17:52:16 +0800

    Implement Archibald greeter chatbot

---
+++ src/main/java/Archibald.java
@@ -0,0 +1,8 @@
+public class Archibald {
+    public static void main(String[] args) {
+        String name = "Archibald";
+        System.out.println("Hello, I am known as " + name);
+        System.out.println("Farewell for now!");
+    }
+}
+
