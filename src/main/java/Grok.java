616322ecb07222ea822c5461f364b64a26e92a44
naveen42x <naveensuresh101@gmail.com>
2024-08-26 13:11:03 +0800

    Implement Level 3: Mark as Done

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Grok {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100]; // Array to store Task objects
         int taskCount = 0;
 
         printLine();
@@ -25,8 +25,22 @@ public class Grok {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 printLine();
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber].markAsDone();
+                printLine();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[taskNumber]);
+                printLine();
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber].markAsNotDone();
+                printLine();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[taskNumber]);
+                printLine();
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 printLine();
                 System.out.println("added: " + input);
