9641b8fb0aefae2e1d47c60a9f5eed2dbf78c427
kennethszj <kennethszj.0123@gmail.com>
2024-08-26 20:29:27 +0800

    Implement level 1 echo functionality

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Buddy {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Buddy");
-        System.out.println(" What can I do for you?");
+        System.out.println(" Hi Daddy, I'm Buddy");
+        System.out.println(" What can I do for you? ⸜(｡˃ ᵕ ˂ )⸝♡");
         System.out.println("____________________________________________________________");
 
+        Scanner scanner = new Scanner(System.in);
+        String input;
 
-        System.out.println(" Bye, see you again !");
-        System.out.println("____________________________________________________________");
+        while (true)
+        {
+            input = scanner.nextLine();
+            if (input.equals("bye"))
+            {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println("     " + input);
+            System.out.println("____________________________________________________________");
+        }
 
+        System.out.println("____________________________________________________________"); //first line after "bye"
+        System.out.println(" Bye Daddy. I will miss you (>ᴗ•) !");
+        System.out.println("____________________________________________________________"); //second line after "bye"
     }
 }
