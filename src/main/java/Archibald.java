e5c84506f2e9fc43bbedb0063f3ae6a98f2714ef
Dri_water <jonasseet@gmail.com>
2024-08-26 18:18:54 +0800

    Implement ability to mark tasks as done and not done

--- src/main/java/Archibald.java
+++ src/main/java/Archibald.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Archibald {
 
-    private static String[] tasks = new String[100];
+    private static Task[] tasks = new Task[100];
     private static int taskCount = 0;
 
     public static void printArchibaldResponse(String message) {
@@ -11,11 +11,11 @@ public class Archibald {
         System.out.println("____________________________________________________________");
     }
 
-    public static void addTask(String task) {
+    public static void addTask(String taskDescription) {
         if (taskCount < tasks.length) {
-            tasks[taskCount] = task;
+            tasks[taskCount] = new Task(taskDescription);
             taskCount++;
-            printArchibaldResponse("added: " + task);
+            printArchibaldResponse("added: " + taskDescription);
         } else {
             printArchibaldResponse("Error: Task list is full!");
         }
@@ -28,6 +28,7 @@ public class Archibald {
         }
         System.out.println("____________________________________________________________");
     }
+
     public static void main(String[] args) {
         String name = "Archibald";
         printArchibaldResponse("Hello, I am known as " + name + ",\nhow may I be of assistance!");
@@ -41,11 +42,43 @@ public class Archibald {
                 break;
             } else if (input.equals("list")) {
                 printList();
+            } else if (input.startsWith("mark")) {
+                markTask(input);
+            } else if (input.startsWith("unmark")) {
+                unmarkTask(input);
             } else {
                 addTask(input);
             }
         }
 
         scanner.close();
+    }
+
+    public static void markTask(String input) {
+        try {
+            int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsDone();
+                printArchibaldResponse("Great Wizard of Skibidiness! Thou hath completed task:\n  " + tasks[taskIndex]);
+            } else {
+                printArchibaldResponse("Error: Invalid task number.");
+            }
+        } catch (Exception e) {
+            printArchibaldResponse("Error: Invalid command format.");
+        }
+    }
+
+    public static void unmarkTask(String input) {
+        try {
+            int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsNotDone();
+                printArchibaldResponse("Big yikers! The task hath been marked undone:\n  " + tasks[taskIndex]);
+            } else {
+                printArchibaldResponse("Error: Invalid task number.");
+            }
+        } catch (Exception e) {
+            printArchibaldResponse("Error: Invalid command format.");
+        }
     }
 }
