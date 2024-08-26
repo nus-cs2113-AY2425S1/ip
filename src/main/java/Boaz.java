a9cd8a76d198cf51fdd02121d50bfd67f968e73a
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-08-26 10:19:32 +0800

    Rename, Greet, Exit

--- src/main/java/Boaz.java
+++ src/main/java/Boaz.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Boaz {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Boaz.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
 }
