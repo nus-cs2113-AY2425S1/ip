2c0c7f17e80abc5f3efadd9d88f0abc762af510b
ChunJia <chiew_chunjia@u.nus.edu>
2024-08-26 20:12:08 +0800

    Add Main.java and implement echo and exit functions

--- src/main/java/Appal.java
+++ src/main/java/Appal.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 public class Appal {
-    public static void main(String[] args) {
-        String chatbot = "Appal";
-        System.out.println("Hello! I'm " + chatbot + "!");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye! Hope to see you again soon!");
+    private boolean isExited = false;
+
+    public void printSeparator() {
+        System.out.println("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     }
+
+    public void printReply(String reply) {
+        printSeparator();
+        System.out.println(reply);
+        printSeparator();
+    }
+
+    public void handleInput() {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        if (line.equals("bye")) {
+            isExited = true;
+            printReply("See ya! An Appal a day, keeps the boredom away!");
+        } else {
+            printReply(line);
+        }
+    }
+
+    public void runAppal() {
+        String chatbot = "Appal";
+        printSeparator();
+        System.out.println("Heyo! I'm your pal, " + chatbot + "!");
+        System.out.println("Let's get things rolling, what would you like to do today?");
+        printSeparator();
+        while (!isExited) {
+            handleInput();
+        }
+   }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Appal appal = new Appal();
+        appal.runAppal();
+    }
+}
