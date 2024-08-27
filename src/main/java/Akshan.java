82d07dc2cefded8d5aa00b0049fe0b258ac7cc09
yijiano <zhangyijian2002@gmail.com>
2024-08-28 02:14:00 +0800

    Add echo and exit

--- src/main/java/Akshan.java
+++ src/main/java/Akshan.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Akshan {
 
     private static final String name = "Akshan";
@@ -19,18 +22,29 @@ public class Akshan {
 
     private static void init(){
         System.out.println(logo);
-        Akshan.printLine();
+        printLine();
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        Akshan.printLine();
+        printLine();
     }
 
     private static void bye(){
+        printLine();
         System.out.println( "Bye. Hope to see you again soon!");
     }
 
     public static void main(String[] args) {
-       Akshan.init();
-       Akshan.bye();
+        String line;
+        Scanner input = new Scanner(System.in);
+
+        init();
+        line = input.nextLine();
+        printLine();
+        while (!Objects.equals(line, "bye")) {
+            System.out.println(line);
+            printLine();
+            line = input.nextLine();
+        }
+        bye();
     }
 }
