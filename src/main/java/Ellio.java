2c3ec3e2b29bdc9c205bc8a3c5642aae18a251dd
Kenneth <63093454+Chinorea@users.noreply.github.com>
2024-08-27 22:08:02 +0800

    Level-2
    Added add task functionality
    Added List task functionality

--- src/main/java/Ellio.java
+++ src/main/java/Ellio.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Ellio {
 
+    private static String[] listTask = new String[100];
+    private static int numberTask = 0;
+
     public static void endProgram(){
         System.out.println(BotText.lineBorder + BotText.goodbyeMessage + BotText.lineBorder);
     }
@@ -11,13 +14,32 @@ public class Ellio {
         System.out.println(BotText.lineBorder + BotText.welcomeMessage + BotText.lineBorder);
     }
 
+    public static void printList(){
+        System.out.print(BotText.lineBorder);
+        for(int i = 0; i < numberTask; i++){
+            System.out.println((i+1) + ". " + listTask[i]);
+        }
+        System.out.println(BotText.lineBorder);
+    }
+
+    public static void addList(String line){
+        listTask[numberTask] = line;
+        numberTask++;
+        System.out.println(BotText.lineBorder + "added: " + line + "\n" + BotText.lineBorder);
+    }
+
     public static void getInput(){
         String line;
         Scanner in = new Scanner(System.in);
         line = (in.nextLine()).toLowerCase();
 
         while(!line.equals("bye")){
-            System.out.println(BotText.lineBorder + line + "\n" + BotText.lineBorder);
+            if(line.equals("list")){
+                printList();
+            }
+            else{
+                addList(line);
+            }
             line = in.nextLine().toLowerCase();
         }
         endProgram();
