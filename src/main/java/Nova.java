416f4fd4eda4dc17e856bb4113c622c4846bbc7d
telferang <telferang@gmail.com>
2024-08-27 15:01:27 +0800

    Add mark as done features to Nova
    Task class: Manage individual tasks, with status tracking
    Nova.java: Modify code to handle Task class and handle invalid input

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,35 +1,69 @@
 import java.util.Scanner;
 
 public class Nova {
-    public static void print(String info) {
+
+    public static void displayMessage(String info) {
         System.out.println("____________________________________________________________\n" +
                 info +
                 "\n____________________________________________________________\n");
     }
 
     public static void main(String[] args) {
-        print(" Hello! I'm Nova\n What can I do for you?");
-        String[] tasks = new String[100];
-        int numberOfTasks = 0;
+
+        displayMessage(" Hello! I'm Nova\n What can I do for you?");
+        Task[] tasks = new Task[100];
+        
         while (true) {
+
             Scanner sc = new Scanner(System.in);
             String info = sc.nextLine();
-            switch (info) {
+            String[] words = info.split(" ");
+            int taskIndex;
+
+            switch (words[0]) {
+
             case "bye":
-                print("Bye. Hope to see you again soon!");
+                displayMessage("Bye. Hope to see you again soon!");
                 return;
-            case "list":
+
+            case "list": //List out all the tasks
                 System.out.println("____________________________________________________________");
-                for (int i = 1; i <= numberOfTasks; i++) {
-                    System.out.println(i + ": " + tasks[i - 1]);
+                for (int i = 1; i <= Task.getNumberOfTasks(); i++) {
+                    System.out.println(i + ".[" + tasks[i - 1].getStatusIcon() + "] " + tasks[i - 1].getDescription());
                 }
                 System.out.println("____________________________________________________________");
                 break;
-            default:
-                print(info);
-                tasks[numberOfTasks] = info;
-                numberOfTasks++;
+
+            case "mark": //Mark a task as completed
+            case "unmark": //Unmark a task completion
+                try {
+                    taskIndex = Integer.parseInt(words[1]);
+                } catch (NumberFormatException e) { //Display error if user did not put a number as the second argument
+                    displayMessage("Invalid Index");
+                    break;
+                } catch (ArrayIndexOutOfBoundsException e) { //Display error if user did not input second argument
+                    displayMessage("Missing Index Argument");
+                    break;
+                }
+
+                if (taskIndex <= 0 || taskIndex > Task.getNumberOfTasks()) { //Display error if index is invalid
+                    displayMessage("Invalid task index: " + taskIndex);
+                } else if (words[0].equals("mark")) { //Mark case
+                    tasks[taskIndex - 1].markDone();
+                    displayMessage("Nice! I've marked this task as done:\n" + "  [" +
+                            tasks[taskIndex - 1].getStatusIcon() + "] " + tasks[taskIndex - 1].getDescription());
+                } else { //Unmark case
+                    tasks[taskIndex - 1].unmarkDone();
+                    displayMessage("OK, I've marked this task as not done yet:\n" + "  [" +
+                            tasks[taskIndex - 1].getStatusIcon() + "] " + tasks[taskIndex - 1].getDescription());
+                }
                 break;
+
+            default: //Add input as a task
+                displayMessage(info);
+                tasks[Task.getNumberOfTasks()] = new Task(info);
+                break;
+
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    private static int numberOfTasks = 0;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        numberOfTasks++;
+    }
+
+    public static int getNumberOfTasks() {
+        return numberOfTasks;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
