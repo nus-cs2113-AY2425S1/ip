3737d78da8fd410c50973545386b641f8141689d
rexkoh425 <rexkoh8@gmail.com>
2024-08-27 20:22:37 +0800

    Added markings when listing items
    1) Allow users to mark and unmark items

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -3,26 +3,41 @@ import java.util.ArrayList;
 public class List {
 
     public ArrayList<String> listOfItems;
+    public ArrayList<Boolean> isMarkedAsDone;
     public int listLength;
 
     public List(){
         listOfItems = new ArrayList<>();
+        isMarkedAsDone = new ArrayList<>();
         listLength = 0;
     }
 
     public void addItem(String item){
         listOfItems.add(item);
+        isMarkedAsDone.add(false);
         listLength++;
     }
 
-    public void removeItem(String item){
-        listOfItems.remove(item);
-        listLength--;
+    public void setAsDone(int listNumber){
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  [X] " + listOfItems.get(listNumber-1));
+        isMarkedAsDone.set(listNumber-1 , true);
+    }
+
+    public void setAsNotDone(int listNumber){
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  [ ] " + listOfItems.get(listNumber-1));
+        isMarkedAsDone.set(listNumber-1 , false);
     }
 
     public void listItems(){
+        System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < listLength; i++){
-            System.out.printf("%d. " + listOfItems.get(i) + "\n", i);
+            char markingChar = ' ';
+            if(isMarkedAsDone.get(i)){
+                markingChar = 'X';
+            }
+            System.out.printf("%d.[%c] " + listOfItems.get(i) + "\n", i , markingChar);
         }
     }
 
--- src/main/java/TheThinker.java
+++ src/main/java/TheThinker.java
@@ -5,19 +5,31 @@ public class TheThinker {
     public static String name = "TheThinker";
 
     public static void main(String[] args) {
-        separation();
-        greet();
-        separation();
+        printSeparation();
+        printGreeting();
+        printSeparation();
         Scanner in = new Scanner(System.in);
         String userInput;
         List toDoList = new List();
 
         do{
             userInput = in.nextLine();
-            separation();
+            printSeparation();
+
+            if(hasMarkOrUnmark(userInput)){
+                String[] actionAndItemNumber = userInput.split(" ");
+                if(actionAndItemNumber[0].trim().equalsIgnoreCase("mark")){
+                    toDoList.setAsDone(Integer.parseInt(actionAndItemNumber[1].trim()));
+                    continue;
+                }else if(actionAndItemNumber[0].trim().equalsIgnoreCase("unmark")){
+                    toDoList.setAsNotDone(Integer.parseInt(actionAndItemNumber[1].trim()));
+                    continue;
+                }
+            }
+
             switch(userInput){
             case "bye":
-                bye();
+                printBye();
                 break;
 
             case "list" :
@@ -29,22 +41,26 @@ public class TheThinker {
                 System.out.println("added: " + userInput);
                 break;
             }
-            separation();
+            printSeparation();
 
         }while(!userInput.equals("bye"));
     }
 
-    public static void greet(){
+    public static void printGreeting(){
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
     }
 
-    public static void bye(){
+    public static void printBye(){
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void separation(){
+    public static void printSeparation(){
         System.out.println("____________________________________________________________");
+    }
+
+    public static boolean hasMarkOrUnmark(String userInput){
+        return(userInput.contains("mark") || userInput.contains("unmark"));
     }
 
 }
