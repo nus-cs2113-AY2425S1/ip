e8399dd54aa95460f66dd83fc6be95ecaa82b3d3
TPH777 <pinghuitan80@gmail.com>
2024-08-27 20:49:35 +0800

    Store text and return when requested
    Store user texts in a String array of fixed size of 100.
    When user command "list", print the texts.

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,19 +1,37 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Iris {
+    public static String[] list = new String[100];
+    public static int numInList = 0;
+
+    public static void printDivider() {
+        System.out.println("---------------------------------------------");
+    }
+
     public static boolean printText(String text) {
+        printDivider();
         boolean isEnded = false;
         if (text.equalsIgnoreCase("bye")) {
-            text = "Bye. Hope to see you again soon!";
+            System.out.println("Bye. Hope to see you again soon!");
             isEnded = true;
+        } else if (text.equalsIgnoreCase("list")) {
+            String[] listWithoutNull = Arrays.copyOf(list, numInList);
+            for (int i = 0; i < listWithoutNull.length; i++) {
+                System.out.println(i + ": " + listWithoutNull[i]);
+            }
+        } else {
+            list[numInList] = text;
+            System.out.println("Added: " + text);
+            numInList++;
         }
-        System.out.println(text);
-        System.out.println("---------------------------------------------");
+        printDivider();
         return isEnded;
     }
     public static void main(String[] args) {
         // Greets
-        printText("Hello! I'm Iris!\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Iris!\n" + "What can I do for you?");
+        printDivider();
         boolean isEnded = false;
         while (!isEnded) {
             Scanner in = new Scanner(System.in); // Initialisation
