ee93d49994ea4f61e0adf782ff0671ee83308ead
wahkia <gohah021@gmail.com>
2024-08-27 22:13:55 +0800

    Add initial chatbot greeting and farewell code
    This is the first entry of the project. It contains a Java program
    that outputs a simple chatbot greeting and farewell message.

---
+++ src/main/java/Lia.java
@@ -0,0 +1,19 @@
+public class Lia {
+
+    public static void main(String[] args) {
+        String botName = "Lia";
+
+        // Call the function to print messages
+        printLine();
+        System.out.println(" Hello! I'm " + botName);
+        System.out.println(" What can I do for you?");
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    // Function to print the line
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+}
