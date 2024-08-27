23a0d1f9ee9bc881f9f8c07b4b2e8fe20c720a42
CheeKiangg <ngchkiang@gmail.com>
2024-08-27 09:28:32 +0800

    Add Level-2
    store text and display back to user when requested

--- src/main/java/CodeCatalyst.java
+++ src/main/java/CodeCatalyst.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class CodeCatalyst {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("        ________________________________________________________");
-        System.out.println("        Hello, I'm CodeCatalyst.");
-        System.out.println("        What can I do for you?");
+        System.out.println("         Hello, I'm CodeCatalyst.");
+        System.out.println("         What can I do for you?");
         System.out.println("        ________________________________________________________\n");
 
         while (true) {
             String task = input.nextLine();
             System.out.println("        ________________________________________________________");
             if (task.equals("bye")) {
-                System.out.println("        Bye. Hope to see you again soon!");
+                System.out.println("         Bye. Hope to see you again soon!");
                 System.out.println("        ________________________________________________________\n");
                 break;
+            } else if (task.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("         " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = task;
+                taskCount += 1;
+                System.out.println("         added: " + task);
             }
-            System.out.println("        " + task);
             System.out.println("        ________________________________________________________\n");
         }
     }
