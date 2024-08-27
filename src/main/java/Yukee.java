f1caf56a2a625e9ae49bd054c43366c0f3c3010f
YukeeHong <zhaiyihong1124@gmail.com>
2024-08-27 17:33:41 +0800

    level-0

---
+++ src/main/java/Yukee.java
@@ -0,0 +1,12 @@
+public class Yukee {
+    public static void main(String[] args) {
+        String logo = " Y   Y  U   U  K   K  EEEEE  EEEEE\n"
+                + "  Y Y   U   U  K  K   E      E    \n"
+                + "   Y    U   U  KKK    EEEE   EEEE \n"
+                + "   Y    U   U  K  K   E      E    \n"
+                + "   Y     UUU   K   K  EEEEE  EEEEE\n";
+        System.out.println("Hello! I'm Yukee\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
