0d3661aa6f06f4d6ee81aef2114f2e6201c5c7eb
TPH777 <pinghuitan80@gmail.com>
2024-08-27 19:46:48 +0800

    Change chatbot name to Iris

---
+++ src/main/java/Iris.java
@@ -0,0 +1,11 @@
+public class Iris {
+    public static void printText(String text) {
+        System.out.println(text);
+        System.out.println("---------------------------------------------");
+    }
+    public static void main(String[] args) {
+        // Greets
+        printText("Hello! I'm Iris!\n" + "What can I do for you?");
+        printText("Bye. Hope to see you again soon!");
+    }
+}
