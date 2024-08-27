8bb65b16c8592667967bc58d053ff6dbca37d6b1
Sukkaito <40479420+Sukkaito@users.noreply.github.com>
2024-08-28 00:45:04 +0800

    Add Level-1 incremement

--- src/main/java/SuBOT.java
+++ src/main/java/SuBOT.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SuBOT {
     public static void main(String[] args) {
         String logo = """
@@ -11,6 +13,19 @@ public class SuBOT {
         System.out.println(logo);
         System.out.println("How can I help you today?");
         System.out.println("-------------------------------");
+        Scanner sc = new Scanner(System.in);
+        String command;
+        boolean stopFlag = false;
+        while (!stopFlag) {
+            command = sc.nextLine();
+            System.out.println("-------------------------------");
+            if (command.equals("bye"))  {
+                stopFlag = true;
+                System.out.println("Bye bye...");
+            }
+            else System.out.println(command);
+            System.out.println("-------------------------------");
+        }
         System.out.println("System shutting down...");
         System.out.println("Goodbye world!");
     }
