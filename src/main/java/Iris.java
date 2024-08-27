24b808d879d941da9630afa11fee5b7f03a2892c
TPH777 <pinghuitan80@gmail.com>
2024-08-27 20:26:49 +0800

    Echo user command

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Iris {
-    public static void printText(String text) {
+    public static boolean printText(String text) {
+        boolean isEnded = false;
+        if (text.equalsIgnoreCase("bye")) {
+            text = "Bye. Hope to see you again soon!";
+            isEnded = true;
+        }
         System.out.println(text);
         System.out.println("---------------------------------------------");
+        return isEnded;
     }
     public static void main(String[] args) {
         // Greets
         printText("Hello! I'm Iris!\n" + "What can I do for you?");
-        printText("Bye. Hope to see you again soon!");
+        boolean isEnded = false;
+        while (!isEnded) {
+            Scanner in = new Scanner(System.in); // Initialisation
+            String line = in.nextLine();
+            isEnded = printText(line);
+        }
     }
 }
