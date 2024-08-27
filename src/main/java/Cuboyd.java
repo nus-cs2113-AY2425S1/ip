0e9743ba28982149e79351f2d4c3454902e3f33d
Terence Chan Zun Mun <zunmun@gmail.com>
2024-08-27 16:07:20 +0800

    Add echo, bye

--- src/main/java/Cuboyd.java
+++ src/main/java/Cuboyd.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Cuboyd {
     public static void main(String[] args) {
         String name = "Cuboyd";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        // Command Entry
+        String line;
+        Scanner sc = new Scanner(System.in);
+        boolean isAskingInput = true;
+        while (isAskingInput){
+            System.out.print("> ");
+            line = sc.nextLine();
+            switch(line){
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    isAskingInput = false;
+                    break;
+                default:
+                    System.out.println(line);
+                    break;
+            }
+        }
     }
 }
