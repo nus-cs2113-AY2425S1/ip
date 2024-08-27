6292a841afa5142251a9a905a97251b24e84dbed
TPH777 <pinghuitan80@gmail.com>
2024-08-27 21:58:48 +0800

    Add mark and unmark functionality
    Added marked/unmarked boxes beside list
    Abstraction of codes within chat to individual functions

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,40 +1,77 @@
 import java.util.Scanner;
 
 public class Iris {
-    public static String[] list = new String[100];
-    public static int numInList = 0;
+    public static Task[] tasks = new Task[100];
+    public static int numOfTasks = 0;
 
     public static void printDivider() {
         System.out.println("---------------------------------------------");
     }
 
-    public static boolean printText(String text) {
+    public static void listTasks() {
+        if (numOfTasks == 0) {
+            System.out.println("No tasks added.");
+        } else {
+            for (int i = 0; i < tasks.length && tasks[i] != null; i++) {
+                Task currentTask = tasks[i];
+                System.out.println((i + 1) + "." + currentTask.getStatusIcon() + currentTask.description);
+            }
+        }
+    }
+
+    public static void addTask(String text) {
+        tasks[numOfTasks] = new Task(text);
+        System.out.println("Added: " + text);
+        numOfTasks++;
+    }
+
+    public static void editTask(String text) {
+        try {
+            int numbersInText = Integer.parseInt(text.replaceAll("[^0-9]", ""));
+            if (numbersInText > numOfTasks || numbersInText < 1) {
+                System.out.println("The task does not exist");
+            } else {
+                Task taskToEdit = tasks[(numbersInText) - 1];
+                if (text.contains("unmark")) {
+                    taskToEdit.unmarkFromDone();
+                    System.out.println("OK, I've marked this task as not done yet:\n" + "[ ] " + taskToEdit.description);
+                } else {
+                    taskToEdit.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n" + "[X] " + taskToEdit.description);
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Please provide a valid task number.");
+        }
+    }
+
+    public static boolean chat(String text) {
         printDivider();
         boolean isEnded = false;
         if (text.equalsIgnoreCase("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
             isEnded = true;
         } else if (text.equalsIgnoreCase("list")) {
-            for (int i = 0; i < list.length && list[i] != null; i++) {
-                System.out.println(i + ": " + list[i]);
-            }
+            listTasks();
+        } else if (text.contains("mark")) {
+            editTask(text);
+        } else if (text.isEmpty()) {
+            System.out.println("Do you need any further assistance?");
         } else {
-            list[numInList] = text;
-            System.out.println("Added: " + text);
-            numInList++;
+            addTask(text);
         }
         printDivider();
         return isEnded;
     }
+
     public static void main(String[] args) {
-        // Greets
-        System.out.println("Hello! I'm Iris!\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Iris!\n" + "What can I do for you?"); // Greets
         printDivider();
         boolean isEnded = false;
+        Scanner in = new Scanner(System.in); // Initialisation
         while (!isEnded) {
-            Scanner in = new Scanner(System.in); // Initialisation
             String line = in.nextLine();
-            isEnded = printText(line);
+            isEnded = chat(line);
         }
     }
 }
