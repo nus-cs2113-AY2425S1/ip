b54bc98da6948656e797c24f8ea106f187278993
CheeKiangg <ngchkiang@gmail.com>
2024-08-27 09:08:03 +0800

    Add level-1
    echo commands from user

--- src/main/java/CodeCatalyst.java
+++ src/main/java/CodeCatalyst.java
@@ -1,7 +1,26 @@
+import org.w3c.dom.ls.LSInput;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class CodeCatalyst {
     public static void main(String[] args) {
-        System.out.println("Hello, I'm CodeCatalyst.");
-        System.out.println("What can I do for you?  \n");
-        System.out.println("Bye. Hope to see you again soon!  \n");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("        ________________________________________________________");
+        System.out.println("        Hello, I'm CodeCatalyst.");
+        System.out.println("        What can I do for you?");
+        System.out.println("        ________________________________________________________\n");
+
+        while (true) {
+            String task = input.nextLine();
+            System.out.println("        ________________________________________________________");
+            if (task.equals("bye")) {
+                System.out.println("        Bye. Hope to see you again soon!");
+                System.out.println("        ________________________________________________________\n");
+                break;
+            }
+            System.out.println("        " + task);
+            System.out.println("        ________________________________________________________\n");
+        }
     }
 }
