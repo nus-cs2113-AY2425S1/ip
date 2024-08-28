907e5a9d109a7852bbb2c30a0ac68f0f520f834c
yeekian <53704991+yeekian@users.noreply.github.com>
2024-08-28 23:34:07 +0800

    Chatbot: Follow coding standard

--- src/main/java/Mel.java
+++ src/main/java/Mel.java
@@ -16,24 +16,27 @@ public class Mel {
             line = in.nextLine();
             System.out.println("\t________________________________________");
 
-
             if (line.equals("bye")) {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t________________________________________");
                 break;
+
             } else if (line.equals("list")) {
                 userList.printList();
                 System.out.println("\t________________________________________");
+
             } else if (line.substring(0, 4).equals("mark")) {
                 userList.markItem(line);
                 System.out.println("\t________________________________________");
+
             } else if (line.substring(0, 6).equals("unmark")) {
                 userList.unmarkItem(line);
                 System.out.println("\t________________________________________");
+
             } else {
-                    System.out.println("\t" + "added: " + line);
-                    System.out.println("\t________________________________________");
-                    userList.addItem(line);
+                System.out.println("\t" + "added: " + line);
+                System.out.println("\t________________________________________");
+                userList.addItem(line);
             }
         }
     }
