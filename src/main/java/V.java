09aa704b412605b89133038fe83fecbd40213e13
KHsienKit <hsienkitkhor@gmail.com>
2024-08-28 20:31:45 +0800

    Add list functionality to V

--- src/main/java/V.java
+++ src/main/java/V.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class V {
 
@@ -16,10 +17,19 @@ public class V {
         System.out.println("What can I do for you?");
         System.out.println(LINE_SEPERATOR);
     }
+
+    public static void displayList(String[] list, int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.println(String.format("%d. %s", i + 1, list[i]));
+        }
+    }
+
     public static void main(String[] args) {
         greet();
 
         boolean isOnline = true;
+        String[] list = new String[100];
+        int count = 0;
         String line;
         Scanner in = new Scanner(System.in);
         
@@ -28,12 +38,16 @@ public class V {
             if (line.equals("bye")) {
                 in.close();
                 isOnline = false;
+            } else if (line.equals("list")) {
+                displayList(list, count);
             } else {
+                list[count] = line;
+                count++;
                 System.out.println(LINE_SEPERATOR);
-                System.out.println(line);
+                System.out.println("added: " + line);
                 System.out.println(LINE_SEPERATOR);
             }
         }
-        System.out.println(LINE_SEPERATOR + "\nSee ya");
+        System.out.println(LINE_SEPERATOR + "\nSee ya\n" + LINE_SEPERATOR);
     }
 }
