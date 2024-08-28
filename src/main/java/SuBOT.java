e630aa8f72ddf6bb1c0572f6abeb7b695e28873d
Sukkaito <40479420+Sukkaito@users.noreply.github.com>
2024-08-28 12:28:40 +0800

    Add Level-3 increment
    Changes:
    - "list" command no longer shows command history, but shows list of tasks instead.
    
    Feature:
    - Ability to mark/unmark task as _done_
    - Optional ```Task``` class

--- src/main/java/SuBOT.java
+++ src/main/java/SuBOT.java
@@ -1,6 +1,52 @@
 import java.util.Scanner;
 
 public class SuBOT {
+    private static boolean stopFlag = false;
+    private static Task[] tasks = new Task[100];
+    private static int taskCount;
+
+    /**
+     * Process user's input command
+     * @param command Input command
+     */
+    public static void process(String command) {
+        String[] argv = command.split(" ");
+        int argc = argv.length;
+
+        String action = argv[0];
+        int taskIndex;
+
+        switch (action) {
+            case "bye":
+                stopFlag = true;
+                System.out.println("Bye bye...");
+                break;
+            case "list":
+                for (int i = 0; i < taskCount; ++i) {
+                    System.out.printf("%d.", i+1);
+                    tasks[i].printTask();
+                }
+                break;
+            case "mark":
+            case "unmark":
+                try {
+                    taskIndex = Integer.parseInt(argv[1]) - 1;
+                    tasks[taskIndex].setDone(action.equals("mark"));
+                    System.out.printf("I have %sed the following task for you:\n", action);
+                    tasks[taskIndex].printTask();
+                }
+                catch (Exception e) {
+                    System.out.println("Invalid argument(s): " + e);
+                    System.out.println("Usage: mark/unmark <taskNumber>");
+                }
+                break;
+            default:
+                tasks[taskCount++] = new Task(command);
+                System.out.println(command);
+                break;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = """
                        ____  ____  ______
@@ -15,26 +61,11 @@ public class SuBOT {
         System.out.println("-------------------------------");
         Scanner sc = new Scanner(System.in);
         String command;
-        String[] commands = new String[100];
-        int commandCount = 0;
-        boolean stopFlag = false;
+        
         while (!stopFlag) {
             command = sc.nextLine();
             System.out.println("-------------------------------");
-            switch (command) {
-                case "bye":
-                    stopFlag = true;
-                    System.out.println("Bye bye...");
-                    break;
-                case "list":
-                    for (int i = 0; i < commandCount; ++i) {
-                        System.out.printf("%d. %s\n", i+1, commands[i]);
-                    }
-                    break;
-                default:
-                    commands[commandCount++] = command;
-                    System.out.println(command);
-            }
+            process(command);
             System.out.println("-------------------------------");
         }
         System.out.println("System shutting down...");
---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    private String command;
+    private boolean isDone;
+
+    public Task(String command, boolean isDone) {
+        this.command = command;
+        this.isDone = isDone;
+    }
+
+    public Task(String command) {
+        this.command = command;
+        this.isDone = false;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public void printTask() {
+        String mark = (this.isDone) ? "X" : " ";
+        System.out.printf("[%s] %s\n", mark, this.command);
+    }
+}
