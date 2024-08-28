34f213bf2f6d78d73c53cddbd34ceb80157c9241
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-08-29 00:09:35 +0800

    Add echo functionality

--- src/main/java/Bosco.java
+++ src/main/java/Bosco.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class Bosco {
+    private static void printHorizontalRule() {
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String hr = "____________________________________________________________";
-        System.out.println(hr + "\nHello! I'm Bosco the All-Purpose Drone.");
-        System.out.println("What can I do for you?\n" + hr);
-        System.out.println("Bye. Hope to see you again soon!\n" + hr);
+        Scanner in = new Scanner(System.in);
+        String userInput;
+        String userCommand;
+
+        printHorizontalRule();
+        System.out.println("\t Hello! I'm Bosco APD.");
+        System.out.println("\t What can I do for you?");
+        printHorizontalRule();
+
+        while (true) {
+            userInput = in.nextLine();
+            userCommand = userInput.split("\\s+", 2)[0];
+            if (userCommand.equals("bye")) {
+                break;
+            }
+            printHorizontalRule();
+            System.out.println("\t " + userInput);
+            printHorizontalRule();
+        }
+
+        printHorizontalRule();
+        System.out.println("\t Bye! Hope to see you again soon!");
+        printHorizontalRule();
     }
 }
