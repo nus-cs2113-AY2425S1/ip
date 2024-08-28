d0d3e24a52409ee6d19a746c4cb7761bf7950a11
KHsienKit <hsienkitkhor@gmail.com>
2024-08-28 17:22:49 +0800

    Make V echo commands

--- src/main/java/V.java
+++ src/main/java/V.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class V {
+
+    private static final String LINE_SEPERATOR = "____________________________________________________________";
 
     public static void greet() {
         String logo = " _       _ \n"
@@ -6,15 +10,30 @@ public class V {
                 + " \\ \\   / / \n"
                 + "  \\ \\_/ / \n"
                 + "   \\___/ \n";
-        System.out.print("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
+        System.out.print(logo);
+        System.out.println(LINE_SEPERATOR);
         System.out.println("Hello! I'm V");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE_SEPERATOR);
     }
     public static void main(String[] args) {
         greet();
+
+        boolean isOnline = true;
+        String line;
+        Scanner in = new Scanner(System.in);
+        
+        while (isOnline) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                in.close();
+                isOnline = false;
+            } else {
+                System.out.println(LINE_SEPERATOR);
+                System.out.println(line);
+                System.out.println(LINE_SEPERATOR);
+            }
+        }
+        System.out.println(LINE_SEPERATOR + "\nSee ya");
     }
 }
