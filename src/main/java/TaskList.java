debb98b87926213842aa1ef026873c2aa4dc723c
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-08-28 16:23:29 +0800

    Add mark/unmark task feature
    Create new class Task in Task.java to record
    description and whether task is done

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,14 +1,30 @@
 public class TaskList {
-    private static String[] taskList = new String[100];
+    private static Task[] taskList = new Task[100];
     private static int taskCount = 0;
 
-    public static void addTask(String task) {
+    public static void addTask(Task task) {
         taskList[taskCount++] = task;
+    }
+
+    public static boolean checkValidTaskId(int taskId) {
+        return taskId >= 0 && taskId < taskCount;
+    }
+
+    public static void markTaskAsDone (int taskId) {
+        taskList[taskId].markAsDone();
+    }
+
+    public static void markTaskAsUndone (int taskId) {
+        taskList[taskId].markAsUndone();
+    }
+
+    public static String singleTaskDetails (int taskId) {
+        return taskList[taskId].taskNameWithStatus();
     }
 
     public static void printList() {
         for (int i = 0; i < taskCount; i++) {
-            System.out.println((i+1) + ". " + taskList[i]);
+            System.out.println((i+1) + ". " + taskList[i].taskNameWithStatus());
         }
     }
 }
--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -3,18 +3,42 @@ import java.util.Scanner;
 public class Tyrone {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean shouldExit = false;
         System.out.println("Hello! I'm Tyrone.\nWhat can I do for you?");
-        String input = scanner.nextLine();
 
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
+        while (!shouldExit) {
+            String command = scanner.nextLine();
+            String[] dissectedCommand = command.split(" ");
+            if (command.equals("bye")) {
+                shouldExit = true;
+            } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 TaskList.printList();
+            } else if (dissectedCommand[0].equals("mark")) {
+                int taskId = Integer.parseInt(dissectedCommand[1]) - 1;
+                if (TaskList.checkValidTaskId(taskId)) {
+                    TaskList.markTaskAsDone(taskId);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + TaskList.singleTaskDetails(taskId));
+                } else {
+                    System.out.println("Invalid task ID.");
+                }
+            } else if (dissectedCommand[0].equals("unmark")) {
+                int taskId = Integer.parseInt(dissectedCommand[1]) - 1;
+                if (TaskList.checkValidTaskId(taskId)) {
+                    TaskList.markTaskAsUndone(taskId);
+                    System.out.println("Ok, I've marked this task as not done yet:");
+                    System.out.println("  " + TaskList.singleTaskDetails(taskId));
+                } else {
+                    System.out.println("Invalid task ID.");
+                }
             } else {
-                TaskList.addTask(input);
-                System.out.println("added: " + input);
+                Task newTask = new Task(command);
+                TaskList.addTask(newTask);
+                System.out.println("added: " + newTask.taskNameWithStatus());
             }
-            input = scanner.nextLine();
         }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
