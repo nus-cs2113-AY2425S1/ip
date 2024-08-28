15810c5f67cb8a29846f6417aed9a6c4845eb34a
Jiaming <shenjiaming27@gmail.com>
2024-08-28 15:20:06 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Bron.java
@@ -0,0 +1,12 @@
+public class Bron {
+    public static void main(String[] args) {
+        String logo = "  ____    ____      ____    _     _\n"
+                    + " |  _ \\  |   _\\   /  __  \\ | \\   | |\n"
+                    + " | |_) | | |_) |  | |  | | |  \\  | | \n"
+                    + " |  _ <  | ___/   | |  | | | |\\\\ | |\n"
+                    + " | |_) | | | \\ \\  | |__| | | | \\\\| |\n"
+                    + " |____/  |_|  \\_\\  \\____/  |_|  \\__|\n";
+        System.out.println(logo + "Hello! I'm Bron\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+
+    }
+}
