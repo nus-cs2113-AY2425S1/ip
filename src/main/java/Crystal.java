d9cff70a6faf311346d694bce4093418ea4a1ed7
crystal-bys <yinshuangbu@gmail.com>
2024-08-28 23:58:09 +0800

    Level 0 completed - name of bot changed to Crystal - greets user and exits when run

--- src/main/java/Crystal.java
+++ src/main/java/Crystal.java
@@ -1,10 +1,12 @@
 public class Crystal {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Crystal.\n" +
-                "What can I do for you today?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println("""
+                ____________________________________________________________
+                Hello! I'm Crystal.
+                What can I do for you today?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """);
     }
 }
