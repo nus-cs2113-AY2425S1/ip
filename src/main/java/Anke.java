39245290bb478a69c79a1e2fb5483adf605961eb
anlamm <anlammaaa@gmail.com>
2024-08-28 17:09:05 +0800

    Level 3. Mark as Done
    added Task class to store the name and status of each task

--- src/main/java/Anke.java
+++ src/main/java/Anke.java
@@ -5,7 +5,7 @@ public class Anke {
         System.out.println("Hello! I'm Anke.");
         System.out.println("What can I do for you?\n");
         String line = "";
-        String[] lines = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
         while (!line.equals("bye")) {
             Scanner in = new Scanner(System.in);
@@ -14,12 +14,23 @@ public class Anke {
                 break;
             } else if (line.equals("list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + lines[i]);
+                    System.out.println((i + 1) + ". " + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
                 System.out.println("");
                 continue;
+            } else if (line.length() > 5 && line.substring(0, 4).equals("mark") && Integer.parseInt(line.substring(5)) <= count && Integer.parseInt(line.substring(5)) > 0) {
+                tasks[Integer.parseInt(line.substring(5)) - 1].setDone(true);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + tasks[Integer.parseInt(line.substring(5)) - 1].getStatusIcon() + "] " + tasks[Integer.parseInt(line.substring(5)) - 1].getDescription() + "\n");
+                continue;
+            } else if (line.length() > 7 && line.substring(0, 6).equals("unmark") && Integer.parseInt(line.substring(7)) <= count && Integer.parseInt(line.substring(7)) > 0) {
+                tasks[Integer.parseInt(line.substring(7)) - 1].setDone(false);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + tasks[Integer.parseInt(line.substring(7)) - 1].getStatusIcon() + "] " + tasks[Integer.parseInt(line.substring(7)) - 1].getDescription() + "\n");
+                continue;
             }
-            lines[count] = line;
+            Task task = new Task(line);
+            tasks[count] = task;
             count++;
             System.out.println("added: " + line + "\n");
         }
