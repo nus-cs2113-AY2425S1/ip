06ecd084ab8f00f4403b8fd8f4044a12f2a6ce89
yeekian <53704991+yeekian@users.noreply.github.com>
2024-08-28 13:11:28 +0800

    Chatbot: add echo

--- src/main/java/Mel.java
+++ src/main/java/Mel.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Mel {
     public static void main(String[] args) {
         System.out.println("________________________________________");
         System.out.println("Hello! I'm Mel");
         System.out.println("What can I do for you?");
         System.out.println("________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________________");
 
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            System.out.println("________________________________________");
+
+
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("________________________________________");
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println("________________________________________");
+            }
+        }
     }
 }
