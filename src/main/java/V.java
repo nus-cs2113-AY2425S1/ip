c7d834d0cc16850d086377706528a7754a46e99b
KHsienKit <hsienkitkhor@gmail.com>
2024-08-28 21:08:46 +0800

    Add mark as done function to V
    Created a new class called Task to hold the description and whether the task is done or not
    V can now parse commands and check the first word to see what kind of command to execute

--- src/main/java/V.java
+++ src/main/java/V.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class V {
 
     private static final String LINE_SEPERATOR = "____________________________________________________________";
+
+    // Print 2 line seperators between a block of text for cleaner CLI
+    public static void printBlock(String text) {
+        System.out.println(LINE_SEPERATOR);
+        System.out.println(text);
+        System.out.println(LINE_SEPERATOR);
+    }
 
     public static void greet() {
         String logo = " _       _ \n"
@@ -12,40 +18,43 @@ public class V {
                 + "  \\ \\_/ / \n"
                 + "   \\___/ \n";
         System.out.print(logo);
-        System.out.println(LINE_SEPERATOR);
-        System.out.println("Hello! I'm V");
-        System.out.println("What can I do for you?");
-        System.out.println(LINE_SEPERATOR);
+        printBlock("Hi I'm V\nWhat can I do for you?");
     }
 
-    public static void displayList(String[] list, int length) {
+    public static void displayList(Task[] list, int length) {
+        System.out.println(LINE_SEPERATOR);
         for (int i = 0; i < length; i++) {
-            System.out.println(String.format("%d. %s", i + 1, list[i]));
+            System.out.println(String.format("%d.[%s] %s", i + 1, list[i].getStatus(), list[i].getDescription()));
         }
+        System.out.println(LINE_SEPERATOR);
     }
 
     public static void main(String[] args) {
         greet();
 
         boolean isOnline = true;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
         String line;
-        Scanner in = new Scanner(System.in);
+        String[] lineArr;
+        Scanner input = new Scanner(System.in);
         
         while (isOnline) {
-            line = in.nextLine();
-            if (line.equals("bye")) {
-                in.close();
+            line = input.nextLine();
+            lineArr = line.split(" ");
+            if (lineArr[0].equals("bye")) {
+                input.close();
                 isOnline = false;
-            } else if (line.equals("list")) {
+            } else if (lineArr[0].equals("list")) {
+                displayList(list, count);
+            } else if (lineArr[0].equals("mark")) {
+                int position = Integer.parseInt(lineArr[1]);
+                list[position - 1].markAsDone();
                 displayList(list, count);
             } else {
-                list[count] = line;
+                list[count] = new Task(line);
                 count++;
-                System.out.println(LINE_SEPERATOR);
-                System.out.println("added: " + line);
-                System.out.println(LINE_SEPERATOR);
+                printBlock("added: " + line);
             }
         }
         System.out.println(LINE_SEPERATOR + "\nSee ya\n" + LINE_SEPERATOR);
