0194f78e4f1b2b42d8a5cea12feb7967d0cd0ca8
Ridiculouswifi <thtan02@gmail.com>
2024-08-28 10:58:37 +0800

    Level-0

---
+++ src/main/java/Freedom.java
@@ -0,0 +1,11 @@
+public class Freedom {
+    public static void main(String[] args) {
+        String logo = "___________________________________\n";
+        String message = """
+                Hello! I'm Freedom
+                What can I do for you?
+                """;
+        String closing = "Bye. Hope to see you again soon!\n";
+        System.out.println(logo + message + logo + closing + logo);
+    }
+}
