523310ffe27bfeb1d4f1e0c613e09797613260a9
AndrewNguyen4 <nguyenminhduc5etp@gmail.com>
2024-08-28 20:54:44 +0800

    Implement a skeletal version of Areus.
    Greets the user and exits with Areus.java

---
+++ src/main/java/Aerus.java
@@ -0,0 +1,10 @@
+public class Aerus {
+    public static void main(String[] args) {
+        String hline = "____________________________________________________________";
+        System.out.println(hline);
+        System.out.println("Hello! I'm Aerus!\nWhat can I do for you?");
+        System.out.println(hline);
+        System.out.println("See you next time!");
+        System.out.println(hline);
+    }
+}
