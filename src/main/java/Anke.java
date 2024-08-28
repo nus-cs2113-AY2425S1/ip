a0113c1f44bfec3f7a234953bf955319c24ee763
anlamm <anlammaaa@gmail.com>
2024-08-28 16:34:28 +0800

    Level 2. Add, List

--- src/main/java/Anke.java
+++ src/main/java/Anke.java
@@ -5,15 +5,24 @@ public class Anke {
         System.out.println("Hello! I'm Anke.");
         System.out.println("What can I do for you?\n");
         String line = "";
+        String[] lines = new String[100];
+        int count = 0;
         while (!line.equals("bye")) {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             if (line.equals("bye")) {
                 break;
+            } else if (line.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + lines[i]);
+                }
+                System.out.println("");
+                continue;
             }
-            System.out.println(line);
+            lines[count] = line;
+            count++;
+            System.out.println("added: " + line + "\n");
         }
-        System.out.println("Bye. Hope to see you again soon!\n");
-
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
