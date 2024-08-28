d034cf019c2fd8b3259f6fdeffa62be84d2fa493
nirala-ts <tanishka.nirala@gmail.com>
2024-08-28 19:43:52 +0800

    Add echo feature

--- src/main/java/Rizzler.java
+++ src/main/java/Rizzler.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class Rizzler {
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        String coolFaceEmoji = "\uD83D\uDE0E";
+        String fistBumpEmoji = "\uD83D\uDC4A";
+        String rockstarHandEmoji = "\uD83E\uDD1F\uD83C\uDFFD";
+        String fireEmoji = "\uD83D\uDD25";
+
+        while (!command.equals("bye")) {
+            if (command.contains("!")) {
+                System.out.println(command + " " + rockstarHandEmoji + fireEmoji);
+            }
+            else {
+                System.out.println(command + " " + rockstarHandEmoji);
+            }
+            command = scanner.nextLine();
+        }
+
+        System.out.println("Peace out, Rizzler’s got places to be! " + coolFaceEmoji + fistBumpEmoji);
+    }
     public static void main(String[] args) {
         String logo =
                   " _____                          _ \n"
@@ -10,11 +34,12 @@ public class Rizzler {
                 + "|_|  \\_\\ |_| /______/ /______/ |_|  \\_____| |_| \n";
 
 
-        System.out.println("Hello from\n" + logo);
+        String partyPopperEmoji = "\uD83C\uDF89";
+        String rocketEmoji = "\uD83D\uDE80";
 
-        System.out.println("Hello! I'm Rizzler.");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(logo);
+        System.out.println("Yo! I'm Rizzler.");
+        System.out.println("What can I do to make your day pop? " + partyPopperEmoji + rocketEmoji);
+        echo();
     }
 }
-
