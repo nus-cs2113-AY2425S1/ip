cd058cfb1e5b7756d909b2a8e5455a3643a5aebc
gao23 <gao2351624311@gmail.com>
2024-08-28 13:04:15 +0800

    Commit new Chatbot skeleton Wildpeace.java

--- src/main/java/Wildpeace.java
+++ src/main/java/Wildpeace.java
@@ -1,4 +1,59 @@
+import java.util.Scanner;
+
 public class Wildpeace {
+    public static void initialise()
+    {
+        System.out.println("What can I do for you, enter '1' for echo, '2' for storing your plan, '0' to exit");
+        Scanner initialInputScanner = new Scanner(System.in);
+        String initialInput = initialInputScanner.nextLine();
+        switch (initialInput) {
+        case "1":
+            echo();
+            break;
+        case "2":
+            storeData();
+
+        }
+    }
+    public static void storeData()
+    {
+        String line;
+        String[] storedItems = new String[100];
+        int dataIndex = 0;
+        boolean exit = false;
+        while (!exit) {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if(line.equalsIgnoreCase("bye"))
+            {
+                exit = true;
+            }
+            else if(line.equalsIgnoreCase("list"))
+            {
+                for(int i = 0; i < dataIndex; i++)
+                {
+                    System.out.println(dataIndex + ". " + storedItem);
+                }
+            }
+            storedItems[dataIndex] = line;
+            dataIndex++;
+            System.out.println("Added: " + line);
+        }
+    }
+    public static void echo()
+    {
+        String line;
+        boolean exit = false;
+        while (!exit) {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                exit = true;
+            }
+            System.out.println(line);
+        }
+        initialise();
+    }
     public static void main(String[] args) {
 
         String logo = "__        ___ _     _                           \n" +
@@ -8,7 +63,12 @@ public class Wildpeace {
                       "   \\_/\\_/  |_|_|\\__,_| .__/ \\___|\\__,_|\\___\\___|\n" +
                       "                     |_|                        ";
         System.out.println("Hello from\n" + logo);
-        System.out.println("What can I do for you");
-        System.out.println("See you next time, bye!");
+
+
+
+        initialise();
+
+
+        //System.out.println("See you next time, bye!");
     }
 }
