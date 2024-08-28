196e2282107bbaac07c87e03c2bbcad7d430a361
hzxnancy <hzxnancy@gmail.com>
2024-08-28 23:11:13 +0800

    Add echo commands from user

--- src/main/java/Cy.java
+++ src/main/java/Cy.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Cy {
+    public static void printLine(){
+        System.out.println("______________________________________");
+    }
     public static void main(String[] args) {
         System.out.println("Hello I'm Cy");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        while (!input.equalsIgnoreCase("bye")){
+            printLine();
+            System.out.println(input);
+            printLine();
+            input = scan.nextLine();
+        }
+
+        printLine();
+        System.out.println("Bye. Hope to see you again");
+        printLine();
     }
 }
