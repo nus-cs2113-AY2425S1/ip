76a8de0d97a8bb33a46e4aba170cf50bf2b77cf0
e0968962@u.nus.edu <e0968962@u.nus.edu>
2024-08-28 22:23:58 +0800

    Level 1: Echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         String logo = "  ____        _     \n"
@@ -8,6 +10,23 @@ public class Bob {
         System.out.println(logo);
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
     }
 }
