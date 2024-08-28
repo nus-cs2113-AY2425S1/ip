250b0010a275ddfb5525f1204d55cfaf9c75e2f7
yeekian <53704991+yeekian@users.noreply.github.com>
2024-08-28 19:29:03 +0800

    Chatbot: add indentation

--- src/main/java/Mel.java
+++ src/main/java/Mel.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class Mel {
     public static void main(String[] args) {
-        System.out.println("________________________________________");
-        System.out.println("Hello! I'm Mel");
-        System.out.println("What can I do for you?");
-        System.out.println("________________________________________");
+        System.out.println("\t________________________________________");
+        System.out.println("\tHello! I'm Mel");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t________________________________________");
 
         String line;
         Scanner in = new Scanner(System.in);
 
         while (true) {
             line = in.nextLine();
-            System.out.println("________________________________________");
+            System.out.println("\t________________________________________");
 
 
             if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("________________________________________");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t________________________________________");
                 break;
             } else {
-                System.out.println(line);
-                System.out.println("________________________________________");
+                System.out.println("\t" + line);
+                System.out.println("\t________________________________________");
             }
         }
     }
