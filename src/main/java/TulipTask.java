d4e2c74a42581f133982d7e427d3ff310840edd9
kestryix <verilim3802@gmail.com>
2024-08-28 15:07:49 +0800

    echo

--- src/main/java/TulipTask.java
+++ src/main/java/TulipTask.java
@@ -11,8 +11,6 @@ public class TulipTask {
                 "  `  `---'`---'`|---'  `  `---^`---'`   `\n" +
                 "                |";
 
-        ArrayList<String> list = new ArrayList<String>();
-
         System.out.println(logo);
 
         System.out.println("--------------------------------------------");
@@ -20,8 +18,26 @@ public class TulipTask {
         System.out.println("What can I do for you today?");
         System.out.println("--------------------------------------------");
 
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            if (Objects.equals(input.toLowerCase(), "bye")) {
+                break;
+            }
+
+            echo(input);
+        }
+
+        System.out.println("--------------------------------------------");
         System.out.println("Bye! Hope to see you again soon :)");
         System.out.println("--------------------------------------------");
 
+    }
+
+    public static void echo (String input) {
+        System.out.println("--------------------------------------------");
+        System.out.println(input);
+        System.out.println("--------------------------------------------");
     }
 }
