bcd58d28008e643c86bfe0360580853661422044
isaacsaw25 <isaacsaw25@gmail.com>
2024-08-28 21:39:39 +0800

    Add echo functionality and exit command

--- src/main/java/CheonsaBot.java
+++ src/main/java/CheonsaBot.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class CheonsaBot {
+    public static String horizontalLine = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________";
         String logo = """
                        (\\ -=- /)
                        ( \\( )/ )
@@ -14,7 +17,22 @@ public class CheonsaBot {
         System.out.print(logo);
         System.out.println("How may I assist you today?");
         System.out.println(horizontalLine);
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String userInput = scanner.nextLine();  // Read user input
+            if (userInput.equalsIgnoreCase("bye")) {
+                sayBye();
+                break;
+            }
+            System.out.println(horizontalLine);
+            System.out.println(userInput);
+            System.out.println(horizontalLine);
+        }
+    }
+
+    public static void sayBye() {
+        System.out.println(horizontalLine);
         System.out.println("Bye, see you again soon!");
         System.out.println(horizontalLine);
     }
