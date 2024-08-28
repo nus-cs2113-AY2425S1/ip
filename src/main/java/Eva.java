0171530464bb751f8cad449df244886ede3a377d
Chiang-HuiXin <huixinchiang@gmail.com>
2024-08-29 00:08:09 +0800

    Update code according to the requirements of level 1: Echo
    Add lines in between the texts to increase readibility. Add while loop to echo messages.

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class Eva {
+    public static final String HORIZONTAL_LINE = "---------------------------------------------------------------";
+
     public static void main(String[] args) {
-       System.out.println("Hello! I'm Eva!");
-       System.out.println("What can I do for you?");
-       System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Eva!");
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while(true)
+        {
+            line = in.nextLine();
+            if(line.equalsIgnoreCase("Bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(HORIZONTAL_LINE);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(HORIZONTAL_LINE);
+        }
     }
 
 }
