04c5b4d87eb2c73c7bfa18510f39f9f24e532dd8
iaso1774 <E1353699@u.nus.edu>
2024-08-28 12:01:32 +0800

    Level-1

--- src/main/java/Gertrude.java
+++ src/main/java/Gertrude.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 public class Gertrude {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         System.out.println("Hello, I'm Gertrude.");
         System.out.println("What can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        while(true) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            System.out.println("____________________________________________________________");
+            if(line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
