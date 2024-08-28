f20092f481582a89585908f2300cae92f066e294
glenda_1506 <glenda.xuanyin@gmail.com>
2024-08-28 23:21:22 +0800

    Add echo function

--- src/main/java/lovespiritual.java
+++ src/main/java/lovespiritual.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class lovespiritual {
     public static void main(String[] args) {
         String line = "________________________________________________________";
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(line);
+        System.out.println("Hello! I'm lovespiritual");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
         while (true) {
-            System.out.println(line);
-            System.out.println("Hello! I'm lovespiritual");
-            System.out.println("What can I do for you?");
-            System.out.println(line);
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println(line);
-            break;
+            String input = in.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
         }
     }
 }
