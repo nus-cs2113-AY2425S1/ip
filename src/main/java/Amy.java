8e67cefa176867e2eb0cd1233067c8d12d17cfe2
Metanyu <nguyenthu.vu2511@gmail.com>
2024-08-29 02:44:54 +0800

    level 1-1. add echo option for amy

--- src/main/java/Amy.java
+++ src/main/java/Amy.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Amy {
-    public static void main(String[] args) {
+    public static void doEcho(String line){
+        System.out.println(line + "! I wonder what I can do with this information (●'◡'●)");
+    }
+    public static void doGreeting(){
         String name = "Amy";
-        String greet = "Hello! I'm " + name + "\nWhat can I do for you? \n";
+        String greet = "Hello! I'm " + name + " ✿" + "\nWhat can I do for you? (❁´◡`❁) \n";
+        System.out.println(greet);
+    }
+    public static void exit(){
+        String bye = "Bye. Hope to see you again soon! (*/ω＼*)\n";
+        System.out.println(bye);
+    }
+    public static void filler(){
         String filler = "____________________________________________________________" + "\n";
-        String bye = "Bye. Hope to see you again soon!\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println(filler + greet + filler + bye + filler);
+        System.out.println(filler);
+    }
+    public static void main(String[] args) {
+        filler();
+        doGreeting();
+        filler();
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while(!line.equals("bye")){
+            doEcho(line);
+            filler();
+            line = in.nextLine();
+        }
+        exit();
     }
 }
