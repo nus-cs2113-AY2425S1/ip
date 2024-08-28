1e459d27bba1bb76b0fe591e87b57dee9148f408
vagrant <vagrant@vagrant.vm>
2024-08-28 09:59:16 +0000

    Finished task 1

--- src/main/java/Doot.java
+++ src/main/java/Doot.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Doot {
+    private static final String DIVIDER = "____________________________________________________________\n\n";
     public static void main(String[] args) {
-        System.out.println("Hello! I'm  Doot\n What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(DIVIDER + "Hello! I'm  Doot\nWhat can I do for you?\n" + DIVIDER);
+        String currentInput = scanner.nextLine();
+        while (!currentInput.equals("bye")){
+            System.out.print(DIVIDER + currentInput + "\n" + DIVIDER);
+            currentInput = scanner.nextLine();
+        }
+        System.out.print(DIVIDER + "Bye. Hope to see you again soon!" + "\n" + DIVIDER);
     }
+
+    public static void findCommand(String command){
+        switch(command){
+            case ("list"):
+                break;
+        }
+    }
+
 }
