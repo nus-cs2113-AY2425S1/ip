d03709224b6940d3e3b9795a7dc8af0de11cdb1e
anlamm <anlammaaa@gmail.com>
2024-08-28 16:23:00 +0800

    Level 1. Echo

--- src/main/java/Anke.java
+++ src/main/java/Anke.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Anke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Anke.");
         System.out.println("What can I do for you?\n");
+        String line = "";
+        while (!line.equals("bye")) {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!\n");
+
     }
 }
