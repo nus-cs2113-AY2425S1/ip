f6ee1dba61bc66248897343b331eec303d942c8d
glenn-chew <glennchew15@gmail.com>
2024-08-28 20:53:23 +0800

    Set helloMessage and byeMessage as a final String

--- src/main/java/Apsea.java
+++ src/main/java/Apsea.java
@@ -2,12 +2,12 @@ public class Apsea {
     public static void main(String[] args) {
         final String STRAIGHT_LINE = "    ________________________________________________________\n";
 
-        String helloMessage = STRAIGHT_LINE
+        final String helloMessage = STRAIGHT_LINE
                 + "    Hello! I'm Apsea!\n"
                 + "    What can I do for you?\n"
                 + STRAIGHT_LINE;
 
-        String byeMessage = STRAIGHT_LINE
+        final String byeMessage = STRAIGHT_LINE
                 + "    Bye. Hope to see you again soon!\n"
                 + STRAIGHT_LINE;
 
