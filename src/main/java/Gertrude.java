27d5d9310f49a14e04168b9bd32369c33e809eef
iaso1774 <E1353699@u.nus.edu>
2024-08-28 12:13:07 +0800

    Level-2

--- src/main/java/Gertrude.java
+++ src/main/java/Gertrude.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Gertrude {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int task_counter = 0;
         System.out.println("Hello, I'm Gertrude.");
         System.out.println("What can I do for you?");
 
@@ -14,8 +16,15 @@ public class Gertrude {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            }else if(line.equals("list")) {
+                for(int i = 1; i <= task_counter; i++) {
+                    System.out.println(i + ". " + tasks[i-1]);
+                }
+            }else {
+                tasks[task_counter] = line;
+                task_counter++;
+                System.out.println("added: " + line);
             }
-            System.out.println(line);
             System.out.println("____________________________________________________________");
         }
     }
