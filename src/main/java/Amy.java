ee729ecafe3037fd2981e53287783e56fa4f0094
Metanyu <nguyenthu.vu2511@gmail.com>
2024-08-29 03:28:28 +0800

    level 1-2. add option to retrieve list of user input

--- src/main/java/Amy.java
+++ src/main/java/Amy.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 
 public class Amy {
+    private static String[] text = new String[100];
+    private static int inputNum = 0;
     public static void doEcho(String line){
         System.out.println(line + "! I wonder what I can do with this information (●'◡'●)");
+    }
+    public static void getUserInputList(){
+        for(int i = 0; i<inputNum; i++){
+            System.out.println((i+1) + ". " + text[i]);
+        }
+    }
+    public static void addUserInputList(String input){
+        text[inputNum++] = input;
+        System.out.println("I added <" + input + "> to the list! ☆*: .｡. o(≧▽≦)o .｡.:*☆");
     }
     public static void doGreeting(){
         String name = "Amy";
@@ -24,9 +35,15 @@ public class Amy {
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
         while(!line.equals("bye")){
-            doEcho(line);
+            if(line.equals("list")){
+                getUserInputList();
+            }
+            else {
+                addUserInputList(line);
+            }
+            //doEcho(line);
             filler();
-            line = in.nextLine();
+            line = in.nextLine().trim();
         }
         exit();
     }
