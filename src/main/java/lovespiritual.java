1d851ffe8b2a67a589851f42fd89724486f4beda
glenda_1506 <glenda.xuanyin@gmail.com>
2024-08-28 23:12:03 +0800

    Add greet function and exit function

--- src/main/java/lovespiritual.java
+++ src/main/java/lovespiritual.java
@@ -1,10 +1,14 @@
 public class lovespiritual {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "________________________________________________________";
+        while (true) {
+            System.out.println(line);
+            System.out.println("Hello! I'm lovespiritual");
+            System.out.println("What can I do for you?");
+            System.out.println(line);
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(line);
+            break;
+        }
     }
 }
