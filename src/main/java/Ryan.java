cf5e658cf8d4e8ad860eec5c5fe9fea04d88b611
Yisoong <yisoong23@gmail.com>
2024-08-29 23:57:58 +0800

    Change Duke to Ryan

---
+++ src/main/java/Ryan.java
@@ -0,0 +1,9 @@
+public class Ryan {
+    public static void main(String[] args) {
+        Utils.horizontalLine();
+        System.out.println("Hello! I'm Ryan\nWhat can I do for you?");
+        Utils.horizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        Utils.horizontalLine();
+    }
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,5 @@
+public class Utils {
+    public static void horizontalLine (){
+        System.out.println ("_________________________________________________________");
+    }
+}
