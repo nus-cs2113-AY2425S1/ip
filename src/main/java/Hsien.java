3059c46cc5cf4a2e4a6d02e2dab83418ead1b219
KuanHsienn <liangkuanhsien@gmail.com>
2024-08-29 20:07:16 +0800

    Add skeleton code

--- src/main/java/Hsien.java
+++ src/main/java/Hsien.java
@@ -1,1 +1,18 @@
-https://github.com/KuanHsienn/ip.git
+public class Hsien {
+
+    public static void printLine() {
+        System.out.println("-".repeat(50));
+    }
+
+    public static void main(String[] args) {
+        printLine();
+        // Greet
+        System.out.println("Hello! I am Hsien, your personal chatbot\n");
+        printLine();
+
+        // Exit
+        System.out.println("Have a good day! Bye!");
+        printLine();
+
+    }
+}
