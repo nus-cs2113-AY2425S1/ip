ba8f27522332500b07797f16706fa1531b9f2935
Yvorm <abhar2809@gmail.com>
2024-08-29 23:48:19 +0800

    Implement Level-0

---
+++ src/main/java/Terri.java
@@ -0,0 +1,28 @@
+public class Terri {
+    public static void main(String[] args) {
+        String logo = "\n" +
+                "$$$$$$$$\\                               $$\\ \n" +
+                "\\__$$  __|                              \\__|\n" +
+                "   $$ |    $$$$$$\\   $$$$$$\\   $$$$$$\\  $$\\ \n" +
+                "   $$ |   $$  __$$\\ $$  __$$\\ $$  __$$\\ $$ |\n" +
+                "   $$ |   $$$$$$$$ |$$ |  \\__|$$ |  \\__|$$ |\n" +
+                "   $$ |   $$   ____|$$ |      $$ |      $$ |\n" +
+                "   $$ |   \\$$$$$$$\\ $$ |      $$ |      $$ |\n" +
+                "   \\__|    \\_______|\\__|      \\__|      \\__|\n" +
+                "                                            \n";
+
+        System.out.println("Hello from\n" + logo);
+
+        String divider = "____________________________________________________________";
+
+        System.out.println("Heya! I'm Terri.");
+        System.out.println("What can I help ya with today?\n"+divider);
+
+        System.out.println("Bye then! See ya soon!\n"+divider);
+
+
+
+
+
+    }
+}
