b5772d9db63358bb7e87d7ecb90c96af4d19201a
shaunngoh <shaungoh682@gmail.com>
2024-08-29 14:20:04 +0800

    Remove old DianaAssistant class

--- src/main/java/DianaAssistant.java
+++ src/main/java/DianaAssistant.java
@@ -14,55 +14,76 @@ public class DianaAssistant {
             if ("bye".equals(input)) {
                 break;
             }
-            if ("list".equals(input)) {
-                for (int i = 0; i < list.size(); i++) {
-                    Task t = list.get(i);
-                    System.out.println((i + 1) + ". " + t.toString());
-                }
+
+            switch(getCommand(input)) {
+                case "list":
+                    printList(list);
+                    break;
+                case "mark":
+                    toMark(list, input, true);
+                    break;
+                case "unmark":
+                    toMark(list, input, false);
+                    break;
+                default:
+                    addInput(input, list);
+                    break;
             }
-            else if (input.startsWith("mark ")) {
-                try {
-                    int taskNum = Integer.valueOf(input.substring("mark ".length())) - 1;
-                    if (taskNum >= 0 && taskNum < list.size()) {
-                        Task task = list.get(taskNum);
-                        task.MarkAsDone();
-                        System.out.println("Nice! I've marked this task as done\n" + task.toString());
-
-                    } else {
-                        System.out.println("Invalid task number.");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid task number.");
-                }
-            }
-
-            else if (input.startsWith("unmark ")) {
-                try {
-                    int taskNum = Integer.valueOf(input.substring("unmark ".length())) - 1;
-
-                    if (taskNum >= 0 && taskNum < list.size()) {
-                        Task task = list.get(taskNum);
-                        task.MarkAsNotDone();
-                        System.out.println("Okay, I've makred this task as not done yet\n" + task.toString());
-                    }
-                    else {
-                        System.out.println("Invalid task number.");
-                    }
-                }
-                catch (NumberFormatException e) {
-                    System.out.println("Invalid task number.");
-                }
-            }
-
-            else {
-                Task task = new Task(input);
-                System.out.println("added: " + input);
-                list.add(task);
-            }
-
         }
 
         System.out.println("Bye! Hope to see you again soon.");
         scanner.close();
     }
+
+    private String getCommand(String input) {
+        if (input.equals("list")) {
+            return "list";
+        }
+        if (input.startsWith("mark ")) {
+            return "mark";
+        }
+        if (input.startsWith("unmark ")) {
+            return "unmark";
+        }
+        return "add";
+    }
+
+    private void printList (List<Task> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            Task task = list.get(i);
+            System.out.println((i + 1) + ". " + task.toString());
+        }
+        return;
+    }
+
+    private void toMark (List<Task> list, String input, boolean toMark) {
+        try {
+            int TaskNum = Integer.valueOf(input.substring(input.indexOf(" ") + 1)) - 1;
+            if (TaskNum >= 0 && TaskNum < list.size()) {
+                Task task = list.get(TaskNum);
+                if (toMark) {
+                    task.markAsDone();
+                    System.out.println("Nice! I've marked this task as done\n" + task.toString());
+                }
+                else {
+                    task.markAsNotDone();
+                    System.out.println("Okay, I've marked this task as not done yet\n" + task.toString());
+                }
+            }
+            else {
+                System.out.println("Please enter a number between 1 and " + (list.size() - 1));
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Please enter a number between 1 and " + (list.size() - 1));
+        }
+    }
+
+    private void addInput (String input, List<Task> list) {
+        Task task = new Task(input);
+        System.out.println("added: " + input);
+        list.add(task);
+    }
+
 }
