d3a20d4300b45193299cd6d44a4549b53aab2c83
DucPhong135 <e1129787@u.nus.edu>
2024-08-29 13:52:42 +0800

    added Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.io.*;
 
     public class Duke {
 
-        private static String[] taskList = new String[100];
+        private static Task[] taskList = new Task[100];
         private static int taskCount = 0;
 
         public Duke() {
@@ -13,17 +14,40 @@ import java.util.Scanner;
         }
 
         public static void add(String task) {
-            taskList[taskCount] = task;
+            Task newTask = new Task(task);
+            taskList[taskCount] = newTask;
             taskCount++;
             System.out.println("added: " + task);
         }
 
         public static void list() {
             for (int i = 0; i < taskCount; i++) {
-                System.out.println((i+1) + ". " + taskList[i]);
+                System.out.println((i+1) + ". " +"[" + taskList[i].getStatusIcon() + "] "
+                        + taskList[i].getDescription());
             }
         }
 
+        public void mark(int index) {
+            if(index > taskCount+1 || index < 0) {
+                System.out.println("Index out of bounds");
+                return;
+            }
+            taskList[index-1].markAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("[" + taskList[index-1].getStatusIcon() + "] "
+                    + taskList[index-1].getDescription());
+        }
+
+        public void unmark(int index) {
+            if(index > taskCount+1 || index < 0) {
+                System.out.println("Index out of bounds");
+                return;
+            }
+            taskList[index-1].markAsNotDone();
+            System.out.println("Ok, I've marked this task as not done yet:");
+            System.out.println("[" + taskList[index-1].getStatusIcon() + "] "
+                    + taskList[index-1].getDescription());
+        }
 
         public void execute() {
             System.out.println("Hello I'm Lambo");
@@ -33,12 +57,19 @@ import java.util.Scanner;
             SuperLoop:
             while (true) {
                 String input = input_reader.nextLine();
-                switch (input) {
+                String[] inputComponent = input.split(" ");
+                switch (inputComponent[0]) {
                     case "bye":
                         System.out.println("Bye. Hope to see you again soon!");
                         break SuperLoop;
                     case "list":
                         list();
+                        break;
+                    case "mark":
+                        mark(Integer.parseInt(inputComponent[1]));
+                        break;
+                    case "unmark":
+                        unmark(Integer.parseInt(inputComponent[1]));
                         break;
                     default:
                         if(input.isEmpty()) {
