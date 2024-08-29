e2cf27536402e7d65416effa5f82e048ea1b2538
Yisoong <yisoong23@gmail.com>
2024-08-30 00:31:54 +0800

    Add exit functionality

--- src/main/java/Ryan.java
+++ src/main/java/Ryan.java
@@ -2,14 +2,27 @@ import java.util.Scanner;
 
 public class Ryan {
     public static void main(String[] args) {
+        boolean exit = false;
         Scanner scanner = new Scanner(System.in);
+
         Utils.horizontalLine();
         System.out.println("Hello! I'm Ryan\nWhat can I do for you?");
         Utils.horizontalLine();
-        String command = scanner.nextLine();
-        Utils.horizontalLine();
-        System.out.println(command);
-        Utils.horizontalLine();
+
+        while (!exit) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                exit = true;
+                Utils.horizontalLine();
+                continue;
+            }
+
+            Utils.horizontalLine();
+            System.out.println(command);
+            Utils.horizontalLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         Utils.horizontalLine();
     }
