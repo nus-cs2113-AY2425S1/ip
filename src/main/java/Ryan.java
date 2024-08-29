9bc8ea6fa7be27a3172fec36b7f04ec4bf0c85ee
Yisoong <yisoong23@gmail.com>
2024-08-30 00:15:24 +0800

    Add echo functionality

--- src/main/java/Ryan.java
+++ src/main/java/Ryan.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Ryan {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Utils.horizontalLine();
         System.out.println("Hello! I'm Ryan\nWhat can I do for you?");
+        Utils.horizontalLine();
+        String command = scanner.nextLine();
+        Utils.horizontalLine();
+        System.out.println(command);
         Utils.horizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
         Utils.horizontalLine();
