517cbb7c5f7b68853c19f336e5133fadcb159161
Kai Qin <tonyqin2003@berkeley.edu>
2024-08-29 19:55:07 +0800

    level-0

--- src/main/java/bro.java
+++ src/main/java/bro.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class bro {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +6,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm bro");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
