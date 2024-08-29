b06afb6db1113cfc5898d77eee7e3ab65d8c3981
crystal-bys <yinshuangbu@gmail.com>
2024-08-29 16:43:07 +0800

    Level 1 completed. Echo completed.

--- src/main/java/Crystal.java
+++ src/main/java/Crystal.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Crystal {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
         System.out.println("""
                 ____________________________________________________________
                 Hello! I'm Crystal.
                 What can I do for you today?
                 ____________________________________________________________
-                Bye. Hope to see you again soon!
-                ____________________________________________________________
                 """);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println("____________________________________________________________\n"
+                    + line + "\n"
+                    + "____________________________________________________________");
+            line = in.nextLine();
+        }
+        System.out.println("____________________________________________________________\n"
+                + "Bye, hope to see you again soon!\n"
+                + "____________________________________________________________");
+
     }
 }
