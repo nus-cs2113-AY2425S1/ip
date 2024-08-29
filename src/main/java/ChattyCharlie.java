b31dc7b29479f546557f0ab6c59d443df08121cb
BevLow <lowbeverlypersonal@gmail.com>
2024-08-30 01:16:21 +0800

    Commit update Echo

--- src/main/java/ChattyCharlie.java
+++ src/main/java/ChattyCharlie.java
@@ -2,11 +2,45 @@ import java.util.Scanner;
 
 public class ChattyCharlie {
 
-    public static void Echo() { //Echo as a function
+    public static class List {
+        private String[] list;
+        private int size;
+
+        //constructor
+        public List() {
+            list = new String[100];
+            size = 0;
+        }
+
+        //Method to add an item to the list
+        public void add(String text) {
+            //add the text into the list
+            list[size] = text;
+            //account for the item
+            size++;
+        }
+
+        //To print list
+        public void toPrint() {
+            //print all
+            System.out.println("ToDo List:");
+            for (int i = 0; i < size; i++) {
+                int number = i+1;
+                System.out.println("        " + number + ". " + list[i]);
+            }
+        }
+    }
+
+    public static void toDoMaker() { //Echo as a function
         String line;
-        String you = "User:    ";
+        String you = "User: ";
+
         //make the scanner
         Scanner in = new Scanner(System.in);
+
+        //create an instance of list class
+        List toDo = new List();
+
         //accept an insert
         while (true) {
             System.out.print(you);
@@ -16,9 +50,14 @@ public class ChattyCharlie {
             if (line.contains("Bye") || line.contains("bye")) {
                 break;
             }
-
-            String response = "Output:  ";
-            System.out.println(response + line);
+            //add or print
+            if (line.equals("list")) {
+                toDo.toPrint();
+            } else {
+                //add the item
+                toDo.add(line);
+                System.out.println("        Added: " + line);
+            }
         }
     }
 
@@ -33,15 +72,15 @@ public class ChattyCharlie {
                 + " |       |    \n"
                 + " |_______|    \n"
                 + "              \n";
-        String charlie = "Charlie: ";
+        String charlie = "Smarty:  ";
 
-        String greeting = "Hello! I'm ChattyCharlie, your friendly companion.\n"
-                + "         What can I help you with today?\n" ;
+        String greeting = "Hello! I'm SmartTasker, your consistent buddy.\n"
+                + "         What shall we do today?\n" ;
 
-        String farewell = "Bye, have a pleasant day today!";
+        String farewell = "All the best in clearing your list!";
 
         System.out.println(logo + charlie+ greeting);
-        Echo();
+        toDoMaker();
         System.out.println(charlie + farewell);
 
     }
