d030c2d05426866397ea15656844a99a82fd60b3
Paulifyer <ethansoh0308@gmail.com>
2024-08-30 00:27:51 +0800

    Added a list and adding to the list

--- src/main/java/Nateh.java
+++ src/main/java/Nateh.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class Nateh {
+    public static void printList(String[] list, int length) {
+        String lineBreak = "____________________________________________________________\n";
+        System.out.print(lineBreak);
+        for (int i = 0; i < length; i++) {
+            System.out.print(i+1 + ". ");
+            System.out.println(list[i]);
+        }
+        System.out.print(lineBreak);
+    }
+
     public static void main(String[] args) {
         String lineBreak = "____________________________________________________________\n";
         String skeleton = """
@@ -31,15 +41,27 @@ public class Nateh {
                              ^^\\..___,.--`""";
         Scanner in = new Scanner(System.in);
         String input = "";
+        String[] list = new String[100];
+        int length = 0;
         System.out.print(lineBreak);
         System.out.println("Hello! I'm Nateh\nWhat can I do for you?");
         System.out.println(skeleton);
         System.out.print(lineBreak);
         while (!input.equals("bye")) {
             input = in.nextLine();
-            System.out.print(lineBreak);
-            System.out.println(input);
-            System.out.print(lineBreak);
+            switch (input) {
+                case "list":
+                    printList(list, length);
+                    break;
+                case "bye":
+                    break;
+                default:
+                    list[length] = input;
+                    length++;
+                    System.out.print(lineBreak);
+                    System.out.println("added: " + input);
+                    System.out.print(lineBreak);
+            }
         }
         System.out.print(lineBreak);
         System.out.println("Bye. Hope to see you again soon!");
