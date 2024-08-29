f3b3fff1dc5dfd1ef56d03b20f00a7dcad199b77
Ridiculouswifi <thtan02@gmail.com>
2024-08-29 12:04:22 +0800

    Level-1: Added the Echo Feature

--- src/main/java/Freedom.java
+++ src/main/java/Freedom.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Freedom {
     public static void main(String[] args) {
-        String logo = "___________________________________\n";
+        String logo = "_________________________________________\n";
         String message = """
-                Hello! I'm Freedom
-                What can I do for you?
+                \tHello! I'm Freedom
+                \tWhat can I do for you?
                 """;
-        String closing = "Bye. Hope to see you again soon!\n";
-        System.out.println(logo + message + logo + closing + logo);
+        String closing = "\tBye. Hope to see you again soon!\n";
+
+        System.out.println(logo + message + logo);
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String line = in.nextLine();
+
+            if (line.equals("bye")) {
+                break;
+            }
+            String command = "\t" + line + "\n";
+            System.out.println(logo + command + logo);
+        }
+
+        System.out.println(logo + closing + logo);
     }
 }
