d63130c6802d97863431880a766b6ce5a156d2e0
CheeKiangg <ngchkiang@gmail.com>
2024-08-29 17:07:33 +0800

    Improve readability and error handling

--- src/main/java/CodeCatalyst.java
+++ src/main/java/CodeCatalyst.java
@@ -2,53 +2,85 @@ import java.util.Scanner;
 
 public class CodeCatalyst {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        System.out.println("        ________________________________________________________");
-        System.out.println("         Hello, I'm CodeCatalyst.");
-        System.out.println("         What can I do for you?");
-        System.out.println("        ________________________________________________________\n");
+        printGreeting();
 
         while (true) {
-            String input = in.nextLine();
-            System.out.println("        ________________________________________________________");
+            String input = scanner.nextLine();
+            printDivider();
 
             if (input.equals("bye")) {
-                System.out.println("         Bye. Hope to see you again soon!");
-                System.out.println("        ________________________________________________________\n");
+                printGoodbye();
                 break;
             } else if (input.equals("list")) {
-                System.out.println("         Here are the tasks in your list: ");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("         " + (i + 1) + ". " + tasks[i]);
-                }
+                printTaskList(tasks, taskCount);
             } else if (input.startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(input.substring(5));
-                if (taskNumber >= 1 && (taskNumber - 1) < taskCount) {
-                    tasks[taskNumber - 1].markAsDone();
-                    System.out.println("         Nice! I've marked this task as done: ");
-                    System.out.println("         " + tasks[taskNumber - 1]);
-                } else {
-                    System.out.println("         Invalid task number.");
-                }
-            } else if (input.startsWith("unmark")) {
-                int taskNumber = Integer.parseInt(input.substring(7));
-                if (taskNumber >= 1 && (taskNumber - 1) < taskCount) {
-                    tasks[taskNumber - 1].markAsNotDone();
-                    System.out.println("         Ok, I've marked this task as not done yet: ");
-                    System.out.println("         " + tasks[taskNumber - 1] );
-                } else {
-                    System.out.println("         Invalid task number.");
-                }
+                handleMarkCommand(tasks, taskCount, input);
+            } else if (input.startsWith("unmark ")) {
+                handleUnmarkCommand(tasks, taskCount, input);
             } else {
                 tasks[taskCount] = new Task(input);
                 taskCount += 1;
                 System.out.println("         added: " + input);
             }
-            System.out.println("        ________________________________________________________\n");
+            printDivider();
+        }
+    }
+
+    private static void printDivider() {
+        System.out.println("        ________________________________________________________\n");
+    }
+
+    private static void printGreeting() {
+        printDivider();
+        System.out.println("         Hello, I'm CodeCatalyst.");
+        System.out.println("         What can I do for you?");
+        printDivider();
+    }
+
+    private static void printGoodbye() {
+        System.out.println("         Bye. Hope to see you again soon!");
+        printDivider();
+    }
+
+    private static void printTaskList(Task[] tasks, int taskCount) {
+        System.out.println("         Here are the tasks in your list: ");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("         " + (i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    private static void handleMarkCommand(Task[] tasks, int taskCount, String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.substring(5));
+            if (taskNumber >= 1 && (taskNumber - 1) < taskCount) {
+                tasks[taskNumber - 1].markAsDone();
+                System.out.println("         Nice! I've marked this task as done: ");
+                System.out.println("         " + tasks[taskNumber - 1]);
+            } else {
+                System.out.println("         Invalid task number.");
+            }
+        } catch (NumberFormatException invalidNumberFormat) {
+            System.out.println("         " + input.substring(5) + " is not a number");
+        }
+    }
+
+    private static void handleUnmarkCommand(Task[] tasks, int taskCount, String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.substring(7));
+            if (taskNumber >= 1 && (taskNumber - 1) < taskCount) {
+                tasks[taskNumber - 1].markAsNotDone();
+                System.out.println("         Ok, I've marked this task as not done yet: ");
+                System.out.println("         " + tasks[taskNumber - 1] );
+            } else {
+                System.out.println("         Invalid task number.");
+            }
+        } catch (NumberFormatException invalidNumberFormat) {
+            System.out.println("         " + input.substring(7) + " is not a number");
         }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,23 +2,45 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a Task with the specified description. The task is initially not done.
+     *
+     * @param description The task description
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return "X" if the task is done, otherwise a space character.
+     */
     public String getStatusIcon(){
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone(){
         this.isDone = true;
     }
 
+    /** Marks the task as not done.
+     *
+     */
     public void markAsNotDone(){
         this.isDone = false;
     }
 
+    /**
+     * Returns the string representation of the task
+     *
+     * @return A string representing the task with its status icon
+     */
+    @Override
     public String toString(){
         return ("[" + getStatusIcon() + "] " + description);
     }
