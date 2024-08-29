6c3952f88a109a469e965d8f019bdab09e1b439f
unknown <nivedit@comp.nus.edu.sg>
2024-08-30 02:28:36 +0800

    Add line breaks

--- src/main/java/Bitwise.java
+++ src/main/java/Bitwise.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Bitwise {
+    // ASCII art from https://ascii-generator.site/t/
+    private static final String logo = " ____   _  _              _            \n"
+            + "|  _ \\ (_)| |            (_)           \n"
+            + "| |_) | _ | |_ __      __ _  ___   ___ \n"
+            + "|  _ < | || __|\\ \\ / /| |/ __| / _ \\ \n"
+            + "| |_) || || |_  \\ V  V / | |\\__ \\|  __/\n"
+            + "|____/ |_| \\__|  \\_/\\_/  |_||___/ \\___|\n"
+            + "                                       \n";
+    private static final String sectionBreak = "==================================================\n";
+    private static final String lineBreak = "--------------------------------------------------\n";
     private static String[] taskList = new String[100];
     private static int numberOfTasks = 0;
 
     public static void main(String[] args) {
-        // ASCII art from https://ascii-generator.site/t/
-        String logo = " ____   _  _              _            \n"
-                + "|  _ \\ (_)| |            (_)           \n"
-                + "| |_) | _ | |_ __      __ _  ___   ___ \n"
-                + "|  _ < | || __|\\ \\ / /| |/ __| / _ \\ \n"
-                + "| |_) || || |_  \\ V  V / | |\\__ \\|  __/\n"
-                + "|____/ |_| \\__|  \\_/\\_/  |_||___/ \\___|\n"
-                + "                                       \n";
-        String sectionBreak = "==================================================\n";
-        String lineBreak = "--------------------------------------------------\n";
         System.out.println(sectionBreak + "Hello from\n" + logo);
         System.out.print("How may I help you today?\n" + lineBreak);
         mainManager();
         //echoUserInput();
-        System.out.println("Bye, see you soon!\n" + sectionBreak);
+        System.out.println(lineBreak + "Bye, see you soon!\n" + sectionBreak);
     }
     public static void mainManager() {
         String userInput;
@@ -30,6 +30,7 @@ public class Bitwise {
             if (userInput.equalsIgnoreCase("bye")) {
                 return;
             }
+            System.out.print(lineBreak);
             if (userInput.equalsIgnoreCase("list")) {
                 printTaskList();
             }
@@ -37,6 +38,7 @@ public class Bitwise {
                 addToList(userInput);
                 System.out.println("Added: " + userInput);
             }
+            System.out.print(lineBreak);
         }
     }
     public static void echoUserInput() {
