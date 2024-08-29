f4d62be8fc3194707ef140d09076fb842ad840f6
lucas-sc0 <e0959193@u.nus.edu>
2024-08-29 21:41:43 +0800

    Add increment level 1
    Improved the skeletal version of Monday so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Monday.java
+++ src/main/java/Monday.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Monday {
     public static void main(String[] args) {
         String logo = " __  __                 _             \n"
@@ -9,10 +11,28 @@ public class Monday {
         String line = "____________________________________________________________";
 
         System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
         System.out.println(line);
+
         System.out.println("Hello! I'm MONDAY\nWhat can I do for you?");
+
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while (true) {
+            input = scanner.nextLine();
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+        }
+
     }
 }
