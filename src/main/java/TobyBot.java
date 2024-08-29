a0477385780933e1bef9e1d17ec1a7b08bb66194
Toby-Yu <e1344677@u.nus.edu>
2024-08-30 01:48:03 +0800

    level 1, level 2, level 3 : add, list, mark as done

--- src/main/java/TobyBot.java
+++ src/main/java/TobyBot.java
@@ -1,10 +1,100 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TobyBot {
+    private static final String LINE = "____________________________________________________________";
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(LINE);
         System.out.println("Hello! I'm TobyBot");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(LINE);
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            } else if (input.equals("list")) {
+                listTasks();
+            } else if (input.startsWith("mark ")) {
+                markTask(input);
+            } else if (input.startsWith("unmark ")) {
+                unmarkTask(input);
+            } else {
+                addTask(input);
+            }
+
+            System.out.println(LINE);
+        }
+    }
+
+    private static void listTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + "." + tasks.get(i));
+        }
+    }
+
+    private static void addTask(String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+        System.out.println("added: " + description);
+    }
+
+    private static void markTask(String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+            Task task = tasks.get(taskNumber);
+            task.markAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + task);
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println("Invalid task number.");
+        }
+    }
+
+    private static void unmarkTask(String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+            Task task = tasks.get(taskNumber);
+            task.markAsNotDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + task);
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println("Invalid task number.");
+        }
+    }
+}
+
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
