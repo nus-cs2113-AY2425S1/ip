804cbf78415da92b78dfb4a93ea2ed7498f72a6c
fuyixuan <yixuanfu762@gmail.com>
2024-08-29 16:33:18 +0800

    Level-2

--- src/main/java/Sirius.java
+++ src/main/java/Sirius.java
@@ -4,13 +4,30 @@ public class Sirius {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("-----------------------" );
-        System.out.print("Hello! I'm Sirius!\n" + "What can I do for you?\n" + "-----------------------\n" );
+        System.out.print("""
+                Hello! I'm Sirius!
+                What can I do for you?
+                -----------------------
+                """);
+        String[] list = new String[100];
+        int taskCounter = 0;
         while (true) {
             String userInput = scanner.nextLine();
             if (!userInput.equals("bye")) {
-                System.out.println("-----------------------");
-                System.out.println(userInput);
-                System.out.println("-----------------------\n");
+                if (userInput.equals("list")){
+                    System.out.println("-----------------------");
+                    for (int i = 0; i < taskCounter; i++) {
+                        System.out.print(i + 1 + ". ");
+                        System.out.println(list[i]);
+                    }
+                    System.out.println("-----------------------\n");
+                }
+                else {
+                    System.out.println("-----------------------");
+                    list[taskCounter++] = userInput;
+                    System.out.println("added: " + userInput);
+                    System.out.println("-----------------------\n");
+                }
             }
             else {
                 System.out.println("-----------------------");
