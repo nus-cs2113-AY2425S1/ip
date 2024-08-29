878a51a8805b422f1fb0e567702cd93c2aa7d7e1
3CCLY <cluyang22@hotmail.com>
2024-08-29 12:49:17 +0800

    Ran.java: shift bye message to bidFarewell method

--- src/main/java/Ran.java
+++ src/main/java/Ran.java
@@ -16,9 +16,15 @@ public class Ran {
 	System.out.println("What can I do for you?");
 	System.out.println("____________________________________________________________");
     }
+    
+    public static void bidFarewell() {
+	System.out.println("____________________________________________________________");
+	System.out.println("Bye. Hope to see you again soon!");
+	System.out.println("____________________________________________________________");
+    }
 
     public static void main(String[] args) {
 	greet();
-	System.out.println("Bye. Hope to see you again soon!");
+        bidFarewell();
     }
 }
