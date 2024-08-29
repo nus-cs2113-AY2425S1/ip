9fbf0c6d6bbfa707a79195db772262a6cca03971
BevLow <lowbeverlypersonal@gmail.com>
2024-08-29 23:51:59 +0800

    Commit Echo Function and Gitignore Class

--- src/main/java/ChattyCharlie.java
+++ src/main/java/ChattyCharlie.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class ChattyCharlie {
+
+    public static void Echo() { //Echo as a function
+        String line;
+        String you = "User:    ";
+        //make the scanner
+        Scanner in = new Scanner(System.in);
+        //accept an insert
+        while (true) {
+            System.out.print(you);
+            line = in.nextLine();
+
+            //if the line contains bye, it signals the end
+            if (line.contains("Bye") || line.contains("bye")) {
+                break;
+            }
+
+            String response = "Input:   ";
+            System.out.println(response + line);
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____________________________________________________________\n"
-                + "   _____      \n"
+        String logo = "   _____      \n"
                 + "  /     \\     \n"
                 + " |  O O  |    \n"
                 + " | \\___/ |    \n"
@@ -10,14 +32,17 @@ public class ChattyCharlie {
                 + " |       |    \n"
                 + " |       |    \n"
                 + " |_______|    \n"
-                + "              \n"
-                + "____________________________________________________________\n";
-        String greeting = "Hello! I'm ChattyCharlie\n"
-                + "What can I help you with today?\n"
-                + "____________________________________________________________\n";
+                + "              \n";
+        String charlie = "Charlie: ";
+
+        String greeting = "Hello! I'm ChattyCharlie, your friendly companion.\n"
+                + "         What can I help you with today?\n" ;
 
         String farewell = "Bye, have a pleasant day today!";
 
-        System.out.println(logo + greeting + farewell);
+        System.out.println(logo + charlie+ greeting);
+        Echo();
+        System.out.println(charlie + farewell);
+
     }
 }
