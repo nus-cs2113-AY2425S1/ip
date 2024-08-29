fa6aa8f01316889b8504490424c75bd2b1b721d6
crystal-bys <yinshuangbu@gmail.com>
2024-08-29 17:24:42 +0800

    Level 2 completed. Add and list completed.

--- src/main/java/Crystal.java
+++ src/main/java/Crystal.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Crystal {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         System.out.println("""
                 ____________________________________________________________
                 Hello! I'm Crystal.
@@ -11,11 +13,22 @@ public class Crystal {
                 ____________________________________________________________
                 """);
         line = in.nextLine();
+
         while (!line.equals("bye")) {
-            System.out.println("____________________________________________________________\n"
-                    + line + "\n"
-                    + "____________________________________________________________");
+            if (line.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                list.add(line);
+                System.out.println("____________________________________________________________\n"
+                        + "added: " + line + "\n"
+                        + "____________________________________________________________");
+            }
             line = in.nextLine();
+
         }
         System.out.println("____________________________________________________________\n"
                 + "Bye, hope to see you again soon!\n"
