7591aa66f1b771c249d01e161cd25e06866bb5ee
hzxnancy <hzxnancy@gmail.com>
2024-08-29 20:03:37 +0800

    Implement add and display list function

--- src/main/java/Cy.java
+++ src/main/java/Cy.java
@@ -1,25 +1,47 @@
 import java.util.Scanner;
 
 public class Cy {
-    public static void printLine(){
+    public static void printLine() {
         System.out.println("______________________________________");
     }
+
+    public static void printList(String[] items) {
+        printLine();
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                System.out.println(i+1 + ". " + items[i]);
+            } else {
+                break;
+            }
+        }
+        printLine();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello I'm Cy");
+        System.out.println("Hello, I'm Cy");
         System.out.println("What can I do for you?");
+        String[] items = new String[100];
+        int count = 0;
 
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
 
-        while (!input.equalsIgnoreCase("bye")){
-            printLine();
-            System.out.println(input);
-            printLine();
+        while (!input.equalsIgnoreCase("bye")) {
+
+            if (input.equalsIgnoreCase("list")) {
+                printList(items);
+            } else {
+                items[count] = input;
+                count++;
+                printLine();
+                System.out.println("added: " + input);
+                printLine();
+            }
             input = scan.nextLine();
         }
 
         printLine();
-        System.out.println("Bye. Hope to see you again");
+        System.out.println("Bye. Hope to see you again soon!");
         printLine();
     }
 }
