8856bf8df3e6758572979759ef6be13cc31a8bd5
fuyixuan <yixuanfu762@gmail.com>
2024-08-29 16:36:24 +0800

    level-3 submission

--- src/main/java/Sirius.java
+++ src/main/java/Sirius.java
@@ -9,22 +9,71 @@ public class Sirius {
                 What can I do for you?
                 -----------------------
                 """);
-        String[] list = new String[100];
+        class Task{
+            private String taskName;
+            private boolean isMarked;
+            public Task() {
+                this.taskName = "";
+                this.isMarked = false;
+            }
+            public Task(String taskName, boolean isMarked) {
+                this.taskName = taskName;
+                this.isMarked = isMarked;
+            }
+            public String getTaskName() {
+                return taskName;
+            }
+            public boolean isMarked() {
+                return isMarked;
+            }
+            public void setTaskName(String taskName) {
+                this.taskName = taskName;
+            }
+            public void setMarked(boolean isMarked) {
+                this.isMarked = isMarked;
+            }
+            public void printTask() {
+                if (isMarked) {
+                    System.out.println("[X] " + taskName);
+                }
+                else {
+                    System.out.println("[ ] " + taskName);
+                }
+            }
+        }
+        Task[] list = new Task[100];
         int taskCounter = 0;
         while (true) {
             String userInput = scanner.nextLine();
             if (!userInput.equals("bye")) {
                 if (userInput.equals("list")){
                     System.out.println("-----------------------");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCounter; i++) {
                         System.out.print(i + 1 + ". ");
-                        System.out.println(list[i]);
+                        list[i].printTask();
                     }
+                    System.out.println("-----------------------\n");
+                }
+                else if (userInput.startsWith("mark ")) {
+                    System.out.println("-----------------------");
+                    int taskNumber = Integer.parseInt(userInput.replace("mark ", ""));
+                    list[taskNumber-1].setMarked(true);
+                    System.out.println("Nice! I've marked this task as done:");
+                    list[taskNumber-1].printTask();
+                    System.out.println("-----------------------\n");
+                }
+                else if (userInput.startsWith("unmark ")) {
+                    System.out.println("-----------------------");
+                    int taskNumber = Integer.parseInt(userInput.replace("unmark ", ""));
+                    list[taskNumber-1].setMarked(false);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    list[taskNumber-1].printTask();
                     System.out.println("-----------------------\n");
                 }
                 else {
                     System.out.println("-----------------------");
-                    list[taskCounter++] = userInput;
+                    list[taskCounter++] = new Task(userInput, false);
                     System.out.println("added: " + userInput);
                     System.out.println("-----------------------\n");
                 }
@@ -39,4 +88,3 @@ public class Sirius {
         scanner.close();
     }
 }
-
