c2696be0a9c81b8aa74eb76d1f2967bed0af2cb2
DucPhong135 <e1129787@u.nus.edu>
2024-08-29 13:17:47 +0800

    Add a separate main class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,29 +2,51 @@ import java.util.Scanner;
 
     public class Duke {
 
+        private static String[] taskList = new String[100];
+        private static int taskCount = 0;
 
+        public Duke() {
+        }
 
         public static void echo(String message) {
             System.out.println(message);
         }
 
+        public static void add(String task) {
+            taskList[taskCount] = task;
+            taskCount++;
+            System.out.println("added: " + task);
+        }
+
+        public static void list() {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i+1) + ". " + taskList[i]);
+            }
+        }
 
 
         public static void execute() {
             System.out.println("Hello I'm Lambo");
             System.out.println("What can I do for you?");
             Scanner input_reader = new Scanner(System.in);
-            while(true) {
+
+            SuperLoop:
+            while (true) {
                 String input = input_reader.nextLine();
-                if(input.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                }
-                else{
-                    echo(input);
+                switch (input) {
+                    case "bye":
+                        System.out.println("Bye. Hope to see you again soon!");
+                        break SuperLoop;
+                    case "list":
+                        list();
+                        break;
+                    default:
+                        if(input == "") {
+                            continue SuperLoop;
+                        }
+                        add(input);
+                        break;
                 }
             }
         }
-
-
     }
