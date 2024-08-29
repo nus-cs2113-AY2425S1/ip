6574a22724925d702cd404953aeae0fab95d910b
AndrewNguyen4 <nguyenminhduc5etp@gmail.com>
2024-08-29 21:09:18 +0800

    Add a list of inputs to display when requested
    Aerus can keep track of user inputs (up to 100 items).
    Implement printContent() function to facilitate printing one-liners.

--- src/main/java/Aerus.java
+++ src/main/java/Aerus.java
@@ -1,26 +1,47 @@
 import java.util.Scanner;
 
 public class Aerus {
+    public static void printContent(String content) {
+        String dividerLine = "____________________________________________________________";
+        System.out.println(dividerLine);
+        System.out.println(content);
+        System.out.println(dividerLine);
+    }
+
     public static void main(String[] args) {
         String dividerLine = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
-        String userInput = "";
-        System.out.println(dividerLine);
-        System.out.println("Hello! I'm Aerus!\nWhat can I do for you?");
-        System.out.println(dividerLine);
+        String userInput;
+        String tasks[] = new String[100];
+        int taskCount = 0;
+
+        printContent("Hello! I'm Aerus!\nWhat can I do for you?");
 
         while (true) {
             userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 break;
             }
-            System.out.println(dividerLine);
-            System.out.println(userInput);
-            System.out.println(dividerLine);
+
+            if (userInput.equals("list")) {
+                if(taskCount == 0) {
+                    printContent("You don't have any tasks!");
+                    continue;
+                }
+
+                System.out.println(dividerLine);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(i+1 + ". " + tasks[i]);
+                }
+                System.out.println(dividerLine);
+                continue;
+            }
+
+            tasks[taskCount] = userInput;
+            taskCount++;
+            printContent("added: " + userInput);
         }
 
-        System.out.println(dividerLine);
-        System.out.println("See you next time!");
-        System.out.println(dividerLine);
+        printContent("See you next time!");
     }
 }
