8a5e97748b4b68b891505a0c96d4cbb2279a1087
rahuljai-05 <e1186335@u.nus.edu>
2024-08-29 15:10:38 +0800

    Level-1
    Add functionality to greet and bid farewell to user

---
+++ src/main/java/Fenix.java
@@ -0,0 +1,17 @@
+public class Fenix {
+    public static void main(String[] args) {
+        String horizontalLine = "_".repeat(60);
+        String greeting = "Hello I'm Fenix";
+        String request = "What can I do for you ?";
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.println(horizontalLine);
+        System.out.println(greeting + "\n" + request);
+        System.out.println(horizontalLine);
+        System.out.println(farewell);
+        System.out.println(horizontalLine);
+    }
+}
+
+
+
+
