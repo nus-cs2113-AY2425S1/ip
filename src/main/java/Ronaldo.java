70cedcca5c7e6f51ca749b93acbdae839cf406b5
Holy-An <anderson96728106@gmail.com>
2024-08-30 00:02:51 +0800

    Add echo feature

--- src/main/java/Ronaldo.java
+++ src/main/java/Ronaldo.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Ronaldo {
     public static void main(String[] args) {
         String indent = "___________________________________________ \n";
         System.out.println( indent + "Hello! I'm Ronaldo, Cristiano Ronaldo! The greatest footballer of all time. SIUUUUUUU! \n"+ "Hehehe what can I do for you? \n"  + indent + "Bye. Hope to see you soon! SIUUUUUUU!\n" + indent);
+
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you soon! SIUUUUUUU!");
+                break;
+            } else {
+                System.out.println(input + "\n");
+            }
+        }
     }
 }
