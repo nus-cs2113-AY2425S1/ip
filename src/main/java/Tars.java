3a643dfbca39e81f50514773156d2780e7cf002a
Jinlin <e1346020@u.nus.edu>
2024-08-29 15:12:29 +0800

    Complete Level-2 increment.

--- src/main/java/Tars.java
+++ src/main/java/Tars.java
@@ -9,10 +9,14 @@ public class Tars {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         // Opening speech.
-        System.out.println("------------------------------------------------------------");
-        System.out.println("Hello! I'm Tars.");
-        System.out.println("Ready to conquer the world? Or maybe just help with something smaller? What can I do for you?");
-        System.out.println("------------------------------------------------------------");
+        System.out.println("    " + "------------------------------------------------------------");
+        System.out.println("    " + "Hello! I'm Tars.");
+        System.out.println("    " + "Ready to conquer the world? Or maybe just help with something smaller? What can I do for you?");
+        System.out.println("    " + "------------------------------------------------------------");
+
+        // Define an array to store tasks with a maximum capacity of 100 tasks.
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         // Waiting for user input.
         Scanner scanner = new Scanner(System.in);
@@ -23,16 +27,28 @@ public class Tars {
             input = scanner.nextLine();
             if (input.equals("bye"))
             {
-                System.out.println("------------------------------------------------------------");
-                System.out.println("    Oh no, you're leaving already? Fine, be that way. But don't forget, I'll be right here waiting... forever!");
-                System.out.println("------------------------------------------------------------");
+                System.out.println("    " + "------------------------------------------------------------");
+                System.out.println("    " + "Oh no, you're leaving already? Fine, be that way. But don't forget, I'll be right here waiting... forever!");
+                System.out.println("    " + "------------------------------------------------------------");
                 break;
+            }
+            else if (input.equals("list"))
+            {
+                System.out.println("    " + "------------------------------------------------------------");
+                System.out.println("    " + "Here are your tasks: ");
+                for (int i = 0; i < taskCount; i++)
+                {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("    " + "------------------------------------------------------------");
             }
             else
             {
-                System.out.println("------------------------------------------------------------");
-                System.out.println("    " + input);
-                System.out.println("------------------------------------------------------------");
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("    " + "------------------------------------------------------------");
+                System.out.println("    " + "    " + "added: " + input);
+                System.out.println("    " + "------------------------------------------------------------");
             }
         }
 
