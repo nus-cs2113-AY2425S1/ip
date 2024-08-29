aaa0cf57024d460e9d72fd0b862d74ec146e017d
3CCLY <cluyang22@hotmail.com>
2024-08-29 12:38:08 +0800

    Ran.java: add greet function with ASCII art

--- src/main/java/Ran.java
+++ src/main/java/Ran.java
@@ -1,7 +1,24 @@
 public class Ran {
-    public static void main(String[] args) {
-	System.out.println("Hello! I'm Ran.");
+    public static void greet() {
+	System.out.println("____________________________________________________________");
+	String logo = "     ___           ___           ___\n"
+	    + "    /\\  \\         /\\  \\         /\\__\\\n"
+	    + "   /::\\  \\       /::\\  \\       /::|  |\n"
+	    + "  /:/\\:\\  \\     /:/\\:\\  \\     /:|:|  |\n"
+	    + " /::\\~\\:\\  \\   /::\\~\\:\\  \\   /:/|:|  |__\n"
+	    + "/:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/ |:| /\\__\\\n"
+	    + "\\/_|::\\/:/  / \\/__\\:\\/:/  / \\/__|:|/:/  /\n"
+	    + "   |:|::/  /       \\::/  /      |:/:/  /\n"
+	    + "   |:|\\/__/        /:/  /       |::/  /\n"
+	    + "   |:|  |         /:/  /        /:/  /\n"
+	    + "    \\|__|         \\/__/         \\/__/\n";
+	System.out.println(logo + "Hello! I'm Ran.");
 	System.out.println("What can I do for you?");
+	System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+	greet();
 	System.out.println("Bye. Hope to see you again soon!");
     }
 }
