4b0614d4df60cd9717482c99866f38422293034d
DucPhong135 <e1129787@u.nus.edu>
2024-08-29 12:43:48 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
 
+    public static void echo(String message) {
+        System.out.println(message);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello I'm Lambo");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner input_reader = new Scanner(System.in);
+        while(true) {
+            String input = input_reader.nextLine();
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else{
+                echo(input);
+            }
+        }
     }
 
 
