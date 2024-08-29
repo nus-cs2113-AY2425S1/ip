c4699cf5b82b8dd4520d258f7587cb871b453f94
ThienDuc <ntduc4@gmail.com>
2024-08-30 00:30:03 +0800

    Coding standard
    Also error handling

--- src/main/java/DBot.java
+++ src/main/java/DBot.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class DBot {
     private static boolean isOn;
@@ -21,30 +19,56 @@ public class DBot {
             System.out.print("Command: ");
             String line = in.nextLine().strip();
             System.out.println("____________________________________________________________");
-            if(line.equals("bye")) {
+
+            if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 isOn = false;
             } else if (line.equals("list")) {
-                for (int i = 0; i < datas.size(); i++) {
-                    System.out.printf("%d.", i + 1);
-                    System.out.println(datas.get(i).toString());
-                }
+                list();
             } else if (line.startsWith("mark ")) {
-                int option = Integer.parseInt(line.substring(line.indexOf(" ") + 1));
-                System.out.println("Nice! I've marked this task as done:");
-                datas.get(option - 1).mark();
-                System.out.println(datas.get(option - 1).toString());
+                mark(line);
             } else if (line.startsWith("unmark ")) {
-                int option = Integer.parseInt(line.substring(line.indexOf(" ") + 1));
-                System.out.println("OK, I've marked this task as not done yet:");
-                datas.get(option - 1).unmark();
-                System.out.println(datas.get(option - 1).toString());
+                unmark(line);
             } else {
-                datas.add(new Task(line));
-                System.out.print("added: ");
-                System.out.println(line);
+                add(line);
             }
+
             System.out.println("____________________________________________________________");
+        }
+    }
+
+    private static void add(String line) {
+        datas.add(new Task(line));
+        System.out.print("added: ");
+        System.out.println(line);
+    }
+
+    private static void list() {
+        for (int i = 0; i < datas.size(); i++) {
+            System.out.printf("%d.", i + 1);
+            System.out.println(datas.get(i).toString());
+        }
+    }
+
+    private static void mark(String line) {
+        try {
+            int option = Integer.parseInt(line.substring(line.indexOf(" ") + 1));
+            System.out.println("Nice! I've marked this task as done:");
+            datas.get(option - 1).mark();
+            System.out.println(datas.get(option - 1).toString());
+        } catch (Exception e) {
+            System.out.println("Invalid input");
+        }
+    }
+
+    private static void unmark(String line) {
+        try {
+            int option = Integer.parseInt(line.substring(line.indexOf(" ") + 1));
+            System.out.println("OK, I've marked this task as not done yet:");
+            datas.get(option - 1).unmark();
+            System.out.println(datas.get(option - 1).toString());
+        } catch (Exception e) {
+            System.out.println("Invalid input");
         }
     }
 }
