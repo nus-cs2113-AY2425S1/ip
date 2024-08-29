a4d197e55c0e4bce1e5aad5999cc7885c7c02067
Paulifyer <ethansoh0308@gmail.com>
2024-08-29 23:05:52 +0800

    Renamed, added the greet and exit

--- src/main/java/Nateh.java
+++ src/main/java/Nateh.java
@@ -1,6 +1,36 @@
-public class Duke {
+public class Nateh {
     public static void main(String[] args) {
-        String lineBreak = "____________________________________________________________\n"
-
+        String lineBreak = "____________________________________________________________\n";
+        String skeleton = """
+                                         {    }
+                                          K,   }
+                                         /  `Y`
+                                    _   /   /
+                                   {_'-K.__/
+                                     `/-.__L._
+                                     /  ' /`\\_}
+                                    /  ' /
+                            ____   /  ' /
+                     ,-'~~~~    ~~/  ' /_
+                   ,'             ``~~~%%',
+                  (                     %  Y
+                 {                      %% I
+                {      -                 %  `.
+                |       ',                %  )
+                |        |   ,..__      __. Y
+                |    .,_./  Y ' / ^Y   J   )|
+                \\           |' /   |   |   ||
+                 \\          L_/    . _ (_,.'(
+                  \\,   ,      ^^""' / |      )
+                    \\_  \\          /,L]     /
+                      '-_`-,       ` `   ./`
+                         `-(_            )
+                             ^^\\..___,.--`""";
+        System.out.print(lineBreak);
+        System.out.println("Hello! I'm Nateh\nWhat can I do for you?");
+        System.out.println(skeleton);
+        System.out.print(lineBreak);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.print(lineBreak);
     }
 }
