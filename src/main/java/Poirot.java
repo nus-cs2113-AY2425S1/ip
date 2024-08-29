cd60db60bf2f6900e3d1f1a79803f4f8efd2d086
Trung Bui <thetrung205@gmail.com>
2024-08-29 16:54:40 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Poirot.java
@@ -0,0 +1,10 @@
+public class Poirot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Hello! I'm POIROT\n");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+    }
+}
