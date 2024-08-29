8c54b6b23cf5d2b4a698663b7174403f905691b4
Chiang-HuiXin <huixinchiang@gmail.com>
2024-08-29 16:14:04 +0800

    Add code for level 3
    Created an array of size 100 for list.
    Add String into array when scanned.
    Iterate the list and print out in bullet points when user type list.

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
+
 public class Eva {
     public static final String HORIZONTAL_LINE = "---------------------------------------------------------------";
+
 
     public static void main(String[] args) {
         System.out.println(HORIZONTAL_LINE);
@@ -9,21 +11,45 @@ public class Eva {
         System.out.println("What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
 
+
         String line;
         Scanner in = new Scanner(System.in);
+        int count = 0;
+        String[] sentenceArray = new String[100];
+
 
         while(true)
         {
             line = in.nextLine();
+
+
+            if(line.equalsIgnoreCase("list"))
+            {
+                for(int i = 0; i < count; i ++)
+                {
+                    System.out.println(i + 1 + ". " + sentenceArray[i]);
+                }
+                System.out.println(HORIZONTAL_LINE);
+                continue;
+            }
+
+
+            sentenceArray[count] = line;
+            count++;
+
+
             if(line.equalsIgnoreCase("Bye"))
             {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(HORIZONTAL_LINE);
                 break;
             }
-            System.out.println(line);
+
+
+            System.out.println("added: " + line);
             System.out.println(HORIZONTAL_LINE);
         }
     }
+
 
 }
