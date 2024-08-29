52fee57502b700d3b71d17e0b1e756f27bfa1784
fuyixuan <yixuanfu762@gmail.com>
2024-08-29 16:21:03 +0800

    Level-1 submission

--- src/main/java/Sirius.java
+++ src/main/java/Sirius.java
@@ -2,9 +2,24 @@ import java.util.Scanner;
 import java.lang.Integer;
 public class Sirius {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("-----------------------" );
-        System.out.println("Hello! I'm Sirius!\n" + "What can I do for you?\n" + "\n-----------------------\n" );
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-----------------------" );
+        System.out.print("Hello! I'm Sirius!\n" + "What can I do for you?\n" + "-----------------------\n" );
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (!userInput.equals("bye")) {
+                System.out.println("-----------------------");
+                System.out.println(userInput);
+                System.out.println("-----------------------\n");
+            }
+            else {
+                System.out.println("-----------------------");
+                System.out.println("Bye! Hope to see you soon.");
+                System.out.println("-----------------------");
+                break;
+            }
+        }
+        scanner.close();
     }
 }
+
