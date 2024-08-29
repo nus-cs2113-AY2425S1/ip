ec9adf7cd2d6ce9e9d311c7ff0422906c34141f1
Ridiculouswifi <thtan02@gmail.com>
2024-08-29 18:15:43 +0800

    Level-2: Added add and list feature

--- src/main/java/Freedom.java
+++ src/main/java/Freedom.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Freedom {
     public static void main(String[] args) {
-        String logo = "_________________________________________\n";
+        String[] storage = new String[100];
+        int lastIndex = 0;
+        String logo = "\t_________________________________________\n";
         String message = """
                 \tHello! I'm Freedom
                 \tWhat can I do for you?
@@ -18,10 +21,33 @@ public class Freedom {
             if (line.equals("bye")) {
                 break;
             }
-            String command = "\t" + line + "\n";
-            System.out.println(logo + command + logo);
+            handleInput(line, storage, lastIndex);
+            if (!line.equals("list")) {
+                lastIndex++;
+            }
         }
 
         System.out.println(logo + closing + logo);
+    }
+
+    public static void handleInput(String input, String[] storage, int lastIndex) {
+        if (input.equals("list")) {
+            printList(Arrays.copyOf(storage, lastIndex));
+            return;
+        }
+        storage[lastIndex] = input;
+        String logo = "\t_________________________________________\n";
+        System.out.println(logo + "\tadded: " + input + "\n" + logo);
+    }
+
+    public static void printList(String[] storage) {
+        String logo = "\t_________________________________________\n";
+        int counter = 0;
+        System.out.print(logo);
+        for (String item: storage) {
+            System.out.println("\t" + (counter + 1) + ". " + item);
+            counter++;
+        }
+        System.out.print(logo);
     }
 }
