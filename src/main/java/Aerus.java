794eb49a73f0d2ff39ce17abf758f32d0ee19481
AndrewNguyen4 <nguyenminhduc5etp@gmail.com>
2024-08-29 20:26:35 +0800

    Implement the Echo feature for the skeletal version of Aerus
    Repeat user input, and exit when the user types "bye".
    Rename variable "hline" to "dividerLine".

--- src/main/java/Aerus.java
+++ src/main/java/Aerus.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Aerus {
     public static void main(String[] args) {
-        String hline = "____________________________________________________________";
-        System.out.println(hline);
+        String dividerLine = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        System.out.println(dividerLine);
         System.out.println("Hello! I'm Aerus!\nWhat can I do for you?");
-        System.out.println(hline);
+        System.out.println(dividerLine);
+
+        while (true) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(dividerLine);
+            System.out.println(userInput);
+            System.out.println(dividerLine);
+        }
+
+        System.out.println(dividerLine);
         System.out.println("See you next time!");
-        System.out.println(hline);
+        System.out.println(dividerLine);
     }
 }
