4f308d27a8996186fa0cfa7b58a21bc757606bda
Trung Bui <thetrung205@gmail.com>
2024-08-29 17:12:51 +0800

    Level 2. Add, List

--- src/main/java/Poirot.java
+++ src/main/java/Poirot.java
@@ -1,11 +1,34 @@
 import java.util.Scanner;
 
 public class Poirot {
+    private static String[] list_actions = new String[100];
+    private static int last_index = 0;
 
     public static void echo(String msg) {
         System.out.println("____________________________________________________________\n");
         System.out.println(msg);
         System.out.println("____________________________________________________________\n");
+    }
+
+    public static void print(String[] list) {
+        if (last_index == 0) {
+            System.out.println("__________________________________________________________\n");
+            System.out.println("No actions available");
+        } else {
+            System.out.println("___________________________________________________________\n");
+            for (int i = 0; i < last_index; i++) {
+                System.out.println(i + 1 + "." + list_actions[i] + " ");
+            }
+        }
+        System.out.println("___________________________________________________________\n");
+    }
+
+    public static void add(String action) {
+        list_actions[last_index] = action;
+        last_index++;
+        System.out.println("___________________________________________________________\n");
+        System.out.println("added: " + action);
+        System.out.println("___________________________________________________________\n");
     }
 
     public static void main(String[] args) {
@@ -17,10 +40,15 @@ public class Poirot {
         Scanner scan = new Scanner(System.in);
         while (working) {
             String input = scan.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                working = false;
-            } else {
-                echo(input);
+            switch (input) {
+                case "list":
+                    print(list_actions);
+                    break;
+                case "bye":
+                    working = false;
+                    break;
+                default:
+                    add(input);
             }
         }
         System.out.println("____________________________________________________________\n");
