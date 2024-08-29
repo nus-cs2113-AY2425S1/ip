601500b103c6214a293823cafc81d9a4aa783cf8
lucas-sc0 <e0959193@u.nus.edu>
2024-08-29 22:30:19 +0800

    Add increment level 2
    Added the ability to store whatever text entered by the user and display them back to the user when requested. Also added indents for Monday's inputs for clarity.

--- src/main/java/Monday.java
+++ src/main/java/Monday.java
@@ -10,29 +10,40 @@ public class Monday {
                 + "                                |___/  \n";
         String line = "____________________________________________________________";
 
+        // Array to store tasks
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello from\n" + logo);
 
         Scanner scanner = new Scanner(System.in);
         String input;
 
         System.out.println(line);
-
-        System.out.println("Hello! I'm MONDAY\nWhat can I do for you?");
-
+        System.out.println("    Hello! I'm MONDAY\n    What can I do for you?");
         System.out.println(line);
 
         while (true) {
             input = scanner.nextLine();
             System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
 
-            if (input.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+            if (input.equalsIgnoreCase("list")) {
+                // Display the list of tasks
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                }
+            } else if (input.equalsIgnoreCase("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else {
+                // Add the task to the list
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("    added: " + input);
             }
-        }
 
+            System.out.println(line);
+        }
     }
 }
