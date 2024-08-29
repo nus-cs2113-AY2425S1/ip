185e05242c0c62e75cff111bd0a79d42e0fe372d
unknown <nivedit@comp.nus.edu.sg>
2024-08-30 02:52:22 +0800

    Add method to mark and unmark

--- src/main/java/Bitwise.java
+++ src/main/java/Bitwise.java
@@ -14,6 +14,7 @@ public class Bitwise {
     private static final String indentation = "        ";
     private static String[] taskList = new String[100];
     private static int numberOfTasks = 0;
+    private static boolean[] isCompleted = new boolean[100];
 
     public static void main(String[] args) {
         System.out.println(sectionBreak + indentation + "Hello from\n" + logo);
@@ -32,6 +33,14 @@ public class Bitwise {
             System.out.print(lineBreak);
             if (userInput.equalsIgnoreCase("list")) {
                 printTaskList();
+            }
+            else if (userInput.contains("unmark")) {
+                String taskName = userInput.substring(userInput.indexOf(" ") + 1);
+                markCompletionStatus(taskName, false);
+            }
+            else if (userInput.contains("mark")) {
+                String taskName = userInput.substring(userInput.indexOf(" ") + 1);
+                markCompletionStatus(taskName, true);
             }
             else {
                 addToList(userInput);
@@ -57,7 +66,18 @@ public class Bitwise {
     }
     public static void printTaskList() {
         for (int i = 0; i < numberOfTasks; i++) {
-            System.out.println(indentation + Integer.toString(i + 1) + ". " + taskList[i]);
+            char completionChar = isCompleted[i] ? 'X' : ' ';
+            System.out.println(indentation + Integer.toString(i + 1) + ". " + "[" + completionChar + "] " + taskList[i]);
         }
+    }
+    public static void markCompletionStatus(String taskName, boolean isComplete) {
+        for (int i = 0; i < numberOfTasks; i++) {
+            if (taskList[i].equalsIgnoreCase(taskName)) {
+                isCompleted[i] = isComplete;
+            }
+        }
+        String message = isComplete ? "Awesome, I've marked this task as completed!" : "I've added the task back in";
+        System.out.println(indentation + message);
+        printTaskList();
     }
 }
