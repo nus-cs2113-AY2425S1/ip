6fe4a3492ddf3812149d4f683c5972c9d2ee3e29
Jiaming <shenjiaming27@gmail.com>
2024-08-29 17:12:47 +0800

    Echo and Add List

--- src/main/java/Bron.java
+++ src/main/java/Bron.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bron {
     public static void main(String[] args) {
         String logo = "  ____    ____      ____    _     _\n"
@@ -6,7 +8,29 @@ public class Bron {
                     + " |  _ <  | ___/   | |  | | | |\\\\ | |\n"
                     + " | |_) | | | \\ \\  | |__| | | | \\\\| |\n"
                     + " |____/  |_|  \\_\\  \\____/  |_|  \\__|\n";
-        System.out.println(logo + "Hello! I'm Bron\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
+        Scanner input = new Scanner(System.in);
+        System.out.println(logo + "Hello! I'm Bron\n" + "What can I do for you?\n");
+        String line = input.nextLine();
+        String[] tasks = new String[100];
+        int taskCount = 0;
+        while (line != null) {
 
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println("Catch you on the flip cuh");
+                break;
+            }
+            if (line.equalsIgnoreCase("list")) {
+                int listCount = 0;
+                while(tasks[listCount] != null) {
+                    System.out.println(listCount + 1 + ":" + tasks[listCount++]);
+                }
+            }
+            else {
+                System.out.println("added" + ". " + line);
+                tasks[taskCount++] = line;
+            }
+
+            line = input.nextLine();
+        }
     }
 }
