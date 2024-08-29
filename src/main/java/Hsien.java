8dcf05334bdc2b31accee4b52f15becd7e98974d
KuanHsienn <liangkuanhsien@gmail.com>
2024-08-29 22:56:30 +0800

    Add echo feature

--- src/main/java/Hsien.java
+++ src/main/java/Hsien.java
@@ -1,18 +1,43 @@
+import java.util.Scanner;
+
 public class Hsien {
 
     public static void printLine() {
-        System.out.println("-".repeat(50));
+        System.out.println("-".repeat(50) + "\n");
+    }
+
+    public static void printLogo() {
+        System.out.println(" _   _         _____        _ _   _ ");
+        System.out.println("| | | | ##### |_   _|##### |   \\ | |");
+        System.out.println("| |_| |#        | |  #     | |\\ \\| |");
+        System.out.println("|  _  | #####   | |  ##### | | \\ \\ |");
+        System.out.println("| | | |      # _| |_ #     | |  \\  |");
+        System.out.println("|_| |_| ##### |_____|##### |_|   \\_|\n");
     }
 
     public static void main(String[] args) {
         printLine();
+        printLogo();
         // Greet
         System.out.println("Hello! I am Hsien, your personal chatbot\n");
         printLine();
 
-        // Exit
-        System.out.println("Have a good day! Bye!");
-        printLine();
+        String message;
+        Scanner in = new Scanner(System.in);
 
+        while (true) {
+            message = in.nextLine();
+            if (message.equals("bye")) {
+                // Exit
+                System.out.println("Have a good day! Bye!");
+                printLine();
+                break;
+            } else {
+                System.out.println(message + "\n");
+                printLine();
+            }
+        }
+
+        in.close();
     }
 }
