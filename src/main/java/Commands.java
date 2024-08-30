a99766a4c6bdd26d52fa4e50b4c26c5c98c1c038
LAPTOP-52GQO9KI\zhito <e0725202@u.nus.edu>
2024-08-30 16:23:24 +0800

    added commands class

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,2 +1,25 @@
-package main.java;public class Commands {
+package main.java;
+
+public class Commands {
+
+    private static int MAX_LENGTH = 100;
+    public String[] lines = new String[MAX_LENGTH];
+    public int length = 0;
+
+    public Commands(String[] lines, int length) {
+        this.lines = lines;
+        this.length = length;
+    }
+
+    public Commands() {
+    }
+
+    public void add(String nextLine) {
+        this.lines[length] = nextLine;
+        length++;
+
+    }
+    public void echo() {
+        System.out.println(lines[length]);
+    }
 }
--- src/main/java/MrStripes.java
+++ src/main/java/MrStripes.java
@@ -1,9 +1,18 @@
 package main.java;
+import java.util.Scanner;
+import Commands.;
 
-public class GenericChatbot {
+public class MrStripes {
     public static void main(String[] args) {
         String Greeting = " hello";
         String Goodbye = "goodbye!";
+        Scanner scanner = new Scanner(System.in);
+        Commands commands = new Commands();
+        while (true) {
+            commands.add(scanner.nextLine());
+            commands.echo();
+        }
+
         System.out.println(Greeting);
         System.out.println(Goodbye);
     }
