61a974efe63120860cd2f18f19240e4c71e1c78a
Ajay Shanker <ajayshanker1998@gmail.com>
2024-08-30 15:41:43 +0800

    Formate Code

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,81 +1,81 @@
 public class Task {
-  protected String description; // Description of the task
-  protected boolean isDone;     // Status of the task
+    protected String description; // Description of the task
+    protected boolean isDone;     // Status of the task
 
-  private static int numberOfTasks = 0; // Number of tasks in the list
+    private static int numberOfTasks = 0; // Number of tasks in the list
 
-  /**
-   * Constructor for the Task class.
-   *
-   * @param description
-   */
-  public Task(String description) {
-    this.description = description;
-    this.isDone = false;
+    /**
+     * Constructor for the Task class.
+     *
+     * @param description
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
 
-    numberOfTasks++;
-  }
+        numberOfTasks++;
+    }
 
-  /**
-   * Returns the description of the task.
-   *
-   * @return description of the task
-   */
-  public String getDescription() {
-    return this.description;
-  }
+    /**
+     * Returns the description of the task.
+     *
+     * @return description of the task
+     */
+    public String getDescription() {
+        return this.description;
+    }
 
-  /**
-   * Changes the description of the task.
-   *
-   * @param newDescription
-   */
-  public void changeDescription(String newDescription) {
-    this.description = newDescription;
-  }
+    /**
+     * Changes the description of the task.
+     *
+     * @param newDescription
+     */
+    public void changeDescription(String newDescription) {
+        this.description = newDescription;
+    }
 
-  /**
-   * Returns the status icon of the task.
-   *
-   * @return status icon of the task
-   */
-  public String getStatusIcon() {
-    return (isDone ? "✓" : " ");
-  }
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return status icon of the task
+     */
+    public String getStatusIcon() {
+        return (isDone ? "✓" : " ");
+    }
 
-  /**
-   * Marks the task as done.
-   */
-  public void markAsDone() {
-    this.isDone = true;
+    /**
+     * Marks the task as done.
+     */
+    public void markAsDone() {
+        this.isDone = true;
 
-    System.out.print("Nice! I've marked this task as done: \n  ");
-    printTask();
-  }
+        System.out.print("Nice! I've marked this task as done: \n  ");
+        printTask();
+    }
 
-  /**
-   * Marks the task as not done.
-   */
-  public void markAsUndone() {
-    this.isDone = false;
+    /**
+     * Marks the task as not done.
+     */
+    public void markAsUndone() {
+        this.isDone = false;
 
-    System.out.print("OK, I've marked this task as not done yet: \n  ");
-    printTask();
-  }
+        System.out.print("OK, I've marked this task as not done yet: \n  ");
+        printTask();
+    }
 
-  /**
-   * Returns the number of tasks in the list.
-   *
-   * @return number of tasks in the list
-   */
-  public static int getNumberOfTasks() {
-    return numberOfTasks;
-  }
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return number of tasks in the list
+     */
+    public static int getNumberOfTasks() {
+        return numberOfTasks;
+    }
 
-  /**
-   * Prints the task to the console.
-   */
-  public void printTask() {
-    System.out.println("[" + this.getStatusIcon() + "] " + this.description);
-  }
+    /**
+     * Prints the task to the console.
+     */
+    public void printTask() {
+        System.out.println("[" + this.getStatusIcon() + "] " + this.description);
+    }
 }
