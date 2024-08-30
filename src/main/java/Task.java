c9e9cb711a0fe448afc9541828270b7583db505f
Ajay Shanker <ajayshanker1998@gmail.com>
2024-08-30 15:07:58 +0800

    Add more methods to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -25,10 +25,16 @@ public class Task {
 
   public void markAsDone() {
     this.isDone = true;
+
+    System.out.print("Nice! I've marked this task as done: \n  ");
+    printTask();
   }
 
   public void markAsUndone() {
     this.isDone = false;
+
+    System.out.print("OK, I've marked this task as not done yet: \n  ");
+    printTask();
   }
 
   public static int getNumberOfTasks() {
