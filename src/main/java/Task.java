114a74cede9c5687f4d7e03274c0a50babbceb7a
LTK-1606 <tzekang1606@gmail.com>
2024-08-30 16:42:19 +0800

    Add mark as done

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
+    public void setStatus(boolean status) {
+        this.isDone = status;
+    }
+}
