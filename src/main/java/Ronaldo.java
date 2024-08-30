42dc8dad2da0016253af82fb1fba63435f98e809
Holy-An <anderson96728106@gmail.com>
2024-08-30 10:10:00 +0800

    Add mark and unmark feature

--- src/main/java/Ronaldo.java
+++ src/main/java/Ronaldo.java
@@ -15,6 +15,10 @@ public class Ronaldo {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you soon! SIUUUUUUU!");
                 break;
+            } else if (input.startsWith("mark")) {
+                handleTask(input);
+            } else if (input.startsWith("unmark")) {
+                handleTask(input);
             } else if (input.equals("list")) {
                 System.out.println("Here are the goals to complete in order for you to reach your dreams:\n");
                 for (int i = 0; i < tasks.size(); i++) {
@@ -28,4 +32,25 @@ public class Ronaldo {
             }
         }
     }
+
+    public static void handleTask(String input) {
+        String[] words = input.split(" ");
+        int taskNumber = Integer.parseInt(words[words.length - 1]) - 1;
+        if (taskNumber > tasks.size()) {
+            System.out.println("Goal number is greater than the number of goals you have!\n");
+        } else if (input.startsWith("mark")) {
+            System.out.println("SIUUU! Congrats, one step closer to achieving your dreams! This goal is now achieved:");
+            tasks.get(taskNumber).markAsDone();
+            System.out.println(" " + "[" + tasks.get(taskNumber).getStatusIcon() + "] " + tasks.get(taskNumber).description);
+            System.out.println(" ");
+        } else if (input.startsWith("unmark")) {
+            System.out.println("Ronaldo is disappointed in you. Work harder! This goal is now yet to achieve:");
+            tasks.get(taskNumber).markAsUndone();
+            System.out.println(" " + "[" + tasks.get(taskNumber).getStatusIcon() + "] " + tasks.get(taskNumber).description);
+            System.out.println(" ");
+        }
+    }
 }
+
+
+
