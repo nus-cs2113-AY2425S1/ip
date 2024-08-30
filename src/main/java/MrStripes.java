e5b61e16455568ddd2776fdead9ee951a1ad8498
LAPTOP-52GQO9KI\zhito <e0725202@u.nus.edu>
2024-08-30 16:23:04 +0800

    added commands class

---
+++ src/main/java/MrStripes.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class GenericChatbot {
+    public static void main(String[] args) {
+        String Greeting = " hello";
+        String Goodbye = "goodbye!";
+        System.out.println(Greeting);
+        System.out.println(Goodbye);
+    }
+}
