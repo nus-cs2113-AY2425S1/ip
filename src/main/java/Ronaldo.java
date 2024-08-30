c99cbe06366ebce73a306f9b15ee8ef73c8a2da8
Holy-An <anderson96728106@gmail.com>
2024-08-30 09:39:33 +0800

    Add list and task feature

--- src/main/java/Ronaldo.java
+++ src/main/java/Ronaldo.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ronaldo {
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String indent = "___________________________________________ \n";
-        System.out.println( indent + "Hello! I'm Ronaldo, Cristiano Ronaldo! The greatest footballer of all time. SIUUUUUUU! \n"+ "Hehehe what can I do for you? \n"  + indent + "Bye. Hope to see you soon! SIUUUUUUU!\n" + indent);
+        System.out.println( indent + "Hello! I'm Ronaldo, Cristiano Ronaldo! The greatest footballer of all time. SIUUUUUUU! \n"+ "Hehehe what can I do for you? \n"  + indent);
 
         Scanner in = new Scanner(System.in);
 
@@ -12,8 +15,16 @@ public class Ronaldo {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you soon! SIUUUUUUU!");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("Here are the goals to complete in order for you to reach your dreams:\n");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).description);
+                }
+                System.out.println(" "); //Indentation
             } else {
-                System.out.println(input + "\n");
+                System.out.println("Your goal has been added: " + input + "\n");
+                Task t = new Task(input);
+                tasks.add(t);
             }
         }
     }
