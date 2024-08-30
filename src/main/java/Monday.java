bdd24a4b3ceb5568e2404980e6e3345598998a7b
lucas-sc0 <e0959193@u.nus.edu>
2024-08-31 00:11:25 +0800

    Add increment A-CodingStandard
    Ensured code complies with standard provided and refactored the code to provide more abstaction and align with OOP.

--- src/main/java/Monday.java
+++ src/main/java/Monday.java
@@ -1,65 +1,98 @@
 import java.util.Scanner;
 
 public class Monday {
+    private static final String LOGO = " __  __                 _             \n"
+            + "|  \\/  | ___  _ __   __| | __ _ _   _ \n"
+            + "| |\\/| |/ _ \\| '_ \\ / _` |/ _` | | | |\n"
+            + "| |  | | (_) | | | | (_| | (_| | |_| |\n"
+            + "|_|  |_|\\___/|_| |_|\\__,_|\\__,_|\\__, |\n"
+            + "                                |___/  \n";
+    private static final String LINE = "____________________________________________________________";
+    private Task[] tasks = new Task[100];
+    private int taskCount = 0;
+
     public static void main(String[] args) {
-        String logo = " __  __                 _             \n"
-                + "|  \\/  | ___  _ __   __| | __ _ _   _ \n"
-                + "| |\\/| |/ _ \\| '_ \\ / _` |/ _` | | | |\n"
-                + "| |  | | (_) | | | | (_| | (_| | |_| |\n"
-                + "|_|  |_|\\___/|_| |_|\\__,_|\\__,_|\\__, |\n"
-                + "                                |___/  \n";
-        String line = "____________________________________________________________";
+        Monday monday = new Monday();
+        monday.run();
+    }
 
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+    private void run() {
+        printWelcomeMessage();
+        handleUserInput();
+    }
 
-        System.out.println("Hello from\n" + logo);
+    private void printWelcomeMessage() {
+        System.out.println("Hello from\n" + LOGO);
+        System.out.println(LINE);
+        System.out.println("    Hello! I'm MONDAY\n    What can I do for you?");
+        System.out.println(LINE);
+    }
 
+    private void handleUserInput() {
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        System.out.println(line);
-        System.out.println("    Hello! I'm MONDAY\n    What can I do for you?");
-        System.out.println(line);
-
         while (true) {
             input = scanner.nextLine();
-            System.out.println(line);
+            System.out.println(LINE);
 
             if (input.equalsIgnoreCase("list")) {
-                System.out.println("    Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("    " + (i + 1) + "." + tasks[i]);
-                }
+                listTasks();
             } else if (input.startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
-                if (taskNumber >= 0 && taskNumber < taskCount) {
-                    tasks[taskNumber].markAsDone();
-                    System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println("      " + tasks[taskNumber]);
-                } else {
-                    System.out.println("    Invalid task number.");
-                }
+                markTaskAsDone(input);
             } else if (input.startsWith("unmark ")) {
-                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
-                if (taskNumber >= 0 && taskNumber < taskCount) {
-                    tasks[taskNumber].markAsNotDone();
-                    System.out.println("    OK, I've marked this task as not done yet:");
-                    System.out.println("      " + tasks[taskNumber]);
-                } else {
-                    System.out.println("    Invalid task number.");
-                }
+                unmarkTaskAsNotDone(input);
             } else if (input.equalsIgnoreCase("bye")) {
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println(line);
+                printGoodbyeMessage();
                 break;
             } else {
-                tasks[taskCount] = new Task(input);
-                taskCount++;
-                System.out.println("    added: " + input);
+                addTask(input);
             }
 
-            System.out.println(line);
+            System.out.println(LINE);
         }
+    }
+
+    private void listTasks() {
+        System.out.println("    Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("    " + (i + 1) + "." + tasks[i]);
+        }
+    }
+
+    private void markTaskAsDone(String input) {
+        int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+        if (isValidTaskNumber(taskNumber)) {
+            tasks[taskNumber].markAsDone();
+            System.out.println("    Nice! I've marked this task as done:");
+            System.out.println("      " + tasks[taskNumber]);
+        } else {
+            System.out.println("    Invalid task number.");
+        }
+    }
+
+    private void unmarkTaskAsNotDone(String input) {
+        int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+        if (isValidTaskNumber(taskNumber)) {
+            tasks[taskNumber].markAsNotDone();
+            System.out.println("    OK, I've marked this task as not done yet:");
+            System.out.println("      " + tasks[taskNumber]);
+        } else {
+            System.out.println("    Invalid task number.");
+        }
+    }
+
+    private void addTask(String input) {
+        tasks[taskCount] = new Task(input);
+        taskCount++;
+        System.out.println("    added: " + input);
+    }
+
+    private void printGoodbyeMessage() {
+        System.out.println("    Bye. Hope to see you again soon!");
+    }
+
+    private boolean isValidTaskNumber(int taskNumber) {
+        return taskNumber >= 0 && taskNumber < taskCount;
     }
 }
