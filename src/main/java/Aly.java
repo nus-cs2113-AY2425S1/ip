4dc8b6d487a620271679567184304cb66747212a
LTK-1606 <tzekang1606@gmail.com>
2024-08-30 13:23:38 +0800

    Change name to Aly

---
+++ src/main/java/Aly.java
@@ -0,0 +1,14 @@
+public class Aly {
+    public static void main(String[] args) {
+        String logo = "    _      _     _   _\n"
+                + "   / \\    | |   \\ \\ / /\n"
+                + "  / _ \\   | |    \\ V / \n"
+                + " / ___ \\  | |__   | |  \n"
+                + "/_/   \\_\\ |____|  |_|  \n";
+        System.out.print("Hello! My name is \n" + logo);
+        System.out.println("----------------------------");
+        System.out.println("I'm hungry, what do you want?");
+        System.out.println("----------------------------");
+        System.out.println("Bye! I'm going to eat MacDonalds now!");
+    }
+}
