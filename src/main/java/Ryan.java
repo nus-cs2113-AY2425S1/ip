6c6d5e9d554bdc488237a828cd13943f4b83ed1b
Yisoong <yisoong23@gmail.com>
2024-08-30 15:29:46 +0800

    Revert "Add scanner.close and format code"
    This reverts commit 96fe3b4196792ebd52f78baab84c21aae684e2b3.

--- src/main/java/Ryan.java
+++ src/main/java/Ryan.java
@@ -24,9 +24,9 @@ public class Ryan {
                     System.out.println((i + 1) + "." + tasks.get(i));
                 }
                 Utils.horizontalLine();
-            } else if (command.startsWith("mark")) {
+            } else if (command.startsWith("mark")){
                 int index = Integer.parseInt(command.split(" ")[1]) - 1;
-                if (index >= 0 && index < tasks.size()) {
+                if (index >= 0 && index < tasks.size()){
                     tasks.get(index).mark();
                     System.out.println("Nice! I've marked this task as done:\n" + tasks.get(index));
                 } else {
@@ -42,17 +42,16 @@ public class Ryan {
                     System.out.println("Invalid task number.");
                 }
                 Utils.horizontalLine();
-            } else {
+            }else {
                 Utils.horizontalLine();
                 Task task = new Task(command);
                 tasks.add(task);
-                System.out.println("Added: " + command);
+                System.out.println("added: " + command);
                 Utils.horizontalLine();
             }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
         Utils.horizontalLine();
-        scanner.close();
     }
 }
