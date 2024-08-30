df2a38a97198a74dc52a0dc569d68ef2e7861a8d
hzxnancy <hzxnancy@gmail.com>
2024-08-30 09:43:22 +0800

    Implement mark as done function

--- src/main/java/Cy.java
+++ src/main/java/Cy.java
@@ -5,11 +5,29 @@ public class Cy {
         System.out.println("______________________________________");
     }
 
-    public static void printList(String[] items) {
+    public static void markItem(String[] splitInputs, Task[] items) {
+        int index = Integer.parseInt(splitInputs[1]) - 1;
+        items[index].isDone = true;
+        printLine();
+        System.out.println("Nice! I've marked this task as done :");
+        System.out.println("[X] " + items[index].description);
+        printLine();
+    }
+
+    public static void unmarkItem(String[] splitInputs, Task[] items) {
+        int index = Integer.parseInt(splitInputs[1]) - 1;
+        items[index].isDone = false;
+        printLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("[ ] " + items[index].description);
+        printLine();
+    }
+
+    public static void printList(Task[] items) {
         printLine();
         for (int i = 0; i < items.length; i++) {
             if (items[i] != null) {
-                System.out.println(i+1 + ". " + items[i]);
+                System.out.println((i + 1) + ". [" + items[i].getStatusIcon() + "] " + items[i].description);
             } else {
                 break;
             }
@@ -17,10 +35,18 @@ public class Cy {
         printLine();
     }
 
+    public static void addItem(Task[] items, int count, String input) {
+        items[count] = new Task(input);
+        printLine();
+        System.out.println("added: " + input);
+        printLine();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, I'm Cy");
         System.out.println("What can I do for you?");
-        String[] items = new String[100];
+
+        Task[] items = new Task[100];
         int count = 0;
 
         Scanner scan = new Scanner(System.in);
@@ -28,14 +54,17 @@ public class Cy {
 
         while (!input.equalsIgnoreCase("bye")) {
 
+            String[] splitInputs = input.split(" ");
+
             if (input.equalsIgnoreCase("list")) {
                 printList(items);
+            } else if (splitInputs[0].equalsIgnoreCase("mark")) {
+                markItem(splitInputs, items);
+            } else if (splitInputs[0].equalsIgnoreCase("unmark")) {
+                unmarkItem(splitInputs, items);
             } else {
-                items[count] = input;
+                addItem(items, count, input);
                 count++;
-                printLine();
-                System.out.println("added: " + input);
-                printLine();
             }
             input = scan.nextLine();
         }
