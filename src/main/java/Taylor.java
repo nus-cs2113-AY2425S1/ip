7305458242fa4e9b99973983c336183188182d4b
Aaron Liu <aaronzzz10@foxmail.com>
2024-08-30 10:48:19 +0800

    Level-0

---
+++ src/main/java/Taylor.java
@@ -0,0 +1,10 @@
+public class Taylor {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Taylor");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
