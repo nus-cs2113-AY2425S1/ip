cd5391adaef8e565413d6290cca0a516fc6825e9
LuxLucky7 <e1451199@u.nus.edu>
2024-08-30 11:46:13 +0800

    Implement level 0

---
+++ src/main/java/Edith.java
@@ -0,0 +1,11 @@
+public class Edith {
+    public static void main(String[] args) {
+        String myName="Edith";
+        String horizontalLine="______________________________________________";
+        System.out.println("Hello I am " + myName + ".");
+        System.out.println("What can I do for you?\n");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(horizontalLine);
+    }
+}
