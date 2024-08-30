67c4b9925de64a5fe216df8aa1715f1bd36a87c7
Aaron Liu <aaronzzz10@foxmail.com>
2024-08-30 10:54:10 +0800

    Level-1

--- src/main/java/Taylor.java
+++ src/main/java/Taylor.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Taylor {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Taylor");
         System.out.println("What can I do for you?");
         System.out.println(line);
+
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+            input = sc.nextLine();
+        }
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
