513ec9db20f94455ecd64f2ed6344ba54dfe1da8
LTK-1606 <tzekang1606@gmail.com>
2024-08-30 14:47:39 +0800

    Add list

--- src/main/java/Aly.java
+++ src/main/java/Aly.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Aly {
@@ -7,8 +8,9 @@ public class Aly {
     }
 
     private static void initialise() {
+        printLine();
         System.out.println("I'm hungry, what do you want?");
-        System.out.println("Enter 1 for echo function, 2 for list function, 0 to exit");
+        System.out.println("Faster lah! Enter 1 for echo function, 2 for list function, 0 to exit");
         printLine();
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
@@ -16,10 +18,17 @@ public class Aly {
         case "0":
             exit();
         case "1":
-            echo();
-        default:
-            System.out.println("Invalid input");
+            System.out.println("Enter 0 to return to main menu anytime!");
             printLine();
+            echo();
+            break;
+        case "2":
+            System.out.println("Enter 0 to return to main menu anytime!");
+            printLine();
+            list();
+            break;
+        default:
+            System.out.println("Can read instructions anot? Try again!");
             initialise();
         }
     }
@@ -30,14 +39,57 @@ public class Aly {
         System.exit(0);
     }
 
-    public static void echo() {
-        String line;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Waiting for Input...");
-        line = in.nextLine();
-        printLine();
-        System.out.println(line);
-        printLine();
+    private static void echo() {
+        boolean isExit = false;
+        while (!isExit) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            System.out.println("Waiting for Input...");
+            line = in.nextLine();
+            if (line.equals("0")) {
+                isExit = true;
+                System.out.println("Returning to main menu!");
+            } else {
+                printLine();
+                System.out.println(line);
+                printLine();
+            }
+        }
+        initialise();
+    }
+
+    private static void list() {
+        ArrayList<String> listItems = new ArrayList<>();
+        boolean isExit = false;
+        while (!isExit) {
+            System.out.println("Enter 'list' to see your list of tasks, 'add' to add another task");
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            switch (input) {
+            case "list":
+                printLine();
+                int count = 1;
+                for (String item : listItems) {
+                    System.out.println(count + ". " + item);
+                    count += 1;
+                }
+                printLine();
+                break;
+            case "add":
+                printLine();
+                System.out.println("Enter your task:");
+                Scanner addItem = new Scanner(System.in);
+                String task = addItem.nextLine();
+                listItems.add(task);
+                printLine();
+                System.out.println("added task: " + task);
+                printLine();
+                break;
+            case "0":
+                isExit = true;
+                System.out.println("Returning to main menu!");
+            }
+        }
         initialise();
     }
 
@@ -49,7 +101,6 @@ public class Aly {
                 + " / ___ \\  | |__   | |  \n"
                 + "/_/   \\_\\ |____|  |_|  \n";
         System.out.print("Hello! My name is \n" + logo);
-        printLine();
         initialise();
     }
 }
