690789e2a4758d86669511c9ff861e327aca2810
LTK-1606 <tzekang1606@gmail.com>
2024-08-30 16:42:19 +0800

    Add mark as done

--- src/main/java/Aly.java
+++ src/main/java/Aly.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Aly {
@@ -8,29 +7,40 @@ public class Aly {
     }
 
     private static void initialise() {
-        printLine();
-        System.out.println("I'm hungry, what do you want?");
-        System.out.println("Faster lah! Enter 1 for echo function, 2 for list function, 0 to exit");
-        printLine();
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        switch (input) {
-        case "0":
-            exit();
-        case "1":
-            System.out.println("Enter 0 to return to main menu anytime!");
+        Task[] tasks = new Task[100];
+        boolean isExit = false;
+        while (!isExit) {
             printLine();
-            echo();
-            break;
-        case "2":
-            System.out.println("Enter 0 to return to main menu anytime!");
+            System.out.println("I'm hungry, what do you want?");
+            System.out.println("Faster lah! Enter 1 for echo function, 2 for list function, 3 for task marking function, 0 to exit");
             printLine();
-            list();
-            break;
-        default:
-            System.out.println("Can read instructions anot? Try again!");
-            initialise();
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            switch (input) {
+            case "0":
+                isExit = true;
+                break;
+            case "1":
+                System.out.println("Enter 0 to return to main menu anytime!");
+                printLine();
+                echo();
+                break;
+            case "2":
+                System.out.println("Enter 0 to return to main menu anytime!");
+                printLine();
+                tasks = list(tasks);
+                break;
+            case "3":
+                System.out.println("Enter 0 to return to main menu anytime!");
+                printLine();
+                tasks = markTask(tasks);
+                break;
+            default:
+                System.out.println("Can read instructions anot? Try again!");
+                break;
+            }
         }
+        exit();
     }
 
     private static void exit() {
@@ -58,9 +68,15 @@ public class Aly {
         initialise();
     }
 
-    private static void list() {
-        ArrayList<String> listItems = new ArrayList<>();
+    private static Task[] list(Task[] listItems) {
         boolean isExit = false;
+        int index = 0;
+        for (int i = 0; i < 101; i++) {
+            if (listItems[i] == null) {
+                index = i;
+                break;
+            }
+        }
         while (!isExit) {
             System.out.println("Enter 'list' to see your list of tasks, 'add' to add another task");
             Scanner in = new Scanner(System.in);
@@ -69,8 +85,11 @@ public class Aly {
             case "list":
                 printLine();
                 int count = 1;
-                for (String item : listItems) {
-                    System.out.println(count + ". " + item);
+                for (Task listItem : listItems) {
+                    if (listItem == null) {
+                        break;
+                    }
+                    System.out.println(count + ". " + listItem.description);
                     count += 1;
                 }
                 printLine();
@@ -80,7 +99,8 @@ public class Aly {
                 System.out.println("Enter your task:");
                 Scanner addItem = new Scanner(System.in);
                 String task = addItem.nextLine();
-                listItems.add(task);
+                listItems[index] = (new Task(task));
+                index += 1;
                 printLine();
                 System.out.println("added task: " + task);
                 printLine();
@@ -88,9 +108,74 @@ public class Aly {
             case "0":
                 isExit = true;
                 System.out.println("Returning to main menu!");
+                break;
+            default:
+                printLine();
+                System.out.println("Can read instructions anot? Try again!");
+                printLine();
+                break;
             }
         }
-        initialise();
+        return listItems;
+    }
+
+    private static Task[] markTask(Task[] taskList) {
+        boolean isExit = false;
+        while (!isExit) {
+            System.out.println("Enter 'list' to see task list, 'mark' or 'unmark' with a number to toggle task status");
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            String[] splitInput = input.split(" ");
+            if (input.equals("list")) {
+                printLine();
+                System.out.println("Here is your task list:");
+                int count = 1;
+                for (Task task : taskList) {
+                    if (task == null) {
+                        break;
+                    }
+                    System.out.println(count + "." + task);
+                    count += 1;
+                }
+                printLine();
+            }
+            else if (splitInput.length == 2 && input.startsWith("mark")) {
+                printLine();
+                int indexNum = Integer.parseInt(splitInput[1]);
+                if (indexNum > taskList.length || indexNum < 0) {
+                    System.out.println("Task number out of bounds!");
+                    printLine();
+                }
+                else {
+                    taskList[indexNum-1].setStatus(true);
+                    System.out.println("\"" + taskList[indexNum-1].description + "\" marked as done!");
+                    printLine();
+                }
+            }
+            else if (splitInput.length == 2 && input.startsWith("unmark")) {
+                printLine();
+                int indexNum = Integer.parseInt(splitInput[1]);
+                if (indexNum > taskList.length || indexNum < 0) {
+                    System.out.println("Task number out of bounds!");
+                    printLine();
+                }
+                else {
+                    taskList[indexNum-1].setStatus(false);
+                    System.out.println("\"" + taskList[indexNum-1].description + "\" marked as undone!");
+                    printLine();
+                }
+            }
+            else if (input.equals("0")) {
+                isExit = true;
+                System.out.println("Returning to main menu!");
+            }
+            else {
+                printLine();
+                System.out.println("Can read instructions anot? Try again!");
+                printLine();
+            }
+        }
+        return taskList;
     }
 
     public static void main(String[] args) {
