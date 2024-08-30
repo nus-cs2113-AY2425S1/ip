4978e71462686df4c837439c97f778b0b6bf6b9a
shyaamald <dwivedishyaamal@gmail.com>
2024-08-30 16:37:09 +0800

    Change chatbot name

---
+++ src/main/java/Transcendent.java
@@ -0,0 +1,11 @@
+public class Transcendent {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Greetings.");
+        System.out.println("I am Transcendent.");
+        System.out.println("How may I be of assistance?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Farewell.");
+        System.out.println("____________________________________________________________");
+    }
+}
