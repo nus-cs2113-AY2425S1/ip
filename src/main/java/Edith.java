b6632eef51391f94d4ce0e0f81aabbb5b96dd0a0
LuxLucky7 <e1451199@u.nus.edu>
2024-08-30 11:51:50 +0800

    Implement level 1

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,12 +1,38 @@
-public class Edith {
-    public static void main(String[] args) {
-        String myName="Edith";
-        String horizontalLine="______________________________________________";
-        System.out.println("Hello I am " + myName + ".");
-        System.out.println("What can I do for you?\n");
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(horizontalLine);
+import java.util.Scanner;
 
+public class Edith {
+    Scanner sc=new Scanner(System.in);
+    private static final String myName="Edith";
+    private final static String horizontalLine="______________________________________________";
+
+
+    public void giveIntroduction() {
+        System.out.println("Hello I am " + myName + ".");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+    }
+
+    public void talkToUser() {
+        do {
+            String temporyString= sc.nextLine();
+            System.out.println(horizontalLine);
+            if(temporyString.equals("bye")){
+                sayGoodbye();
+                return;
+            }
+
+            System.out.println("added : " + temporyString);
+            System.out.println(horizontalLine);
+        } while (true);
+    }
+    public void sayGoodbye() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(horizontalLine + "\n");
+    }
+
+    public static void main(String[] args) {
+        Edith obj = new Edith();
+        obj.giveIntroduction();
+        obj.talkToUser();
     }
 }
