bb0ee366328f3ef1373868184839bc9d9e60c7e2
tkhahns <111437863+tkhahns@users.noreply.github.com>
2024-08-31 22:10:46 +0800

    Level 1. Echo

--- src/main/java/KBot.java
+++ src/main/java/KBot.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class KBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
-        System.out.println("Hello from KBot");
+        System.out.println("Hello! I'm KBot");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        boolean working = true;
+
+
+        while (working) {
+            String input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                working = false;
+            }
+        }
+
+        scanner.close();
     }
 }
