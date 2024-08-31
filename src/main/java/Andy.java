f4e58501dfa4695905898bfe90fb994eae20223e
andreusxcarvalho <andreusrobycarvalho@gmail.com>
2024-08-31 15:22:20 +0800

    Added MarkAsDone functionality for list

--- src/main/java/Andy.java
+++ src/main/java/Andy.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Andy {
 
-    private static List<String> itemList = new ArrayList<>(); // List to store items
+    private static List<Task> itemList = new ArrayList<>(); // List to store tasks
 
     public static void main(String[] args) {
         greetUser(); // method to greet the user
@@ -17,6 +17,10 @@ public class Andy {
 
             if (input.startsWith("list")) {
                 handleListInput(input);
+            } else if (input.startsWith("mark ")) {
+                markTaskAsDone(input);
+            } else if (input.startsWith("unmark ")) {
+                markTaskAsNotDone(input);
             } else {
                 echo(input); // Call echo method to repeat the input
             }
@@ -31,6 +35,8 @@ public class Andy {
         System.out.println("What can I do for you?");
         System.out.println("Type 'list' followed by an item to add it to the list.");
         System.out.println("Type 'list show' to display all items in the list.");
+        System.out.println("Type 'mark <number>' to mark a task as done.");
+        System.out.println("Type 'unmark <number>' to mark a task as not done.");
         System.out.println("_______________________________________");
     }
 
@@ -40,10 +46,11 @@ public class Andy {
         if (parts.length > 1) {
             String command = parts[1];
 
+
             if (command.equals("show")) {
-                showList();
+                showList(); // Correctly handle 'list show' to display the list
             } else {
-                addItemToList(command);
+                addItemToList(command); // Add the item to the list
             }
         } else {
             System.out.println("Please provide a valid list command or item.");
@@ -51,7 +58,7 @@ public class Andy {
     }
 
     private static void addItemToList(String item) {
-        itemList.add(item);
+        itemList.add(new Task(item));
         System.out.println("Item added to the list: " + item);
     }
 
@@ -61,11 +68,33 @@ public class Andy {
         if (itemList.isEmpty()) {
             System.out.println("The list is empty.");
         } else {
-            for (String item : itemList) {
-                System.out.println("- " + item);
+            for (int i = 0; i < itemList.size(); i++) {
+                System.out.println((i + 1) + "." + itemList.get(i));
             }
         }
         System.out.println("_______________________________________");
+    }
+
+    private static void markTaskAsDone(String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+            itemList.get(taskNumber).setDone(true);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("[X] " + itemList.get(taskNumber).getDescription());
+        } catch (Exception e) {
+            System.out.println("Invalid task number. Please try again.");
+        }
+    }
+
+    private static void markTaskAsNotDone(String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+            itemList.get(taskNumber).setDone(false);
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("[ ] " + itemList.get(taskNumber).getDescription());
+        } catch (Exception e) {
+            System.out.println("Invalid task number. Please try again.");
+        }
     }
 
     private static void echo(String input) {
@@ -76,5 +105,33 @@ public class Andy {
         System.out.println("_______________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_______________________________________");
+    }
+
+    // Task class to represent each task in the list
+    static class Task {
+        private String description;
+        private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public boolean isDone() {
+            return isDone;
+        }
+
+        public void setDone(boolean done) {
+            isDone = done;
+        }
+
+        @Override
+        public String toString() {
+            return (isDone ? "[X] " : "[ ] ") + description;
+        }
     }
 }
