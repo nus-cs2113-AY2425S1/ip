785409e9be74c2b8801c4eeaf75a2b604b69b0e5
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-08-31 12:19:27 +0800

    Add Level 2 Add, List
    Add ability to store user input and output all of them when requested.
    Add line divider strings

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -10,6 +10,9 @@ public class Yapper {
                     + "Buh-Byeeee!!! Cya soon!\n"
                     + "____________________________________________________________";
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(Greet);
         Scanner in = new Scanner(System.in);
         while (true) {
@@ -17,8 +20,19 @@ public class Yapper {
             if (line.equals("bye")) {
                 System.out.println(Exit);
                 break;
+            } else if (line.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+                continue;
             }
-            System.out.println(line);
+            tasks[taskCount] = line;
+            taskCount++;
+            System.out.println("____________________________________________________________");
+            System.out.println("added: " + line);
+            System.out.println("____________________________________________________________");
         }
     }
 }
