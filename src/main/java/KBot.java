ff602a7a739fdb6431bb963a50b60c2d69f4ef62
tkhahns <111437863+tkhahns@users.noreply.github.com>
2024-08-31 21:41:33 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/KBot.java
@@ -0,0 +1,10 @@
+public class KBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello from KBot");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
