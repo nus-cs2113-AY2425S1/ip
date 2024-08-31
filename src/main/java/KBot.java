4f36b853c91d4c16452a651278a9325bb7aa6bb3
tkhahns <111437863+tkhahns@users.noreply.github.com>
2024-08-31 22:44:07 +0800

    Level 2. Add, List

--- src/main/java/KBot.java
+++ src/main/java/KBot.java
@@ -1,28 +1,76 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class KBot {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+    private ArrayList<String> tasks;
+    private Scanner scanner;
+
+    public KBot() {
+        tasks = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+        greetUser();
+
+        while (true) {
+            String input = getUserInput();
+
+            switch (input) {
+                case "bye":
+                    exit();
+                    return;
+
+                case "list":
+                    listTasks();
+                    break;
+
+                default:
+                    addTask(input);
+                    break;
+            }
+        }
+    }
+
+    private void greetUser() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm KBot");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        boolean working = true;
+    }
 
+    private String getUserInput() {
+        return scanner.nextLine();
+    }
 
-        while (working) {
-            String input = scanner.nextLine();
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
+    private void addTask(String task) {
+        tasks.add(task);
+        System.out.println("____________________________________________________________");
+        System.out.println("added: " + task);
+        System.out.println("____________________________________________________________");
+    }
 
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                working = false;
-            }
+    private void listTasks() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
         }
+        System.out.println("____________________________________________________________");
+    }
 
+    private void exit() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    private void closeScanner() {
         scanner.close();
+    }
+
+    public static void main(String[] args) {
+        KBot bot = new KBot();
+        bot.run();
     }
 }
