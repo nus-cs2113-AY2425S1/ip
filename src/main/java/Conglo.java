c66bb957ef8ca699dc92096d9b0a423c2cc85908
ZiliaAJY <141621081+ZiliaAJY@users.noreply.github.com>
2024-08-31 23:58:15 +0800

    Add Todo, Event, and Deadline features
    - Add 'Task.java', 'Deadline.java', 'Todo.java' and 'Event.java' files
    - Implement classes Todo, Deadline and Event classes to inherit from a Task class

--- src/main/java/Conglo.java
+++ src/main/java/Conglo.java
@@ -2,50 +2,18 @@ import java.util.Scanner;
 
 public class Conglo {
 
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
-
-        public void markAsDone() {
-            isDone = true;
-        }
-
-        public void markAsNotDone() {
-            isDone = false;
-        }
-    }
-
+    protected static final int MAX_TASKS = 100;
     protected static String command;
+    protected static int taskCount = 0;
 
     public static void main(String[] args) {
         greetings();
         Scanner scanner = new Scanner(System.in);
-        Task[] listing = new Task[100];
+        Task[] listing = new Task[MAX_TASKS];
         command = scanner.nextLine();
-        int i = 0;
-        Task input = new Task(command);
-        while (!input.description.equals("bye")) {
-            String[] words = input.description.split(" ");
-            if (input.description.equals("list")) {
-                listOut(listing, i);
-            } else if (words[0].equals("mark") || words[0].equals("unmark")) {
-                markList(words, listing, i);
-            } else {
-                echo(input.description);
-                listing[i] = input;
-                i++;
-            }
+        while (!command.equals("bye")) {
+            processCommand(command, listing);
             command = scanner.nextLine();
-            input = new Task(command);
         }
         scanner.close();
         sayGoodbye();
@@ -61,24 +29,24 @@ public class Conglo {
         printLineSeparator();
     }
 
-    public static void echo(String input) {
-        System.out.println("added: " + input);
+    public static void echo(Task task) {
+        String taskSuffix = taskCount > 1 ? " tasks" : " task";
+        System.out.println("All done! Task added to list:");
+        System.out.println(" " + task.toString());
+        System.out.println("The list has " + taskCount + taskSuffix + " now.");
         printLineSeparator();
     }
 
-    public static void listOut(Task[] listing, int size) {
-        int i = 0;
-        while (i < size) {
-            System.out.print((i + 1) + ". [" + listing[i].getStatusIcon() + "]");
-            System.out.println(listing[i].description);
-            i++;
+    public static void listTasks(Task[] listing) {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + listing[i].toString());
         }
         printLineSeparator();
     }
 
-    public static void markList(String[] words, Task[] listing, int size) {
-        int i = Integer.parseInt(words[1]) - 1;
-        if (i >= size) {
+    public static void markList(String[] words, Task[] listing) {
+        int i = Integer.parseInt(words[1].substring(0, 1)) - 1;
+        if (i >= taskCount || i < 0) {
             System.out.println("Invalid task number");
             return;
         }
@@ -90,6 +58,49 @@ public class Conglo {
             System.out.println("OK, I've marked this task as not done yet:");
         }
         System.out.println("[" + listing[i].getStatusIcon() + "] " + listing[i].description );
+    }
+
+    public static void setDeadline(String sentence, Task[] listing) {
+        String[] words = sentence.split(" /by ");
+        Deadline deadline = new Deadline(words[0], words[1]);
+        listing[taskCount] = deadline;
+        taskCount++;
+        echo(deadline);
+    }
+
+    public static void setEvent(String sentence, Task[] listing) {
+        String[] words = sentence.split(" /from | /to ");
+        Event event = new Event(words[0], words[1], words[2]);
+        listing[taskCount] = event;
+        taskCount++;
+        echo(event);
+    }
+
+    public static void processCommand(String command, Task[] listing) {
+        String[] words = command.split(" ", 2);
+        switch(words[0]) {
+        case "list":
+            listTasks(listing);
+            break;
+        case "unmark":
+        case "mark":
+            markList(words, listing);
+            break;
+        case "todo":
+            Todo todo = new Todo(words[1]);
+            listing[taskCount] = todo;
+            taskCount++;
+            echo(todo);
+            break;
+        case "deadline":
+            setDeadline(words[1], listing);
+            break;
+        case "event":
+            setEvent(words[1], listing);
+            break;
+        default:
+            echo(new Task(command));
+        }
     }
 
     public static void sayGoodbye() {
