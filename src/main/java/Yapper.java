d62d2c54f5d4a302e04e506585284700f015e2a1
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-08-31 11:17:43 +0800

    Add level-0 increments Rename, Greet, Exit

---
+++ src/main/java/Yapper.java
@@ -0,0 +1,12 @@
+public class Yapper {
+    public static void main(String[] args) {
+        String Greet = "____________________________________________________________"
+                     + "Hello! I'm Yapper\n"
+                     + "What can I do for you?";
+        String Exit = "____________________________________________________________"
+                    + "Bye. Hope to see you again soon!"
+                    + "____________________________________________________________";
+        System.out.println(Greet);
+        System.out.println(Exit);
+    }
+}
