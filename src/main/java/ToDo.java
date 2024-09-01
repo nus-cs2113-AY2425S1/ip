21f0a6bb29e1c548ee58a24b4b1f465cc45383d5
louisjoety <kolouis002@gmail.com>
2024-09-01 17:14:27 +0800

    Improve ToDo class by adding constructor and overriding toString method for the marker

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,2 +1,12 @@
 public class ToDo extends Task{
+
+    //To-Do Constructor inherits Task
+    public ToDo(String description){
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
