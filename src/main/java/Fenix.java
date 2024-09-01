a3d358fd870367955145a103950db7c3c45d0cb6
rahuljai-05 <e1186335@u.nus.edu>
2024-09-01 15:13:09 +0800

    Enable chatbot to echo user commands
    Modified the chatbot to repeat user input
    and exit when the user says "bye".
    This change allows the chatbot to mimic user commands
    as a basic interactive feature.

--- src/main/java/Fenix.java
+++ src/main/java/Fenix.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class Fenix {
+
+    public static final int HORIZONTAL_LINE_LENGTH = 60;
+    public static final String HORIZONTAL_LINE =
+            "~".repeat(HORIZONTAL_LINE_LENGTH);
+    public static final String GREETING =
+            "Greetings. I am Fenix, your digital assistant.";
+    public static final String SERVICE_PROMPT =
+            "How may I be of service to you today?";
+    public static final String FAREWELL =
+            "It has been a pleasure assisting you. Farewell.";
+    public static final Scanner IN = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String horizontalLine = "_".repeat(60);
-        String greeting = "Hello I'm Fenix";
-        String request = "What can I do for you ?";
-        String farewell = "Bye. Hope to see you again soon!";
-        System.out.println(horizontalLine);
-        System.out.println(greeting + "\n" + request);
-        System.out.println(horizontalLine);
-        System.out.println(farewell);
-        System.out.println(horizontalLine);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(GREETING);
+        System.out.println(SERVICE_PROMPT);
+        System.out.println(HORIZONTAL_LINE);
+        String userInput = IN.nextLine();
+        // Exit the loop when the user says bye
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(userInput);
+            System.out.println(HORIZONTAL_LINE);
+            userInput = IN.nextLine();
+        }
+        System.out.println(FAREWELL);
+        System.out.println(HORIZONTAL_LINE);
+        IN.close();
     }
 }
-
-
-
-
