8ffaf9bf8dd8f138be6470668bf05aac52ac80da
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-09-01 23:43:54 +0800

    Refactor handleCommand by extracting multiple methods
    Before this, handleCommand was a very long method with poor readability.
    
    Let's extract out code in handleCommand into multiple methods, with one method
    to handle each command type. This shortens the handleCommand method which
    makes it much more readable.

--- src/main/java/CommandHandler.java
+++ src/main/java/CommandHandler.java
@@ -7,47 +7,75 @@ public class CommandHandler {
     public static void handleCommand(String command) {
         String[] dissectedCommand = command.split(" ");
         if (command.equals("list")) {
-            System.out.println("Here are the tasks in your list:");
-            TaskList.printList();
+            handleList();
         } else if (dissectedCommand[0].equals("mark")) {
-            int taskId = Integer.parseInt(dissectedCommand[1]) - 1;
-            if (TaskList.isValidTaskId(taskId)) {
-                TaskList.markTaskAsDone(taskId);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + TaskList.getSingleTaskDetails(taskId));
-            } else {
-                System.out.println("Invalid task ID.");
-            }
+            handleMark(dissectedCommand);
         } else if (dissectedCommand[0].equals("unmark")) {
-            int taskId = Integer.parseInt(dissectedCommand[1]) - 1;
-            if (TaskList.isValidTaskId(taskId)) {
-                TaskList.markTaskAsUndone(taskId);
-                System.out.println("Ok, I've marked this task as not done yet:");
-                System.out.println("  " + TaskList.getSingleTaskDetails(taskId));
-            } else {
-                System.out.println("Invalid task ID.");
-            }
+            handleUnmark(dissectedCommand);
         } else if (dissectedCommand[0].equals("todo")) {
-            Todo newTodo = new Todo(command.substring(command.indexOf(" ") + 1));
-            TaskList.addTask(newTodo);
-            System.out.println("added: " + newTodo.getNameWithStatus());
+            handleTodo(command);
         } else if (dissectedCommand[0].equals("deadline")) {
-            String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /by"));
-            String deadline = command.substring(command.indexOf("/by") + 4);
-            Deadline newDeadline = new Deadline(description, deadline);
-            TaskList.addTask(newDeadline);
-            System.out.println("added: " + newDeadline.getNameWithStatus());
+            handleDeadline(command);
         } else if (dissectedCommand[0].equals("event")) {
-            String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /from"));
-            String start = command.substring(command.indexOf("/from") + 6, command.indexOf(" /to"));
-            String end = command.substring(command.indexOf("/to") + 4);
-            Event newEvent = new Event(description, start, end);
-            TaskList.addTask(newEvent);
-            System.out.println("added: " + newEvent.getNameWithStatus());
+            handleEvent(command);
         } else {
-            Task newTask = new Task(command);
-            TaskList.addTask(newTask);
-            System.out.println("added: " + newTask.getNameWithStatus());
+            handleTask(command);
+        }
+    }
+
+    private static void handleList() {
+        System.out.println("Here are the tasks in your list:");
+        TaskList.printList();
+    }
+
+    private static void handleTask(String command) {
+        Task newTask = new Task(command);
+        TaskList.addTask(newTask);
+        System.out.println("added: " + newTask.getNameWithStatus());
+    }
+
+    private static void handleEvent(String command) {
+        String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /from"));
+        String start = command.substring(command.indexOf("/from") + 6, command.indexOf(" /to"));
+        String end = command.substring(command.indexOf("/to") + 4);
+        Event newEvent = new Event(description, start, end);
+        TaskList.addTask(newEvent);
+        System.out.println("added: " + newEvent.getNameWithStatus());
+    }
+
+    private static void handleDeadline(String command) {
+        String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /by"));
+        String deadline = command.substring(command.indexOf("/by") + 4);
+        Deadline newDeadline = new Deadline(description, deadline);
+        TaskList.addTask(newDeadline);
+        System.out.println("added: " + newDeadline.getNameWithStatus());
+    }
+
+    private static void handleTodo(String command) {
+        Todo newTodo = new Todo(command.substring(command.indexOf(" ") + 1));
+        TaskList.addTask(newTodo);
+        System.out.println("added: " + newTodo.getNameWithStatus());
+    }
+
+    private static void handleUnmark(String[] dissectedCommand) {
+        int taskId = Integer.parseInt(dissectedCommand[1]) - 1;
+        if (TaskList.isValidTaskId(taskId)) {
+            TaskList.markTaskAsUndone(taskId);
+            System.out.println("Ok, I've marked this task as not done yet:");
+            System.out.println("  " + TaskList.getSingleTaskDetails(taskId));
+        } else {
+            System.out.println("Invalid task ID.");
+        }
+    }
+
+    private static void handleMark(String[] dissectedCommand) {
+        int taskId = Integer.parseInt(dissectedCommand[1]) - 1;
+        if (TaskList.isValidTaskId(taskId)) {
+            TaskList.markTaskAsDone(taskId);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + TaskList.getSingleTaskDetails(taskId));
+        } else {
+            System.out.println("Invalid task ID.");
         }
     }
 }
