03ddc81af44884c20f9b8bda7d1cc2779bf6219d
Terence Chan Zun Mun <zunmun@gmail.com>
2024-09-01 21:48:14 +0800

    Refactor and Add Command Parser with argument support

--- src/main/java/Cuboyd.java
+++ src/main/java/Cuboyd.java
@@ -1,7 +1,47 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Cuboyd {
+
+    public static HashMap<String, String> parseCommandToArguments(String line) {
+        HashMap<String, String> argumentsList = new HashMap<>();
+        String[] lineArgs = line.split(" ");
+
+        // Command
+        if (lineArgs.length <= 0) {
+            argumentsList.put("command","");
+            return argumentsList;
+        }
+        argumentsList.put("command",lineArgs[0]);
+
+        // Arguments
+        String currArgumentName = "main";
+        StringBuilder currArgument = new StringBuilder();
+
+        for (int i=1; i<lineArgs.length; i++) {
+            if (lineArgs[i].isEmpty()) { // Should be redundant but just in case
+                continue;
+            }
+            if (lineArgs[i].charAt(0) == '/') {
+                // New argument
+                if (!currArgument.toString().isEmpty()){
+                    argumentsList.put(currArgumentName, currArgument.toString().strip());
+                }
+                currArgumentName = lineArgs[i];
+                currArgument.setLength(0);
+            } else {
+                // Add on to existing argument
+                currArgument.append(" ").append(lineArgs[i]);
+            }
+        }
+        // Add last command
+        if (!currArgument.toString().isEmpty()) {
+            argumentsList.put(currArgumentName, currArgument.toString().strip());
+        }
+        return argumentsList;
+    }
+
     public static void main(String[] args) {
         String name = "Cuboyd";
         System.out.println("Hello! I'm " + name);
@@ -16,26 +56,26 @@ public class Cuboyd {
 
         // Command Entry
         String line;
-        String[] lineArgs;
+        HashMap<String, String> argumentsList;
         Scanner scanner = new Scanner(System.in);
         boolean isAskingInput = true;
         while (isAskingInput){
             System.out.print("> ");
             line = scanner.nextLine();
-            lineArgs = line.split(" ");
-            switch(lineArgs.length > 0 ? lineArgs[0] : ""){
+            argumentsList = parseCommandToArguments(line);
+            switch(argumentsList.get("command")){
                 case "list":
                     for (int currentItemIndex = 0; currentItemIndex< tasks.size(); currentItemIndex++){
                         System.out.println(String.valueOf(currentItemIndex+1) + "." + tasks.get(currentItemIndex));
                     }
                     break;
                 case "mark":
-                    if (lineArgs.length < 2){
+                    if (argumentsList.get("main") == null){
                         System.out.println("No index was given!");
                         break;
                     }
                     try {
-                        index = Integer.parseInt(lineArgs[1]) - 1;
+                        index = Integer.parseInt(argumentsList.get("main")) - 1;
                     } catch (NumberFormatException e){
                         System.out.println("That is not a valid index!");
                         break;
@@ -51,12 +91,12 @@ public class Cuboyd {
                     System.out.println("  " + currentTask);
                     break;
                 case "unmark":
-                    if (lineArgs.length < 2){
+                    if (argumentsList.get("main") == null){
                         System.out.println("No index was given!");
                         break;
                     }
                     try {
-                        index = Integer.parseInt(lineArgs[1]) - 1;
+                        index = Integer.parseInt(argumentsList.get("main")) - 1;
                     } catch (NumberFormatException e){
                         System.out.println("That is not a valid index!");
                         break;
