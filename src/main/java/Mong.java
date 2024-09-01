536437fd811fd19e9d60694b5e8c49c0aa04bab0
sarahchow03 <76396931+sarahchow03@users.noreply.github.com>
2024-09-01 17:58:40 +0800

    Add increment Level-4
    Level-4 of Mong supports the tracking of three types of data, namely
    Todos, Deadlines, and Events. To implement these changes, we use
    Inheritance and Polymorphism.
    
    Enumerators for TaskType: we decipher the first word in the command line
    as the TaskType. Currently, Mong supports LIST, BYE, MARK, UNMARK,
    EVENT, TODO and DEADLINE as valid task types.
    
    Improve code quality: In line with the Week 4 instructions, we improve
    code quality through the use of constants (rather than magic numbers),
    and following of coding standards. For example, indentations of switch
    statements are in-line and naming standards.
    
    Inheritance and polymorphism: Event, Todo, and Deadlines are child
    classes of the Task class. The toString() function takes on a
    different form based on the whether the object is a Event, Todo, or
    Deadline object.

--- src/main/java/Mong.java
+++ src/main/java/Mong.java
@@ -2,28 +2,82 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Mong {
+
+    public static final int LENGTH_DEADLINE = 8;
+    public static final int LENGTH_BY = 3;
+    public static final int LENGTH_TODO = 4;
+    public static final int LENGTH_EVENT = 5;
+    public static final int LENGTH_FROM = 4;
+    public static final int LENGTH_TO = 2;
+    public static final String HORIZONTAL_LINE = "--------------------------------------------------";
+
     /**
      * Prints a horizontal line with width of 25 characters.
      */
     public static void printHorizontalLine() {
-        for (int i = 0; i < 25; i += 1) {
-            System.out.print("-");
-        }
-        System.out.println();
+        System.out.println(HORIZONTAL_LINE);
     }
 
     /**
      * Prints out an indexed list of commands given to Mong starting from 1.
-     * If the item has been marked as 'completed', the [ ] box will be checked with an '[X]'.
      */
     public static void printIndexedList(Task[] list) {
         for (int i = 0; i < list.length; i++) {
-            if (list[i].isCompleted()) {
-                System.out.println(Integer.toString(i + 1) + ". " + "[X] " + list[i].getText());
-            } else {
-                System.out.println(Integer.toString(i + 1) + ". " + "[ ] " + list[i].getText());
-            }
+            System.out.println(i + 1 + ". " + list[i]);
         }
+        System.out.println("MONG my god! You have " + Task.currentIndex + " task(s) in the list!");
+    }
+
+    /**
+     * Add a Task of type Deadline.
+     * A deadline consists of the task description and deadline in String format.
+     */
+    public static void addDeadline(Task[] list, String input) {
+        int endOfCommand = input.indexOf("deadline") + LENGTH_DEADLINE;
+        int endOfBy = input.lastIndexOf("/by") + LENGTH_BY;
+        int startOfBy = input.indexOf("/by");
+        String description = input.substring(endOfCommand, startOfBy);
+        String deadline = input.substring(endOfBy + 1);
+        list[Task.currentIndex] = new Deadline(description, deadline);
+        System.out.println("Mong-ed! This item has been added: ");
+        System.out.println(list[Task.currentIndex - 1]);
+    }
+
+    /**
+     * Add a Task of type Todo.
+     * A deadline consists of the task description and deadline in String format.
+     */
+    public static void addTodo(Task[] list, String input) {
+        int endOfCommand = input.indexOf("todo") + LENGTH_TODO;
+        String description = input.substring(endOfCommand);
+        list[Task.currentIndex] = new Todo(description);
+        System.out.println("Mong-ed! This item has been added: ");
+        System.out.println(list[Task.currentIndex - 1]);
+    }
+
+    /**
+     * Add a Task of type Event.
+     * A deadline consists of the task description and deadline in String format.
+     */
+    public static void addEvent(Task[] list, String input) {
+        int endOfCommand = input.indexOf("event") + LENGTH_EVENT;
+        int endOfFrom = input.lastIndexOf("/from") + LENGTH_FROM;
+        int startOfFrom = input.indexOf("/from");
+        int endOfTo = input.lastIndexOf("/to") + LENGTH_TO;
+        int startOfTo = input.indexOf("/to");
+        String description = input.substring(endOfCommand, startOfFrom);
+        String from = input.substring(endOfFrom + 2, startOfTo - 1);
+        String to = input.substring(endOfTo + 1);
+        list[Task.currentIndex] = new Event(description, from, to);
+        System.out.println("Mong-ed! This item has been added: ");
+        System.out.println(list[Task.currentIndex - 1]);
+    }
+
+    /**
+     * Returns the TaskType enum value of the first word in the input.
+     */
+    public static TaskType decipherTaskType(String command) {
+        return TaskType.fromCommand(command);
     }
 
     /**
@@ -33,31 +87,45 @@ public class Mong {
      * If the command's first word is "unmark", it will set the isCompleted variable of the Task to false.
      * If the command "bye" is sent, the program will exit.
      */
-    public static void addItems() {
+    public static void addByTask() {
         Scanner in = new Scanner(System.in);
-        String command = in.nextLine();
+        String input = in.nextLine();
         Task[] list = new Task[100];
-        int itemIndex = 0;
-        while (!command.equals("bye")) {
+        int itemIndex;
+        TaskType command = decipherTaskType(input.split(" ")[0]);
+        while (command != TaskType.BYE) {
             printHorizontalLine();
-            if (command.equals("list")) {
+            switch(command) {
+            case LIST:
                 // print items in an indexed list
                 printIndexedList(Arrays.copyOf(list, Task.currentIndex));
-            } else if (command.split(" ")[0].equals("mark")) {
+                break;
+            case MARK:
                 // the itemIndex is -1 than the input from the user
-                itemIndex = Integer.parseInt(command.split(" ")[1]) - 1;
+                itemIndex = Integer.parseInt(input.split(" ")[1]) - 1;
                 list[itemIndex].setCompleted(true);
-            } else if (command.split(" ")[0].equals("unmark")) {
+                break;
+            case UNMARK:
                 // the itemIndex is -1 than the input from the user
-                itemIndex = Integer.parseInt(command.split(" ")[1]) - 1;
+                itemIndex = Integer.parseInt(input.split(" ")[1]) - 1;
                 list[itemIndex].setCompleted(false);
-            } else {
-                // add an item to the list
-                list[Task.currentIndex] = new Task(command);
-                System.out.println("Mong-ed! This item has been added: " + command);
+                break;
+            case DEADLINE:
+                addDeadline(list, input);
+                break;
+            case TODO:
+                addTodo(list, input);
+                break;
+            case EVENT:
+                addEvent(list, input);
+                break;
+            default:
+                System.out.println("MooONG?! Not a valid command...");
+                break;
             }
             printHorizontalLine();
-            command = in.nextLine();
+            input = in.nextLine();
+            command = decipherTaskType(input.split(" ")[0]);
         }
     }
 
@@ -77,7 +145,7 @@ public class Mong {
         System.out.println("Hello, I am\n" + logo);
         System.out.println("What can I do for you?");
         printHorizontalLine();
-        addItems();
+        addByTask();
         printHorizontalLine();
         System.out.println("Mong-mong... See you again next time!");
         printHorizontalLine();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,15 +1,15 @@
 public class Task {
-    private String text;
+    private String description;
     private boolean isCompleted;
     private int index;
     public static int currentIndex = 0;
 
-    public String getText() {
-        return text;
+    public String getDescription() {
+        return description;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public boolean isCompleted() {
@@ -23,10 +23,10 @@ public class Task {
         isCompleted = completed;
         if (isCompleted) {
             System.out.println("Mong >_<!!I have marked it as completed:");
-            System.out.println("[X] " + text);
+            System.out.println(this);
         } else {
             System.out.println("Mong-mong :<! The task has been unmarked:");
-            System.out.println("[ ] " + text);
+            System.out.println(this);
         }
     }
 
@@ -39,8 +39,16 @@ public class Task {
         currentIndex++;
     }
 
-    public Task(String text) {
-        setText(text);
+    @Override
+    public String toString() {
+        if (isCompleted) {
+            return "[X] " + description;
+        }
+        return "[ ] "+ description;
+    }
+
+    public Task(String description) {
+        setDescription(description);
         // default value for isCompleted is false
         this.isCompleted = false;
         setIndex();
