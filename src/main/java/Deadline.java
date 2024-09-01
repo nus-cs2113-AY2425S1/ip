0eecb401b5633669b49cc50c48c201b6a3acea16
louisjoety <kolouis002@gmail.com>
2024-09-01 17:18:08 +0800

    Improve Deadline class by adding constructor and overriding toString method for the marker

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,16 @@
-public class Deadline extends Task{
+public class Deadline extends Task {
+
+    protected String by;
+
+    //Deadline Constructor inherits Task
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    //Override toString method to show marker
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
 }
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -5,6 +5,7 @@ public class ToDo extends Task{
         super(description);
     }
 
+    //Override toString method to show marker
     @Override
     public String toString() {
         return "[T]" + super.toString();
