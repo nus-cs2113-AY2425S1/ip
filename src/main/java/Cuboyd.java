5946b98e59b6e99d0cef7dfe064e88ba51ead153
Terence Chan Zun Mun <zunmun@gmail.com>
2024-09-01 22:10:33 +0800

    Refactor common UI elements

--- src/main/java/Cuboyd.java
+++ src/main/java/Cuboyd.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Cuboyd {
-
+    // General Parsing /////////////////////////////////////////////////////////////////////////////////////////////////
     public static HashMap<String, String> parseCommandToArguments(String line) {
         HashMap<String, String> argumentsList = new HashMap<>();
         String[] lineArgs = line.split(" ");
@@ -41,15 +41,40 @@ public class Cuboyd {
         }
         return argumentsList;
     }
-    public static void main(String[] args) {
+
+    // Helpers /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static final int INVALID_TASK_INDEX = -1;
+    public static int parseValidTaskIndex(HashMap<String, String> argumentsList, ArrayList<Task> tasks){
+        int index = INVALID_TASK_INDEX;
+        if (argumentsList.get("main") == null){
+            return index;
+        }
+        try {
+            index = Integer.parseInt(argumentsList.get("main")) - 1;
+        } catch (NumberFormatException e){
+            return index;
+        }
+        if (index < 0 || index >= tasks.size()){
+            return index;
+        }
+        return index;
+    }
+    public static void displayIntroText(){
         String name = "Cuboyd";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+    }
+    public static void addTaskWithUI(ArrayList<Task> tasks, Task task) {
+        tasks.add(task);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task.toString());
+        System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+    }
 
-        // Initialise List
+    // Main Loop ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static void main(String[] args) {
+        displayIntroText();
         ArrayList<Task> tasks = new ArrayList<>();
-
-        // For Marking/ Unmarking
         Task currentTask;
         int index;
 
@@ -75,10 +100,7 @@ public class Cuboyd {
                         break;
                     }
                     currentTask = new ToDo(argumentsList.get("main"));
-                    tasks.add(currentTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + currentTask);
-                    System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    addTaskWithUI(tasks, currentTask);
                     break;
                 case "deadline":
                     if (argumentsList.get("main") == null){
@@ -90,10 +112,7 @@ public class Cuboyd {
                         break;
                     }
                     currentTask = new Deadline(argumentsList.get("main"), argumentsList.get("/by"));
-                    tasks.add(currentTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + currentTask);
-                    System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    addTaskWithUI(tasks, currentTask);
                     break;
                 case "event":
                     if (argumentsList.get("main") == null){
@@ -113,50 +132,27 @@ public class Cuboyd {
                             argumentsList.get("/from"),
                             argumentsList.get("/to")
                     );
-                    tasks.add(currentTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + currentTask);
-                    System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    addTaskWithUI(tasks, currentTask);
                     break;
                 case "mark":
-                    if (argumentsList.get("main") == null){
-                        System.out.println("No index was given!");
-                        break;
-                    }
-                    try {
-                        index = Integer.parseInt(argumentsList.get("main")) - 1;
-                    } catch (NumberFormatException e){
-                        System.out.println("That is not a valid index!");
-                        break;
-                    }
-                    if (index < 0 || index >= tasks.size()){
+                    index = parseValidTaskIndex(argumentsList, tasks);
+                    if (index == INVALID_TASK_INDEX){
                         System.out.println("That is not a valid index!");
                         break;
                     }
                     currentTask = tasks.get(index);
                     currentTask.markAsDone();
-
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + currentTask);
+                    System.out.println("  " + currentTask.toString());
                     break;
                 case "unmark":
-                    if (argumentsList.get("main") == null){
-                        System.out.println("No index was given!");
-                        break;
-                    }
-                    try {
-                        index = Integer.parseInt(argumentsList.get("main")) - 1;
-                    } catch (NumberFormatException e){
-                        System.out.println("That is not a valid index!");
-                        break;
-                    }
-                    if (index < 0 || index >= tasks.size()){
+                    index = parseValidTaskIndex(argumentsList, tasks);
+                    if (index == INVALID_TASK_INDEX){
                         System.out.println("That is not a valid index!");
                         break;
                     }
                     currentTask = tasks.get(index);
                     currentTask.markAsUndone();
-
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println("  " + currentTask);
                     break;
