754d422281ac9c1467103bc09fa988bc2ab36151
rahuljai-05 <e1186335@u.nus.edu>
2024-09-01 21:19:06 +0800

    Add task management functionality to Fenix
    Added the ability for Fenix to store and list user tasks.
    Improved structure by separating concerns
    with methods for greeting and bidding farewell,
    accepting, getting and processing user input,
    storing tasks and showing all tasks.
    Modified the output format to differentiate between
    user commands and Fenix's responses when modifications
    are made.

--- src/main/java/Fenix.java
+++ src/main/java/Fenix.java
@@ -2,31 +2,77 @@ import java.util.Scanner;
 
 public class Fenix {
 
-    public static final int HORIZONTAL_LINE_LENGTH = 60;
-    public static final String HORIZONTAL_LINE =
-            "~".repeat(HORIZONTAL_LINE_LENGTH);
+    public static final int HORIZONTAL_LINE_USER_COMMAND_LENGTH = 60;
+    public static final int HORIZONTAL_LINE_FENIX_MODIFICATION_LENGTH = 52;
+    public static final String HORIZONTAL_LINE_USER_COMMAND =
+            "~".repeat(HORIZONTAL_LINE_USER_COMMAND_LENGTH);
+    public static final String HORIZONTAL_LINE_FENIX_MODIFICATION =
+            "\t\t" + "*".repeat(HORIZONTAL_LINE_FENIX_MODIFICATION_LENGTH);
+    public static final String ADD = "added: ";
     public static final String GREETING =
             "Greetings. I am Fenix, your digital assistant.";
     public static final String SERVICE_PROMPT =
             "How may I be of service to you today?";
     public static final String FAREWELL =
             "It has been a pleasure assisting you. Farewell.";
-    public static final Scanner IN = new Scanner(System.in);
+    public static int taskNumber = 0;
+    public static String[] taskArray = new String[100];
+    private final Scanner scanner;
 
-    public static void main(String[] args) {
-        System.out.println(HORIZONTAL_LINE);
+    // Constructor
+    public Fenix() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void greet() {
+        System.out.println(HORIZONTAL_LINE_USER_COMMAND);
         System.out.println(GREETING);
         System.out.println(SERVICE_PROMPT);
-        System.out.println(HORIZONTAL_LINE);
-        String userInput = IN.nextLine();
-        // Exit the loop when the user says bye
-        while (!userInput.equalsIgnoreCase("bye")) {
-            System.out.println(userInput);
-            System.out.println(HORIZONTAL_LINE);
-            userInput = IN.nextLine();
+    }
+
+    public void acceptUserInput() {
+        System.out.println(HORIZONTAL_LINE_USER_COMMAND);
+        String userInput = getUserInput();
+        processUserInput(userInput);
+    }
+
+    private String getUserInput() {
+        return scanner.nextLine();
+    }
+
+    private void processUserInput(String userInput) {
+        switch (userInput) {
+        case "bye":
+            bidFarewell();
+            break;
+        case "list":
+            showAllTasks();
+            acceptUserInput();
+            break;
+        default:
+            storeTask(userInput);
+            acceptUserInput();
+            break;
         }
+    }
+
+    public void bidFarewell() {
         System.out.println(FAREWELL);
-        System.out.println(HORIZONTAL_LINE);
-        IN.close();
+        System.out.println(HORIZONTAL_LINE_USER_COMMAND);
+        scanner.close();
+    }
+
+    public void showAllTasks() {
+        for (int i = 0; i < taskArray.length && taskArray[i] != null; i += 1) {
+            System.out.println("\t" + i + ". " + taskArray[i]);
+        }
+    }
+
+    public void storeTask(String userInput) {
+        taskArray[taskNumber] = userInput;
+        taskNumber += 1;
+        System.out.println(HORIZONTAL_LINE_FENIX_MODIFICATION);
+        System.out.println("\t\t" + ADD + userInput);
+        System.out.println(HORIZONTAL_LINE_FENIX_MODIFICATION);
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Fenix fenix = new Fenix();
+        fenix.greet();
+        fenix.acceptUserInput();
+    }
+}
