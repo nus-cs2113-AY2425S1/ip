e100b8aa3545071f08eba1acd04e7c299fe97d79
wx-03 <tngwenxi@gmail.com>
2024-09-01 16:22:37 +0800

    Refactor code by shifting code from main into sendMessage() method.

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Luke {
 
-    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private static final String HORIZONTAL_LINE =
+            "____________________________________________________________";
 
     private static Task[] tasks = new Task[100];
     private static int size = 0;
@@ -22,58 +23,57 @@ public class Luke {
         System.out.println(HORIZONTAL_LINE);
     }
 
+    private static void sendMessage(String userInput) {
+        if (userInput.equalsIgnoreCase("Bye")) {
+            printReply("Bye. Hope to see you again soon!");
+            System.exit(0);
+        }
+        if (userInput.equalsIgnoreCase("list")) {
+            list();
+            return;
+        }
+        String[] inputArr = userInput.split(" ");
+        if (inputArr[0].equalsIgnoreCase("mark")) {
+            int idx = -1;
+            try {
+                idx = Integer.parseInt(inputArr[1]) - 1;
+            } catch(NumberFormatException e) {
+                printReply("Please input a number");
+            }
+            if (idx < 0 || idx >= size) {
+                printReply("Invalid index");
+            }
+            tasks[idx].setAsDone();
+            printReply(String.format("Marked as done:\n  [X] %s", tasks[idx].getDescription()));
+        } else if (inputArr[0].equalsIgnoreCase("unmark")) {
+            int idx = -1;
+            try {
+                idx = Integer.parseInt(inputArr[1]) - 1;
+            } catch(NumberFormatException e) {
+                printReply("Please input a number");
+            }
+            if (idx < 0 || idx >= size) {
+                printReply("Invalid index");
+            }
+            tasks[idx].setAsUndone();
+            printReply(String.format("Unmarked:\n  [ ] %s", tasks[idx].getDescription()));
+        } else {
+            printReply(String.format("Added task: %s", userInput));
+            tasks[size] = new Task(userInput);
+            size++;
+        }
+
+    }
+
     public static void main(String[] args) {
 
         printReply("Hello! I'm Luke\nWhat can I do for you?");
         Scanner in = new Scanner(System.in);
         String line;
-        boolean exit = false;
 
-        while (!exit) {
+        while (true) {
             line = in.nextLine();
-            if (line.equalsIgnoreCase("Bye")) {
-                printReply("Bye. Hope to see you again soon!");
-                exit = true;
-                continue;
-            }
-            if (line.equalsIgnoreCase("list")) {
-                list();
-                continue;
-            }
-            String[] lineArr = line.split(" ");
-            if (lineArr[0].equalsIgnoreCase("mark")) {
-                int idx;
-                try {
-                    idx = Integer.parseInt(lineArr[1]) - 1;
-                } catch(NumberFormatException e) {
-                    printReply("Please input a number");
-                    continue;
-                }
-                if (idx < 0 || idx >= size) {
-                    printReply("Invalid index");
-                    continue;
-                }
-                tasks[idx].setAsDone();
-                printReply(String.format("Marked as done:\n  [X] %s", tasks[idx].getDescription()));
-            } else if (lineArr[0].equalsIgnoreCase("unmark")) {
-                int idx;
-                try {
-                    idx = Integer.parseInt(lineArr[1]) - 1;
-                } catch(NumberFormatException e) {
-                    printReply("Please input a number");
-                    continue;
-                }
-                if (idx < 0 || idx >= size) {
-                    printReply("Invalid index");
-                    continue;
-                }
-                tasks[idx].setAsUndone();
-                printReply(String.format("Unmarked:\n  [ ] %s", tasks[idx].getDescription()));
-            } else {
-                printReply(String.format("Added task: %s", line));
-                tasks[size] = new Task(line);
-                size++;
-            }
+            sendMessage(line);
         }
     }
 }
