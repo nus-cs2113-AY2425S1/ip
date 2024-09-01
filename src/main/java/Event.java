40e109073a879d3f851d9681ec30ca170c6d1bd9
louisjoety <kolouis002@gmail.com>
2024-09-01 17:22:46 +0800

    Improve Event class by adding constructor and overriding toString method for the marker

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,17 @@
-public class Event extends Task{
+public class Event extends Task {
+    private String start;
+    private String end;
+
+    //Event Constructor inherits Task
+    public Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    //Override toString method to show marker
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+    }
 }
