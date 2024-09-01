755fd3913d0b5b06e8ecb89ec5a79ad372ee26c5
vagrant <vagrant@vagrant.vm>
2024-09-01 05:03:38 +0000

    Fixed commands, added Deadline, Event, ToDo

--- src/main/java/Doot.java
+++ src/main/java/Doot.java
@@ -17,27 +17,94 @@ public class Doot {
         scanner.close();
     }
 
-    public static void findCommand(String command){
+    public static void findCommand(String command) {
         String wordDigit = "\\w+ \\d{1,3}$";
-        int digit = -1;
-        if (command.matches(wordDigit)){
-            digit = Integer.parseInt(command.substring(command.indexOf(" ") + 1));
-            command = command.substring(0, command.indexOf(" "));
+        String wordSlashOnce = "\\w+ / \\w+";
+        String wordSlashTwice = "\\w+ / \\w+ / \\w+";
+
+        if (command.matches(wordDigit)) {
+            handleWordDigit(command);
+        } else if (command.matches(wordSlashTwice)) {
+            handleWordSlashTwice(command);
+        } else if (command.matches(wordSlashOnce)) {
+            handleWordSlashOnce(command);
+        } else {
+            handleDefault(command);
         }
-        switch(command){
-            case ("list"):
+    }
+
+    private static void handleWordDigit(String command) {
+        int digit = Integer.parseInt(command.substring(command.indexOf(" ") + 1));
+        command = command.substring(0, command.indexOf(" "));
+        switch (command) {
+            case "list":
                 printList();
                 break;
-            case ("mark"):
+            case "mark":
                 markTask(digit);
                 break;
-            case ("unmark"):
+            case "unmark":
                 unmarkTask(digit);
                 break;
             default:
                 addToList(command);
                 break;
         }
+    }
+
+    private static void handleWordSlashOnce(String command) {
+        String[] parts = command.split(" / ");
+        String wordOne = parts[0];
+        String wordTwo = parts[1];
+        switch (wordOne) {
+            case "todo":
+                makeToDo(wordOne);
+                break;
+            case "deadline":
+                makeDeadline(wordOne, wordTwo);
+                break;
+            default:
+                addToList(command);
+                break;
+        }
+    }
+
+    private static void handleWordSlashTwice(String command) {
+        String[] parts = command.split(" / ");
+        String wordOne = parts[0];
+        String wordTwo = parts[1];
+        String wordThree = parts[2];
+        switch (wordOne) {
+            case "event":
+                makeEvent(wordOne, wordTwo, wordThree);
+                break;
+            default:
+                addToList(command);
+                break;
+        }
+    }
+
+    private static void handleDefault(String command) {
+        addToList(command);
+    }
+
+    public static void makeDeadline(String description, String by){
+        taskList[taskIdx] = new Deadline(description, by);
+        taskIdx++;
+        System.out.print(DIVIDER + "added: " + taskList[taskIdx-1].getDescription() + "\n" + DIVIDER);
+    }
+
+    public static void makeEvent(String description, String to, String from){
+        taskList[taskIdx] = new Event(description, to, from);
+        taskIdx++;
+        System.out.print(DIVIDER + "added: " + taskList[taskIdx-1].getDescription() + "\n" + DIVIDER);
+
+    }
+
+    public static void makeToDo(String description){
+        taskList[taskIdx] = new ToDo(description);
+        taskIdx++;
+        System.out.print(DIVIDER + "added: " + description + "\n" + DIVIDER);
     }
 
     public static void markTask(int idx){
@@ -61,7 +128,7 @@ public class Doot {
         int curIdx = 1;
         while (curIdx != taskIdx + 1){
             Task curTask = taskList[curIdx-1];
-            System.out.println(curIdx + ". " + curTask.getStatusIcon() + curTask.getDescription());
+            System.out.println(curIdx + ". " + curTask.toString());
             curIdx++;
         }
         System.out.print(DIVIDER);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -23,4 +23,8 @@ public class Task {
     public String getDescription(){
         return description;
     }
+
+    public String toString(){
+        return getStatusIcon() + description;
+    }
 }
