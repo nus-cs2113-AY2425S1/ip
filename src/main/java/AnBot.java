3b8dd2a3f85cbb45849c41cd6605610b4456d6f6
Nguyen Hoang Minh Ngoc <nguyenhoangminhngoc2005@gmail.com>
2024-09-02 23:00:58 +0800

    Update AnBot.java

--- src/main/java/AnBot.java
+++ src/main/java/AnBot.java
@@ -1,10 +1,11 @@
 public class AnBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "AnBot";
+        System.out.println("Hello from " + logo);
+        System.out.println("----");
+        System.out.println("What can I do for you?");
+        System.out.println("---");
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
