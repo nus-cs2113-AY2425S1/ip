b2051a3be43d817fd11552b7ce45590f18627960
shyaamald <dwivedishyaamal@gmail.com>
2024-09-02 15:47:23 +0800

    Add Printer, InputHandler and List classes and add() and list() methods

--- src/main/java/Transcendent.java
+++ src/main/java/Transcendent.java
@@ -4,7 +4,8 @@ public class Transcendent {
 
     public static void main(String[] args) {
         Printer.printEntry();
-        echo();
+        List.init();
+        InputHandler.takeInput();
         Printer.printExit();
     }
 
@@ -29,20 +30,58 @@ public class Transcendent {
             Printer.printSeparator();
         }
 
-    }
-
-    private static void echo() {
-        String line;
-        while (true) {
-            Scanner input = new Scanner(System.in);
-            line = input.nextLine();
-            if (line.equals("bye")) {
-                break;
-            }
+        private static void printAddConfirm(String task) {
             Printer.printSeparator();
-            System.out.println(line);
+            System.out.println("Added: " + task);
             Printer.printSeparator();
         }
+
+    }
+
+    private static class InputHandler {
+
+        private static void takeInput() {
+            String command;
+            while (true) {
+                Scanner input = new Scanner(System.in);
+                command = input.nextLine();
+                if (command.equals("bye")) {
+                    break;
+                }
+                if (command.equals("list")) {
+                    List.list();
+                } else {
+                    List.add(command);
+                }
+            }
+        }
+
+    }
+
+    private static class List {
+
+        private static int listCount = 0;
+
+        private static String[] tasks;
+
+        private static void init() {
+            tasks = new String[100];
+        }
+
+        private static void add(String task) {
+            tasks[listCount] = task;
+            listCount += 1;
+            Printer.printAddConfirm(task);
+        }
+
+        private static void list() {
+            Printer.printSeparator();
+            for (int i = 0; i < listCount; i += 1) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+            Printer.printSeparator();
+        }
+
     }
 
 }
