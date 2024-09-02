af3955d9cc74e0e12d9f2503077dc8a26a82f4ba
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-09-02 20:05:28 +0800

    Amend Tyrone main method to improve readability.
    Main path (handleCommand) was initially indented in an if block
    which checks if the bye command has been entered.
    
    Let's amend the code to reduce the level of indentation of the main path.

--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class Tyrone {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean shouldExit = false;
 
         System.out.println("Hello! I'm Tyrone.\nWhat can I do for you?");
-        while (!shouldExit) {
+
+        while (true) {
             String command = scanner.nextLine();
-            shouldExit = CommandHandler.isExitCommand(command);
-            if (!shouldExit) {
-                CommandHandler.handleCommand(command);
+            if(CommandHandler.isExitCommand(command)) {
+                break;
             }
+            CommandHandler.handleCommand(command);
         }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
