116346832f46722dbdd3e09ab5ca9d3a4e52bdd9
crystal-bys <yinshuangbu@gmail.com>
2024-09-03 00:50:47 +0800

    A-CodeQuality completed. Considered KISS and avoiding magic numbers, but realised that was practised earlier already. Removed unused code and considered making the happy path prominent in main but understood that that was unavoidable.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,10 +11,6 @@ public class Task {
         return (isDone ? "[X]" : "[ ]"); // mark done task with X
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public void markAsDone() {
         this.isDone = true;
     }
