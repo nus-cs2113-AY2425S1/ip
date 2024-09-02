100da0c762524a112af2a4014fa8668ff156846a
yeekian <53704991+yeekian@users.noreply.github.com>
2024-09-02 23:58:10 +0800

    Mel: Refactor line prints

--- src/main/java/Mel.java
+++ src/main/java/Mel.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Mel {
+
+    public static final String DRAW_HORIZONTAL_LINE = "\t________________________________________";
+
     public static void main(String[] args) {
         System.out.println("\t________________________________________");
         System.out.println("\tHello! I'm Mel");
@@ -14,29 +17,28 @@ public class Mel {
         while (true) {
             System.out.print(System.lineSeparator());
             line = in.nextLine();
-            System.out.println("\t________________________________________");
+            System.out.println(DRAW_HORIZONTAL_LINE);
 
             if (line.equals("bye")) {
                 System.out.println("\tBye. Hope to see you again soon!");
-                System.out.println("\t________________________________________");
+                System.out.println(DRAW_HORIZONTAL_LINE);
                 break;
 
             } else if (line.equals("list")) {
                 userList.printList();
-                System.out.println("\t________________________________________");
+                System.out.println(DRAW_HORIZONTAL_LINE);
 
-            } else if (line.substring(0, 4).equals("mark")) {
+            } else if (line.length() >= 6 && line.substring(0, 4).equals("mark")) {
                 userList.markItem(line);
-                System.out.println("\t________________________________________");
+                System.out.println(DRAW_HORIZONTAL_LINE);
 
-            } else if (line.substring(0, 6).equals("unmark")) {
+            } else if (line.length() >= 8 && line.substring(0, 6).equals("unmark")) {
                 userList.unmarkItem(line);
-                System.out.println("\t________________________________________");
+                System.out.println(DRAW_HORIZONTAL_LINE);
 
             } else {
-                System.out.println("\t" + "added: " + line);
-                System.out.println("\t________________________________________");
                 userList.addItem(line);
+                System.out.println(DRAW_HORIZONTAL_LINE);
             }
         }
     }
