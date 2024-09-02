33aedd5f182e251f7f6a232cce7eb98054e18517
cxc0418 <cxc0418@gmail.com>
2024-09-02 21:15:49 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -27,7 +27,7 @@ public class King {
     public static void exit() {
         System.out.println("____________________________________________________________\n");
 
-         if (repeatCount >= 5) {
+        if (repeatCount >= 5) {
             System.out.println(" I am repeating no more!!!\n");
         } else {
             System.out.println(" Bye. Hope to see you again soon!\n");
@@ -48,11 +48,20 @@ public class King {
         } else if (userInput.startsWith("mark")) {
             tasks.get(parseTaskIndex(userInput)).markAsDone();
             chat();
-        }  else if (userInput.startsWith("unmark")) {
+        } else if (userInput.startsWith("unmark")) {
             tasks.get(parseTaskIndex(userInput)).markAsUndone();
             chat();
+        } else if (userInput.startsWith("todo")) {
+            addToDoTask(userInput);
+            chat();
+        } else if (userInput.startsWith("deadline")) {
+            addDeadlineTask(userInput);
+            chat();
+        } else if (userInput.startsWith("event")) {
+            addEventTask(userInput);
+            chat();
         } else {
-            addTask(userInput);
+            addToDoTask(userInput);
             chat();
         }
 
@@ -69,21 +78,97 @@ public class King {
 
         for (int i = 0; i < tasksCount; i++) {
             int indexNum = i + 1;
-            System.out.println(indexNum  + ". " + tasks.get(i));
+            System.out.println(indexNum + ". " + tasks.get(i));
         }
 
         System.out.println("____________________________________________________________");
     }
 
 
-    public static void addTask(String text) {
+    public static void addToDoTask(String text) {
         tasksCount += 1;
-        Task t = new Task(text);
+        Task t = new Task(text, 0);
         tasks.add(t);
+        printAddedTaskDescription(t);
+    }
+
+    public static void addDeadlineTask(String text) {
+        tasksCount += 1;
+        String[] taskSpecifics = text.split(" ");
+        int seperationIndex = 0;
+        String taskContent = "";
+        String taskEndTime = "";
+
+        for (int i = 0; i < taskSpecifics.length; i++) {
+            if (taskSpecifics[i].equals("/by")) {
+                seperationIndex = i;
+            }
+        }
+        for (int i = 1; i < seperationIndex; i++) {
+            taskContent += taskSpecifics[i];
+            if (i < seperationIndex - 1) {
+                taskContent += " ";
+            }
+        }
+        for (int i = seperationIndex + 1; i < taskSpecifics.length; i++) {
+            taskEndTime += taskSpecifics[i];
+            if (i < taskSpecifics.length - 1) {
+                taskEndTime += " ";
+            }
+        }
+
+        Task t = new Task(taskContent, 1, taskEndTime);
+        tasks.add(t);
+        printAddedTaskDescription(t);
+    }
+
+    public static void addEventTask(String text) {
+        tasksCount += 1;
+        String[] taskSpecifics = text.split(" ");
+        int seperationIndexFirst = 0;
+        int seperationIndexSecond = 0;
+        String taskContent = "";
+        String taskStartTime = "";
+        String taskEndTime = "";
+
+        for (int i = 0; i < taskSpecifics.length; i++) {
+            if (taskSpecifics[i].equals("/from")) {
+                seperationIndexFirst = i;
+            } else if (taskSpecifics[i].equals("/to")) {
+                seperationIndexSecond = i;
+            }
+        }
+        for (int i = 1; i < seperationIndexFirst; i++) {
+            taskContent += taskSpecifics[i];
+            if (i < seperationIndexFirst - 1) {
+                taskContent += " ";
+            }
+        }
+        for (int i = seperationIndexFirst + 1; i < seperationIndexSecond; i++) {
+            taskStartTime += taskSpecifics[i];
+            if (i < seperationIndexSecond - 1) {
+                taskStartTime += " ";
+            }
+        }
+        for (int i = seperationIndexSecond + 1; i < taskSpecifics.length; i++) {
+            taskEndTime += taskSpecifics[i];
+            if (i < taskSpecifics.length - 1) {
+                taskEndTime += " ";
+            }
+        }
+
+        Task t = new Task(taskContent, 2, taskStartTime, taskEndTime);
+        tasks.add(t);
+        printAddedTaskDescription(t);
+    }
+
+    public static void printAddedTaskDescription(Task t) {
         System.out.println("____________________________________________________________\n"
-                + "added: " + text + "\n"
-                + "____________________________________________________________"
-        );
+                + "Got it. I've added this task:\n   "
+                + t.getTaskDescription() +
+                "\nNow you have " + tasksCount + " tasks in the list.");
+
+        System.out.println("____________________________________________________________\n");
     }
 
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,21 +1,63 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected int taskType; // 0 for ToDos, 1 for Deadlines, 2 for Events
+    protected String startTime;
+    protected String endTime;
+
+    public Task(String description, int taskType) {
+        this.description = description;
+        this.isDone = false;
+        this.taskType = taskType;
+    }
+
+    public Task(String description, int taskType, String endTime) {
+        this.description = description;
+        this.isDone = false;
+        this.taskType = taskType;
+        this.endTime = endTime;
+    }
+
+    public Task(String description, int taskType, String startTime, String endTime) {
+        this.description = description;
+        this.isDone = false;
+        this.taskType = taskType;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
     public Task(String description) {
         this.description = description;
-        this.isDone = false;
+    }
+
+    public String getTaskTypeIcon() {
+        return switch (taskType) {
+            case 0 -> "[T]";
+            case 1 -> "[D]";
+            case 2 -> "[E]";
+            default -> "[ ]";
+        };
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " ");
+        return (isDone ? "[X] " : "[ ] ");
+    }
+
+    public String getTaskDescription() {
+        if (this.taskType == 1) {
+            return getTaskTypeIcon() + getStatusIcon() + description
+                    + " (by: " + endTime + ")";
+        } else if (this.taskType == 2) {
+            return getTaskTypeIcon() + getStatusIcon() + description
+                    + " (from: " + startTime + " to: " + endTime + ")";
+        }
+        return getTaskTypeIcon() + getStatusIcon() + description;
     }
 
     public void markAsDone() {
         this.isDone = true;
         System.out.println("____________________________________________________________\n"
-                + "Nice! I've marked this task as done:\n"
-                + "[" + this.getStatusIcon() + "] " + this.description);
+                + "Nice! I've marked this task as done:\n" + getTaskDescription());
 
         System.out.println("____________________________________________________________");
     }
@@ -23,14 +65,13 @@ public class Task {
     public void markAsUndone() {
         this.isDone = false;
         System.out.println("____________________________________________________________\n"
-                + "OK, I've marked this task as not done yet:\n"
-                + "[" + this.getStatusIcon() + "] " + this.description);
+                + "OK, I've marked this task as not done yet:\n" + getTaskDescription());
 
         System.out.println("____________________________________________________________");
     }
 
     @Override
     public String toString() {
-        return "[" + getStatusIcon() + "] " + description;
+        return getTaskDescription();
     }
 }
