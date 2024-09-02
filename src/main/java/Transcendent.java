9c3c403725fab4181a38ea7b213c62fdf041e4c7
shyaamald <dwivedishyaamal@gmail.com>
2024-09-03 01:25:13 +0800

    Implement mark/unmark functionalities

--- src/main/java/Transcendent.java
+++ src/main/java/Transcendent.java
@@ -36,6 +36,38 @@ public class Transcendent {
             Printer.printSeparator();
         }
 
+        private static void printMarkConfirm(int marked) {
+            Printer.printSeparator();
+            System.out.println("I have marked the following task.");
+            System.out.println((marked + 1) + "." + List.tasks[marked].getStatusIcon() + " " + List.tasks[marked].description);
+            Printer.printSeparator();
+        }
+
+        private static void printUnmarkConfirm(int unmarked) {
+            Printer.printSeparator();
+            System.out.println("I have unmarked the following task.");
+            System.out.println((unmarked + 1) + "." + List.tasks[unmarked].getStatusIcon() + " " + List.tasks[unmarked].description);
+            Printer.printSeparator();
+        }
+
+        private static void printInvalidCommand() {
+            Printer.printSeparator();
+            System.out.println("This is an invalid instruction.");
+            Printer.printSeparator();
+        }
+
+        private static void printAlreadyMarked() {
+            Printer.printSeparator();
+            System.out.println("This task is already marked.");
+            Printer.printSeparator();
+        }
+
+        private static void printAlreadyUnmarked() {
+            Printer.printSeparator();
+            System.out.println("This task is already unmarked.");
+            Printer.printSeparator();
+        }
+
     }
 
     private static class InputHandler {
@@ -50,6 +82,32 @@ public class Transcendent {
                 }
                 if (command.equals("list")) {
                     List.list();
+                } else if (command.startsWith("mark ")) {
+                    String numberString = command.split(" ")[1];
+                    int indexToBeMarked;
+                    try {
+                        indexToBeMarked = Integer.parseInt(numberString) - 1;
+                        if (indexToBeMarked >= List.listCount) {
+                            Printer.printInvalidCommand();
+                        } else {
+                            List.mark(indexToBeMarked);
+                        }
+                    } catch (NumberFormatException e) {
+                        Printer.printInvalidCommand();
+                    }
+                } else if (command.startsWith("unmark ")) {
+                    String numberString = command.split(" ")[1];
+                    int indexToBeUnmarked;
+                    try {
+                        indexToBeUnmarked = Integer.parseInt(numberString) - 1;
+                        if (indexToBeUnmarked >= List.listCount) {
+                            Printer.printInvalidCommand();
+                        } else {
+                            List.unmark(indexToBeUnmarked);
+                        }
+                    } catch (NumberFormatException e) {
+                        Printer.printInvalidCommand();
+                    }
                 } else {
                     List.add(command);
                 }
@@ -60,26 +118,59 @@ public class Transcendent {
 
     private static class List {
 
-        private static int listCount = 0;
+        private static class Task {
+            protected String description;
+            protected boolean isDone;
 
-        private static String[] tasks;
+            public Task(String description) {
+                this.description = description;
+                this.isDone = false;
+            }
 
-        private static void init() {
-            tasks = new String[100];
+            public String getStatusIcon() {
+                return (isDone ? "[X]" : "[ ]");
+            }
         }
 
-        private static void add(String task) {
-            tasks[listCount] = task;
+        private static int listCount = 0;
+
+        private static Task[] tasks;
+
+        private static void init() {
+            tasks = new Task[100];
+        }
+
+        private static void add(String taskDesc) {
+            Task newTask = new Task(taskDesc);
+            tasks[listCount] = newTask;
             listCount += 1;
-            Printer.printAddConfirm(task);
+            Printer.printAddConfirm(taskDesc);
         }
 
         private static void list() {
             Printer.printSeparator();
             for (int i = 0; i < listCount; i += 1) {
-                System.out.println((i + 1) + ". " + tasks[i]);
+                System.out.println((i + 1) + "." + tasks[i].getStatusIcon() + " " + tasks[i].description);
             }
             Printer.printSeparator();
+        }
+
+        private static void mark(int toBeMarked){
+            if (tasks[toBeMarked].isDone) {
+                Printer.printAlreadyMarked();
+            } else {
+                tasks[toBeMarked].isDone = true;
+                Printer.printMarkConfirm(toBeMarked);
+            }
+        }
+
+        private static void unmark(int toBeUnmarked) {
+            if (!tasks[toBeUnmarked].isDone) {
+                Printer.printAlreadyUnmarked();
+            } else {
+                tasks[toBeUnmarked].isDone = false;
+                Printer.printUnmarkConfirm(toBeUnmarked);
+            }
         }
 
     }
