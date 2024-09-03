285897852cc8f8cb586aff3edb34e8f2b3f1d148
Nguyen Hoang Minh Ngoc <nguyenhoangminhngoc2005@gmail.com>
2024-09-03 17:11:38 +0800

    Update AnBot.java

--- src/main/java/AnBot.java
+++ src/main/java/AnBot.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
+
 public class AnBot {
+
+    private static final String WELCOME_MESSAGE = "\t Hello! Welcome to "; 
+    private static final String ASKING_MESSAGE  = "\t What can I do for you? "; 
+    private static final String EXIT_MESSAGE = "\t Bye. Hope to see you again soon!"; 
+    private static final String SEPARATOR = "\t ___________________________"; 
     public static void main(String[] args) {
         String logo = "AnBot";
-        System.out.println("Hello from " + logo);
-        System.out.println("----");
-        System.out.println("What can I do for you?");
-        System.out.println("---");
-        System.out.println("Bye. Hope to see you again soon!");
 
+        // Print the welcome messages
+        System.out.println(SEPARATOR);
+        System.out.println(WELCOME_MESSAGE + logo);
+        System.out.println(ASKING_MESSAGE);
+        System.out.println(SEPARATOR);
+
+        // Read user input
+        String input;
+        Scanner in = new Scanner(System.in); 
+
+        while (true) {
+            input = in.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(SEPARATOR);
+                System.out.println(EXIT_MESSAGE); 
+                System.out.println(SEPARATOR);
+                break; 
+            }
+
+            else {
+                System.out.println(SEPARATOR);
+                System.out.println("\t" + input); 
+                System.out.println(SEPARATOR);
+            }
+        }
     }
 }
