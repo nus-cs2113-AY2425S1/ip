2b151e1c8e12eea29e121247d640babcdd603089
ChunJia <chiew_chunjia@u.nus.edu>
2024-09-03 11:59:52 +0800

    Appal.java: Improve readability in handleInput()
    Extracted the welcome message, as the code fragment can be grouped
    together.
    
    Using switch() along with constants instead of if-else control flow
    makes code more understandable, for example to understand valid command
    inputs.

--- src/main/java/Appal.java
+++ src/main/java/Appal.java
@@ -1,8 +1,27 @@
 import java.util.Scanner;
 
 public class Appal {
+    // Constants for commands
+    public static final String COMMAND_BYE = "bye";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_TODO = "todo";
+    public static final String COMMAND_DEADLINE = "deadline";
+    public static final String COMMAND_EVENT = "event";
+    public static final String COMMAND_MARK = "mark";
+    public static final String COMMAND_UNMARK = "unmark";
+    public static final int COMMAND_INDEX = 0;
+
+    // Attributes
     private boolean isExited = false;
     private Task[] taskList = new Task[100];
+
+    public void welcomeUser() {
+        String chatbot = "Appal";
+        printSeparator();
+        System.out.println("Heyo! I'm your pal, " + chatbot + "!");
+        System.out.println("Let's get things rolling, what would you like to do today?");
+        printSeparator();
+    }
 
     public void printSeparator() {
         System.out.println("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
@@ -20,6 +39,12 @@ public class Appal {
         printSeparator();
     }
 
+    public void printNotice() {
+        printSeparator();
+        System.out.println("Oops! I don't recognise this command :(");
+        printSeparator();
+    }
+
     public void printOneTask(Task task) {
         System.out.println(task);
     }
@@ -31,7 +56,7 @@ public class Appal {
         taskList[listNumber].setDone(isMark);
         printSeparator();
         if (isMark) {
-            System.out.println("Great job! One more step towards success :)");
+            System.out.println("Task done! One more step towards success :)");
         } else {
             System.out.println("What's next on the agenda? :D");
         }
@@ -50,11 +75,6 @@ public class Appal {
         printSeparator();
     }
 
-    public void addToList(String task) {
-        int totalToDos = Task.getTotalTasks();
-        taskList[totalToDos] = new Task(task);
-    }
-
     public void addToDo(String instruction) {
         int totalToDos = Task.getTotalTasks();
         String task = instruction.replace("todo ", "");
@@ -65,7 +85,7 @@ public class Appal {
         int totalToDos = Task.getTotalTasks();
         String[] words = instruction.split("/");
         String task = words[0].replace("deadline ", "");
-        String by = words[1].replace("by ", "");
+        String by = words[1].replace("by", "").trim();
         taskList[totalToDos] = new Deadline(task, by);
     }
 
@@ -73,46 +93,52 @@ public class Appal {
         int totalToDos = Task.getTotalTasks();
         String[] words = instruction.split("/");
         String task = words[0].replace("event ", "");
-        String from = words[1].replace("from ", "");
-        String to = words[2].replace("to ", "");
+        String from = words[1].replace("from", "");
+        String to = words[2].replace("to", "");
         taskList[totalToDos] = new Event(task, from, to);
     }
 
     public void handleInput() {
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
-        if (line.equals("bye")) {
+        String[] words = line.split(" ");
+        String command = words[COMMAND_INDEX];
+        switch (command) {
+        case COMMAND_BYE:
             isExited = true;
             printBye();
-        } else if (line.equals("list")) {
+            break;
+        case COMMAND_LIST:
             printTaskList();
-        } else if (line.contains("unmark")) {
-            markTask(line, false);
-        } else if (line.contains("mark")) {
-            markTask(line, true);
-        } else if (line.contains("todo")) {
+            break;
+        case COMMAND_TODO:
             addToDo(line);
             printReply(line);
-        } else if (line.contains("deadline")) {
+            break;
+        case COMMAND_DEADLINE:
             addDeadline(line);
             printReply(line);
-        } else if (line.contains("event")) {
+            break;
+        case COMMAND_EVENT:
             addEvent(line);
             printReply(line);
-        } else {
-            addToList(line);
-            printReply(line);
+            break;
+        case COMMAND_MARK:
+            markTask(line, true);
+            break;
+        case COMMAND_UNMARK:
+            markTask(line, false);
+            break;
+        default:
+            printNotice();
+            break;
         }
     }
 
     public void runAppal() {
-        String chatbot = "Appal";
-        printSeparator();
-        System.out.println("Heyo! I'm your pal, " + chatbot + "!");
-        System.out.println("Let's get things rolling, what would you like to do today?");
-        printSeparator();
+        welcomeUser();
         while (!isExited) {
             handleInput();
         }
-   }
+    }
 }
