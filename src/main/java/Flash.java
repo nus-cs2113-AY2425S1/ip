eeda7cda8b7307bb8fed0397ae314276d4afb45b
AdiMangalam <adimangalam2003@gmail.com>
2024-09-03 17:10:11 +0800

    Level-0: Implemented Rename, Greet and Exit

---
+++ src/main/java/Flash.java
@@ -0,0 +1,13 @@
+public class Flash {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Flash");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Here, you can add a scanner to read user input in the future increments
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
