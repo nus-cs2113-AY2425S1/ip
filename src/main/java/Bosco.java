43d697b9a1a6c1a0c01644a7accd035d1107b8fa
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-09-03 14:11:16 +0800

    Add Todo, Deadline and Event subclasses

--- src/main/java/Bosco.java
+++ src/main/java/Bosco.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Bosco {
@@ -21,41 +22,70 @@ public class Bosco {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String userInput;
+        String userInputString;
         String[] words;
         Task selectedTask;
 
         printWelcomeMessage();
 
         while (true) {
-            userInput = in.nextLine();
-            words = userInput.split(" ");
-            if (words[0].equals("bye")) {
+            userInputString = in.nextLine();
+            words = userInputString.split("\\s+", 2);
+            String commandType = words[0];
+            if (commandType.equals("bye")) {
                 break;
             }
+            String commandArgs = (words.length == 2 ? words[1] : words[0]);
+
             System.out.println(DIVIDER);
-            switch (words[0]) {
+            switch (commandType) {
             case "list":
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println("\t " + (i + 1) + "." + tasks[i]);
                 }
                 break;
             case "mark":
-                selectedTask = tasks[Integer.parseInt(words[1]) - 1];
+                selectedTask = tasks[Integer.parseInt(commandArgs) - 1];
                 selectedTask.markAsDone();
                 System.out.println("\t Nice! I've marked this task as done:");
                 System.out.println("\t   " + selectedTask);
                 break;
             case "unmark":
-                selectedTask = tasks[Integer.parseInt(words[1]) - 1];
+                selectedTask = tasks[Integer.parseInt(commandArgs) - 1];
                 selectedTask.markAsNotDone();
                 System.out.println("\t OK, I've marked this task as not done yet:");
                 System.out.println("\t   " + selectedTask);
                 break;
-            default:
-                tasks[taskCount] = new Task(userInput);
+            case "todo":
+                Todo newTodo = new Todo(commandArgs);
+                tasks[taskCount] = newTodo;
                 taskCount++;
-                System.out.println("\t added: " + userInput);
+                System.out.println("\t added: " + newTodo);
+                break;
+            case "deadline":
+                int indexOfByPrefix = commandArgs.indexOf("/by");
+                String description = commandArgs.substring(0, indexOfByPrefix).strip();
+                String by = commandArgs.substring(indexOfByPrefix + 3).strip();
+                Deadline newDeadline = new Deadline(description, by);
+                tasks[taskCount] = newDeadline;
+                taskCount++;
+                System.out.println("\t added: " + newDeadline);
+                break;
+            case "event":
+                int indexOfFromPrefix = commandArgs.indexOf("/from");
+                int indexOfToPrefix = commandArgs.indexOf("/to");
+                description = commandArgs.substring(0, indexOfFromPrefix).strip();
+                String from = commandArgs.substring(indexOfFromPrefix+5, indexOfToPrefix).strip();
+                String to = commandArgs.substring(indexOfToPrefix+3).strip();
+                Event newEvent = new Event(description, from, to);
+                tasks[taskCount] = newEvent;
+                taskCount++;
+                System.out.println("\t added: " + newEvent);
+                break;
+            default:
+                tasks[taskCount] = new Task(userInputString);
+                taskCount++;
+                System.out.println("\t added: " + userInputString);
                 break;
             }
             System.out.println(DIVIDER);
