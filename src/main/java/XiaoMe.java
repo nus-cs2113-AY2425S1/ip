c9e2cfe4fa9c16e7a917e66838a3c0ead0f82ac2
gskang <windstick26@gmail.com>
2024-09-03 22:38:46 +0800

    Add support for tracking 3 types of tasks: ToDos, Deadlines, Events
    ToDos: tasks without any date/time attached to it
    Deadlines: tasks that need to be done before a specific date/time
    Events: tasks that start at a specific date/time and ends at a specific date/time

--- src/main/java/XiaoMe.java
+++ src/main/java/XiaoMe.java
@@ -3,22 +3,42 @@ import java.util.Scanner;
 
 
 public class XiaoMe {
-    static Task[] taskStorage= new Task[100];
-    static int NumOfTasks = 0;
+    static Task[] tasks = new Task[100];
+    static Integer taskCount = 0;
 
-    public static boolean isMark(String line) {
-        // checks if user input is a mark / unmark command
+    public enum Type {
+        MARK,
+        LIST,
+        TODO,
+        EVENT,
+        DEADLINE,
+        TASK,
+        BYE
+    }
+
+    public static Type checkType(String line) {
+        // checks what kind of command was received by XiaoMe
+
+        if (Objects.equals(line, "bye")) {
+            return Type.BYE;
+        } else if (Objects.equals(line, "list")) {
+            return Type.LIST;
+        }
 
         String[] words = line.trim().split(" ");
+        String first = words[0];
 
-        if (words.length != 2) {
-            // input is not 2 words long
-            return false;
+        if (Objects.equals(first, "todo")) {
+            return Type.TODO;
+        } else if (Objects.equals(first, "deadline")) {
+            return Type.DEADLINE;
+        } else if (Objects.equals(first, "event")) {
+            return Type.EVENT;
+        } else if (Objects.equals(first, "mark") || Objects.equals(first, "unmark")) {
+            return Type.MARK;
         }
-        // first word is not "mark" or "unmark"
-        return Objects.equals(words[0], "mark") || Objects.equals(words[0], "unmark");
 
-        //todo check if 2nd word is an integer
+        return Type.TASK;
     }
 
     public static void main(String[] args) {
@@ -26,61 +46,106 @@ public class XiaoMe {
         String line;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("""
-                ____________________________________________________________\s
-                Hello! I'm XiaoMe
-                What can I do for you?
-                ____________________________________________________________
+        System.out.println("""               
+                \t____________________________________________________________\s
+                \tHello! I'm XiaoMe
+                \tWhat can I do for you?
+                \t____________________________________________________________
                 """);
+
         while (true) {
             line = in.nextLine();
+            Type type = checkType(line);
 
-            if (Objects.equals(line, "bye")) {
+            if (type == Type.BYE) {
                 // user input is bye: end programme
                 System.out.println("""
-                        ____________________________________________________________
-                        Bye. Hope to see you again soon!
-                        ____________________________________________________________
-                        
-                        """);
+                        \t____________________________________________________________
+                        \tBye. Hope to see you again soon!
+                        \t____________________________________________________________
+                       \s""");
                 break; // stop programme
-            } else if (Objects.equals(line, "list")) {
+            } else if (type == Type.LIST) {
                 // user input is list: display past tasks
                 System.out.println("""
-                        ____________________________________________________________
-                        Here are the tasks in your list:""");
-                for (int i = 0; i < NumOfTasks; i++) {
-                    System.out.println((i + 1) + ".[" + taskStorage[i].getStatusIcon() + "] " + taskStorage[i].getDescription());
+                        \t____________________________________________________________
+                        \tHere are the tasks in your list:""");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("\t\t" + (i + 1) + "." + tasks[i].toString());
                 }
-                System.out.println("____________________________________________________________\n");
-            } else if (isMark(line)) {
+                System.out.println("\t____________________________________________________________\n");
+
+            } else if (type == Type.MARK) {
                 // user input is mark/unmark x: mark corresponding task as done or undone
                 String[] words = line.split(" ");
                 int taskCount = Integer.parseInt(words[1]) - 1;
+
                 if (Objects.equals(words[0], "mark")) {
-                    taskStorage[taskCount].setDone(true);
+                    tasks[taskCount].setDone(true);
 
-                    System.out.println("____________________________________________________________\n"
-                            + "Nice! I've marked this task as done:\n"
-                            + "\t[" + taskStorage[taskCount].getStatusIcon() + "] " + taskStorage[taskCount].getDescription()
-                            + "\n____________________________________________________________\n");
+                    System.out.println("\t____________________________________________________________\n"
+                            + "\tNice! I've marked this task as done:\n"
+                            + "\t\t" + tasks[taskCount].toString()
+                            + "\n\t____________________________________________________________\n");
                 } else {
-                    taskStorage[taskCount].setDone(false);
+                    tasks[taskCount].setDone(false);
 
-                    System.out.println("____________________________________________________________\n"
-                            + "OK, I've marked this task as not done yet:\n"
-                            + "\t[" + taskStorage[taskCount].getStatusIcon() + "] " + taskStorage[taskCount].getDescription()
-                            + "\n____________________________________________________________\n");
+                    System.out.println("\t____________________________________________________________\n"
+                            + "\tOK, I've marked this task as not done yet:\n"
+                            + "\t\t" + tasks[taskCount].toString()
+                            + "\n\t____________________________________________________________\n");
                 }
+
+            } else if (type == Type.EVENT) {
+
+                // user is creating a new event
+                String[] words = line.split("/");
+
+                tasks[taskCount] = new Event(words[0].replace("event", "").trim(), words[1].replace("from ", "").trim(), words[2].replace("to ", "").trim()); // add task to storage
+                taskCount += 1;
+
+                System.out.println("\t____________________________________________________________\n"
+                        + "\tGot it. I've added this task:\n"
+                        + "\t\t" + tasks[taskCount - 1].toString() + "\n"
+                        + "\tNow you have " + taskCount + " tasks in the list.\n"
+                        + "\t____________________________________________________________\n");
+
+            } else if (type == Type.DEADLINE) {
+
+                // user is creating a new deadline
+                String[] words = line.split("/by");
+
+                tasks[taskCount] = new Deadline(words[0].replace("deadline", "").trim(), words[1].trim()); // add task to storage
+                taskCount += 1;
+
+                System.out.println("\t____________________________________________________________\n"
+                        + "\tGot it. I've added this task:\n"
+                        + "\t\t" + tasks[taskCount - 1].toString() + "\n"
+                        + "\tNow you have " + taskCount + " tasks in the list.\n"
+                        + "\t____________________________________________________________\n");
+
+            } else if (type == Type.TODO) {
+
+                // user is creating a new Todo
+                String string = line.replace("todo ", "");
+                tasks[taskCount] = new Todo(string.trim()); // add task to storage
+                taskCount += 1;
+
+                System.out.println("\t____________________________________________________________\n"
+                        + "\tGot it. I've added this task:\n"
+                        + "\t\t" + tasks[taskCount - 1].toString() + "\n"
+                        + "\tNow you have " + taskCount + " tasks in the list.\n"
+                        + "\t____________________________________________________________\n");
 
             } else {
                 // user is creating a new task
-                System.out.println("____________________________________________________________\n"
-                        + "added: " + line + "\n"
-                        + "____________________________________________________________\n");
+                System.out.println("\t____________________________________________________________\n"
+                        + "\tGot it. I've added this task:\n"
+                        + "\t\t" + line + "\n"
+                        + "\t____________________________________________________________\n");
 
-                taskStorage[NumOfTasks] = new Task(line); // add task to storage
-                NumOfTasks += 1;
+                tasks[taskCount] = new Task(line); // add task to storage
+                taskCount += 1;
             }
 
         }
