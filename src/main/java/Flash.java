3dbc7edc3cf3752cc63888367e6f104132cc65c3
AdiMangalam <adimangalam2003@gmail.com>
2024-09-03 19:30:23 +0800

    Level-1: Implemented Echo

--- src/main/java/Flash.java
+++ src/main/java/Flash.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class Flash {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Flash");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Here, you can add a scanner to read user input in the future increments
-
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        String input;
+        while(true) {
+            input = in.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
