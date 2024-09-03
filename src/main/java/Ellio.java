ca32c8e22d50beabf34d8e5a9e932d452765a5b6
Kenneth <63093454+Chinorea@users.noreply.github.com>
2024-09-04 01:30:22 +0800

    A-CodeQuality
    Improve code quality
    Peform regression testing with code changes

--- src/main/java/Ellio.java
+++ src/main/java/Ellio.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Ellio {
 
-    private static Task[] listTask = new Task[100];
-    private static int numberTask = 0;
     public static final int DEADLINE_INPUT_SPACING = 9;
     public static final int EVENT_INPUT_SPACING = 6;
-
+    public static final int MAX_TASK = 100;
+    private static Task[] listTasks = new Task[MAX_TASK];
+    private static int numberTask = 0;
 
     public static void endProgram(){
         System.out.println(BotText.lineBorder + BotText.messageGoodbye + BotText.lineBorder);
@@ -19,30 +19,29 @@ public class Ellio {
     public static void printList(){
         System.out.print(BotText.lineBorder + BotText.messageList);
         for(int i = 0; i < numberTask; i++){
-            System.out.println((i+1) + "." + listTask[i].getTask());
+            System.out.println((i+1) + "." + listTasks[i].getTask());
         }
         System.out.println(BotText.lineBorder);
-    }
-
-    public static String extractTaskDescriptionFromInput(String line){
-        String[] words = line.split(" ");
-        String resultString = "";
-        for(int i = 1; i < words.length; i++){
-            resultString += words[i] + " ";
-        }
-        return resultString;
     }
 
     public static void addToDo(String line){
         String description = line.replace("todo ", "");
         Todo newTodo = new Todo(description);
-        listTask[numberTask] = newTodo;
+        listTasks[numberTask] = newTodo;
         numberTask++;
         System.out.println(BotText.lineBorder + "Got it. I've added this task:\n  " + newTodo.getTask());
         System.out.println("Now you have " + numberTask + " tasks in the list.\n" + BotText.lineBorder);
     }
 
     public static void addDeadline(String line){
+        Deadline newDeadline = formatDeadline(line);
+        listTasks[numberTask] = newDeadline;
+        numberTask++;
+        System.out.println(BotText.lineBorder + "Got it. I've added this task:\n  " + newDeadline.getTask());
+        System.out.println("Now you have " + numberTask + " tasks in the list.\n" + BotText.lineBorder);
+    }
+
+    private static Deadline formatDeadline(String line) {
         // Find the indices of the first and second occurrences of "/"
         int firstSlashIndex = line.indexOf("/");
 
@@ -53,14 +52,18 @@ public class Ellio {
         //add Semicolon behind by
         deadline = deadline.replace("by", "by:");
 
-        Deadline newDeadline = new Deadline(description, deadline);
-        listTask[numberTask] = newDeadline;
-        numberTask++;
-        System.out.println(BotText.lineBorder + "Got it. I've added this task:\n  " + newDeadline.getTask());
-        System.out.println("Now you have " + numberTask + " tasks in the list.\n" + BotText.lineBorder);
+        return new Deadline(description, deadline);
     }
 
     public static void addEvent(String line){
+        Event newEvent = formatEvent(line);
+        listTasks[numberTask] = newEvent;
+        numberTask++;
+        System.out.println(BotText.lineBorder + "Got it. I've added this task:\n  " + newEvent.getTask());
+        System.out.println("Now you have " + numberTask + " tasks in the list.\n" + BotText.lineBorder);
+    }
+    
+    private static Event formatEvent(String line) {
         // Find the indices of the first and second occurrences of "/"
         int firstSlashIndex = line.indexOf("/");
         int secondSlashIndex = line.indexOf("/", firstSlashIndex + 1);
@@ -74,21 +77,17 @@ public class Ellio {
         eventStart = eventStart.replace("from", "from:");
         eventEnd = eventEnd.replace("to", "to:");
 
-        Event newEvent = new Event(description, eventStart, eventEnd);
-        listTask[numberTask] = newEvent;
-        numberTask++;
-        System.out.println(BotText.lineBorder + "Got it. I've added this task:\n  " + newEvent.getTask());
-        System.out.println("Now you have " + numberTask + " tasks in the list.\n" + BotText.lineBorder);
+        return new Event(description, eventStart, eventEnd);
     }
 
     public static void markList(int index){
-        listTask[index-1].markTaskAsDone();
-        System.out.println(BotText.lineBorder + BotText.messageMarked + "  " + listTask[index-1].getTask() + "\n" + BotText.lineBorder);
+        listTasks[index-1].markTaskAsDone();
+        System.out.println(BotText.lineBorder + BotText.messageMarked + "  " + listTasks[index-1].getTask() + "\n" + BotText.lineBorder);
     }
 
     public static void unmarkList(int index){
-        listTask[index-1].unmarkTaskAsDone();
-        System.out.println(BotText.lineBorder + BotText.messageUnmark + "  " + listTask[index-1].getTask() + "\n" + BotText.lineBorder);
+        listTasks[index-1].unmarkTaskAsDone();
+        System.out.println(BotText.lineBorder + BotText.messageUnmark + "  " + listTasks[index-1].getTask() + "\n" + BotText.lineBorder);
     }
 
     public static void getInput(){
@@ -98,7 +97,7 @@ public class Ellio {
 
         while(!input.equals("bye")){
             printInput(input);
-            input = in.nextLine().toLowerCase();
+            input = in.nextLine();
         }
     }
 
@@ -107,12 +106,12 @@ public class Ellio {
             printList();
         }
         else if(input.startsWith("mark")){
-            String[] words = input.split(" ");
-            markList(Integer.parseInt(words[1]));
+            String indexNum = getTaskIndex(input);
+            markList(Integer.parseInt(indexNum));
         }
         else if(input.startsWith("unmark")){
-            String[] words = input.split(" ");
-            unmarkList(Integer.parseInt(words[1]));
+            String indexNum = getTaskIndex(input);
+            unmarkList(Integer.parseInt(indexNum));
         }
         else if(input.startsWith("todo")){
             addToDo(input);
@@ -123,6 +122,11 @@ public class Ellio {
         else if(input.startsWith("event")){
             addEvent(input);
         }
+    }
+
+    private static String getTaskIndex(String input) {
+        String[] words = input.split(" ");
+        return words[1];
     }
 
     public static void main(String[] args) {
