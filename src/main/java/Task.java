f4cec72e5433a033a8dac25be21c2ef28623591a
gskang <windstick26@gmail.com>
2024-09-03 15:11:47 +0800

    Create 3 new classes
    Classes Deadline, Event, and Todo inherit from Task class. These classes
     are used to support multiple task types.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,9 @@ public class Task {
     public void setDone(boolean isDone) {
         this.isDone = isDone;
     }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
