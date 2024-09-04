a3690534fa93638b0b5175947983c93f44b91a52
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-09-05 00:52:27 +0800

    Add Level-4 increment: Todo, Event, Deadline
    Modify Atom.java to allow user to specify, store and track 3 types of tasks, namely todo, deadline and event. By specifying the type of task before adding it to the list, the list now displays more important info, like the type of task, the deadline of the task, the timeline of the event, etc. depending on the type of task. In order to achieve this, the contents of the printList() method are also changed. Constants are declared at the top and used whenever applicable so that they make sense, resulting in improved code readability, but most importantly it gets rid of any magic numbers.

--- src/main/java/Atom.java
+++ src/main/java/Atom.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Atom {
+
+    public static final int TODO_START_INDEX = 5;
+    public static final int DEADLINE_START_INDEX = 9;
+    public static final int BY_START_INDEX_OFFSET = 4;
+    public static final int EVENT_START_INDEX = 6;
+    public static final int FROM_START_INDEX_OFFSET = 6;
+    public static final int TO_START_INDEX_OFFSET = 4;
+
     public static void printDivider() {
         System.out.println("__________________________________________________");
     }
@@ -11,7 +19,17 @@ public class Atom {
 
         System.out.println("Here is your list:\n");
         for (Task item : list) {
-            System.out.println(index + "." + "[" + item.getStatus() + "] "  + item.getItem());
+
+            System.out.print(index + "." + "[" + item.setTaskType() + "]" +
+                    "[" + item.getStatus() + "] " + item.getItem());
+
+            if (item.setTaskType().equals("D")) {
+                System.out.print("(by: " + item.getBy() + ")");
+            } else if (item.setTaskType().equals("E")) {
+                System.out.print("(from: " + item.getFrom() + "to: " + item.getTo() + ")");
+            }
+
+            System.out.println();
             index++;
         }
     }
@@ -37,6 +55,9 @@ public class Atom {
         System.out.println("* \"list\" -> view list of tasks");
         System.out.println("* \"mark <task id>\" -> mark task as DONE");
         System.out.println("* \"unmark <task id>\" -> mark task as UNDONE");
+        System.out.println("* \"todo <task>\" -> set task as TODO");
+        System.out.println("* \"deadline <task> /by <date/time>\" -> set task as DEADLINE");
+        System.out.println("* \"event <task> /from <date/time> /to <date/time>\" -> set task as EVENT");
 
         printDivider();
 
@@ -48,9 +69,10 @@ public class Atom {
 
         line = scanner.nextLine().trim();
         String[] words = line.split(" ");
-        String keyword = words[0];
+        String keyword = words[0].toLowerCase();
 
         while (!line.equalsIgnoreCase("bye")) {
+
             printDivider();
 
             if (line.equalsIgnoreCase("list")) {
@@ -59,9 +81,7 @@ public class Atom {
                 } else {
                     System.out.println("Oh oh! List is empty.");
                 }
-            }
-            else if (keyword.equalsIgnoreCase("mark") ||
-                    (keyword.equalsIgnoreCase("unmark"))) {
+            } else if (keyword.equals("mark") || (keyword.equals("unmark"))) {
                 try {
                     int taskId = Integer.parseInt(words[1]) - 1;
 
@@ -76,14 +96,52 @@ public class Atom {
                             System.out.println("Got it. Task successfully marked as UNDONE!");
                         }
 
-                        System.out.println("[" + currTask.getStatus() + "] " + currTask.getItem());
+                        System.out.println("> [" + currTask.setTaskType() + "]["
+                                + currTask.getStatus() + "] " + currTask.getItem());
                     } else {
                         System.out.println("Whoops! Task id not found.");
                     }
                 } catch (Exception exception) {
-                    System.out.println("Oops! Cannot identify task id.");
-                    System.out.println("Please input valid task id in the correct format.");
+                    System.out.println("Oops! Incorrect command format.");
                 }
+
+            } else if (keyword.equals("todo")) {
+                Todo todo = new Todo(line.substring(TODO_START_INDEX));
+                tasksList[Task.getTaskCount() - 1] = todo;
+
+                System.out.println("Gotcha! TODO task added to list!");
+                System.out.println("> [" + todo.setTaskType() + "]" + "[ ] " + todo.getItem());
+                System.out.println("You now have " + Task.getTaskCount() + " tasks in your list!");
+
+            } else if (keyword.equals("deadline")) {
+                int deadlineEndIndex = line.indexOf('/');
+                String deadlineName = line.substring(DEADLINE_START_INDEX,deadlineEndIndex);
+                String by = line.substring(deadlineEndIndex + BY_START_INDEX_OFFSET);
+
+                Deadline deadline = new Deadline(deadlineName, by);
+                tasksList[Task.getTaskCount() - 1] = deadline;
+
+                System.out.println("Gotcha! DEADLINE task added to list");
+                System.out.println("> [" + deadline.setTaskType() + "]" + "[ ] "
+                        + deadline.getItem() + "(by: " + deadline.getBy() + ")");
+                System.out.println("You now have " + Task.getTaskCount() + " tasks in your list!");
+
+            } else if (keyword.equals("event")) {
+                int eventEndIndex = line.indexOf('/');
+                String eventName = line.substring(EVENT_START_INDEX,eventEndIndex);
+
+                int fromEndIndex = line.lastIndexOf('/');
+                String from = line.substring(eventEndIndex + FROM_START_INDEX_OFFSET, fromEndIndex);
+                String to = line.substring(fromEndIndex + TO_START_INDEX_OFFSET);
+
+                Event event = new Event(eventName, from, to);
+                tasksList[Task.getTaskCount() - 1] = event;
+
+                System.out.println("Gotcha! EVENT task added to list");
+                System.out.println("> [" + event.setTaskType() + "]" + "[ ] " + event.getItem()
+                        + "(from: " + event.getFrom() + "to: " + event.getTo() + ")");
+                System.out.println("You now have " + Task.getTaskCount() + " tasks in your list!");
+
             } else {
                 Task task = new Task(line);
                 tasksList[Task.getTaskCount() - 1] = task;
