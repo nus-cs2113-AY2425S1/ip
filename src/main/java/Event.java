7373ebbec5aa85ddcb0aedd37a5f7ad11bfd0cc3
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-09-05 00:17:04 +0800

    Add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,26 @@
+public class Event extends Task{
+
+    protected String from;
+    protected String to;
+
+    public Event(String item, String from, String to) {
+        super(item);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String getFrom() {
+        return from;
+    }
+
+    @Override
+    public String getTo() {
+        return to;
+    }
+
+    @Override
+    public String setTaskType() {
+        return "E";
+    }
+}
