10b1dd81c6d83a5ae9afc8dc881e801faace6116
LAPTOP-52GQO9KI\zhito <e0725202@u.nus.edu>
2024-09-04 11:27:24 +0800

    added mark unmark feature.

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -3,44 +3,58 @@
 public class Commands {
 
     private static final int MAX_LENGTH = 100;
-    private String[] lines = new String[MAX_LENGTH];
+    private Task[] tasks = new Task[MAX_LENGTH];
 
     public int length = 0;
 
     private boolean isExited = false;
 
-    public Commands(String[] lines, int length) {
-        this.lines = lines;
-        this.length = length;
-    }
     public Commands() {
 
     }
 
     public void accept(String nextLine) {
-        if (nextLine.equals("exit")){
-            this.isExited = true;
-        } else if (nextLine.equals("list")) {
-            this.list();
-        } else {
-            this.lines[length] = nextLine;
+
+        int index = (nextLine.indexOf(' '));
+        String commandKeyword = (index == -1)? nextLine : nextLine.substring(0, index).trim();
+
+        final String commandText = (index == -1)? nextLine : nextLine.substring(index).trim();
+        switch (commandKeyword) {
+        case "exit" -> this.isExited = true;
+        case "list" -> this.list();
+        case "add" -> {
+            this.tasks[length] = new Task(nextLine.substring(index));
             this.echo();
             length++;
+        }
+        case "mark" -> {
+            this.tasks[Integer.parseInt(commandText)].setDone(true);
+            echo(Integer.parseInt(commandText));
+        }
+        case "unmark" -> {
+            this.tasks[Integer.parseInt(commandText)].setDone(false);
+            echo(Integer.parseInt(commandText));
+        }
         }
 
     }
 
+
     private void list() {
         for (int i = 0; i < length; i++) {
-            System.out.println( i +": "+ lines[i]);
+            System.out.println( i +": "+ tasks[i]);
         }
     }
 
     private void echo() {
-        System.out.println("added: " + lines[length]);
+        System.out.println("added: " + tasks[length]);
+    }
+    private void echo( int index) {
+        System.out.println(tasks[index]);
     }
 
     public boolean isAcceptingCommands () {
         return !isExited;
     }
+
 }
