7fa0ea11707ca7f4fc5f963db0b254a500ac1860
lucas-sc0 <e0959193@u.nus.edu>
2024-09-04 19:57:09 +0800

    Add increment automated text ui testing
    Used the input/output redirection technique to semi-automate the testing of Duke.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -4,18 +4,17 @@ REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
-if exist ACTUAL.TXT del ACTUAL.TXT
+del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac -cp C:\Users\Lucas\Desktop\CS2113\ip\src\main\java -Xlint:none -d ..\bin C:\Users\Lucas\Desktop\CS2113\ip\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+REM run the program, feed commands from input.txt file and redirect the output to ACTUAL.TXT
+java -classpath ..\bin Monday < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
