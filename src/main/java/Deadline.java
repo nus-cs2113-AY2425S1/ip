cb8fe3f6e822899e4c8c60b0f9f96aa8806f0ad8
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-09-05 00:16:43 +0800

    Add Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,19 @@
+public class Deadline extends Task{
+
+    String by;
+
+    public Deadline(String item, String by) {
+        super(item);
+        this.by = by;
+    }
+
+    @Override
+    public String getBy() {
+        return by;
+    }
+
+    @Override
+    public String setTaskType() {
+        return "D";
+    }
+}
