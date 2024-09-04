ae4e70e3f15115b0ccb563136999715454bee2d8
3CCLY <cluyang22@hotmail.com>
2024-09-04 09:16:43 +0800

    Add indentation to lines

--- src/main/java/Ran.java
+++ src/main/java/Ran.java
@@ -3,63 +3,65 @@ import java.util.Scanner;
 public class Ran {
     private static boolean isTerminated = false;
     private static int listCount = 0;
+    private static final String LINE = "\t____________________________________________________________";
 
     public static void greet() {
-        System.out.println("____________________________________________________________");
-        String logo = "     ___           ___           ___\n"
-                + "    /\\  \\         /\\  \\         /\\__\\\n"
-                + "   /::\\  \\       /::\\  \\       /::|  |\n"
-                + "  /:/\\:\\  \\     /:/\\:\\  \\     /:|:|  |\n"
-                + " /::\\~\\:\\  \\   /::\\~\\:\\  \\   /:/|:|  |__\n"
-                + "/:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/ |:| /\\__\\\n"
-                + "\\/_|::\\/:/  / \\/__\\:\\/:/  / \\/__|:|/:/  /\n"
-                + "   |:|::/  /       \\::/  /      |:/:/  /\n"
-                + "   |:|\\/__/        /:/  /       |::/  /\n"
-                + "   |:|  |         /:/  /        /:/  /\n"
-                + "    \\|__|         \\/__/         \\/__/\n";
-        System.out.println(logo + "Hello! I'm Ran.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+        String logo = "\t     ___           ___           ___\n"
+                + "\t    /\\  \\         /\\  \\         /\\__\\\n"
+                + "\t   /::\\  \\       /::\\  \\       /::|  |\n"
+                + "\t  /:/\\:\\  \\     /:/\\:\\  \\     /:|:|  |\n"
+                + "\t /::\\~\\:\\  \\   /::\\~\\:\\  \\   /:/|:|  |__\n"
+                + "\t/:/\\:\\ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/ |:| /\\__\\\n"
+                + "\t\\/_|::\\/:/  / \\/__\\:\\/:/  / \\/__|:|/:/  /\n"
+                + "\t   |:|::/  /       \\::/  /      |:/:/  /\n"
+                + "\t   |:|\\/__/        /:/  /       |::/  /\n"
+                + "\t   |:|  |         /:/  /        /:/  /\n"
+                + "\t    \\|__|         \\/__/         \\/__/\n";
+        System.out.println(logo + "\tHello! I'm Ran.");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println(LINE);
     }
 
     public static void bidFarewell() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 
     public static void addTask(String input, Task[] list) {
         list[listCount] = new Task(input);
         listCount++;
-        System.out.println("____________________________________________________________");
-        System.out.println("added: " + input);
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+        System.out.println("\tadded: " + input);
+        System.out.println(LINE);
     }
 
     public static void showList(Task[] list) {
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
         for (int i = 0; i < listCount; i++) {
-            System.out.println((i + 1) + ".[" + list[i].getStatusIcon() + "] " + list[i].getDescription());
+            System.out.println("\t" + (i + 1) + ".[" + list[i].getStatusIcon() 
+                    + "] " + list[i].getDescription());
         }
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
     }
 
     public static void markTask(Task[] list, String taskNum) {
         int taskNumber = Integer.parseInt(taskNum) - 1;
         list[taskNumber].setAsDone();
-        System.out.println("____________________________________________________________");
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  [X] " + list[taskNumber].getDescription());
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t  [X] " + list[taskNumber].getDescription());
+        System.out.println(LINE);
     }
 
     public static void unmarkTask(Task[] list, String taskNum) {
         int taskNumber = Integer.parseInt(taskNum) - 1;
         list[taskNumber].setAsUndone();
-        System.out.println("____________________________________________________________");
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  [ ] " + list[taskNumber].getDescription());
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+        System.out.println("\tOK, I've marked this task as not done yet:");
+        System.out.println("\t  [ ] " + list[taskNumber].getDescription());
+        System.out.println(LINE);
     }
 
     public static void main(String[] args) {
