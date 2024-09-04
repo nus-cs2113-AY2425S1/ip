3545f8f6728b5053faf1fa4162a423f3dfe98aef
AdiMangalam <adimangalam2003@gmail.com>
2024-09-04 09:50:12 +0800

    Level-2: Implemented Add & List

--- src/main/java/Flash.java
+++ src/main/java/Flash.java
@@ -1,8 +1,29 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Flash {
-    public static void main(String[] args) {
 
+    private static final String[] tasks = new String[100];
+    private static int taskCount = 0;
+
+    public static void displayTasks() {
+        System.out.println("____________________________________________________________");
+        for(int i = 0; i < taskCount; i++) {
+            System.out.println(i+1 + "." + tasks[i]);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void addTask(String input) {
+        tasks[taskCount] = input;
+        taskCount++;
+        System.out.println("____________________________________________________________");
+        System.out.println("added: " + input);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("____________________________________________________________");
@@ -18,10 +39,11 @@ public class Flash {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                displayTasks();
+            } else {
+                addTask(input);
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
     }
 }
