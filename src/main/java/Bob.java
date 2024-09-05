81f26d974ea804e5bb95a2c8be07f4127895b235
e0968962@u.nus.edu <e0968962@u.nus.edu>
2024-09-05 12:47:31 +0800

    Test using the input/output redirection technique

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -5,11 +5,11 @@ public class Bob {
     public static final String SEPARATOR = "____________________________________________________________";
 
     public static void main(String[] args) {
-        String logo = "  ____        _     \n"
-                + " | |_) \\ ___ | |___  \n"
-                + " |  _ //  _  \\   _ \\ \n"
+        String logo = "  ____        _\n"
+                + " | |_) \\ ___ | |___\n"
+                + " |  _ //  _  \\   _ \\\n"
                 + " | |_)\\\\ (_) /  |_) |\n"
-                + " |____/ \\___/|_|___/ \n";
+                + " |____/ \\___/|_|___/\n";
         System.out.println(logo);
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
