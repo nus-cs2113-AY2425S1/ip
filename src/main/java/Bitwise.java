d5addc466b1caa19d5f5ecdc0b77e651e74d78bb
unknown <nivedit@comp.nus.edu.sg>
2024-09-06 16:31:56 +0800

    Extract classes and methods

--- src/main/java/Bitwise.java
+++ src/main/java/Bitwise.java
@@ -1,111 +1,96 @@
 import java.util.Scanner;
 
 public class Bitwise {
-    // ASCII art from https://ascii-generator.site/t/
-    private static final String LOGO = " ____   _  _              _            \n"
-            + "|  _ \\ (_)| |            (_)           \n"
-            + "| |_) | _ | |_ __      __ _  ___   ___ \n"
-            + "|  _ < | || __|\\ \\ / /| |/ __| / _ \\ \n"
-            + "| |_) || || |_  \\ V  V / | |\\__ \\|  __/\n"
-            + "|____/ |_| \\__|  \\_/\\_/  |_||___/ \\___|\n"
-            + "                                       \n";
-    private static final String SECTION_BREAK = "==================================================\n";
-    private static final String LINE_BREAK = "--------------------------------------------------\n";
-    private static final String INDENTATION = "        ";
+
     private static Task[] tasksList = new Task[100];
     private static int numberOfTasks = 0;
 
     public static void main(String[] args) {
-        System.out.println(SECTION_BREAK + INDENTATION + "Hello from\n" + LOGO);
-        System.out.print(INDENTATION + "How may I help you today?\n" + LINE_BREAK);
+        OutputManager.printWelcomeMessage();
         mainManager();
-        System.out.println(LINE_BREAK + INDENTATION + "Bye, see you soon!\n" + SECTION_BREAK);
+        OutputManager.printExitMessage();
     }
 
     public static void mainManager() {
         String userInput;
-        while (true) {
-            Scanner in = new Scanner(System.in);
-            userInput = in.nextLine();
-            if (userInput.equalsIgnoreCase("bye")) {
-                return;
-            }
-            System.out.print(LINE_BREAK);
-            if (userInput.equalsIgnoreCase("list")) {
-                printTasksList();
-            }
-            else if (userInput.startsWith("unmark")) {
-                int taskNumber = Integer.parseInt(userInput.substring(userInput.indexOf(" ") + 1));
-                markCompletionStatus(taskNumber, false);
-            }
-            else if (userInput.startsWith("mark")) {
-                int taskNumber = Integer.parseInt(userInput.substring(userInput.indexOf(" ") + 1));
-                markCompletionStatus(taskNumber, true);
-            }
-            else {
-                addToList(userInput);
-            }
-            System.out.print(LINE_BREAK);
+        Status status = Status.RUNNING;
+        while (status != Status.EXIT) {
+            userInput = getUserInput();
+            status = inputHandler(userInput);
         }
+    }
+
+    public static Status inputHandler(String userInput) {
+        if (userInput.equalsIgnoreCase(Constants.COMMAND_END)) {
+            return Status.EXIT;
+        }
+        OutputManager.printLineBreak();
+        if (userInput.equalsIgnoreCase(Constants.COMMAND_LIST)) {
+            OutputManager.printTasksList(tasksList, numberOfTasks);
+        }
+        else if (userInput.startsWith(Constants.COMMAND_UNMARK)) {
+            int taskNumber = Integer.parseInt(userInput.substring(userInput.indexOf(" ") + 1));
+            markCompletionStatus(taskNumber, false);
+        }
+        else if (userInput.startsWith(Constants.COMMAND_MARK)) {
+            int taskNumber = Integer.parseInt(userInput.substring(userInput.indexOf(" ") + 1));
+            markCompletionStatus(taskNumber, true);
+        }
+        else {
+            addToList(userInput);
+        }
+        OutputManager.printLineBreak();
+        return Status.RUNNING;
+    }
+
+    public static String getUserInput() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
     }
 
     public static void echoUserInput() {
         String userInput;
         while (true) {
-            Scanner in = new Scanner(System.in);
-            userInput = in.nextLine();
-            if (userInput.equalsIgnoreCase("bye")) {
+            userInput = getUserInput();
+            if (userInput.equalsIgnoreCase(Constants.COMMAND_END)) {
                 return;
             }
-            System.out.print(LINE_BREAK + INDENTATION + userInput + LINE_BREAK);
+            OutputManager.printEchoInput(userInput);
         }
     }
 
     public static void addToList(String userInput) {
         Task newTask;
         String description;
-        if (userInput.startsWith("todo")) {
+        if (userInput.startsWith(Constants.COMMAND_TODO)) {
             description = userInput.substring(userInput.indexOf(" ") + 1);
             newTask = new Todo(description);
         }
-        else if (userInput.startsWith("deadline")) {
-            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf("/"));
-            String deadline = userInput.substring(userInput.indexOf("/by") + 4);
+        else if (userInput.startsWith(Constants.COMMAND_DEADLINE)) {
+            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf(Constants.COMMAND_INFIX_BY));
+            String deadline = userInput.substring(userInput.indexOf(Constants.COMMAND_INFIX_BY) + 4);
             newTask = new Deadline(description, deadline);
         }
-        else if (userInput.startsWith("event")) {
-            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf("/"));
-            String eventFrom = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to"));
-            String eventTo = userInput.substring(userInput.indexOf("/to") + 4);
+        else if (userInput.startsWith(Constants.COMMAND_EVENT)) {
+            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf(Constants.COMMAND_INFIX_FROM));
+            String eventFrom = userInput.substring(userInput.indexOf(Constants.COMMAND_INFIX_FROM) + 6, userInput.indexOf(Constants.COMMAND_INFIX_TO));
+            String eventTo = userInput.substring(userInput.indexOf(Constants.COMMAND_INFIX_TO) + 4);
             newTask = new Event(description, eventFrom, eventTo);
         }
         else {
             newTask = new Task(userInput);
-            description = userInput;
         }
         tasksList[numberOfTasks] = newTask;
         numberOfTasks++;
-        System.out.println(INDENTATION + "Got it! Added:\n" + INDENTATION + newTask.toString());
-        printNumberOfTasks();
-    }
-
-    public static void printTasksList() {
-        System.out.println(INDENTATION + "Here are the tasks in your list: ");
-        for (int i = 0; i < numberOfTasks; i++) {
-            System.out.println(INDENTATION + Integer.toString(i + 1) + ". " + tasksList[i].toString());
-        }
-        printNumberOfTasks();
-    }
-
-    public static void printNumberOfTasks() {
-        System.out.println(INDENTATION + "Now you have " + numberOfTasks + " tasks in the list.");
+        OutputManager.printMessageAddedTask(newTask.toString());
+        OutputManager.printNumberOfTasks(numberOfTasks);
     }
 
     public static void markCompletionStatus(int taskNumber, boolean isCompleted) {
         int taskIndex = taskNumber - 1;
         tasksList[taskIndex].markCompletionStatus(isCompleted);
-        String message = isCompleted ? "Awesome, I've marked this task as completed!" : "I've added the task back in";
-        System.out.println(INDENTATION + message);
-        printTasksList();
+        String message = isCompleted ? Constants.MESSAGE_MARKED : Constants.MESSAGE_UNMARKED;
+        OutputManager.printMessage(message);
+        OutputManager.printTasksList(tasksList, numberOfTasks);
     }
 }
