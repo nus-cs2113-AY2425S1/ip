543ea7491108cf7fa5f378d330a08280fff63643
TPH777 <pinghuitan80@gmail.com>
2024-09-07 00:13:16 +0800

    Accept more than two date parts
    Removed the limit for 3 text parts (or 2 date parts)
    Used loops instead of switch to deal with the change more efficiently

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,25 +11,19 @@ public class Task {
 
     public Task(String details) {
         this.isDone = false;
-		String[] textParts = details.split("/", 3);
+		String[] textParts = details.split("/");
 		this.description = textParts[0];
 
 		int numOfParts = textParts.length;
-		switch (numOfParts) {
-		case 1:
-			this.date = "";
-			break;
-		case 2:
-			this.date = "("
-					+ formatDate(textParts[1])
-					+ ")";
-			break;
-		case 3:
-			this.date = "("
-					+ formatDate(textParts[1])
-					+ formatDate(textParts[2])
-					+ ")";
-		}
+        if (numOfParts == 1) {
+            this.date = "";
+            return;
+        }
+        String date = "(";
+        for (int i = 1; i < numOfParts; i++) {
+            date = date.concat(formatDate(textParts[i]));
+        }
+        this.date = date.concat(")");
     }
 
     public String getStatus() {
