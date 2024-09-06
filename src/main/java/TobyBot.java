3b30728cb0e8ff0db38c08d795c098e57bb5b675
Toby-Yu <e1344677@u.nus.edu>
2024-09-06 16:26:12 +0800

    Add level-5 handling errors in new branches (branches-Level-5)

--- src/main/java/TobyBot.java
+++ src/main/java/TobyBot.java
@@ -16,24 +16,28 @@ public class TobyBot {
             String input = scanner.nextLine();
             System.out.println(LINE);
 
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(LINE);
-                break;
-            } else if (input.equals("list")) {
-                listTasks();
-            } else if (input.startsWith("mark ")) {
-                markTask(input);
-            } else if (input.startsWith("unmark ")) {
-                unmarkTask(input);
-            } else if (input.startsWith("todo ")) {
-                addTodoTask(input.substring(5));
-            } else if (input.startsWith("deadline ")) {
-                addDeadlineTask(input.substring(9));
-            } else if (input.startsWith("event ")) {
-                addEventTask(input.substring(6));
-            } else {
-                System.out.println("Invalid command.");
+            try {
+                if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(LINE);
+                    break;
+                } else if (input.equals("list")) {
+                    listTasks();
+                } else if (input.startsWith("mark ")) {
+                    markTask(input);
+                } else if (input.startsWith("unmark ")) {
+                    unmarkTask(input);
+                } else if (input.startsWith("todo ")) {
+                    addTodoTask(input.substring(5).trim());
+                } else if (input.startsWith("deadline ")) {
+                    addDeadlineTask(input.substring(9).trim());
+                } else if (input.startsWith("event ")) {
+                    addEventTask(input.substring(6).trim());
+                } else {
+                    throw new TobyBotException("I'm sorry. Your input doesn't fit the format of all tasks :-(");
+                }
+            } catch (TobyBotException e) {
+                System.out.println(e.getMessage());
             }
 
             System.out.println(LINE);
@@ -41,13 +45,20 @@ public class TobyBot {
     }
 
     private static void listTasks() {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + "." + tasks.get(i));
+        if (tasks.isEmpty()) {
+            System.out.println("Your task list is empty.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + "." + tasks.get(i));
+            }
         }
     }
 
-    private static void addTodoTask(String description) {
+    private static void addTodoTask(String description) throws TobyBotException {
+        if (description.isEmpty()) {
+            throw new TobyBotException("The description of a todo cannot be empty.");
+        }
         Task task = new Todo(description);
         tasks.add(task);
         System.out.println("Got it. I've added this task:");
@@ -55,8 +66,11 @@ public class TobyBot {
         System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 
-    private static void addDeadlineTask(String description) {
+    private static void addDeadlineTask(String description) throws TobyBotException {
         String[] parts = description.split(" /by ");
+        if (parts.length < 2 || parts[0].isEmpty()) {
+            throw new TobyBotException("The description or deadline of a task cannot be empty.");
+        }
         Task task = new Deadline(parts[0], parts[1]);
         tasks.add(task);
         System.out.println("Got it. I've added this task:");
@@ -64,8 +78,11 @@ public class TobyBot {
         System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 
-    private static void addEventTask(String description) {
+    private static void addEventTask(String description) throws TobyBotException {
         String[] parts = description.split(" /from | /to ");
+        if (parts.length < 3 || parts[0].isEmpty()) {
+            throw new TobyBotException("The description or timing of an event cannot be empty.");
+        }
         Task task = new Event(parts[0], parts[1], parts[2]);
         tasks.add(task);
         System.out.println("Got it. I've added this task:");
@@ -95,6 +112,12 @@ public class TobyBot {
         } catch (IndexOutOfBoundsException | NumberFormatException e) {
             System.out.println("Invalid task number.");
         }
+    }
+}
+
+class TobyBotException extends Exception {
+    public TobyBotException(String message) {
+        super(message);
     }
 }
 
