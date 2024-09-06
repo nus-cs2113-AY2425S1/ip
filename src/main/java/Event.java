1790926efe1d6ad2c37d16376fe03aad85f21535
unknown <nivedit@comp.nus.edu.sg>
2024-09-06 13:46:20 +0800

    Create Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+public class Event extends Task {
+
+    protected String eventFrom;
+    protected String eventTo;
+    protected static final String symbol = "[E]";
+
+    public Event(String description, String eventFrom, String eventTo) {
+        super(description);
+        this.eventFrom = eventFrom;
+        this.eventTo = eventTo;
+    }
+
+    @Override
+    public String toString() {
+        return symbol + super.toString() + "(from: " + eventFrom + " to: " + eventTo + ")";
+    }
+}
