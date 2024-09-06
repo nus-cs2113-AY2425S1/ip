c0f3649f85b1a2b2ec65d1a3a69f2ee179c61ef4
unknown <nivedit@comp.nus.edu.sg>
2024-09-06 13:18:41 +0800

    Create Todo class

---
+++ src/main/java/Todo.java
@@ -0,0 +1,11 @@
+public class Todo extends Task {
+    protected static final String symbol = "[T]";
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return symbol + super.toString();
+    }
+}
