eb57f94a982944ae8d1383587be7f3ff5fc98b1d
TPH777 <pinghuitan80@gmail.com>
2024-09-07 11:06:42 +0800

    Organise into packages
    Split into exceptions, method and task packages for better readability

--- src/main/java/exceptions/IrisExceptions.java
+++ src/main/java/exceptions/IrisExceptions.java
@@ -1,3 +1,5 @@
+package exceptions;
+
 public class IrisExceptions extends Exception {
     public IrisExceptions(String message) {
         super(message);
--- src/main/java/task/type/Deadline.java
+++ src/main/java/task/type/Deadline.java
@@ -1,3 +1,7 @@
+package task.type;
+
+import task.Task;
+
 public class Deadline extends Task {
     public Deadline(String text) {
         super(text);
