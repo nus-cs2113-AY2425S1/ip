c0875679b51fca65a188bc6adb8a50da0691f9b2
KSanjith <kodeeswaransanjith@gmail.com>
2024-09-07 08:23:03 +0800

    Adopt suggestions from PR reviews

--- src/main/java/Doug.java
+++ src/main/java/Doug.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Doug {
 
-    private static final String dashedLine = "______________________________________________\n";
+    private static final String DASHED_LINE = "______________________________________________\n";
     private static int counter = 0;
-    private static Task[] tasks = new Task[100];
 
     // Declaring Magic Literals
+    static final int MAX_TASKS = 100;
+    private static Task[] tasks = new Task[MAX_TASKS];
+
     static final int DEADLINE_NAME_START = 9;
     static final int DEADLINE_DATE_START = 4;
 
@@ -14,51 +16,66 @@ public class Doug {
     static final int EVENT_FROM_START = 6;
     static final int EVENT_TO_START = 4;
 
+
+    public static boolean checkOutOfBounds(int listIndex) {
+        if (listIndex > counter || listIndex <= 0) {
+            System.out.println(DASHED_LINE + "No can do bud, your input is invalid!\n" + DASHED_LINE);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkListFull() {
+        if (counter >= MAX_TASKS) {
+            System.out.println(DASHED_LINE + "Sorry partner, the list is full!" + DASHED_LINE);
+            return true;
+        }
+        return false;
+    }
+
     public static void doList() {
         if (counter == 0) {
-            System.out.println(dashedLine + "Got nothing on your roster bud.");
-            System.out.print(dashedLine);
+            System.out.println(DASHED_LINE + "Got nothing on your roster bud.");
+            System.out.print(DASHED_LINE);
             return;
         }
 
-        System.out.println(dashedLine + "Here, let me lay out your tasks for you.");
+        System.out.println(DASHED_LINE + "Here, let me lay out your tasks for you.");
         for (int i = 0; i < counter; i++) {
             System.out.println((i+1) + "." + tasks[i].toString());
         }
-        System.out.print(dashedLine);
+        System.out.print(DASHED_LINE);
     }
 
     public static void doMark(String command) {
         int listIndex = Integer.parseInt(command.substring(5));
 
-        if (listIndex > counter || listIndex <= 0) {
-            System.out.println(dashedLine + "No can do bud, your input is invalid!\n" + dashedLine);
+        if (checkOutOfBounds(listIndex)) {
             return;
         }
 
         tasks[listIndex - 1].markAsDone();
-        System.out.println(dashedLine + "Sure thing partner, I'll mark it as done");
-        System.out.print(tasks[listIndex - 1].toString() + "\n" + dashedLine);
+        System.out.println(DASHED_LINE + "Sure thing partner, I'll mark it as done");
+        System.out.print(tasks[listIndex - 1].toString() + "\n" + DASHED_LINE);
     }
 
     public static void doUnmark(String command) {
         int listIndex = Integer.parseInt(command.substring(7));
 
-        if (listIndex > counter || listIndex <= 0) {
-            System.out.println(dashedLine + "No can do bud, your input is invalid!\n" + dashedLine);
+        if (checkOutOfBounds(listIndex)) {
             return;
         }
 
         tasks[listIndex - 1].markAsNotDone();
-        System.out.println(dashedLine + "Sure thing partner, I'll mark it as not done");
-        System.out.print(tasks[listIndex - 1].toString() + "\n" + dashedLine);
+        System.out.println(DASHED_LINE + "Sure thing partner, I'll mark it as not done");
+        System.out.print(tasks[listIndex - 1].toString() + "\n" + DASHED_LINE);
     }
 
     public static void doToDo(String command) {
-        Todo t = new Todo(command.substring(5));
-        tasks[counter] = t;
+        Todo todoTask = new Todo(command.substring(5));
+        tasks[counter] = todoTask;
         counter++;
-        System.out.println(dashedLine + "added: " + command + "\n" + dashedLine);
+        System.out.println(DASHED_LINE + "added: " + command + "\n" + DASHED_LINE);
     }
 
     public static void doDeadline(String command) {
@@ -67,10 +84,10 @@ public class Doug {
         String taskName = command.substring(DEADLINE_NAME_START, indexOfSlash - 1);
         String taskDeadline = command.substring(indexOfSlash + DEADLINE_DATE_START);
 
-        Deadline d = new Deadline(taskName, taskDeadline);
-        tasks[counter] = d;
+        Deadline deadlineTask = new Deadline(taskName, taskDeadline);
+        tasks[counter] = deadlineTask;
         counter++;
-        System.out.println(dashedLine + "added: " + taskName + "\n" + dashedLine);
+        System.out.println(DASHED_LINE + "added: " + taskName + "\n" + DASHED_LINE);
     }
 
     public static void doEvent(String command) {
@@ -81,10 +98,10 @@ public class Doug {
         String taskStart = command.substring(indexOfFirstSlash + EVENT_FROM_START, indexOfSecondSlash - 1);
         String taskEnd = command.substring(indexOfSecondSlash + EVENT_TO_START);
 
-        Event e = new Event(taskName, taskStart, taskEnd);
-        tasks[counter] = e;
+        Event eventTask = new Event(taskName, taskStart, taskEnd);
+        tasks[counter] = eventTask;
         counter++;
-        System.out.println(dashedLine + "added: " + taskName + "\n" + dashedLine);
+        System.out.println(DASHED_LINE + "added: " + taskName + "\n" + DASHED_LINE);
     }
 
     public static void sayWelcome() {
@@ -127,14 +144,14 @@ public class Doug {
 
 
         // Welcome statement
-        System.out.println(dashedLine + "Howdy partner! My name is Doug Dimmadome.\n"
-                + "Now what can I do for ya?\n" + dashedLine);
+        System.out.println(DASHED_LINE + "Howdy partner! My name is Doug Dimmadome.\n"
+                + "Now what can I do for ya?\n" + DASHED_LINE);
     }
 
 
     public static void main(String[] args) {
 
-        sayWelcome();
+        //sayWelcome();
         Scanner input = new Scanner(System.in);
 
         while (true) {
@@ -150,31 +167,28 @@ public class Doug {
             } else if (command.startsWith("unmark")) {
                 doUnmark(command);
             } else if (command.startsWith("todo")) {
-                if (counter >= 100) {
-                    System.out.println(dashedLine + "Sorry partner, the list is full!" + dashedLine);
+                if (checkListFull()) {
                     continue;
                 }
                 doToDo(command);
             } else if (command.startsWith("deadline")) {
-                if (counter >= 100) {
-                    System.out.println(dashedLine + "Sorry partner, the list is full!" + dashedLine);
+                if (checkListFull()) {
                     continue;
                 }
                 doDeadline(command);
             } else if (command.startsWith("event")) {
-                if (counter >= 100) {
-                    System.out.println(dashedLine + "Sorry partner, the list is full!\n" + dashedLine);
+                if (checkListFull()) {
                     continue;
                 }
                 doEvent(command);
             } else {
-                System.out.println(dashedLine + "Something seems off partner...\n" + dashedLine);
+                System.out.println(DASHED_LINE + "Something seems off partner...\n" + DASHED_LINE);
             }
         }
 
         // Closing Statement
-        System.out.println(dashedLine + "It's been a pleasure partner.\n" +
-                "Hope to see you around these parts again soon!\n" + dashedLine);
+        System.out.println(DASHED_LINE + "It's been a pleasure partner.\n" +
+                "Hope to see you around these parts again soon!\n" + DASHED_LINE);
 
     }
 }
