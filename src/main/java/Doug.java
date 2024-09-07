9d0c5bf8ee1c7d808112378d74bb885d29db41db
KSanjith <kodeeswaransanjith@gmail.com>
2024-09-07 11:12:48 +0800

    Add DougException class for exception handling Modify hard-coded parts of code to be dynamic

--- src/main/java/Doug.java
+++ src/main/java/Doug.java
@@ -2,35 +2,26 @@ import java.util.Scanner;
 
 public class Doug {
 
+    // Declaring Magic Literals
     private static final String DASHED_LINE = "______________________________________________\n";
+    static final int MAX_TASKS = 100;
+
+    private static Task[] tasks = new Task[MAX_TASKS];
     private static int counter = 0;
 
-    // Declaring Magic Literals
-    static final int MAX_TASKS = 100;
-    private static Task[] tasks = new Task[MAX_TASKS];
-
-    static final int DEADLINE_NAME_START = 9;
-    static final int DEADLINE_DATE_START = 4;
-
-    static final int EVENT_NAME_START = 6;
-    static final int EVENT_FROM_START = 6;
-    static final int EVENT_TO_START = 4;
+    private static boolean saidBye = false;
 
 
-    public static boolean checkOutOfBounds(int listIndex) {
+    public static void checkOutOfBounds(int listIndex) throws DougException {
         if (listIndex > counter || listIndex <= 0) {
-            System.out.println(DASHED_LINE + "No can do bud, your input is invalid!\n" + DASHED_LINE);
-            return true;
+            throw new DougException(DASHED_LINE + "No can do bud, your input is invalid!\n" + DASHED_LINE);
         }
-        return false;
     }
 
-    public static boolean checkListFull() {
+    public static void checkListFull() throws DougException {
         if (counter >= MAX_TASKS) {
-            System.out.println(DASHED_LINE + "Sorry partner, the list is full!" + DASHED_LINE);
-            return true;
+            throw new DougException(DASHED_LINE + "Sorry partner, the list is full!\n" + DASHED_LINE);
         }
-        return false;
     }
 
     public static void doList() {
@@ -50,58 +41,120 @@ public class Doug {
     public static void doMark(String command) {
         int listIndex = Integer.parseInt(command.substring(5));
 
-        if (checkOutOfBounds(listIndex)) {
-            return;
-        }
+        try {
+            checkOutOfBounds(listIndex);
 
-        tasks[listIndex - 1].markAsDone();
-        System.out.println(DASHED_LINE + "Sure thing partner, I'll mark it as done");
-        System.out.print(tasks[listIndex - 1].toString() + "\n" + DASHED_LINE);
+            tasks[listIndex - 1].markAsDone();
+            System.out.println(DASHED_LINE + "Sure thing partner, I'll mark it as done");
+            System.out.print(tasks[listIndex - 1].toString() + "\n" + DASHED_LINE);
+        } catch (DougException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void doUnmark(String command) {
         int listIndex = Integer.parseInt(command.substring(7));
 
-        if (checkOutOfBounds(listIndex)) {
-            return;
+        try {
+            checkOutOfBounds(listIndex);
+
+            tasks[listIndex - 1].markAsNotDone();
+            System.out.println(DASHED_LINE + "Sure thing partner, I'll mark it as not done");
+            System.out.print(tasks[listIndex - 1].toString() + "\n" + DASHED_LINE);
+        } catch (DougException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void doToDo(String command) throws DougException{
+        checkListFull();
+
+        command = command.replace("todo", "").trim();
+        if (command.isEmpty()) {
+            throw new DougException(DASHED_LINE + "It ain't clear to me what you wanna do pal...\n" + DASHED_LINE);
         }
 
-        tasks[listIndex - 1].markAsNotDone();
-        System.out.println(DASHED_LINE + "Sure thing partner, I'll mark it as not done");
-        System.out.print(tasks[listIndex - 1].toString() + "\n" + DASHED_LINE);
-    }
-
-    public static void doToDo(String command) {
-        Todo todoTask = new Todo(command.substring(5));
+        Todo todoTask = new Todo(command);
         tasks[counter] = todoTask;
         counter++;
-        System.out.println(DASHED_LINE + "added: " + command + "\n" + DASHED_LINE);
+
+        System.out.println(DASHED_LINE + "I've added: " + todoTask + " for you.");
+        System.out.println("Your list is now " + counter + " tasks long partner\n" + DASHED_LINE);
     }
 
-    public static void doDeadline(String command) {
-        int indexOfSlash = command.indexOf('/');
+    public static void doDeadline(String command) throws DougException {
+        checkListFull();
 
-        String taskName = command.substring(DEADLINE_NAME_START, indexOfSlash - 1);
-        String taskDeadline = command.substring(indexOfSlash + DEADLINE_DATE_START);
+        command = command.replace("deadline", "").trim();
+        if (command.isEmpty()) {
+            throw new DougException(DASHED_LINE + "It ain't clear to me what you wanna do here pal...\n" + DASHED_LINE);
+        }
+        if (!command.contains("/by")) {
+            throw new DougException(DASHED_LINE + "You're missing the \"/by\" keyword.\n" + DASHED_LINE);
+        }
+
+        int indexOfSlash = command.indexOf("/by");
+        String taskName = command.substring(0, indexOfSlash).trim();
+        if (taskName.isEmpty()) {
+            throw new DougException(DASHED_LINE + "Didn't name your deadline task did you bud?\n" + DASHED_LINE);
+        }
+
+        command = command.replace(taskName, "");
+        String taskDeadline = command.replace("/by", "").trim();
+        if (taskDeadline.isEmpty()) {
+            throw new DougException(DASHED_LINE + "Forgot to mention when it's due by, huh bud?\n" + DASHED_LINE);
+        }
 
         Deadline deadlineTask = new Deadline(taskName, taskDeadline);
         tasks[counter] = deadlineTask;
         counter++;
-        System.out.println(DASHED_LINE + "added: " + taskName + "\n" + DASHED_LINE);
+
+        System.out.println(DASHED_LINE + "I've added: " + deadlineTask + " for you.");
+        System.out.println("Your list is now " + counter + " tasks long partner\n" + DASHED_LINE);
     }
 
-    public static void doEvent(String command) {
-        int indexOfFirstSlash = command.indexOf('/');
-        int indexOfSecondSlash = command.indexOf('/', indexOfFirstSlash + 1);
+    public static void doEvent(String command) throws DougException {
+        checkListFull();
 
-        String taskName = command.substring(EVENT_NAME_START, indexOfFirstSlash - 1);
-        String taskStart = command.substring(indexOfFirstSlash + EVENT_FROM_START, indexOfSecondSlash - 1);
-        String taskEnd = command.substring(indexOfSecondSlash + EVENT_TO_START);
+        command = command.replace("event", "").trim();
+        if (command.isEmpty()) {
+            throw new DougException(DASHED_LINE + "It ain't clear to me what you wanna do here pal...\n" + DASHED_LINE);
+        }
+        if (!command.contains("/from")) {
+            throw new DougException(DASHED_LINE + "You're missing the \"/from\" keyword.\n" + DASHED_LINE);
+        }
+        if (!command.contains("/to")) {
+            throw new DougException(DASHED_LINE + "You're missing the \"/to\" keyword.\n" + DASHED_LINE);
+        }
+
+        int indexOfFirstSlash = command.indexOf("/from");
+        String taskName = command.substring(0, indexOfFirstSlash).trim();
+        if (taskName.isEmpty()) {
+            throw new DougException(DASHED_LINE + "Didn't name your event did you bud?\n" + DASHED_LINE);
+        }
+
+        command = command.replace(taskName, "");
+        command = command.replace("/from", "").trim();
+        int indexOfSecondSlash = command.indexOf("/to");
+        String taskStart = command.substring(0, indexOfSecondSlash).trim();
+        if (taskStart.isEmpty()) {
+            throw new DougException(DASHED_LINE + "Forgot to mention when " + taskName
+                    + " starts, huh bud?\n" + DASHED_LINE);
+        }
+
+        command = command.replace(taskStart, "");
+        String taskEnd = command.replace("/to", "").trim();
+        if (taskEnd.isEmpty()) {
+            throw new DougException(DASHED_LINE + "Forgot to mention when " + taskName
+                    + " ends, huh bud?\n" + DASHED_LINE);
+        }
 
         Event eventTask = new Event(taskName, taskStart, taskEnd);
         tasks[counter] = eventTask;
         counter++;
-        System.out.println(DASHED_LINE + "added: " + taskName + "\n" + DASHED_LINE);
+
+        System.out.println(DASHED_LINE + "I've added: " + eventTask + " for you.");
+        System.out.println("Your list is now " + counter + " tasks long partner\n" + DASHED_LINE);
     }
 
     public static void sayWelcome() {
@@ -148,41 +201,39 @@ public class Doug {
                 + "Now what can I do for ya?\n" + DASHED_LINE);
     }
 
+    public static void readInputs(String command) throws DougException{
+
+        if (command.equals("bye")) {
+            saidBye = true;
+        } else if (command.equals("list")){
+            doList();
+        } else if (command.startsWith("mark")) {
+            doMark(command);
+        } else if (command.startsWith("unmark")) {
+            doUnmark(command);
+        } else if (command.startsWith("todo")) {
+            doToDo(command);
+        } else if (command.startsWith("deadline")) {
+            doDeadline(command);
+        } else if (command.startsWith("event")) {
+            doEvent(command);
+        } else {
+            throw new DougException(DASHED_LINE + "Something seems off partner...\n" + DASHED_LINE);
+        }
+    }
+
 
     public static void main(String[] args) {
 
         //sayWelcome();
         Scanner input = new Scanner(System.in);
 
-        while (true) {
-
-            String command = input.nextLine().trim();
-
-            if (command.equals("bye")) {
-                break;
-            } else if (command.equals("list")){
-                doList();
-            } else if (command.startsWith("mark")) {
-                doMark(command);
-            } else if (command.startsWith("unmark")) {
-                doUnmark(command);
-            } else if (command.startsWith("todo")) {
-                if (checkListFull()) {
-                    continue;
-                }
-                doToDo(command);
-            } else if (command.startsWith("deadline")) {
-                if (checkListFull()) {
-                    continue;
-                }
-                doDeadline(command);
-            } else if (command.startsWith("event")) {
-                if (checkListFull()) {
-                    continue;
-                }
-                doEvent(command);
-            } else {
-                System.out.println(DASHED_LINE + "Something seems off partner...\n" + DASHED_LINE);
+        while (!saidBye) {
+            try {
+                String command = input.nextLine().trim();
+                readInputs(command);
+            } catch (DougException e) {
+                System.out.println(e.getMessage());
             }
         }
 
---
+++ src/main/java/DougException.java
@@ -0,0 +1,5 @@
+public class DougException extends Exception {
+    public DougException(String message) {
+        super(message);
+    }
+}
