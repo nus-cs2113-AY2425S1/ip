66dcb33d1526b73062b3c7d94b0c14032c553482
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-09-07 09:47:28 +0800

    Add tentative method definitions for error handling

--- src/main/java/ErrorHandler.java
+++ src/main/java/ErrorHandler.java
@@ -1,3 +1,8 @@
 public class ErrorHandler {
-    ;
+    public static void checkIfListFull();
+    public static void checkIfTaskOrdinalWithinRange();
+    public static void checkIfListEmpty();
+    public static void checkIfUserInputEmpty();
+    public static void checkIfUserInputArgsEmpty();
+    public static void checkIfUserInputRecognised();
 }
