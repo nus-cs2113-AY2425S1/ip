b2210b3696693f1e6e6374fa79f7a3cad68c3ff7
ZiliaAJY <141621081+ZiliaAJY@users.noreply.github.com>
2024-09-07 19:35:21 +0800

    Add "CongloException.java" file
    -Contains Conglo's custom exception including unknown command, missing description, invalid or missing task number and invalid format

---
+++ src/main/java/CongloException.java
@@ -0,0 +1,54 @@
+public class CongloException extends Exception {
+
+    public CongloException(String message) {
+        super(message);
+    }
+
+    // Custom exception for unknown commands
+    public static class UnknownCommand extends CongloException {
+        public UnknownCommand() {
+            super("Sorry, I'm not smart enough to understand what you mean.");
+        }
+    }
+
+    // Custom exception for missing description
+    public static class MissingDescription extends CongloException {
+        public MissingDescription(String type) {
+            super("Oopsies, please add a description of " + type + ".");
+        }
+    }
+
+    // Custom exception for invalid task number
+    public static class InvalidTaskNumber extends CongloException {
+        public InvalidTaskNumber() {
+            super("Invalid task number! Please try again.");
+        }
+    }
+
+    // Custom exception for missing task number
+    public static class MissingTaskNumber extends CongloException {
+        public MissingTaskNumber(String action) {
+            super("Please provide task number to " + action + ".");
+        }
+    }
+
+    public static class InvalidFormat extends CongloException {
+        public InvalidFormat(String type) {
+            super(generateMessage(type));
+        }
+
+        private static String generateMessage(String type) {
+            switch (type) {
+            case "deadline":
+                return "Oh dear, your deadline command is a bit off."
+                        + System.lineSeparator() + "Please use: deadline [description] /by [deadline].";
+            case "event":
+                return "Oh no! Your event command needs a bit of tweaking."
+                        + System.lineSeparator() + "Try: event [description] /from [start time] /to [end time].";
+            default:
+                return "Oops! Looks like there's a hiccup in your command."
+                        + System.lineSeparator() + "Please follow the correct format.";
+            }
+        }
+    }
+}
