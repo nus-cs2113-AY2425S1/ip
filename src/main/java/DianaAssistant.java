0b915c7c18ccfcc5e82d9b7dbcefdd149353be2c
shaunngoh <shaungoh682@gmail.com>
2024-09-09 14:25:26 +0800

    Add exception handling to Event, Todo and Deadline, Add new DianaException class

--- src/main/java/DianaAssistant.java
+++ src/main/java/DianaAssistant.java
@@ -16,29 +16,31 @@ public class DianaAssistant {
             if ("bye".equals(input)) {
                 break;
             }
-
-            switch(getCommand(input)) {
-            case "list":
-                printList(tasks);
-                break;
-            case "mark":
-                toMark(tasks, input, true);
-                break;
-            case "unmark":
-                toMark(tasks, input, false);
-                break;
-            case "todo":
-                addTodo(input, tasks);
-                break;
-            case "event":
-                addEvent(input, tasks);
-                break;
-            case "deadline":
-                addDeadline(input, tasks);
-                break;
-            default:
-                System.out.println("Unknown command");
-                break;
+            try {
+                switch (processCommand(input)) {
+                    case "list":
+                        printList(tasks);
+                        break;
+                    case "mark":
+                        toMark(tasks, input, true);
+                        break;
+                    case "unmark":
+                        toMark(tasks, input, false);
+                        break;
+                    case "todo":
+                        addTodo(input, tasks);
+                        break;
+                    case "event":
+                        addEvent(input, tasks);
+                        break;
+                    case "deadline":
+                        addDeadline(input, tasks);
+                        break;
+                    default:
+                        throw new DianaException("Unknown command: " + input);
+                }
+            } catch (DianaException e) {
+                System.out.println(e.getMessage());
             }
         }
 
@@ -46,7 +48,7 @@ public class DianaAssistant {
         scanner.close();
     }
 
-    private String getCommand(String input) {
+    private String processCommand(String input) {
         return input.split(" ")[0];
     }
 
@@ -54,13 +56,17 @@ public class DianaAssistant {
         printEnclosure();
         System.out.println("Hello, I am Diana! A personal assistant that helps you keep track of your tasks :)");
         System.out.println("Here are the list of things that Diana can do for you");
+        printListofCommands();
+        printEnclosure();
+    }
+
+    private void printListofCommands() {
         System.out.println("1. Create a todo task eg. [todo read book]");
         System.out.println("2. Create an event task eg. [event read book /from 2pm /to 4pm]");
         System.out.println("3. Create a deadline task eg. [deadline read book /by 2pm]");
         System.out.println("4. Type either mark or unmark to indicate the completion rate of your task");
         System.out.println("5. Type list to view the your list of tasks.");
         System.out.println("6. Type bye to exit the programme");
-        printEnclosure();
     }
 
     private void printEnclosure() {
@@ -112,19 +118,17 @@ public class DianaAssistant {
         printList(tasks);
     }
 
-    private void addTodo (String input, List<Task> tasks) {
-        String checkTodo = input.trim();
-        if (checkTodo.equals("todo")) {
-            System.out.println("todo descriptions cannot be empty");
-            return;
-        }
+    private void addTodo (String input, List<Task> tasks) throws DianaException  {
         String description = input.substring("todo".length()).trim();
+        if (description.isEmpty()) {
+            throw new DianaException("Description of todo task cannot be empty");
+        }
         Todo todo = new Todo(description);
         tasks.add(todo);
         printAddedTask(description, tasks);
     }
 
-    private void addDeadline (String input, List<Task> tasks) {
+    private void addDeadline (String input, List<Task> tasks) throws DianaException {
         if (input.contains("/by")) {
             String[] parts = input.split("/by", 2);
             String description = parts[0].substring("deadline".length()).trim();
@@ -132,16 +136,13 @@ public class DianaAssistant {
             Task deadline = new Deadline(description, by);
             tasks.add(deadline);
             printAddedTask(description, tasks);
-
         } else {
-            System.out.println("Please enter a valid deadline with the correct format:");
-            System.out.println("Eg: Read book /by 5pm");
-            return;
+            throw new DianaException("Format Invalid. Please use the following format: " +
+                    "deadline <description> /by <time>");
         }
-
     }
 
-    private void addEvent (String input, List<Task> tasks) {
+    private void addEvent (String input, List<Task> tasks) throws DianaException {
         if (input.contains("/from") && input.contains("/to")) {
             String[] parts = input.split("/from", 2);
             String description = parts[0].substring("event".length()).trim();
@@ -152,9 +153,8 @@ public class DianaAssistant {
             tasks.add(event);
             printAddedTask(description, tasks);
         } else {
-            System.out.println("Please enter a valid event with the correct format:");
-            System.out.println("Eg: Read book /from 2pm /to 4pm");
-            return;
+            throw new DianaException("Format Invalid. Please use the following format: " +
+                    "event <description> /from <start time> /to <end time>");
         }
     }
 }
---
+++ src/main/java/DianaException.java
@@ -0,0 +1,5 @@
+ class DianaException extends Exception {
+    public DianaException(String message) {
+        super(message);
+    }
+}
