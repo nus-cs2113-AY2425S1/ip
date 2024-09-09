3c59eab72afdcee23a5e3010dfe72ba9a0e258a8
wahkia <gohah021@gmail.com>
2024-09-09 16:21:58 +0800

    Add a new class called LiaException
    Defined a class LiaException to represent exceptions specific to Lia.

---
+++ src/main/java/LiaException.java
@@ -0,0 +1,14 @@
+/**
+ * Represents exceptions specific to the Lia chatbot.
+ */
+public class LiaException extends Exception {
+
+    /**
+     * Creates a new LiaException with a specified error message.
+     *
+     * @param message The error message to be displayed.
+     */
+    public LiaException(String message) {
+        super(message);
+    }
+}
