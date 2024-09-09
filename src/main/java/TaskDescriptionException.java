9132d6f3fe50577bc5439c79edbe66eebcb0604a
TheDinos <marcussvps2002@gmail.com>
2024-09-09 15:20:56 +0800

    Revert "Added Exception Handling for commands"
    This reverts commit 357a0d1244f822598ad23619ca3643b5178137e5.

--- src/main/java/TaskDescriptionException.java
+++
@@ -1,7 +0,0 @@
-public class TaskDescriptionException extends Exception{
-
-    //Lets the user know which task type caused the exception
-    public TaskDescriptionException(String taskType) {
-        super(taskType);
-    }
-}
--- src/main/java/TaskFieldException.java
+++
@@ -1,7 +0,0 @@
-public class TaskFieldException extends Exception {
-
-    //Lets the user know which Task field is missing
-    public TaskFieldException(String missingField) {
-        super(missingField);
-    }
-}
