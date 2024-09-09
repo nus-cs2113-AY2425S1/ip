676e0f03c9daaa18f37e9ac1d4471e9683171c8c
louisjoety <kolouis002@gmail.com>
2024-09-09 17:09:05 +0800

    Add error messages for marking, unmarking and tasking

--- src/main/java/EchoException.java
+++ src/main/java/EchoException.java
@@ -1,5 +1,27 @@
-public class EchoException extends Exception {
-    public EchoException(String message) {
-        super(message);
+public class EchoException {
+    private EchoException() {}
+
+    public static String todoDescriptionMissing() {
+        return "You must provide a description for the todo task.";
+    }
+
+    public static String deadlineDescriptionMissing() {
+        return "You must provide a description for the deadline task.";
+    }
+
+    public static String eventDescriptionMissing() {
+        return "You must provide a description for the event task.";
+    }
+
+    public static String unknownCommand() {
+        return "I'm sorry, but I don't know what that means.";
+    }
+
+    public static String taskNumberOutOfRange() {
+        return "Task is out of range.";
+    }
+
+    public static String invalidTaskNumberFormat() {
+        return "Give an integer only.";
     }
 }
