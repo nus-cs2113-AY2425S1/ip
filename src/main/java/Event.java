8419b123c877fae4ea9a8871c66f1b5d4dd9f173
wahkia <gohah021@gmail.com>
2024-09-09 16:07:34 +0800

    Refactor Task class into separate ToDo, Deadline, and Event subclasses
    This commit restructures the Task class into three specific subclasses.
    This change improves code organization and provides clearer modeling
    of different task types.

---
+++ src/main/java/Event.java
@@ -0,0 +1,31 @@
+/**
+ * Represents an Event task, which is a task that starts and ends
+ * at specific dates/times.
+ */
+class Event extends Task {
+    protected String start;
+    protected String end;
+
+    /**
+     * Creates a new Event task with the given description, start, and end times.
+     *
+     * @param description A description of the event.
+     * @param start The start time of the event.
+     * @param end The end time of the event.
+     */
+    public Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * Returns a string representation of the Event task.
+     *
+     * @return A string representing the Event task.
+     */
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+    }
+}
---
+++ src/main/java/ToDo.java
@@ -0,0 +1,24 @@
+/**
+ * Represents a ToDo task, which is a task without any specific date or time.
+ */
+class ToDo extends Task {
+
+    /**
+     * Creates a new ToDo task with the given description.
+     *
+     * @param description A description of the ToDo task.
+     */
+    public ToDo(String description) {
+        super(description);
+    }
+
+    /**
+     * Returns a string representation of the ToDo task.
+     *
+     * @return A string representing the ToDo task.
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
