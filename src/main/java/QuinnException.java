e9a5a29833d3f4bc3471092ca6fcd1197447cb3b
kaboomzxc <ong.junzheng@u.nus.edu>
2024-09-10 17:03:52 +0800

    Implement Level-5 and review code quality

---
+++ src/main/java/QuinnException.java
@@ -0,0 +1,5 @@
+public class QuinnException extends Exception {
+    public QuinnException(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,97 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Ui {
+    public Ui() {
+    }
+
+    public String readCommand() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Command:");
+        System.out.print("\t");
+        return sc.nextLine().trim();
+    }
+
+    public void displayWelcome() {
+        String logo = "\t" + "  QQQ   U   U III N   N N   N " + System.lineSeparator()
+                + "\t" + " Q   Q  U   U  I  NN  N NN  N " + System.lineSeparator()
+                + "\t" + " Q   Q  U   U  I  N N N N N N " + System.lineSeparator()
+                + "\t" + " Q   Q  U   U  I  N  NN N  NN " + System.lineSeparator()
+                + "\t" + "  QQQ    UUU  III N   N N   N " + System.lineSeparator()
+                + "\t" + "    Q                       " + System.lineSeparator()
+                + "\t" + "     QQ                     " + System.lineSeparator();
+
+        String welcomeMessage = "\t" + "Hello! I'm Quinn, your Personal Assistant ChatBot."
+                + System.lineSeparator()
+                + System.lineSeparator()
+                + logo
+                + System.lineSeparator()
+                + "\t" + "What can I do for you?";
+
+        displayResponse(welcomeMessage);
+        displayLine();
+    }
+
+    public void displayExit() {
+        String exitMessage = "\t" + "Bye. Hope to see you again soon!";
+        displayResponse(exitMessage);
+    }
+
+    public void displayResponse(String message) {
+        displayLine();
+        System.out.println(message);
+    }
+
+    public void displayError(String message) {
+        displayLine();
+        System.out.println("Error Message:");
+        System.out.println("\t" + "('-')? " + message);
+    }
+
+    public void displayLine() {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+    }
+
+    public String taskAddedMessage(Task task) {
+        return "\t" + "Got it. I've added this task:"
+                + System.lineSeparator() + "\t\t" + task;
+    }
+
+    public String taskDoneMessage(Task task) {
+        return "\t" + "Nice! I've marked this task as done:"
+                + System.lineSeparator() + "\t\t" + task;
+    }
+
+    public String taskNotDoneMessage(Task task) {
+        return "\t" + "OK, I've marked this task as not done yet:"
+                + System.lineSeparator() + "\t\t" + task;
+    }
+
+    public String numOfTasksInListMessage(List<Task> tasks) {
+        return "\t" + "Now you have " + tasks.size()
+                + (tasks.size() > 1 ? " tasks" : " task")
+                + " in the list.";
+    }
+
+    public String tasksInListMessage(List<Task> tasks) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\t")
+                .append(tasks.size() > 1 ? "Here are the tasks in your list:" : "Here is the task in your list:")
+                .append(System.lineSeparator())
+                .append("\t")
+                .append("[Legend: T = todo, D = deadline, E = event]")
+                .append(System.lineSeparator());
+
+        for (int i = 0; i < tasks.size(); i++) {
+            if (i != 0) {
+                sb.append(System.lineSeparator());
+            }
+
+            sb.append("\t").append(i + 1).append(". ").append(tasks.get(i));
+        }
+
+        return sb.toString();
+    }
+}
