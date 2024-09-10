15edd7aabee2500278fe2d6deee4d8c29415e6c7
YukeeHong <zhaiyihong1124@gmail.com>
2024-09-10 21:14:18 +0800

    error handling

---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,5 @@
+public class InvalidCommandException extends YukeeException {
+    public InvalidCommandException() {
+        super("OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+}
