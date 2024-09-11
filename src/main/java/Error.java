bccee11dcd06a85ba1c69ca10be4c2cea93c12c2
Ajay Shanker <ajayshanker1998@gmail.com>
2024-09-11 20:46:54 +0800

    Add Error class to handle error messages and code

---
+++ src/main/java/Error.java
@@ -0,0 +1,33 @@
+
+
+// NOTE: Taken from https://stackoverflow.com/questions/446663/best-way-to-define-error-codes-strings-in-java
+public enum Error {
+
+    EMPTY_ARG(0, "Empty argument found."), ILLEGAL_COMMAND(1, "Invalid command found."), INVAILD_COMMAND_FORMAT(2, "Invalid command format found.");
+
+    private final int code;
+    private final String description;
+
+    private Error(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return description + " (Error Code: " + code + ")";
+    }
+}
+
+// public enum ErrorLevel {
+//     SUCCESS, WARN, ERROR, INFO,
+
+// }
