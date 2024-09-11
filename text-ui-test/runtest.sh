e3bec4e3c98920bf4a0abb4fbc19ef51f433864f
danusan-s <141602943+danusan-s@users.noreply.github.com>
2024-09-11 13:16:18 +0800

    Added testing script

---
+++ text-ui-test/runtest.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# create bin directory if it doesn't exist
+if [ ! -d "../bin" ]
+then
+    mkdir ../bin
+fi
+
+# delete output from previous run
+if [ -e "./ACTUAL.txt" ]
+then
+    rm ACTUAL.txt
+fi
+
+# compile the code into the bin folder, terminates if error occurred
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.txt
+java -classpath ../bin Miku < input.txt > ACTUAL.txt
+
+# compare the output to the expected output
+diff ACTUAL.txt EXPECTED.txt
+if [ $? -eq 0 ]
+then
+    echo "Test result: PASSED"
+    exit 0
+else
+    echo "Test result: FAILED"
+    exit 1
+fi
+
