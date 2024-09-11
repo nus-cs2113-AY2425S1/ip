541db925f50816f79990881464bef47d933e53e0
Philip Chang <philip.chang130499@protonmail.com>
2024-09-11 11:59:57 +0800

    plopBot - Level 02: Add, List // Implemented

--- src/main/java/PlopBot.java
+++ src/main/java/PlopBot.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PlopBot {
 
     // Static variables
     private static final String name = "plopBot";
     private static final String HORIZONTAL_LINE = "//" + "\u2500".repeat(50);
-    private static final String ECHO_LINE = "    " + "\u2500".repeat(50);
+    private static final String ECHO_LINE = "    " + "\u2500".repeat(48);
     private static Scanner scanner = new Scanner(System.in);
+
+    private static ArrayList<String> storedInputs;
 
     public static void main(String[] args) {
         /**
@@ -18,6 +21,7 @@ public class PlopBot {
         System.out.println("Hello from\n" + logo);
          */
         welcomeMessage();
+        storedInputs = new ArrayList<>();
         echo();
     }
 
@@ -29,14 +33,15 @@ public class PlopBot {
         System.out.println("Hello! I'm " + name + ".");
         System.out.println("What can I do for you today?");
         System.out.println("");
+    }
+
+    static void farewellMessage() {
         System.out.println(HORIZONTAL_LINE);
-        System.out.println("");
         System.out.println("Thank you for choosing " + name + ". Have a great day!");
-        System.out.println(HORIZONTAL_LINE);
     }
 
     /**
-     * Echoes the users inputs.
+     * Echoes the user's inputs.
      */
     static void echo() {
         String userInput;
@@ -44,13 +49,31 @@ public class PlopBot {
         while (true) {
             userInput = scanner.nextLine().trim();
 
-            if (userInput.equalsIgnoreCase("exit")) {
+            if (userInput.equalsIgnoreCase("bye") || userInput.equalsIgnoreCase("exit")) {
+                farewellMessage();
                 break;
             }
+            if (userInput.equalsIgnoreCase("list")) {
+                list();
+            }
+            else {
+                storedInputs.add(userInput);
+                System.out.println(ECHO_LINE);
+                System.out.println("    Added: " + userInput);
+                System.out.println(ECHO_LINE);
+            }
 
-            System.out.println(ECHO_LINE);
-            System.out.println("    " + userInput);
-            System.out.println(ECHO_LINE);
+        }
+    }
+
+    /**
+     * Lists the user's inputs.
+     */
+    static void list() {
+        System.out.println(ECHO_LINE);
+        System.out.println("    Stored Inputs:\n");
+        for (int i = 0; i < storedInputs.size(); i++) {
+            System.out.println("    " + (i + 1) + ". " + storedInputs.get(i));
         }
     }
 }
