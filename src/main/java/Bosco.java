9081a3bf2a92c9aac746212dd449ed60f506cfbd
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-09-11 10:01:14 +0800

    Refactor command handling into individual methods

--- src/main/java/Bosco.java
+++ src/main/java/Bosco.java
@@ -6,11 +6,14 @@ public class Bosco {
     private static final String MESSAGE_MARK_DONE = "\t Nice! I've marked this task as done:";
     private static final String MESSAGE_MARK_UNDONE = "\t OK, I've marked this task as not done yet:";
     private static final String MESSAGE_ADDED_TASK = "\t Got it. I've added this task:";
+    private static final String DEADLINE_PREFIX_BY = "/by";
+    private static final String EVENT_PREFIX_FROM = "/from";
+    private static final String EVENT_PREFIX_TO = "/to";
 
     private static final int MAX_TASKS = 100;
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    private static final Task[] tasks = new Task[MAX_TASKS];
+    private static final Task[] taskList = new Task[MAX_TASKS];
     private static int taskCount = 0;
 
     private static void printWelcomeMessage() {
@@ -19,6 +22,14 @@ public class Bosco {
 
     private static void printExitMessage() {
         printMessages("\t Bye! Hope to see you again soon!");
+    }
+
+    private static void printMessages(String... messages) {
+        System.out.println(DIVIDER);
+        for (String message : messages) {
+            System.out.println(message);
+        }
+        System.out.println(DIVIDER);
     }
 
     private static String getUserInput() {
@@ -30,21 +41,103 @@ public class Bosco {
         return userInputString;
     }
 
+    private static void executeCommand(String userInputString) {
+        String[] commandTypeAndArgs = splitCommandTypeAndArgs(userInputString);
+        String commandType = commandTypeAndArgs[0];
+        String commandArgs = commandTypeAndArgs[1];
+        switch (commandType) {
+        case "list":
+            executeListTasks();
+            break;
+        case "mark":
+            executeMarkTask(commandArgs);
+            break;
+        case "unmark":
+            executeUnmarkTask(commandArgs);
+            break;
+        case "todo":
+            executeAddTodo(commandArgs);
+            break;
+        case "deadline":
+            executeAddDeadline(commandArgs);
+            break;
+        case "event":
+            executeAddEvent(commandArgs);
+            break;
+        case "bye":
+        case "exit":
+            executeExitProgram();
+        default:
+            taskList[taskCount] = new Task(userInputString);
+            taskCount++;
+            printMessages("\t added: " + userInputString);
+            break;
+        }
+    }
+
     private static String[] splitCommandTypeAndArgs(String userInputString) {
         String[] stringParts = userInputString.split("\\s+", 2);
         return stringParts.length == 2 ? stringParts : new String[] {stringParts[0], ""};
     }
 
-    private static void printMessages(String... messages) {
+    private static String getTaskCountMessage() {
+        return String.format("\t Now you have %1$d tasks in the list.", taskCount);
+    }
+
+    private static void executeListTasks() {
         System.out.println(DIVIDER);
-        for (String message : messages) {
-            System.out.println(message);
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("\t " + (i + 1) + "." + taskList[i]);
         }
         System.out.println(DIVIDER);
     }
 
-    private static String getTaskCountMessage() {
-        return String.format("\t Now you have %1$d tasks in the list.", taskCount);
+    private static void executeMarkTask(String commandArgs) {
+        Task selectedTask = taskList[Integer.parseInt(commandArgs) - 1];
+        selectedTask.markAsDone();
+        printMessages(MESSAGE_MARK_DONE, INDENT_LARGE + selectedTask);
+    }
+
+    private static void executeUnmarkTask(String commandArgs) {
+        Task selectedTask = taskList[Integer.parseInt(commandArgs) - 1];
+        selectedTask.markAsNotDone();
+        printMessages(MESSAGE_MARK_UNDONE, INDENT_LARGE + selectedTask);
+    }
+
+    private static void executeAddTodo(String commandArgs) {
+        addToTaskList(new Todo(commandArgs.strip()));
+    }
+
+    private static void executeAddDeadline(String commandArgs) {
+        int indexOfByPrefix = commandArgs.indexOf(DEADLINE_PREFIX_BY);
+        String description = commandArgs.substring(0, indexOfByPrefix).strip();
+        String by = removePrefix(commandArgs.substring(indexOfByPrefix), DEADLINE_PREFIX_BY).strip();
+        addToTaskList(new Deadline(description, by));
+    }
+
+    private static void executeAddEvent(String commandArgs) {
+        int indexOfFromPrefix = commandArgs.indexOf(EVENT_PREFIX_FROM);
+        int indexOfToPrefix = commandArgs.indexOf(EVENT_PREFIX_TO);
+        String description = commandArgs.substring(0, indexOfFromPrefix).strip();
+        String from = removePrefix(commandArgs.substring(indexOfFromPrefix, indexOfToPrefix),
+                EVENT_PREFIX_FROM).strip();
+        String to = removePrefix(commandArgs.substring(indexOfToPrefix), EVENT_PREFIX_TO).strip();
+        addToTaskList(new Event(description, from, to));
+    }
+
+    private static String removePrefix(String inputStr, String prefix) {
+        return inputStr.replace(prefix, "");
+    }
+
+    private static void addToTaskList(Task newTask) {
+        taskList[taskCount] = newTask;
+        taskCount++;
+        printMessages(MESSAGE_ADDED_TASK, INDENT_LARGE + newTask, getTaskCountMessage());
+    }
+
+    private static void executeExitProgram() {
+        printExitMessage();
+        System.exit(0);
     }
 
     public static void main(String[] args) {
@@ -55,62 +148,7 @@ public class Bosco {
 
         while (true) {
             String userInputString = getUserInput();
-            String[] commandTypeAndArgs = splitCommandTypeAndArgs(userInputString);
-            String commandType = commandTypeAndArgs[0];
-            String commandArgs = commandTypeAndArgs[1];
-            switch (commandType) {
-            case "list":
-                System.out.println(DIVIDER);
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("\t " + (i + 1) + "." + tasks[i]);
-                }
-                System.out.println(DIVIDER);
-                break;
-            case "mark":
-                selectedTask = tasks[Integer.parseInt(commandArgs) - 1];
-                selectedTask.markAsDone();
-                printMessages(MESSAGE_MARK_DONE, INDENT_LARGE + selectedTask);
-                break;
-            case "unmark":
-                selectedTask = tasks[Integer.parseInt(commandArgs) - 1];
-                selectedTask.markAsNotDone();
-                printMessages(MESSAGE_MARK_UNDONE, INDENT_LARGE + selectedTask);
-                break;
-            case "todo":
-                Todo newTodo = new Todo(commandArgs);
-                tasks[taskCount] = newTodo;
-                taskCount++;
-                printMessages(MESSAGE_ADDED_TASK, INDENT_LARGE + newTodo, getTaskCountMessage());
-                break;
-            case "deadline":
-                int indexOfByPrefix = commandArgs.indexOf("/by");
-                description = commandArgs.substring(0, indexOfByPrefix).strip();
-                String by = commandArgs.substring(indexOfByPrefix + 3).strip();
-                Deadline newDeadline = new Deadline(description, by);
-                tasks[taskCount] = newDeadline;
-                taskCount++;
-                printMessages(MESSAGE_ADDED_TASK, INDENT_LARGE + newDeadline, getTaskCountMessage());
-                break;
-            case "event":
-                int indexOfFromPrefix = commandArgs.indexOf("/from");
-                int indexOfToPrefix = commandArgs.indexOf("/to");
-                description = commandArgs.substring(0, indexOfFromPrefix).strip();
-                String from = commandArgs.substring(indexOfFromPrefix+5, indexOfToPrefix).strip();
-                String to = commandArgs.substring(indexOfToPrefix+3).strip();
-                Event newEvent = new Event(description, from, to);
-                tasks[taskCount] = newEvent;
-                taskCount++;
-                printMessages(MESSAGE_ADDED_TASK, INDENT_LARGE + newEvent, getTaskCountMessage());
-                break;
-            case "bye":
-                printExitMessage();
-                System.exit(0);
-            default:
-                tasks[taskCount] = new Task(userInputString);
-                taskCount++;
-                printMessages("\t added: " + userInputString);
-                break;
-            }
+            executeCommand(userInputString);
         }
     }
 }
