648ec47e376ca262ccd2f1164ebc130efe740e5d
Ridiculouswifi <thtan02@gmail.com>
2024-09-11 16:37:30 +0800

    Slight changes suggested by reviewers

--- src/main/java/Freedom.java
+++ src/main/java/Freedom.java
@@ -4,93 +4,99 @@ public class Freedom {
     protected static Task[] storage = new Task[100];
     static int lastIndex = 0;
 
+    static final String LOGO = "\t________________________________________\n";
+
     public static void main(String[] args) {
-        String logo = "\t_________________________________________\n";
-        String message = """
+        final String START_MESSAGE = """
                 \tHello! I'm Freedom
                 \tWhat can I do for you?
                 """;
-        String closing = "\tBye. Hope to see you again soon!\n";
+        final String CLOSING_MESSAGE = "\tBye. Hope to see you again soon!\n";
 
-        System.out.println(logo + message + logo);
-
+        System.out.println(LOGO + START_MESSAGE + LOGO);
         Scanner in = new Scanner(System.in);
+
+        // Super loop for getting inputs
         while (in.hasNextLine()) {
             String line = in.nextLine();
 
             if (line.equals("bye")) {
                 break;
             }
-            boolean isCommand = handleInput(line);
-            if (!isCommand) {
-                lastIndex++;
-            }
+            handleInput(line);
         }
 
-        System.out.println(logo + closing + logo);
+        System.out.println(LOGO + CLOSING_MESSAGE + LOGO);
     }
 
-    public static boolean handleInput(String input) {
+    public static void handleInput(String input) {
+        final int COMMAND_INDEX = 0;
+
         String[] words = input.split(" ");
         String description;
+        int listNumber;
+        String[] components;
 
-        if (input.equals("list")) {
-            printList();
-            return true;
+        switch (words[COMMAND_INDEX]) {
+            case "list":
+                printList();
+                return;
+            case "mark":
+                markTask(words, true);
+                return;
+            case "unmark":
+                markTask(words, false);
+                return;
+            case "todo":
+                description = input.replaceFirst("todo", "");
+                storage[lastIndex] = new ToDo(description);
+                break;
+            case "deadline":
+                description = input.replaceFirst("deadline", "");
+                components = description.split("/");
+                storage[lastIndex] = new Deadline(components[0], components[1]);
+                break;
+            case "event":
+                description = input.replaceFirst("event", "");
+                components = description.split("/");
+                storage[lastIndex] = new Event(components[0], components[1], components[2]);
+                break;
+            default:
+                storage[lastIndex] = new Task(input);
+                break;
         }
-        if (words[0].equals("mark")) {
-            int listNumber = Integer.parseInt(words[1]);
-            storage[listNumber - 1].markDone();
-            markTaskStatement(storage[listNumber - 1], true);
-            return true;
-        }
-        if (words[0].equals("unmark")) {
-            int listNumber = Integer.parseInt(words[1]);
-            storage[listNumber - 1].markUndone();
-            markTaskStatement(storage[listNumber - 1], false);
-            return true;
-        }
-        if (words[0].equals("todo")) {
-            description = input.replaceFirst("todo", "");
-            storage[lastIndex] = new ToDo(description);
-        }
-        else if (words[0].equals("deadline")) {
-            description = input.replaceFirst("deadline", "");
-            String[] members = description.split("/");
-            storage[lastIndex] = new Deadline(members[0], members[1]);
-        }
-        else if (words[0].equals("event")) {
-            description = input.replaceFirst("event", "");
-            String[] members = description.split("/");
-            storage[lastIndex] = new Event(members[0], members[1], members[2]);
-        } else {
-            storage[lastIndex] = new Task(input);
-        }
-        String logo = "\t_________________________________________\n";
-        System.out.println(logo + "\tGot it. I've added this task: ");
+
+        System.out.println(LOGO + "\tGot it. I've added this task: ");
         System.out.println("\t  " + storage[lastIndex].printLine());
-        System.out.println("\tNow you have " + (lastIndex + 1) + " tasks in the list.\n" + logo);
-        return false;
+        System.out.println("\tNow you have " + (lastIndex + 1) + " tasks in the list.\n" + LOGO);
+        lastIndex++;
     }
 
     public static void printList() {
-        String logo = "\t_________________________________________\n";
-        int counter = 0;
-        System.out.print(logo + "\tHere are the tasks in your list:\n");
+        int counter = 1;
+        System.out.print(LOGO + "\tHere are the tasks in your list:\n");
         for (int i = 0; i < lastIndex; i++) {
-            System.out.println("\t" + (counter + 1) + "." + storage[i].printLine());
+            System.out.println("\t" + counter + "." + storage[i].printLine());
             counter++;
         }
-        System.out.println(logo);
+        System.out.println(LOGO);
     }
 
-    public static void markTaskStatement(Task task, boolean isDone) {
-        String logo = "\t_________________________________________\n";
-        String message = isDone ?
-                "\tNice! I've marked this task as done:" :
-                "\tOk, I've marked this task as not done yet:";
-        System.out.println(logo + message);
-        System.out.println("\t  " + task.printLine());
-        System.out.println(logo);
+    public static void markTask(String[] words, boolean isDone) {
+        final int TASK_INDEX = 1;
+        int listNumber = Integer.parseInt(words[TASK_INDEX]);
+        String message;
+
+        if (isDone) {
+            storage[listNumber - 1].markDone();
+            message = "\tNice! I've marked this task as done:";
+        } else {
+            storage[listNumber - 1].markUndone();
+            message = "\tOk, I've marked this task as not done yet:";
+        }
+
+        System.out.println(LOGO + message);
+        System.out.println("\t  " + storage[listNumber - 1].printLine());
+        System.out.println(LOGO);
     }
 }
