fe28a9140849cc3fd6e3fecbe7e6e6d546a9580d
Yisoong <yisoong23@gmail.com>
2024-09-11 08:52:28 +0800

    Add exception handling

--- src/main/java/Ryan.java
+++ src/main/java/Ryan.java
@@ -15,40 +15,43 @@ public class Ryan {
             String[] splitInput = userInput.split(" ", 2);
             String Command = splitInput[0];
             String inputBody = splitInput.length > 1 ? splitInput[1] : "";
+            try {
+                switch (Command) {
+                    case "bye":
+                        exit = true;
+                        Utils.horizontalLine();
+                        break;
 
-            switch (Command) {
-                case "bye":
-                    exit = true;
-                    Utils.horizontalLine();
-                    break;
+                    case "tasks":
+                        printTasks(tasks);
+                        break;
 
-                case "tasks":
-                    printTasks(tasks);
-                    break;
+                    case "todo":
+                        addTodo(tasks, inputBody);
+                        break;
 
-                case "todo":
-                    addTodo(tasks, inputBody);
-                    break;
+                    case "deadline":
+                        addDeadline(tasks, inputBody);
+                        break;
 
-                case "deadline":
-                    addDeadline(tasks, inputBody);
-                    break;
+                    case "event":
+                        addEvent(tasks, inputBody);
+                        break;
 
-                case "event":
-                    addEvent(tasks, inputBody);
-                    break;
+                    case "mark":
+                        handleMark(tasks, inputBody);
+                        break;
 
-                case "mark":
-                    handleMark(tasks, inputBody);
-                    break;
+                    case "unmark":
+                        handleUnmark(tasks, inputBody);
+                        break;
 
-                case "unmark":
-                    handleUnmark(tasks, inputBody);
-                    break;
-
-                default:
-                    addTask(tasks, userInput);
-                    break;
+                    default:
+                        addTask(tasks, userInput);
+                        break;
+                }
+            }catch (RyanException e){
+                handleError(e);
             }
         }
         printGoodbye();
@@ -71,26 +74,26 @@ public class Ryan {
         Utils.horizontalLine();
     }
 
-    private static void handleMark(ArrayList<Task> tasks, String inputBody) {
+    private static void handleMark(ArrayList<Task> tasks, String inputBody) throws RyanException {
         Utils.horizontalLine();
         int index = parseIndex(inputBody);
         if (isValidIndex(index, tasks.size())) {
             tasks.get(index).mark();
             System.out.println("Nice! I've marked this task as done:\n" + tasks.get(index));
         } else {
-            System.out.println("Invalid task number.");
+            throw new RyanException("Error: Invalid task number.");
         }
         Utils.horizontalLine();
     }
 
-    private static void handleUnmark(ArrayList<Task> tasks, String inputBody) {
+    private static void handleUnmark(ArrayList<Task> tasks, String inputBody) throws RyanException {
         Utils.horizontalLine();
         int index = parseIndex(inputBody);
         if (isValidIndex(index, tasks.size())) {
             tasks.get(index).unmark();
             System.out.println("OK, I've marked this task as not done yet:\n" + tasks.get(index));
         } else {
-            System.out.println("Invalid task number.");
+            throw new RyanException("Error: Invalid task number.");
         }
         Utils.horizontalLine();
     }
@@ -111,7 +114,10 @@ public class Ryan {
         Utils.horizontalLine();
     }
 
-    private static void addTodo(ArrayList<Task> tasks, String command) {
+    private static void addTodo(ArrayList<Task> tasks, String command) throws RyanException{
+        if (command.trim().isEmpty()) {
+            throw new RyanException("Error: Todo task description cannot be empty.");
+        }
         Utils.horizontalLine();
         Task task = new Todo(command);
         tasks.add(task);
@@ -121,15 +127,12 @@ public class Ryan {
         Utils.horizontalLine();
     }
 
-    private static void addDeadline(ArrayList<Task> tasks, String command) {
+    private static void addDeadline(ArrayList<Task> tasks, String command) throws RyanException{
         Utils.horizontalLine();
-
         String[] splitCommand = command.split("/by", 2);
 
         if (splitCommand.length < 2) {
-            System.out.println("Error: Deadline tasks should be in the format 'description /by deadline'.");
-            Utils.horizontalLine();
-            return;
+            throw new RyanException("Error: Deadline tasks should be in the format 'description /by deadline'.");
         }
 
         String description = splitCommand[0].trim();
@@ -143,24 +146,20 @@ public class Ryan {
         Utils.horizontalLine();
     }
 
-    private static void addEvent(ArrayList<Task> tasks, String command) {
+    private static void addEvent(ArrayList<Task> tasks, String command) throws RyanException{
         Utils.horizontalLine();
 
         String[] splitFrom = command.split("/from", 2);
 
         if (splitFrom.length < 2) {
-            System.out.println("Error: Event tasks should be in the format 'description /from start-time /to end-time'.");
-            Utils.horizontalLine();
-            return;
+            throw new RyanException("Error: Event tasks should be in the format 'description /from start-time /to end-time'.");
         }
 
         String description = splitFrom[0].trim();
         String[] splitTo = splitFrom[1].split("/to", 2);
 
         if (splitTo.length < 2) {
-            System.out.println("Error: Event tasks should include both start-time and end-time.");
-            Utils.horizontalLine();
-            return;
+            throw new RyanException("Error: Event tasks should include both start-time and end-time.");
         }
 
         String from = splitTo[0].trim();
@@ -171,6 +170,11 @@ public class Ryan {
         System.out.println("Got it. I've added this task:");
         System.out.println("  " + task);
         System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        Utils.horizontalLine();
+    }
+
+    private static void handleError(RyanException e) {
+        System.out.println(e.getMessage());
         Utils.horizontalLine();
     }
 
