3f70446fb94708a242ab604619aaed7923150cb9
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-12 09:01:24 +0800

    Add new packages

---
+++ src/main/java/erika/settings/Settings.java
@@ -0,0 +1,11 @@
+package erika.settings;
+
+public class Settings {
+    public static final int FROM_LENGTH_OFFSET = 6;
+    public static final int TO_LENGTH_OFFSET = 4;
+    public static final int BY_LENGTH_OFFSET = 4;
+    public static final int SPACE_OFFSET = 1;
+    public static final int BY_REAR_OFFSET = 1;
+    public static final int FROM_REAR_OFFSET = 1;
+    public static final int TO_REAR_OFFSET = 1;
+}
