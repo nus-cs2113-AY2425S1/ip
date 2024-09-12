7b2d750a96767682e15add8f09e1636dcf2e8028
DucPhong135 <phongg132005@gmail.com>
2024-09-12 12:53:14 +0800

    "add some comments"

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,11 +1,13 @@
 
 public class DukeException extends Exception {
 
-
+    //Constructor
     public DukeException(String message) {
         super(message);
     }
 
+
+    //Display warning message
     public void displayMessage() {
         System.out.println("Warning: " + getMessage());
     }
