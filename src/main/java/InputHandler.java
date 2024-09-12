b541ecfbe7ce1c5734fa6908efb51d4fbf4e4521
ryan-txn <tanryan2002@gmail.com>
2024-09-13 04:17:47 +0800

    Reapply "Handle exceptions"
    This reverts commit baaa89231597e861b5a7e4d15e2b2d5c8213fda4.

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -1,40 +1,56 @@
 public class InputHandler {
 
     public static void processInputCases(String input) {
-        if (input.equals("list")) {
-            TaskList.listTasks();
-            return;
-        }
+        try {
+            if (input.equals("list")) {
+                TaskList.listTasks();
+                return;
+            }
 
-        String[] words = input.split(" ");
-        int taskIndex;
+            String[] words = input.split(" ");
+            int taskIndex;
 
-        switch (words[0]) {
-        case "mark":
-            taskIndex = Integer.parseInt(words[1]) - 1;
-            TaskList.markTask(taskIndex);
-            break;
-        case "unmark":
-            taskIndex = Integer.parseInt(input.substring(7)) - 1;
-            TaskList.unmarkTask(taskIndex);
-            break;
-        case "todo":
-            TaskList.addTask(new ToDo(false, input.substring(5)));
-            break;
-        case "deadline":
-            String[] deadlineParts = input.substring(9).split("/by", 2);
-            TaskList.addTask(new Deadline(false, deadlineParts[0], deadlineParts[1]));
-            break;
-        case "event":
-            String[] eventParts = input.split("/from", 2);
+            switch (words[0]) {
+            case "mark":
+                taskIndex = Integer.parseInt(words[1]) - 1;
+                TaskList.markTask(taskIndex);
+                break;
+            case "unmark":
+                taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                TaskList.unmarkTask(taskIndex);
+                break;
+            case "todo":
+                if (words.length > 1 && !words[1].isEmpty()) {
+                    TaskList.addTask(new ToDo(false, input.substring(5)));
+                } else {
+                    throw new IllegalArgumentException(
+                            "____________________________________________________________\n" +
+                            "Please provide what should be in the todo.\n" +
+                            "____________________________________________________________"
+                    );
+                }
+                break;
+            case "deadline":
+                String[] deadlineParts = input.substring(9).split("/by", 2);
+                TaskList.addTask(new Deadline(false, deadlineParts[0], deadlineParts[1]));
+                break;
+            case "event":
+                String[] eventParts = input.split("/from", 2);
 
-            // Split the remaining part by /to
-            String[] timeParts = eventParts[1].split("/to", 2);
+                // Split the remaining part by /to
+                String[] timeParts = eventParts[1].split("/to", 2);
 
-            TaskList.addTask(new Event(false, eventParts[0], timeParts[0], timeParts[1]));
-            break;
-        default:
-            TaskList.addTask(new Task(false, input));
+                TaskList.addTask(new Event(false, eventParts[0], timeParts[0], timeParts[1]));
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        "____________________________________________________________\n" +
+                        "Please use a todo, event or deadline. List all tasks with list or exit with bye!\n" +
+                        "____________________________________________________________"
+                );
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // This will print the custom error message.
         }
     }
 }
