ddefb03f77a1da1712ba8cd213689b3f0c3305aa
danusan-s <141602943+danusan-s@users.noreply.github.com>
2024-09-12 11:56:57 +0800

    Fixed input handling

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Miku {
+
+    private static final String[] TODO_REQUIRED_OPTIONS = {"desc"};
+    private static final String[] DEADLINE_REQUIRED_OPTIONS = {"desc","by"};
+    private static final String[] EVENT_REQUIRED_OPTIONS = {"desc","from","to"};
+
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
         TaskList taskList = new TaskList();
@@ -37,9 +43,15 @@ public class Miku {
     }
 
     private static HashMap<String,String> parseOptions(String line) {
+        if (!line.contains("/")){
+            return new HashMap<>();
+        }
         String[] parameters = line.split("/");
         HashMap<String,String> options = new HashMap<>();
         for (String parameter : parameters) {
+            if (parameter.isEmpty()) {
+                continue;
+            }
             String[] parts = splitCommand(parameter);
             options.put(parts[0], parts[1]);
         }
@@ -69,8 +81,7 @@ public class Miku {
             System.out.println("Insufficient arguments or Invalid command");
             return;
         }
-
-        HashMap<String,String> options = parseOptions(parts[1]);
+        HashMap<String, String> options = parseOptions(parts[1]);
 
         switch (parts[0]) {
         case "mark":
@@ -80,22 +91,22 @@ public class Miku {
             taskList.attemptToUnmarkTask(parts[1]);
             break;
         case "todo":
-            if (doesOptionsMapContainNull(options,new String[] {"desc"})) {
-                System.out.println("Please provide required options");
+            if (doesOptionsMapContainNull(options, TODO_REQUIRED_OPTIONS)) {
+                System.out.println("Please provide required options: "+ Arrays.toString(TODO_REQUIRED_OPTIONS));
                 break;
             }
             taskList.addTask(new Todo(options.get("desc")));
             break;
         case "deadline":
-            if (doesOptionsMapContainNull(options,new String[] {"desc","by"})) {
-                System.out.println("Please provide required options");
+            if (doesOptionsMapContainNull(options, DEADLINE_REQUIRED_OPTIONS)) {
+                System.out.println("Please provide required options: "+Arrays.toString(DEADLINE_REQUIRED_OPTIONS));
                 break;
             }
             taskList.addTask(new Deadline(options.get("desc"), options.get("by")));
             break;
         case "event":
-            if (doesOptionsMapContainNull(options,new String[] {"desc","from","to"})) {
-                System.out.println("Please provide required options");
+            if (doesOptionsMapContainNull(options, EVENT_REQUIRED_OPTIONS)) {
+                System.out.println("Please provide required options: "+Arrays.toString(EVENT_REQUIRED_OPTIONS));
                 break;
             }
             taskList.addTask(new Event(options.get("desc"), options.get("from"), options.get("to")));
