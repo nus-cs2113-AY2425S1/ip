5821da51f0374bbe6b0a698c41a27df03d4350d0
jemehgoh <jeremygoheh@gmail.com>
2024-09-13 22:13:11 +0800

    Create new TaskList class

---
+++ src/main/java/nell/TaskList.java
@@ -0,0 +1,27 @@
+package nell;
+
+import nell.tasks.Task;
+
+public class TaskList {
+    private Task[] tasks;
+    private int taskCount;
+    
+    public TaskList() {
+        tasks = new Task[100];
+        taskCount = 0;
+    }
+
+    public int getTaskCount() {
+        return taskCount;
+    }
+
+    public void addTask(Task taskToAdd) {
+        tasks[taskCount] = taskToAdd;
+        taskCount++;
+    }
+
+    public Task getTaskAtIndex(int index) throws IndexOutOfBoundsException,
+            NullPointerException {
+        return tasks[index];
+    }
+}
