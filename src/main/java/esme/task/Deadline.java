aec3206089bcd13c4b79e7322bce33831e023e7f
ehz0ah <haozhe4547@gmail.com>
2024-09-13 23:19:41 +0800

    Add storage functionality to task class

--- src/main/java/esme/task/Deadline.java
+++ src/main/java/esme/task/Deadline.java
@@ -1,11 +1,22 @@
 package esme.task;
 
 public class Deadline extends Task {
+    private String taskType;
     private String by;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.taskType = "deadline";
+    }
+
+    public String getBy() {
+        return this.by;
+    }
+
+    @Override
+    public String getTaskType() {
+        return this.taskType;
     }
 
     @Override
--- src/main/java/esme/task/Event.java
+++ src/main/java/esme/task/Event.java
@@ -3,11 +3,26 @@ package esme.task;
 public class Event extends Task {
     private String from;
     private String to;
+    private String taskType;
 
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
+        this.taskType = "event";
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    @Override
+    public String getTaskType() {
+        return this.taskType;
     }
 
     @Override
