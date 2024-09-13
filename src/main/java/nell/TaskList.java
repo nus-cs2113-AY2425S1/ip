9a8598294b55664fd8580137586a1e621fb5b07c
jemehgoh <jeremygoheh@gmail.com>
2024-09-13 23:12:43 +0800

    Replace tasks array in TaskList with ArrayList

--- src/main/java/nell/TaskList.java
+++ src/main/java/nell/TaskList.java
@@ -2,29 +2,27 @@ package nell;
 
 import nell.tasks.Task;
 
+import java.util.ArrayList;
+
 public class TaskList {
-    private Task[] tasks;
-    private int taskCount;
+    private ArrayList<Task> tasks;
 
     public TaskList() {
-        tasks = new Task[100];
-        taskCount = 0;
+        this.tasks = new ArrayList<Task>();
     }
 
     public int getTaskCount() {
-        return taskCount;
+        return this.tasks.size();
     }
 
     public void addTask(Task taskToAdd) {
         System.out.println("-> The task has been added to the list:");
-        tasks[taskCount] = taskToAdd;
-        taskCount++;
+        tasks.add(taskToAdd);
         System.out.println("   " + taskToAdd);
-        System.out.println(String.format("   The list now has %d tasks", this.taskCount));
+        System.out.println(String.format("   The list now has %d tasks", this.tasks.size()));
     }
 
-    public Task getTaskAtIndex(int index) throws IndexOutOfBoundsException,
-            NullPointerException {
-        return tasks[index];
+    public Task getTaskAtIndex(int index) throws IndexOutOfBoundsException {
+        return tasks.get(index);
     }
 }
