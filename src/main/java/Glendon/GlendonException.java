51eed9b06b05a78f8994a74984a57d46e7f57486
g13nd0n-admin <e0957797@u.nus.edu>
2024-09-13 21:20:12 +0800

    Add package and distributed the class into the packages

--- src/main/java/Glendon/GlendonException.java
+++ src/main/java/Glendon/GlendonException.java
@@ -1,2 +1,4 @@
+package Glendon;
+
 public class GlendonException extends RuntimeException {
 }
--- src/main/java/Glendon/task/Todo.java
+++ src/main/java/Glendon/task/Todo.java
@@ -1,3 +1,5 @@
+package Glendon.task;
+
 public class Todo extends Task {
 
     public Todo(String description) {
