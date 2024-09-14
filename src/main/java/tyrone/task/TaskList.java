8d3f03a67ffff4a4f0a69986950a0a4f4778c509
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-09-14 23:54:04 +0800

    Change TaskList to store tasks using ArrayList instead of array
    TaskList uses a fixed-sized array to store tasks.
    
    This limits the number of tasks we can store in our list and is
    inefficient in terms of memory usage as we cannot dynamically size
    the array.
    
    Let's replace the array of tasks with an ArrayList to store the tasks.
    
    This lets us store and unlimited number of tasks and allows more
    efficient use of memory through dynamic sizing of the ArrayList.

--- src/main/java/tyrone/task/TaskList.java
+++ src/main/java/tyrone/task/TaskList.java
@@ -1,32 +1,33 @@
 package tyrone.task;
 
+import java.util.ArrayList;
+
 public class TaskList {
-    private static Task[] tasks = new Task[100];
-    private static int taskCount = 0;
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void addTask(Task task) {
-        tasks[taskCount++] = task;
+        tasks.add(task);
     }
 
     public static boolean isValidTaskId(int taskId) {
-        return taskId >= 0 && taskId < taskCount;
+        return taskId >= 0 && taskId < tasks.size();
     }
 
     public static void markTaskAsDone (int taskId) {
-        tasks[taskId].markAsDone();
+        tasks.get(taskId).markAsDone();
     }
 
     public static void markTaskAsUndone (int taskId) {
-        tasks[taskId].markAsUndone();
+        tasks.get(taskId).markAsUndone();
     }
 
     public static String getSingleTaskDetails (int taskId) {
-        return tasks[taskId].getNameWithStatus();
+        return tasks.get(taskId).getNameWithStatus();
     }
 
     public static void printList() {
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println((i+1) + ". " + tasks[i].getNameWithStatus());
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i+1) + ". " + tasks.get(i).getNameWithStatus());
         }
     }
 }
