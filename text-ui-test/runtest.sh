caea483917e8acd2998ac6de58ed1a04578feb4b
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-09-14 17:45:11 +0800

    Update runtest to follow the new packaging

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ..\src\main\java\yapper\exceptions\*.java ..\src\main\java\yapper\instructions\*.java ..\src\main\java\yapper\io\*.java ..\src\main\java\yapper\tasks\*.java ..\src\main\java\yapper\*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Yapper < input.txt > ACTUAL.TXT
+java -classpath ../bin yapper.Yapper < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
