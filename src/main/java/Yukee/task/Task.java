fef1ea9db2eb82440d4237739940321c50a52f34
YukeeHong <zhaiyihong1124@gmail.com>
2024-09-16 19:23:22 +0800

    6,7

--- src/main/java/Yukee/task/Task.java
+++ src/main/java/Yukee/task/Task.java
@@ -26,5 +26,9 @@ public class Task {
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
+
+    public boolean isDone() {
+        return isDone;
+    }
 }
 
