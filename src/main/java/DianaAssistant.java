841d7673625751856f8d578f3bd753d93faaa608
shaunngoh <shaungoh682@gmail.com>
2024-09-16 18:51:07 +0800

    Add deleteTask method, more exceptions

--- src/main/java/DianaAssistant.java
+++ src/main/java/DianaAssistant.java
@@ -13,7 +13,8 @@ public class DianaAssistant {
     public void interact() {
         printWelcomeMessage();
         Scanner scanner = new Scanner(System.in);
-        List<Task> tasks = new ArrayList<>();
+
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         String input;
         while (true) {
@@ -23,26 +24,29 @@ public class DianaAssistant {
             }
             try {
                 switch (processCommand(input)) {
-                    case "list":
-                        printList(tasks);
-                        break;
-                    case "mark":
-                        toMark(tasks, input, true);
-                        break;
-                    case "unmark":
-                        toMark(tasks, input, false);
-                        break;
-                    case "todo":
-                        addTodo(input, tasks);
-                        break;
-                    case "event":
-                        addEvent(input, tasks);
-                        break;
-                    case "deadline":
-                        addDeadline(input, tasks);
-                        break;
-                    default:
-                        throw new DianaException("Unknown command: " + input);
+                case "list":
+                    printList(tasks);
+                    break;
+                case "mark":
+                    toMark(tasks, input, true);
+                    break;
+                case "unmark":
+                    toMark(tasks, input, false);
+                    break;
+                case "todo":
+                    addTodo(input, tasks);
+                    break;
+                case "event":
+                    addEvent(input, tasks);
+                    break;
+                case "deadline":
+                    addDeadline(input, tasks);
+                    break;
+                case "delete":
+                    deleteTask(input, tasks);
+                    break;
+                default:
+                    throw new DianaException("Unknown command: " + input);
                 }
             } catch (DianaException e) {
                 System.out.println(e.getMessage());
@@ -70,8 +74,9 @@ public class DianaAssistant {
         System.out.println("2. Create an event task eg. [event read book /from 2pm /to 4pm]");
         System.out.println("3. Create a deadline task eg. [deadline read book /by 2pm]");
         System.out.println("4. Type either mark or unmark to indicate the completion rate of your task");
-        System.out.println("5. Type list to view the your list of tasks.");
-        System.out.println("6. Type bye to exit the programme");
+        System.out.println("5. Delete a task eg. [delete 1]");
+        System.out.println("6. Type list to view the your list of tasks.");
+        System.out.println("7. Type bye to exit the programme");
     }
 
     private void printEnclosure() {
@@ -89,25 +94,30 @@ public class DianaAssistant {
         return;
     }
 
-    private void toMark (List<Task> tasks, String input, boolean shouldMark) {
+    private void toMark (List<Task> tasks, String input, boolean shouldMark) throws DianaException {
         try {
             String substring = input.substring(input.indexOf(" ") + 1);
-            int TaskNum = Integer.parseInt(substring) - 1;
-            if (TaskNum >= 0 && TaskNum < tasks.size()) {
-                Task task = tasks.get(TaskNum);
-                if (shouldMark) {
-                    task.markAsDone();
-                    printEnclosure();
-                    System.out.println("Nice! I've marked this task as done\n" + task.toString());
-                    printEnclosure();
-                } else {
-                    task.markAsNotDone();
-                    printEnclosure();
-                    System.out.println("Okay, I've marked this task as not done yet\n" + task.toString());
-                    printEnclosure();
-                }
+            int taskNum = Integer.parseInt(substring) - 1;
+
+            if (taskNum < 0) {
+                throw new DianaException("Task number cannot be negative");
+            }
+
+            if (taskNum >= tasks.size()) {
+                throw new DianaException("Task number must be less than " + tasks.size());
+            }
+
+            Task task = tasks.get(taskNum);
+            if (shouldMark) {
+                task.markAsDone();
+                printEnclosure();
+                System.out.println("Nice! I've marked this task as done\n" + task.toString());
+                printEnclosure();
             } else {
-                System.out.println("Please enter a number between 1 and " + (tasks.size() - 1));
+                task.markAsNotDone();
+                printEnclosure();
+                System.out.println("Okay, I've marked this task as not done yet\n" + task.toString());
+                printEnclosure();
             }
         } catch (NumberFormatException e) {
             System.out.println("Number specified must be an integer");
@@ -160,6 +170,32 @@ public class DianaAssistant {
         } else {
             throw new DianaException("Format Invalid. Please use the following format: " +
                     "event <description> /from <start time> /to <end time>");
+        }
+    }
+
+    private void deleteTask (String input, List<Task> tasks) throws DianaException {
+
+        if (tasks.isEmpty()) {
+            throw new DianaException("Task list is empty");
+        }
+
+        try {
+            String substring = input.substring(input.indexOf(" ") + 1);
+            int taskNum = Integer.parseInt(substring) - 1;
+
+            if (taskNum < 0) {
+                throw new DianaException("Task number cannot be negative");
+            }
+
+            if (taskNum >= tasks.size()) {
+                throw new DianaException("Task number must be less than " + tasks.size());
+            }
+
+            System.out.println("Task: " + tasks.get(taskNum).toString() + " has been deleted");
+            tasks.remove(taskNum);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Number specified must be an integer");
         }
     }
 }
