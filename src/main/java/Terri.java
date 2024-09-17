a1e00905fa3dda875f52faf2393a520e323a09fe
Yvorm <abhar2809@gmail.com>
2024-09-17 11:15:46 +0800

    Implement Level-1

--- src/main/java/Terri.java
+++ src/main/java/Terri.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Terri {
     public static void main(String[] args) {
         String logo = "\n" +
@@ -12,17 +15,29 @@ public class Terri {
                 "                                            \n";
 
         System.out.println("Hello from\n" + logo);
-
         String divider = "____________________________________________________________";
-
         System.out.println("Heya! I'm Terri.");
-        System.out.println("What can I help ya with today?\n"+divider);
+        System.out.println("What can I help you with today?\n" + divider);
 
-        System.out.println("Bye then! See ya soon!\n"+divider);
+        // Initialise a scanner to read input
+        Scanner scanner = new Scanner(System.in);
 
+        // Continually check for user input
+        while (true) {
+            String input = scanner.nextLine();
 
+            // Short-circuit check for exit input
+            if (input.equals("bye")) {
+                System.out.println(divider);
+                System.out.println("Bye then! See ya soon!\n"+divider);
+                return;
+            }
 
-
+            // Otherwise, echo back user input and await further input
+            System.out.println(divider);
+            System.out.println(input);
+            System.out.println(divider);
+        }
 
     }
 }
