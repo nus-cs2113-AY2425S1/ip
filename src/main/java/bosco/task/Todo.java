a42ec5fc74ad14fa971eba8df91a2a6281324a84
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-09-19 01:20:10 +0800

    Edit Task classes to initialise with isDone value

--- src/main/java/bosco/task/Todo.java
+++ src/main/java/bosco/task/Todo.java
@@ -1,8 +1,8 @@
 package bosco.task;
 
 public class Todo extends Task {
-    public Todo(String description) {
-        super(description);
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
     }
 
     @Override
