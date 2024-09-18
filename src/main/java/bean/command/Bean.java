700e6f81e862f3733d13c5d629352857edbd0308
raneeng <raneengyinyee@gmail.com>
2024-09-18 16:21:56 +0800

    Added confirmation message

--- src/main/java/bean/command/Bean.java
+++ src/main/java/bean/command/Bean.java
@@ -141,6 +141,7 @@ public class Bean {
     public static void deleteTask(int taskNum) {
         int taskIndex = taskNum - 1;
         tasks.remove(taskIndex);
+        printFormattedReply(INDENT + "Deleted task: " + tasks.get(taskIndex) + ".");
     }
 
     public static void printInvalidInputMessage() {
