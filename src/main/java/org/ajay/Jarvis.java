879414c3fd7cfe4fb867e96e1c0c61faac18b803
Ajay Shanker <ajayshanker1998@gmail.com>
2024-09-19 10:40:22 +0800

    Clean up main after abstraction Prompt class

--- src/main/java/org/ajay/Jarvis.java
+++ src/main/java/org/ajay/Jarvis.java
@@ -17,95 +17,10 @@ import org.ajay.utils.Storage;
 
 public class Jarvis {
     // Constants
-    static final int MAX_TASK_LENGTH = 100; // Maximum length of a string
-
-    private static final String chatBotName = "Jarvis"; // Name of the chatbot
+    public static final String chatBotName = "Jarvis"; // Name of the chatbot
 
     private static ArrayList<Task> taskList = new ArrayList<>(); // ArrayList to store tasks
 
-    static String command; // Variable to store the command
-    static String task; // Variable to store the task
-
-    /**
-     * Prints a break line to the console.
-     */
-    private static void printBreakLine() {
-        System.out.println("────────────────────────────────────────────────────────────");
-    }
-
-    /**
-     * Gracefully exits the program.
-     *
-     * @param status
-     */
-    private static void exit(int status) {
-        System.exit(status);
-    }
-
-    /**
-     * Prints the prompt to the console for visual marker for user to type.
-     */
-    private static void printPrompt() {
-        System.out.print("  ");
-    }
-
-    /**
-     * Prints the greeting messages to the console.
-     */
-    private static void printGreetingMsgs() {
-        String[] greetings = {"Hello! I'm " + chatBotName + "\nWhat can I do for you?"}; // List of greetings
-
-        // Print the greetings
-        for (String greeting : greetings) {
-            printBreakLine(); // Print a break line before each greeting
-            System.out.println(greeting); // Print the greeting
-        }
-        printBreakLine();
-    }
-
-    /**
-     * Prints the goodbye messages to the console.
-     */
-    private static void printGoodbyeMsgs() {
-        String[] goodbyes = {"Bye. Hope to see you again soon!"}; // List of goodbye messages
-
-        // Print the goodbye messages
-        for (String goodbye : goodbyes) {
-            printBreakLine();
-            System.out.println(goodbye); // Print the goodbye message
-        }
-        printBreakLine();
-        exit(0);
-    }
-
-    /**
-     * Prints the tasks to the console.
-     */
-    public static void printTasks() {
-        printBreakLine();
-        Task.printAllTasks(taskList);
-        printBreakLine();
-    }
-
-    /**
-     * Splits the command and task from the input.
-     *
-     * @param lineBufferString
-     */
-    public static void splitCommandAndTask(String lineBufferString) throws EmptyArgumentException {
-
-        if (lineBufferString.isEmpty()) {
-            throw new EmptyArgumentException(Error.EMPTY_ARG.toString());
-        }
-
-        if (lineBufferString.contains(" ")) {
-            command = lineBufferString.split(" ")[0];
-            task = lineBufferString.substring(command.length() + 1);
-        } else {
-            command = lineBufferString;
-            task = null;
-        }
-    }
 
     /**
      * Reads the input from the user and processes it.
@@ -115,55 +30,59 @@ public class Jarvis {
      */
     public static void readInput(Scanner in, String lineBufferString) {
         try {
-            printPrompt(); // Print the prompt to the console
+            Prompt.printPrompt(); // Print the prompt to the console
             lineBufferString = in.nextLine();
-            splitCommandAndTask(lineBufferString);
+            Prompt.splitCommandAndTask(lineBufferString);
 
-            switch (command) {
+            switch (Prompt.command) {
             case "bye":
-                printGoodbyeMsgs();
+                in.close();
+                Prompt.printGoodbyeMsgs();
                 break;
             case "exit": // Habit of typing exit to exit the program
-                printGoodbyeMsgs();
+                in.close();
+                Prompt.printGoodbyeMsgs();
                 break;
             case Task.LIST_COMMAND_STRING: // List all the tasks
-                printTasks();
+                Prompt.printBreakLine();
+                Task.printAllTasks(taskList);
+                Prompt.printBreakLine();
                 break;
             case Todo.COMMAND_STRING: // Add a todo task
                 // taskList[Task.getNumberOfTasks()] = new Todo(task);
-                taskList.add(new Todo(task));
-                printBreakLine();
+                taskList.add(new Todo(Prompt.task));
+                Prompt.printBreakLine();
 
                 Storage.saveTaskList(taskList);
                 break;
             case Deadline.COMMAND_STRING: // Add a deadline task
                 // taskList[Task.getNumberOfTasks()] = new Deadline(task);
-                taskList.add(new Deadline(task));
-                printBreakLine();
+                taskList.add(new Deadline(Prompt.task));
+                Prompt.printBreakLine();
 
                 Storage.saveTaskList(taskList);
                 break;
             case Event.COMMAND_STRING: // Add an event task
                 // taskList[Task.getNumberOfTasks()] = new Event(task);
-                taskList.add(new Event(task));
-                printBreakLine();
+                taskList.add(new Event(Prompt.task));
+                Prompt.printBreakLine();
 
                 Storage.saveTaskList(taskList);
                 break;
             case Task.MARK_COMMAND_STRING: // Mark the task as done
-                int taskNumberMark = Integer.parseInt(task); // Get the task number
+                int taskNumberMark = Integer.parseInt(Prompt.task); // Get the task number
                 Task.markAsDone(taskList, taskNumberMark); // Mark the task as done
 
                 Storage.saveTaskList(taskList);
                 break;
             case Task.UNMARK_COMMAND_STRING: // Mark the task as undone
-                int taskNumberUnmark = Integer.parseInt(task); // Get the task number
+                int taskNumberUnmark = Integer.parseInt(Prompt.task); // Get the task number
                 Task.markAsUndone(taskList, taskNumberUnmark); // Mark the task as undone
 
                 Storage.saveTaskList(taskList);
                 break;
             case Task.DELETE_COMMAND_STRING: // Delete the task
-                int taskNumberDelete = Integer.parseInt(task); // Get the task number
+                int taskNumberDelete = Integer.parseInt(Prompt.task); // Get the task number
                 Task.deleteTask(taskList, taskNumberDelete); // Delete the task
 
                 Storage.saveTaskList(taskList);
@@ -175,9 +94,9 @@ public class Jarvis {
             readInput(in, lineBufferString); // Recursively call the function to read the next input
         } catch (EmptyArgumentException | IllegalCommandException | InvalidCommandFormatException |
                  IllegalArgumentException e) {
-            printBreakLine();
+            Prompt.printBreakLine();
             System.out.println(e);
-            printBreakLine();
+            Prompt.printBreakLine();
             readInput(in, lineBufferString);
         } catch (NoSuchElementException e) {
             // FIXME: Facing unexpected NoSuchElementException error
@@ -186,43 +105,11 @@ public class Jarvis {
     }
 
     public static void main(String[] args) {
-        String logo = """
-                                  @@@@@@@@@@@@@@@@@@@@@@@
-                              @@%     @@          @@      @@@
-                           @@         @@          @@          @@
-                        @@            @@          @@             @@
-                      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                     @                                              *@
-                    @@@                                             @@@
-                   @  @@@                                         @@@  @
-                  @&    @@         @@@@@@@@@@@@@@@@@@@@@         @@.   @@
-                  @      @@@        @@              @@         @@@      @
-                  @       *@@         @@           @(         @@(       @
-                  @       ,@@@@        @@        @@         @@@@        @
-                  @   @@@@@  %@@         @@     @          @@& @@@@@    @
-                  @@@@         @@@        /@  @@         @@@       *@@@@/
-                   @@           @@@         @@          @@@           @@
-                    @@            @@&                 (@@            @@
-                      @       @@@@@@@@               @@@@@@@        @
-                       @@ @@@@*      @@,            @@      @@@@  @&
-                          @@          @@@         @@@          @@
-                            @@@         @@       @@         @@#
-                                @@@@     @@@   @@@     @@@@
-                                       @@@@@@@@@@@@@
-
-                     ██╗    █████╗    ██████╗   ██╗   ██╗  ██╗   ███████╗
-                     ██║   ██╔══██╗   ██╔══██╗  ██║   ██║  ██║   ██╔════╝
-                     ██║   ███████║   ██████╔╝  ██║   ██║  ██║   ███████╗
-                ██   ██║   ██╔══██║   ██╔══██╗  ╚██╗ ██╔╝  ██║   ╚════██║
-                ╚█████╔╝██╗██║  ██║██╗██║  ██║██╗╚████╔╝██╗██║██╗███████║██╗
-                 ╚════╝ ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝ ╚═══╝ ╚═╝╚═╝╚═╝╚══════╝╚═╝
-                """;
-
         String lineBufferString = ""; // Buffer to store the input from the user
         Scanner in = new Scanner(System.in); // Scanner object to read input from the user
 
-        System.out.println("Hello from\n" + logo);
-        printGreetingMsgs();
+        Prompt.printLogo();
+        Prompt.printGreetingMsgs();
 
         try {
             Storage.loadTaskList(taskList);
