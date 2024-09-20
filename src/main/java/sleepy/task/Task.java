96ec7a5651902dd028a2b1ca0eb1ec86ce3af887
DESKTOP-TJ2T7O3\Dell <e1122313@u.nus.edu>
2024-09-20 12:24:28 +0800

    add packages, fixed github comments

---
+++ src/main/java/sleepy/task/Task.java
@@ -0,0 +1,38 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String toFileFormat() {
+        return "T|" + (isDone ? "1" : "0") + "|" + description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
---
+++ src/main/java/sleepy/task/Todo.java
@@ -0,0 +1,16 @@
+public class Todo extends Task{
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "T|" + (isDone() ? "1" : "0") + "|" + getDescription();
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
