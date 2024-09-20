d5b679a412dc8c1077f1eea187a1e23c4dcb5976
ehz0ah <haozhe4547@gmail.com>
2024-09-20 18:54:12 +0800

    Add javadoc comments for ChatBot.java

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -15,9 +15,17 @@ public class ChatBot {
         this.inputScanner = new Scanner(System.in);
         this.ui = new Ui();
         this.storage = new Storage(filePath);
-        this.commandManager = new CommandManager(this.ui,this.storage);
+        this.commandManager = new CommandManager(this.ui, this.storage);
     }
 
+    /**
+     * Initializes the application by attempting to load content from the storage
+     * file.
+     * If the file does not exist, it will be created.
+     * If the creation of the file fails (e.g. due to lack of permissions), prints
+     * an error message and exits the application.
+     * After successful initialization, prints a greeting message to the user.
+     */
     public void bootUp() {
         try {
             if (!commandManager.hasLoadSuccessful()) {
@@ -30,6 +38,13 @@ public class ChatBot {
         ui.greet();
     }
 
+    /**
+     * The main loop of the application.
+     *
+     * This method will continue to loop until the user chooses to exit the
+     * application. It will read the user's input, process it, and then
+     * output the result to the user.
+     */
     public void start() {
         bootUp();
         String line;
@@ -42,6 +57,11 @@ public class ChatBot {
         inputScanner.close();
     }
 
+    /**
+     * The main entry point of the application.
+     *
+     * This creates an instance of the ChatBot and starts the user interface loop.
+     */
     public static void main(String[] args) {
         ChatBot esme = new ChatBot("tasklist.txt");
         esme.start();
