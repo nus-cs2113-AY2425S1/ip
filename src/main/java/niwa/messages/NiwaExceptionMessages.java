c0cc8c38b98f5b495486bec5c7938aab4ec4c79b
LinhHuynhLeDan <22520759@ms.uit.edu.vn>
2024-09-21 00:57:59 +0800

    Add message package

---
+++ src/main/java/niwa/messages/NiwaExceptionMessages.java
@@ -0,0 +1,10 @@
+package niwa.messages;
+
+public class NiwaExceptionMessages {
+    public static final String MESSAGE_INVALID_ARGUMENT = "Wrong arguments! Please check your syntax.";
+    public static final String MESSAGE_INVALID_ARGUMENT_CORRECTION = "Wrong arguments! Please check your syntax: %s";
+    public static final String MESSAGE_INVALID_SYNTAX = "Your syntax is invalid, type 'help' to check for our syntax list!";
+    public static final String MESSAGE_INDEX_OUT_OF_BOUND = "Index is out of bound!";
+    public static final String MESSAGE_DUPLICATE_TASK = "The given task exists in the current list.";
+    public static final String MESSAGE_INDEX_NUMBER_FORMAT = "Task's index must be a number!";
+}
---
+++ src/main/java/niwa/messages/NiwaMesssages.java
@@ -0,0 +1,5 @@
+package niwa.messages;
+
+public class NiwaMesssages {
+    public static final String MESSAGE_LIST_SIZE_INFORM = "You currently have %d task(s) in the list.";
+}
