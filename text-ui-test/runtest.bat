80d501d92dea8bf928b944bbaabbdba48eac92f7
ehz0ah <haozhe4547@gmail.com>
2024-09-20 23:37:13 +0800

    Update automated UI testing

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\ChatBot.java
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\ChatBot.java ..\src\main\java\esme\command\*.java ..\src\main\java\esme\exceptions\*.java ..\src\main\java\esme\storage\*.java ..\src\main\java\esme\task\*.java ..\src\main\java\esme\ui\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
