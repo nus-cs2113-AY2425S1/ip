3c38b70088052848ae12c4dd96bfa9e9c6c5029a
kennethszj <kennethszj.0123@gmail.com>
2024-09-21 01:33:00 +0800

    Add Gui class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,135 +7,109 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Im Shrek");
-        System.out.println("what are you doing in my swamp!??");
-        System.out.println("____________________________________________________________");
 
-        Scanner scanner = new Scanner(System.in); //Scanner object named "scanner"
+        Ui ui = new Ui();
+        ui.showWelcome();
         ArrayList<Task> tasks = new ArrayList<>(); //Array of "task" object to store the tasks
 
         //load tasks from file at the start
         try {
             tasks = loadTasks();
-        } catch (FileNotFoundException e)
-        {
-            System.out.println("No previous task data found. Starting with an empty list.");
+        }
+        catch (FileNotFoundException e) {
+            ui.showLoadingError();
         }
 
         String input;
         while (true) {
-            input = scanner.nextLine();
+            input = ui.readCommand();
 
             try
             {
-                if (input.equals("bye")) //end loop when user inputs "bye"
+                if (input.equals("bye"))
                 {
+                    ui.showGoodbye();
                     break;
                 }
                 else if (input.equals("list")) //display tasks when user inputs "list"
                 {
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Here is your list of nonsense, now get out of my swamp:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println("     " + (i + 1) + "." + tasks.get(i)); //print each line in proper format
-                    }
-                    System.out.println("____________________________________________________________");
+                    ui.showTaskList(tasks);
                 }
                 else if (input.startsWith("mark")) //mark task and print acknowledge message when user marks a task
                 {
                     int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1; //get the integer in the text. minus 1 to get correct index in the "tasks" array
                     tasks.get(taskNumber).markAsDone();
-                    System.out.println("____________________________________________________________");
-                    System.out.println("It’s done. Now, get lost before I toss you out of my swamp!");
-                    System.out.println("     [" + tasks.get(taskNumber).getStatusIcon() + "] " + tasks.get(taskNumber).getDescription());
-                    System.out.println("____________________________________________________________");
+                    ui.showTaskMarked(tasks.get(taskNumber));
                     saveTasks(tasks);
                 }
                 else if (input.startsWith("unmark")) //unmark task and print acknowledge message when user unmarks a task
                 {
                     int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1; //get the integer in the text
                     tasks.get(taskNumber).markAsNotDone();
-                    System.out.println("____________________________________________________________");
-                    System.out.println("It’s unmarked. Gonna change it again? Make up your mind, or get out of my swamp!");
-                    System.out.println("     [" + tasks.get(taskNumber).getStatusIcon() + "] " + tasks.get(taskNumber).getDescription());
-                    System.out.println("____________________________________________________________");
+                    ui.showTaskUnmarked(tasks.get(taskNumber));
                     saveTasks(tasks);
                 }
                 else if (input.startsWith("todo"))
                 {
                     String description = input.substring(5); //at index 5 of the string, the description starts
-                    tasks.add(new Todo(description)); //put new task todo object in task array
-                    printAddedTask(tasks.get(tasks.size() - 1), tasks.size());
+                    Todo newTodo = new Todo(description);
+                    tasks.add(newTodo);
+                    ui.showTaskAdded(newTodo, tasks.size());
                     saveTasks(tasks);
                 }
                 else if (input.startsWith("deadline"))
                 {
                     String[] parts = input.substring(9).split(" /by ");
-                    tasks.add(new Deadline(parts[0], parts[1]));
-                    printAddedTask(tasks.get(tasks.size() - 1), tasks.size());
+                    Deadline newDeadline = new Deadline(parts[0], parts[1]);
+                    tasks.add(newDeadline);
+                    ui.showTaskAdded(newDeadline, tasks.size());
                     saveTasks(tasks);
                 }
                 else if (input.startsWith("event"))
                 {
                     String[] description = input.substring(6).split(" /from ");
                     String[] time = description[1].split(" /to ");
-                    tasks.add(new Event(description[0], time[0], time[1]));
-                    printAddedTask(tasks.get(tasks.size() - 1), tasks.size());
+                    Event newEvent = new Event(description[0], time[0], time[1]);
+                    tasks.add(newEvent);
+                    ui.showTaskAdded(newEvent, tasks.size());
                     saveTasks(tasks);
                 }
                 else if(input.startsWith("delete"))
                 {
                     int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1; //get the integer in the text. minus 1 to get correct index in the "tasks" array
                     Task taskToRemove = tasks.get(taskNumber);
-                    System.out.println("____________________________________________________________");
-                    System.out.println("fine ill remove this task donkey:");
-                    System.out.println("   " + taskToRemove);
-                    System.out.println("____________________________________________________________");
+                    ui.showTaskDeleted(taskToRemove);
                     tasks.remove(taskNumber);
                     saveTasks(tasks);
                 }
-                else //store whatever user input in "tasks" array
+                else //store other input from user as a task object
                 {
-                    tasks.add(new Task(input));
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" added: " + input);
-                    System.out.println("____________________________________________________________");
+                    Task newTask = new Task(input);
+                    tasks.add(newTask);
+                    ui.showTaskAdded(newTask, tasks.size());
                     saveTasks(tasks);
                 }
             }
-            catch (NumberFormatException e){ //catch when trying to convert string to number. but i think wont happen.
-                System.out.println("wrong format, enter a valid task");
+            catch (NumberFormatException e){
+                ui.showFormatError();
             }
-            catch (ArrayIndexOutOfBoundsException e){ //
-                System.out.println("what are you typing donkey? specify the time/task right or get out of my swamp");
+            catch (ArrayIndexOutOfBoundsException e){
+                ui.showInputError();
             }
             catch (NullPointerException e){ //when access nullptr, eg when marking task 6 but only got 5 task
-                System.out.println("cant mark whats not in my swamp. pick an existing task donkey");
+                ui.showIndexError();
             }
             catch (StringIndexOutOfBoundsException e){ //access index in string outside valid range. happen for T/D/E commands
-                System.out.println("you didnt finish your sentence donkey! specify the time/task right or get out of my swamp");
+                ui.showInputError();
             }
             catch (IndexOutOfBoundsException e){
-                System.out.println("that task is not even in my swamp donkey! Try another task. ");
+                ui.showIndexError();
             }
             catch (IOException e){
-                System.out.println("fail to save task to file");
+                ui.showSaveError();
             }
         }
 
-        System.out.println("____________________________________________________________"); //first line after "bye"
-        System.out.println(" Finally, you’re leaving. Now I can have some peace in my swamp.");
-        System.out.println("____________________________________________________________"); //second line after "bye"
-
-    }
-
-    public static void printAddedTask(Task task, int taskCount) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" ive added this task to your pile of nonsense:");
-        System.out.println("   " + task);
-        System.out.println(" Now you have " + taskCount + " tasks in my swamp.");
-        System.out.println("____________________________________________________________");
     }
 
 
@@ -154,7 +128,7 @@ public class Main {
         fw.close();
     }
 
-    // Method to load tasks from a file
+    // Method to load tasks from a file and add it to the ArrayList
     public static ArrayList<Task> loadTasks() throws FileNotFoundException {
         ArrayList<Task> tasks = new ArrayList<>();
         File file = new File("data/duke.txt");
@@ -165,46 +139,44 @@ public class Main {
                 String[] parts = line.split(" \\| ");
                 Task task;
                 switch (parts[0]) {
-                    case "T":
-                        task = new Todo(parts[2]);
-                        if (parts[1].equals("1"))
-                        {
-                            task.markAsDone();
-                        }
-                        tasks.add(task);
-                        break;
-                    case "D":
-                        task = new Deadline(parts[2], parts[3]);
-                        if (parts[1].equals("1"))
-                        {
-                            task.markAsDone();
-                        }
-                        tasks.add(task);
-                        break;
-                    case "E":
-                        task = new Event(parts[2], parts[3], parts[4]);
-                        if (parts[1].equals("1"))
-                        {
-                            task.markAsDone();
-                        }
-                        tasks.add(task);
-                        break;
-                    case " ": //for generic tasks
-                        task = new Task(parts[2]);
-                        if (parts[1].equals("1"))
-                        {
-                            task.markAsDone();
-                        }
-                        tasks.add(task);
-                        break;
+                case "T":
+                    task = new Todo(parts[2]);
+                    if (parts[1].equals("1"))
+                    {
+                        task.markAsDone();
+                    }
+                    tasks.add(task);
+                    break;
+                case "D":
+                    task = new Deadline(parts[2], parts[3]);
+                    if (parts[1].equals("1"))
+                    {
+                        task.markAsDone();
+                    }
+                    tasks.add(task);
+                    break;
+                case "E":
+                    task = new Event(parts[2], parts[3], parts[4]);
+                    if (parts[1].equals("1"))
+                    {
+                        task.markAsDone();
+                    }
+                    tasks.add(task);
+                    break;
+                case " ": //for generic tasks
+                    task = new Task(parts[2]);
+                    if (parts[1].equals("1"))
+                    {
+                        task.markAsDone();
+                    }
+                    tasks.add(task);
+                    break;
                 }
             }
             sc.close();
         }
         return tasks;
     }
-
-
 
 }
 
---
+++ src/main/java/Ui.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("I'm Shrek");
+        System.out.println("What are you doing in my swamp!??");
+        showLine();
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showLoadingError() {
+        System.out.println("No previous task data found. Starting with an empty list.");
+    }
+
+    public void showTaskAdded(Task task, int taskCount) {
+        showLine();
+        System.out.println("I've added this task to your pile of nonsense:");
+        System.out.println("   " + task);
+        System.out.println("Now you have " + taskCount + " tasks in my swamp.");
+        showLine();
+    }
+
+    public void showTaskList(ArrayList<Task> tasks) {
+        showLine();
+        System.out.println("Here is your list of nonsense, now get out of my swamp:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("     " + (i + 1) + "." + tasks.get(i));  // Utilizes Task's toString() method
+        }
+        showLine();
+    }
+
+    public void showTaskMarked(Task task) {
+        showLine();
+        System.out.println("It’s done. Now, get lost before I toss you out of my swamp!");
+        System.out.println("     [" + task.getStatusIcon() + "] " + task.getDescription());
+        showLine();
+    }
+
+    public void showTaskUnmarked(Task task) {
+        showLine();
+        System.out.println("It’s unmarked. Gonna change it again? Make up your mind, or get out of my swamp!");
+        System.out.println("     [" + task.getStatusIcon() + "] " + task.getDescription());
+        showLine();
+    }
+
+    public void showTaskDeleted(Task task) {
+        showLine();
+        System.out.println("Fine I’ll remove this task donkey:");
+        System.out.println("   " + task);
+        showLine();
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+
+    public void showFormatError() {
+        showLine();
+        System.out.println("Wrong format, enter a valid task.");
+        showLine();
+    }
+
+    public void showIndexError() {
+        showLine();
+        System.out.println("That task is not even in my swamp donkey! Try another task.");
+        showLine();
+    }
+
+    public void showInputError() {
+        showLine();
+        System.out.println("What are you typing donkey? Specify the time/task right or get out of my swamp.");
+        showLine();
+    }
+
+    public void showGoodbye() {
+        showLine();
+        System.out.println("Finally, you’re leaving. Now I can have some peace in my swamp.");
+        showLine();
+    }
+
+    public void showSaveError(){
+        showLine();
+        System.out.println("Failed to save file.");
+        showLine();
+    }
+
+    public void closeScanner() {
+        scanner.close();
+    }
+}
+
