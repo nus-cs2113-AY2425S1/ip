42149621c6beab5f441264152ba789401247d373
unknown <nivedit@comp.nus.edu.sg>
2024-09-20 15:14:00 +0800

    Update to use ArrayList in place of primitive array

--- src/main/java/bitwise/utils/OutputManager.java
+++ src/main/java/bitwise/utils/OutputManager.java
@@ -3,16 +3,18 @@ package bitwise.utils;
 import bitwise.constants.Constants;
 import bitwise.tasks.Task;
 
+import java.util.ArrayList;
+
 public class OutputManager {
 
     public static void printNumberOfTasks(int numberOfTasks) {
         System.out.println(Constants.INDENTATION + "Now you have " + numberOfTasks + " tasks in the list.");
     }
 
-    public static void printTasksList(Task[] tasksList, int numberOfTasks) {
+    public static void printTasksList(ArrayList<Task> tasksList, int numberOfTasks) {
         System.out.println(Constants.INDENTATION + Constants.MESSAGE_TASK_LIST);
         for (int i = 0; i < numberOfTasks; i++) {
-            System.out.println(Constants.INDENTATION + Integer.toString(i + 1) + ". " + tasksList[i].toString());
+            System.out.println(Constants.INDENTATION + Integer.toString(i + 1) + ". " + tasksList.get(i).toString());
         }
         OutputManager.printNumberOfTasks(numberOfTasks);
     }
