da7dcb43f38af4c8afb29d582c73b9a613f29c5f
jemehgoh <jeremygoheh@gmail.com>
2024-09-21 22:23:59 +0800

    Create new UnmarkCommand class

---
+++ src/main/java/nell/command/UnmarkCommand.java
@@ -0,0 +1,33 @@
+package nell.command;
+
+import nell.TaskList;
+import nell.common.Messages;
+
+/**
+ * Represents an executable unmark command
+ */
+public class UnmarkCommand extends Command{
+    /**
+     * Constructs a UnmarkCommand object with a specified task list and task index
+     *
+     * @param tasks The specified task list
+     * @param taskIndex The specified task index
+     */
+    public UnmarkCommand(TaskList tasks, int taskIndex){
+        super("unmark", tasks, taskIndex);
+    }
+
+    /**
+     * Executes task - marks a specified task as not done.
+     * If task is not in task list, informs user that the task is invalid.
+     */
+    public void execute() {
+        try {
+            this.tasks.getTaskAtIndex(taskIndex - 1).setDone(false);
+            System.out.println(Messages.UNMARK_MESSAGE);
+            System.out.println(this.tasks.getTaskStringAtIndex(taskIndex - 1));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(Messages.INVALID_TASK_MESSAGE);
+        }
+    }
+}
