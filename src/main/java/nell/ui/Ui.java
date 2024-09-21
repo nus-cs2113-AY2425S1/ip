9e91ddd9dc4b6cbafdc8b091b407cfece24311fd
jemehgoh <jeremygoheh@gmail.com>
2024-09-21 11:13:43 +0800

    Add dataStorage member to Ui class to handle saving of data

--- src/main/java/nell/ui/Ui.java
+++ src/main/java/nell/ui/Ui.java
@@ -1,19 +1,25 @@
 package nell.ui;
 
 import nell.parser.Parser;
+import nell.storage.Storage;
+
+import java.util.Scanner;
 
 /**
  * Represents an object that takes user input and prints messages in Nell.
  */
 public class Ui {
     private Parser parser;
+    private Storage dataStorage;
 
     /**
      * Constructs a new Ui object with a given parser
      *
      * @param parser The parser object for the Ui object
      */
-    public Ui(Parser parser) {
+    public Ui(Parser parser, Storage dataStorage) {
         this.parser = parser;
+        this.dataStorage = dataStorage;
+    }
     }
 }
