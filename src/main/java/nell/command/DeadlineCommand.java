81632e28f7f325d6ee196c3e13145a72776adb89
jemehgoh <jeremygoheh@gmail.com>
2024-09-21 11:59:40 +0800

    Add javadoc comments and made members in Deadline final

--- src/main/java/nell/command/DeadlineCommand.java
+++ src/main/java/nell/command/DeadlineCommand.java
@@ -4,13 +4,23 @@ import nell.TaskList;
 import nell.common.Messages;
 import nell.tasks.Deadline;
 
+/**
+ * Represents an executable deadline command
+ */
 public class DeadlineCommand extends Command{
-    private String description;
-    private String by;
+    private final String description;
+    private final String by;
 
+    /**
+     * Constructs a new DeadlineCommand object with specified task list and command body
+     *
+     * @param tasks The specified task list
+     * @param detail The task description and its by-time
+     * @throws IndexOutOfBoundsException If detail cannot be split into 2 words
+     */
     public DeadlineCommand(TaskList tasks, String detail) throws IndexOutOfBoundsException {
         super("deadline", tasks);
-        String[] details = detail.split("/by");
+        String[] details = detail.split("/by", 2);
 
         if (details.length < 2) {
             throw new IndexOutOfBoundsException(Messages.DEADLINE_ERROR_MESSAGE);
@@ -20,6 +30,9 @@ public class DeadlineCommand extends Command{
         this.by = details[1];
     }
 
+    /**
+     * Executes command - adds new Deadline task to the task list
+     */
     @Override
     public void execute() {
         Deadline deadlineToAdd = new Deadline(this.description, this.by);
