dd9e8b0fa46e1eafbf4ecd338ae664d2a8479a37
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-21 16:47:17 +0800

    Add DateTimeFormatter settings for input and output

--- src/main/java/erika/settings/Settings.java
+++ src/main/java/erika/settings/Settings.java
@@ -1,5 +1,7 @@
 package erika.settings;
 
+import java.time.format.DateTimeFormatter;
+
 public class Settings {
     public static final int FROM_LENGTH_OFFSET = 6;
     public static final int TO_LENGTH_OFFSET = 4;
@@ -12,4 +14,10 @@ public class Settings {
     public static final String DIRECTORY = "data";
     public static final String FILENAME = DIRECTORY + "/tasks.txt";
     public static final String SEPARATOR = ",";
+
+    public static final DateTimeFormatter DATE_IN_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static final DateTimeFormatter DATE_TIME_IN_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+    public static final DateTimeFormatter DATE_OUT_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yy");
+    public static final DateTimeFormatter DATE_TIME_OUT_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
 }
