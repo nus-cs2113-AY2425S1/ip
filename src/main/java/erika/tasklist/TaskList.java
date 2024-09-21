7521b688483d579e3fbd57d00793c152f09a1b22
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-21 18:26:54 +0800

    Remove command processing code from TaskList

--- src/main/java/erika/tasklist/TaskList.java
+++ src/main/java/erika/tasklist/TaskList.java
@@ -1,12 +1,8 @@
 package erika.tasklist;
 
-import erika.console.Console;
-import erika.exception.EmptyListException;
-import erika.exception.OutOfBoundsException;
 import erika.task.Task;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class TaskList {
     private static ArrayList<Task> tasks;
@@ -16,20 +12,6 @@ public class TaskList {
         taskArraySize = tasks.size();
     }
 
-    public void deleteTask(int index) throws OutOfBoundsException {
-        if (index <= 0 || index > getTaskArraySize()) {
-            throw new OutOfBoundsException();
-        }
-        Console.printDeletedMessage(tasks.get(index - 1));
-        tasks.remove(index - 1);
-        decrementTaskArraySize();
-    }
-
-    public void add(Task task) {
-        tasks.add(task);
-        incrementTaskArraySize();
-        Console.printAddedMessage(task);
-    }
     public static void setTaskArraySize(int size) {
         taskArraySize = size;
     }
@@ -46,33 +28,21 @@ public class TaskList {
         taskArraySize--;
     }
 
-    public void markEntry(int index, boolean mark) {
-        Task task = tasks.get(index - 1);
-        task.setMark(mark);
-        if (mark) {
-            Console.printMarkedMessage(task);
-        } else {
-            Console.printUnmarkedMessage(task);
-        }
-    }
-
     public String printFileLine() {
         return (String) tasks.stream()
                         .map(Task::generateFileLine)
                         .collect(Collectors.joining(""));
     }
 
-    public void printListMessage() throws EmptyListException {
-        if(tasks.isEmpty()) {
-            throw new EmptyListException();
-        }
-        Console.printMessage(printList());
+    public Task getTask(int index) {
+        return tasks.get(index);
     }
 
-    public String printList() throws EmptyListException{
-        return IntStream.range(0, taskArraySize)
-                .mapToObj(i -> (i + 1) + ". " + tasks.get(i))
-                .collect(Collectors.joining("\n\t"));
+    public void addTask(Task task) {
+        tasks.add(task);
     }
 
+    public void deleteTask(int index) {
+        tasks.remove(index);
+    }
 }
