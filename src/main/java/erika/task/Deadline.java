e9c4dab7569a58ff85727ed9d0532cb69c1c5ebe
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-21 16:47:54 +0800

    Modify toString and generateFileLine methods to output dates

--- src/main/java/erika/task/Deadline.java
+++ src/main/java/erika/task/Deadline.java
@@ -1,20 +1,41 @@
 package erika.task;
 
+import erika.settings.Settings;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
-    protected String deadline;
+    protected String deadline = null;
+    protected LocalDate deadlineDate = null;
+    protected LocalDateTime deadlineDateTime = null;
 
     public Deadline(String description, String deadline) {
         super(description);
         this.deadline = deadline;
     }
 
+    public Deadline(String description, LocalDate deadlineDate) {
+        super(description);
+        this.deadlineDate = deadlineDate;
+    }
+
+    public Deadline(String description, LocalDateTime deadlineDateTime) {
+        super(description);
+        this.deadlineDateTime = deadlineDateTime;
+    }
+
     @Override
     public String toString() {
-        return String.format("[D][%s] %s (by: %s)", isDone ? "X":" ", description, deadline);
+        return String.format("[D][%s] %s (by: %s)", isDone ? "X":" ", description,
+                (deadlineDateTime != null) ? deadlineDateTime.format(Settings.DATE_TIME_OUT_FORMATTER) : (deadlineDate != null) ?
+                        deadlineDate.format(Settings.DATE_OUT_FORMATTER) : deadline);
     }
 
     @Override
     public String generateFileLine() {
-        return String.format("D,%s,%s,%s\n", isDone ? "1" : "0", description, deadline);
+        return String.format("D,%s,%s,%s\n", isDone ? "1" : "0", description, (deadlineDateTime != null) ?
+                deadlineDateTime.format(Settings.DATE_IN_FORMATTER) : (deadlineDate != null) ?
+                deadlineDate.format(Settings.DATE_TIME_IN_FORMATTER) : deadline);
     }
 }
