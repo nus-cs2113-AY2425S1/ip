c59a62f5e3fedeed18e8d61caca127728934e88c
jemehgoh <jeremygoheh@gmail.com>
2024-09-21 11:14:12 +0800

    Move handling of user interactions to Ui class

--- src/main/java/nell/Nell.java
+++ src/main/java/nell/Nell.java
@@ -6,6 +6,7 @@ import nell.storage.Storage;
 import nell.tasks.Deadline;
 import nell.tasks.Event;
 import nell.tasks.ToDo;
+import nell.ui.Ui;
 
 import java.util.Scanner;
 
@@ -13,47 +14,12 @@ public class Nell {
     private static TaskList tasks = new TaskList();
     private static Storage dataStorage = new Storage("./data/data.txt", tasks);
     private static Parser parser = new Parser(tasks);
-
-    /**
-     * Says bye to the user
-     */
-    private static void sayBye() {
-        System.out.println(Messages.BYE_MESSAGE);
-    }
-    
-    /**
-     * Greet the user upon program startup
-     */
-    private static void greetUser() {
-        System.out.println(Messages.HELLO_MESSAGE);
-        System.out.println(Messages.PROMPT_MESSAGE);
-    }
-
-    /**
-     * Get commands from user and execute commands received
-     */
-    private static void getCommands() {
-        // Initialises scanner to take in user input
-        Scanner input = new Scanner(System.in);
-
-        boolean isGettingCommands = true;
-
-        while (isGettingCommands) {
-            // Get user command and respond accordingly
-            String command = input.nextLine();
-            if (command.equals("bye")) {
-                sayBye();
-                isGettingCommands = false;
-            } else {
-                parser.parseCommand(command);
-            }
-            dataStorage.saveToFile();
-        }
-    }
+    private static Ui ui = new Ui(parser, dataStorage);
 
     public static void main(String[] args) {
         dataStorage.loadFromFile();
-        greetUser();
-        getCommands();
+        ui.greetUser();
+        ui.getCommands();
+        ui.sayBye();
     }
 }
--- src/main/java/nell/ui/Ui.java
+++ src/main/java/nell/ui/Ui.java
@@ -1,5 +1,6 @@
 package nell.ui;
 
+import nell.common.Messages;
 import nell.parser.Parser;
 import nell.storage.Storage;
 
@@ -21,5 +22,41 @@ public class Ui {
         this.parser = parser;
         this.dataStorage = dataStorage;
     }
+
+    /**
+     * Says bye to the user
+     */
+    public void sayBye() {
+        System.out.println(Messages.BYE_MESSAGE);
+    }
+
+    /**
+     * Greet the user upon program startup
+     */
+    public void greetUser() {
+        System.out.println(Messages.HELLO_MESSAGE);
+        System.out.println(Messages.PROMPT_MESSAGE);
+    }
+
+    /**
+     * Get commands from user and execute commands received
+     */
+    public void getCommands() {
+        // Initialises scanner to take in user input
+        Scanner input = new Scanner(System.in);
+
+        boolean isGettingCommands = true;
+
+        while (isGettingCommands) {
+            // Get user command and respond accordingly
+            String command = input.nextLine();
+            if (command.equals("bye")) {
+                sayBye();
+                isGettingCommands = false;
+            } else {
+                parser.parseCommand(command);
+            }
+            dataStorage.saveToFile();
+        }
     }
 }
