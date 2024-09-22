35ed1b7471ce909294d2fc16aa1a70fad7b9ff11
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:15:10 +0800

    Add JavaDoc comments to TaskList

--- src/main/java/erika/tasklist/TaskList.java
+++ src/main/java/erika/tasklist/TaskList.java
@@ -7,15 +7,24 @@ import erika.task.Task;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
+/**
+ * Represents a list of <code>Tasks</code>
+ */
 public class TaskList {
     private static ArrayList<Task> tasks;
     private static int taskArraySize;
+    /**
+     * @param tasks <code>ArrayList<Task></code> that represents a list of <code>Task</code>s
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
         taskArraySize = tasks.size();
     }
-
+    /**
+     * Deletes a <code>Task</code> from the list
+     * @param index represnets the index of the list we wish to delete
+     * @throws OutOfBoundsException when index is out of the range of the list
+     */
     public void deleteTask(int index) throws OutOfBoundsException {
         if (index <= 0 || index > getTaskArraySize()) {
             throw new OutOfBoundsException();
@@ -24,28 +33,48 @@ public class TaskList {
         tasks.remove(index - 1);
         decrementTaskArraySize();
     }
-
+    /**
+     * Adds a <code>Task</code> to the list
+     * @param task represnets the task to be added to the list
+     */
     public void add(Task task) {
         tasks.add(task);
         incrementTaskArraySize();
         Console.printAddedMessage(task);
     }
+    /**
+     * Setter to set the static variable <code>int taskArraySize</code>
+     * @param size represnets the new size to be set
+     */
     public static void setTaskArraySize(int size) {
         taskArraySize = size;
     }
-
+    /**
+     * Getter to obtain the static variable <code>int taskArraySize</code>
+     * @return The size of the array
+     */
     public static int getTaskArraySize() {
         return taskArraySize;
     }
-
+    /**
+     * Setter to increment the static variable <code>int taskArraySize</code> by 1
+     * Used when adding a <code>Task</code>
+     */
     public static void incrementTaskArraySize() {
         taskArraySize++;
     }
-
+    /**
+     * Setter to increment the static variable <code>int taskArraySize</code> by 1
+     * Used when adding a <code>Task</code>
+     */
     public static void decrementTaskArraySize() {
         taskArraySize--;
     }
-
+    /**
+     * Marks an entry in the <code>TaskList</code> as done/not done
+     * @param index Index of list entry to be marked as done/not done
+     * @param mark whether the task is done (true : done , false : not done)
+     */
     public void markEntry(int index, boolean mark) {
         Task task = tasks.get(index - 1);
         task.setMark(mark);
@@ -55,20 +84,31 @@ public class TaskList {
             Console.printUnmarkedMessage(task);
         }
     }
-
+    /**
+     * Converts the Task array into a string to be written to the text file for storage
+     * @return String to be written to the text file
+     */
     public String printFileLine() {
         return (String) tasks.stream()
                         .map(Task::generateFileLine)
                         .collect(Collectors.joining(""));
     }
-
+    /**
+     * Converts the Task array into a string to be written to the console
+     * @throws EmptyListException when list is empty
+     * @return String to be written to the console
+     */
     public void printListMessage() throws EmptyListException {
         if(tasks.isEmpty()) {
             throw new EmptyListException();
         }
         Console.printMessage(printList());
     }
-
+    /**
+     * Helper function to generate the String for printing to the console
+     * @return String to be written to the text file
+     * @throws EmptyListException when the list is empty
+     */
     public String printList() throws EmptyListException{
         return IntStream.range(0, taskArraySize)
                 .mapToObj(i -> (i + 1) + ". " + tasks.get(i))
