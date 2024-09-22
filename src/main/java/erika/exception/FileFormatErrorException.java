85f4efb7c9cbcf8e1faf87bfed4243114af5f111
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:13:51 +0800

    Add JavaDoc comments to FileFormatException

--- src/main/java/erika/exception/FileFormatErrorException.java
+++ src/main/java/erika/exception/FileFormatErrorException.java
@@ -1,8 +1,12 @@
 package erika.exception;
 
-import erika.Erika;
-
+/**
+ * Represents the exception thrown when a Command has an empty description
+ */
 public class FileFormatErrorException extends ErikaException {
+    /**
+     * @param message A <code>String</code> representing the filename of the file that is not found
+     */
     public FileFormatErrorException(String message) {
         super("File: " + message + "not found");
     }
