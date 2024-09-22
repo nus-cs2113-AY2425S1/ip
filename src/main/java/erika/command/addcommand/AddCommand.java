a5f1ce68e5c7ec428e6a02e0168fba6e37266164
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:12:23 +0800

    Add JavaDoc comments to AddCommand

--- src/main/java/erika/command/addcommand/AddCommand.java
+++ src/main/java/erika/command/addcommand/AddCommand.java
@@ -1,27 +1,39 @@
 package erika.command.addcommand;
 
 import erika.command.Command;
-import erika.console.Console;
 import erika.exception.OutOfBoundsException;
 import erika.filesystem.FileSystem;
-import erika.task.Event;
-import erika.ui.Ui;
 import erika.tasklist.TaskList;
 
 import java.io.IOException;
 
+/**
+ * Represents a specific "Add" command as interpreted from the user. An <code>AddCommand</code> object corresponds to
+ * a user command to append a <code>Task</code> object to the list
+ */
+
 public class AddCommand extends Command {
     protected String description;
 
+    /**
+     * @param description Textual description of the entry
+     */
     public AddCommand(String description) {
         super();
         this.description = description;
     }
-
+    /**
+     * Overrides the default Command method execute
+     * @param tasks TaskList object representing the tasks stored
+     * @param fileSystem FileSystem object used to interface with the file system of the host
+     */
     public void execute(TaskList tasks, FileSystem fileSystem) throws IOException, OutOfBoundsException {
 
     }
-
+    /**
+     * Executes the required operations to perform the command issued by the user
+     * @return <code>false</code> since AddCommands are not ExitCommands
+     */
     public boolean isExit() {
         return false;
     }
