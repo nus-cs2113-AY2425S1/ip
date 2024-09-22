20e4f0adccb2e14e082c5158f54eb5f2f629c60b
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:14:33 +0800

    Add JavaDoc comments to FileSystem

--- src/main/java/erika/filesystem/FileSystem.java
+++ src/main/java/erika/filesystem/FileSystem.java
@@ -14,16 +14,24 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-
+/**
+ * Represents the interface between the application and the host's filesystem
+ */
 public class FileSystem {
     private String filePath;
     private String separator;
-
+    /**
+     * @param filePath path to the file used for storage
+     * @param separator character used to delimit the regions of data
+     */
     public FileSystem(String filePath, String separator) {
         this.filePath = filePath;
         this.separator = separator;
     }
-
+    /**
+     * Deletes and recreates the file used for storage
+     * Typically called when a <code>FileFormatException</code> is thrown
+     */
     private void reinitFileSystem() {
         try{
             Console.printMessage(Settings.FILENAME + " is corrupted, re-creating the file");
@@ -32,7 +40,10 @@ public class FileSystem {
             Console.printMessage("IO Error encountered when re-creating " + Settings.FILENAME);
         }
     }
-
+    /**
+     * Creates the file used for storage when the application is run for the very first time
+     * Typically called when a <code>FileNotFoundException</code> is thrown
+     */
     public ArrayList<Task> initializeFileSystem() {
         ArrayList<Task> tasks = new ArrayList<>();
         try{
@@ -41,18 +52,24 @@ public class FileSystem {
             printFileNotFoundMessage();
         } catch (FileFormatErrorException e) {
             reinitFileSystem();
-        } catch (IOException e) {
-            Console.printMessage("IO Error encountered when setting up fileSystem");
         } finally {
             return tasks;
         }
     }
-
-    public void readFromFile(ArrayList<Task> tasks) throws FileNotFoundException, IOException, FileFormatErrorException {
+    /**
+     * Creates the file used for storage when the application is run for the very first time
+     * Reads and parses data from the filePath line by line
+     * @param tasks An <code>ArrayList</code> of the list of tasks
+     */
+    public void readFromFile(ArrayList<Task> tasks) throws FileNotFoundException, FileFormatErrorException {
         File f = new File(filePath);
         if(!f.exists()) {
             createDirectory(Settings.DIRECTORY);
-            writeToFile("",false);
+            try{
+                writeToFile("",false);
+            } catch (IOException _e) {
+                Console.printMessage("IO Error encountered when creating " + Settings.DIRECTORY);
+            }
             throw new FileNotFoundException(filePath);
         }
         Scanner s = new Scanner(f);
@@ -61,35 +78,54 @@ public class FileSystem {
             parseLine(tasks, s.nextLine(), lineNumber++);
         }
     }
-
+    /**
+     * Writes data to the storage file
+     * @param textToAdd Text data to be added.
+     * @param isAppend whether to append or overwrite existing data
+     */
     public void writeToFile(String textToAdd, boolean isAppend) throws IOException {
         FileWriter fw = new FileWriter(filePath, isAppend);
         fw.write(textToAdd);
         fw.close();
     }
-
+    /**
+     * Appends a <code>Task</code> object to the storage file
+     * @param task <code>Task</code> object to be appended to the file
+     */
     public void appendTaskToFile(Task task) throws IOException {
         writeToFile(task.generateFileLine(),true);
     }
-
+    /**
+     * Overwrites task file with data generated from current <code>TaskList</code> list of tasks
+     * @param tasks <code>TaskList</code> lists of tasks
+     */
     public void updateFileSystemWithLocalTasks(TaskList tasks) throws IOException{
-        FileWriter fw = new FileWriter(filePath, false);
         String res = tasks.printFileLine();
-        fw.write(res);
-        fw.close();
+        writeToFile(res,false);
     }
-
+    /**
+     * Prints an error message detailing to the user that the file was not found and that a new one has been created
+     */
     public void printFileNotFoundMessage() {
-        System.out.println("\t" + filePath + " not found. Creating new file.");
+        Console.printMessage("\t" + filePath + " not found. Creating new file.");
     }
-
+    /**
+     * Creates a directory to store the storage file
+     * @param directoryName <code>String</code> storing the location of the directory
+     */
     private void createDirectory(String directoryName) {
         File directory = new File(directoryName);
         if (!directory.exists()) {
             directory.mkdirs();
         }
     }
-
+    /**
+     * Parses a <code>String</code> line and appends the <code>Task</code> object to the <code>TaskList</code> list of
+     * tasks
+     * @param tasks <code>TaskList</code> lists of tasks
+     * @param line <code>String</code> representing the line from the text file
+     * @param lineNumber <code>int</code> representing the current line number. Used for error handling
+     */
     private void parseLine(ArrayList<Task> tasks, String line, int lineNumber) throws FileFormatErrorException {
         String[] tokens = line.split(separator);
         if (tokens.length < 3) {
