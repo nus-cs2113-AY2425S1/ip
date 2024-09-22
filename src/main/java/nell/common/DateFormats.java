4fef80616ff0ecf9a1457ec5e28f695c16f2806b
jemehgoh <jeremygoheh@gmail.com>
2024-09-22 14:47:17 +0800

    Add DateTimeFormatter objects for formatting dates and times for I/O

---
+++ src/main/java/nell/common/DateFormats.java
@@ -0,0 +1,11 @@
+package nell.common;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Container class for date formats
+ */
+public class DateFormats {
+    public static final DateTimeFormatter INPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    public static final DateTimeFormatter OUTPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+}
