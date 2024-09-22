60d1c416b6f8e59134ea5a46152e1f934cb63c34
telferang <telferang@gmail.com>
2024-09-22 19:07:58 +0800

    Add JavaDoc to all the classes

--- src/main/java/nova/command/EventCommand.java
+++ src/main/java/nova/command/EventCommand.java
@@ -6,11 +6,29 @@ import nova.exception.InsufficientSpaceException;
 import nova.exception.InvalidInputException;
 import nova.task.Event;
 
+/**
+ * Represents a command to create an Event task.
+ * This command allows the user to specify a task description, a start time, and an end time.
+ */
 public class EventCommand extends Command {
 
+    /**
+     * The command word for the Event command.
+     */
     public static final String COMMAND_WORD = "event";
+
+    /**
+     * The usage instructions for the Event command.
+     */
     private static final String EVENT_USAGE = "Usage: event <task description> /from <start time> /to <end time>.";
 
+    /**
+     * Executes the Event command by validating input, checking space,
+     * and adding a new Event task to the task manager.
+     *
+     * @param inputs     The input arguments provided by the user.
+     * @param taskManager The TaskList instance managing tasks.
+     */
     public static void execute(String[] inputs, TaskList taskManager) {
         String[] validatedInput;
         try {
@@ -24,6 +42,14 @@ public class EventCommand extends Command {
         }
     }
 
+    /**
+     * Validates the input for creating an Event task.
+     * Ensures that the input has the correct format and contains necessary components.
+     *
+     * @param inputs The input arguments provided by the user.
+     * @return An array containing the task description, start time, and end time.
+     * @throws InvalidInputException If the input format is invalid.
+     */
     protected static String[] validateEventInput(String[] inputs) {
         if (inputs.length != 2) {
             throw new InvalidInputException("No description entered.");
@@ -36,11 +62,11 @@ public class EventCommand extends Command {
         }
         String[] splitInput = inputs[1].split(" /from ");
         if (splitInput.length != 2) {
-            throw new InvalidInputException("description/start/end not entered.");
+            throw new InvalidInputException("Description/start/end not entered.");
         }
         String[] eventDetails = splitInput[1].split(" /to ");
         if (eventDetails.length != 2) {
-            throw new InvalidInputException("description/start/end not entered.");
+            throw new InvalidInputException("Description/start/end not entered.");
         }
         return new String[]{splitInput[0], eventDetails[0], eventDetails[1]};
     }
--- src/main/java/nova/task/Deadline.java
+++ src/main/java/nova/task/Deadline.java
@@ -2,17 +2,39 @@ package nova.task;
 
 import nova.Storage;
 
-public class Deadline extends Task{
+/**
+ * Represents a Deadline task that extends the base Task class.
+ * A Deadline task has a description, a completion status, and a due date.
+ */
+public class Deadline extends Task {
 
+    /**
+     * The due date for the Deadline task.
+     */
     String by;
 
-    public Deadline (String description, String by) {
+    /**
+     * Constructs a new Deadline task with the specified description and due date.
+     * The task is initially not done, and an acknowledgement message is printed.
+     *
+     * @param description The description of the Deadline task.
+     * @param by          The due date for the Deadline task.
+     */
+    public Deadline(String description, String by) {
         super(description);
         this.by = by;
         printAcknowledgementMessage(getTaskInfo());
     }
 
-    public Deadline (String isDone, String description, String by) {
+    /**
+     * Constructs a Deadline task with the specified status, description, and due date.
+     * The task is marked as done if the status string is "X".
+     *
+     * @param isDone     The status of the task ("X" for done, otherwise not done).
+     * @param description The description of the Deadline task.
+     * @param by          The due date for the Deadline task.
+     */
+    public Deadline(String isDone, String description, String by) {
         super(description);
         this.by = by;
         if (isDone.equals("X")) {
@@ -20,10 +42,22 @@ public class Deadline extends Task{
         }
     }
 
+    /**
+     * Returns a string representation of the Deadline task, including its status icon, description, and due date.
+     *
+     * @return The Deadline task information as a string.
+     */
+    @Override
     public String getTaskInfo() {
         return "[D][" + this.getStatusIcon() + "] " + description + " (by: " + by + ")";
     }
 
+    /**
+     * Returns a string representation of the Deadline task for storage purposes.
+     *
+     * @return A string representing the Deadline task for storage.
+     */
+    @Override
     public String getTaskStorageInfo() {
         return "D" + DIVIDER + this.getStatusIcon() + DIVIDER + description + DIVIDER + by;
     }
