449eea5b1515bd70564be0db9db29ebcb430292d
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:14:46 +0800

    Add JavaDoc comments to Deadline

--- src/main/java/erika/task/Deadline.java
+++ src/main/java/erika/task/Deadline.java
@@ -1,18 +1,28 @@
 package erika.task;
-
+/**
+ * Represents a deadline task
+ * Inherits Task class
+ */
 public class Deadline extends Task {
     protected String deadline;
-
+    /**
+     * @param description <code>String</code> that represents the description of the deadline
+     * @param deadline <code>String</code> that represents a textual representation of the deadline
+     */
     public Deadline(String description, String deadline) {
         super(description);
         this.deadline = deadline;
     }
-
+    /**
+     * Overrides the <code>toString</code> method and prints out the deadline in a specific format for printing to console
+     */
     @Override
     public String toString() {
         return String.format("[D][%s] %s (by: %s)", isDone ? "X":" ", description, deadline);
     }
-
+    /**
+     * Overrides the <code>toString</code> method and prints out the deadline in a specific format for writing to text file
+     */
     @Override
     public String generateFileLine() {
         return String.format("D,%s,%s,%s\n", isDone ? "1" : "0", description, deadline);
