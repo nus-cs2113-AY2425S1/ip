12ebf873faf6dfa877fef3f44da0939f675cc80d
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:12:43 +0800

    Add JavaDoc comments to AddEventCommand

--- src/main/java/erika/command/addcommand/AddEventCommand.java
+++ src/main/java/erika/command/addcommand/AddEventCommand.java
@@ -9,12 +9,24 @@ import java.io.IOException;
 public class AddEventCommand extends AddCommand {
     String from;
     String to;
+    /**
+     * @param description Textual description of the entry
+     * @param from Textual value representing the start date/time of the event
+     * @param to Textual value representing the end date/time of the event
+     */
     public AddEventCommand(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
-
+    /**
+     * Overrides the default Command method execute
+     * Adds a new <code>Event</code> object to the <code>TaskList</code> list
+     * Appends a new line to the text file in the host filesystem
+     * @param tasks TaskList object representing the tasks stored
+     * @param fileSystem FileSystem object used to interface with the file system of the host
+     * @throws IOException when there is an error in accessing the text file
+     */
     @Override
     public void execute(TaskList tasks, FileSystem fileSystem) throws IOException {
         Event newEvent = new Event(description, from, to);
