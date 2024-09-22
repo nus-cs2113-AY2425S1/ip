7458847cea19e679d577be94d9f28fa3ac355529
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 15:44:57 +0800

    Modify Settings to enable more robust error-checking of commands

--- src/main/java/erika/settings/Settings.java
+++ src/main/java/erika/settings/Settings.java
@@ -1,19 +1,15 @@
 package erika.settings;
-/**
- * Represents a Settings object used to store all constants
- */
-
 
 import java.time.format.DateTimeFormatter;
 
 public class Settings {
-    public static final int FROM_LENGTH_OFFSET = 6;
-    public static final int TO_LENGTH_OFFSET = 4;
-    public static final int BY_LENGTH_OFFSET = 4;
+    public static final int FROM_LENGTH_OFFSET = 7;
+    public static final int TO_LENGTH_OFFSET = 5;
+    public static final int BY_LENGTH_OFFSET = 5;
     public static final int SPACE_OFFSET = 1;
-    public static final int BY_REAR_OFFSET = 1;
-    public static final int FROM_REAR_OFFSET = 1;
-    public static final int TO_REAR_OFFSET = 1;
+    public static final int BY_REAR_OFFSET = 0;
+    public static final int FROM_REAR_OFFSET = 0;
+    public static final int TO_REAR_OFFSET = 0;
 
     public static final String DIRECTORY = "data";
     public static final String FILENAME = DIRECTORY + "/tasks.txt";
