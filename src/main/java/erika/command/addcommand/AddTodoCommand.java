e6c76739d074e77eac584f67a94173614933f9e2
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:12:53 +0800

    Add JavaDoc comments to AddTodoCommand

--- src/main/java/erika/command/addcommand/AddTodoCommand.java
+++ src/main/java/erika/command/addcommand/AddTodoCommand.java
@@ -1,6 +1,5 @@
 package erika.command.addcommand;
 
-import erika.console.Console;
 import erika.filesystem.FileSystem;
 import erika.task.Todo;
 import erika.tasklist.TaskList;
@@ -12,6 +11,14 @@ public class AddTodoCommand extends AddCommand{
         super(description);
     }
     @Override
+    /**
+     * Overrides the default Command method execute
+     * Adds a new <code>Todo</code> object to the <code>TaskList</code> list
+     * Appends a new line to the text file in the host filesystem
+     * @param tasks TaskList object representing the tasks stored
+     * @param fileSystem FileSystem object used to interface with the file system of the host
+     * @throws IOException when there is an error in accessing the text file
+     */
     public void execute(TaskList tasks, FileSystem fileSystem) throws IOException{
         Todo newTodo = new Todo(description);
         tasks.add(newTodo);
