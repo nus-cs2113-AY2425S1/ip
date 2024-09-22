44c24ec5b05e03b2b4049d53e43a35a82661b9cf
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:14:00 +0800

    Add JavaDoc comments to FormatErrorException

--- src/main/java/erika/exception/FormatErrorException.java
+++ src/main/java/erika/exception/FormatErrorException.java
@@ -1,6 +1,11 @@
 package erika.exception;
-
+/**
+ * Represents the exception thrown when the read file has an incorrect format
+ */
 public class FormatErrorException extends ErikaException {
+    /**
+     * @param detailedMessage A <code>String</code> detailed message regarding the possible sources of this error
+     */
     public FormatErrorException(String detailedMessage) {
         super("Error: command format error, " + detailedMessage);
     }
