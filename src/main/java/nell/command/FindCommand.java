b066b9dc1e4b430b6eed4eef85247e2985d7e26c
jemehgoh <jeremygoheh@gmail.com>
2024-09-22 15:27:03 +0800

    Add javadoc to methods in FindCommand

--- src/main/java/nell/command/FindCommand.java
+++ src/main/java/nell/command/FindCommand.java
@@ -8,11 +8,20 @@ import nell.TaskList;
 public class FindCommand extends Command{
     private final String keyword;
 
+    /**
+     * Constructs a new FindCommand object, with the specified task list and keyword
+     *
+     * @param tasks The specified task list
+     * @param keyword The specified keyword
+     */
     public FindCommand(TaskList tasks, String keyword) {
         super("find", tasks);
         this.keyword = keyword;
     }
 
+    /**
+     * Executes command - gets a list of tasks that contain keyword
+     */
     public void execute() {
         System.out.println("-> The matching tasks are as follows:");
         System.out.println(tasks.getMatchingTaskList(keyword));
