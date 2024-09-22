40d2699a6be4ab28633bcfde7f421a338b3c9fb8
telferang <telferang@gmail.com>
2024-09-22 17:22:55 +0800

    Add optional date parameter to list command
    Allow user to list Event and Deadline the is on the selected date

--- src/main/java/nova/command/ListCommand.java
+++ src/main/java/nova/command/ListCommand.java
@@ -4,15 +4,43 @@ import nova.TaskList;
 import nova.Ui;
 import nova.task.Task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
+    public static final String LIST_ALL_MESSAGE = "Listing all tasks";
+    public static final String LIST_TASK_DATE_MESSAGE = "Listing all task on ";
+    public static final String SPACING = "\n     ";
 
-    public static void execute(TaskList taskManager) {
-        Ui.displaySeparator();
-        for (int i = 0; i < Task.getNumberOfTasks(); i++) {
-            System.out.println("     " + (i + 1) + "." + taskManager.getTaskInfo(i));
+    public static void execute(String[] inputs, TaskList taskManager) {
+        try {
+            LocalDate date = LocalDate.parse(inputs[1]);
+            listDate(taskManager, date);
+        } catch (ArrayIndexOutOfBoundsException | DateTimeParseException e) {
+            listAll(taskManager);
         }
-        Ui.displaySeparator();
+
+    }
+
+    public static void listDate(TaskList taskList, LocalDate date) {
+        String result = LIST_TASK_DATE_MESSAGE + date;
+        int counter = 1;
+        for (int i = 0; i < Task.getNumberOfTasks(); i++) {
+            if (taskList.isDate(i, date)) {
+                result += SPACING + counter + "." + taskList.getTaskInfo(i);
+                counter++;
+            }
+        }
+        Ui.displayMessage(result);
+    }
+
+    public static void listAll(TaskList taskList) {
+        String result = LIST_ALL_MESSAGE;
+        for (int i = 0; i < Task.getNumberOfTasks(); i++) {
+            result += SPACING + (i + 1) + "." + taskList.getTaskInfo(i);
+        }
+        Ui.displayMessage(result);
     }
 }
