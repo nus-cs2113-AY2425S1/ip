525e04ce48a09e7200cedc9371f831e8aa9576f0
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:14:10 +0800

    Add JavaDoc comments to OutOfBoundsException

--- src/main/java/erika/exception/OutOfBoundsException.java
+++ src/main/java/erika/exception/OutOfBoundsException.java
@@ -1,5 +1,7 @@
 package erika.exception;
-
+/**
+ * Represents the exception thrown when the the supplied index of an indexing operation is out of bounds
+ */
 public class OutOfBoundsException extends ErikaException{
     public OutOfBoundsException() {
         super("Error: Supplied index is out of bounds");
