efaffd0ddda540e1dbe43ac19419a5116caa1c0a
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:11:54 +0800

    Add JavaDoc comments to ListCommand

--- src/main/java/erika/command/ListCommand.java
+++ src/main/java/erika/command/ListCommand.java
@@ -1,15 +1,23 @@
 package erika.command;
 
 import erika.exception.EmptyListException;
-import erika.exception.OutOfBoundsException;
 import erika.filesystem.FileSystem;
-import erika.console.Console;
 import erika.tasklist.TaskList;
 
+
 public class ListCommand extends Command{
+    /**
+     * @param tasks TaskList object representing the tasks stored
+     * @param fileSystem FileSystem object used to interface with the file system of the host
+     */
     public void execute(TaskList tasks, FileSystem fileSystem) throws EmptyListException {
         tasks.printListMessage();
     }
+
+    /**
+     * Checks if the command is an exit command
+     * @return <code>false</code> since ListCommands are not ExitCommands
+     */
     public boolean isExit() {
         return false;
     }
