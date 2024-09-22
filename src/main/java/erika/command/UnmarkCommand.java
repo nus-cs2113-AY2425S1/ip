86c8f47a232463d2cc04dfcbcccfc7d97dbe4b12
RCPilot1604 <ethan.chua1604@gmail.com>
2024-09-22 10:12:15 +0800

    Add JavaDoc comments to UnmarkCommand

--- src/main/java/erika/command/UnmarkCommand.java
+++ src/main/java/erika/command/UnmarkCommand.java
@@ -2,17 +2,26 @@ package erika.command;
 
 import erika.exception.OutOfBoundsException;
 import erika.filesystem.FileSystem;
-import erika.task.Task;
-import erika.ui.Ui;
 import erika.tasklist.TaskList;
 
 import java.io.IOException;
-
+/**
+ * Represents a specific "Unmark" command as interpreted from the user. An <code>Unmark</code> object corresponds
+ * to a user command to mark a <code>Task</code> object as not done
+ */
 public class UnmarkCommand extends Command{
     private int index;
+    /**
+     * @param index The index of the item the user wishes to unmark
+     */
     public UnmarkCommand(int index) {
         this.index = index;
     }
+    /**
+     * Overrides the default Command method execute
+     * @param tasks TaskList object representing the tasks stored
+     * @param fileSystem FileSystem object used to interface with the file system of the host
+     */
     public void execute(TaskList tasks, FileSystem fileSystem) throws OutOfBoundsException, IOException {
         if (index <= 0 || index > tasks.getTaskArraySize()) {
             throw new OutOfBoundsException();
@@ -20,6 +29,10 @@ public class UnmarkCommand extends Command{
         tasks.markEntry(index, false);
         fileSystem.updateFileSystemWithLocalTasks(tasks);
     }
+    /**
+     * Checks if the command is an exit command
+     * @return <code>false</code> since UnmarkCommands are not ExitCommands
+     */
     public boolean isExit() {
         return false;
     }
