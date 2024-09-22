e01c93d85d1a3a0f9a30b803dae4e75aa66ed63a
seanngja <ngjooansean@gmail.com>
2024-09-22 23:24:55 +0800

    Add ability to process dates and datetimes

--- src/main/java/dobby/tasks/Deadline.java
+++ src/main/java/dobby/tasks/Deadline.java
@@ -1,23 +1,70 @@
 package dobby.tasks;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
     protected String by;
+    protected LocalDate date;
+    protected LocalDateTime dateAndTime;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+
+        try {
+            dateAndTime = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        } catch (DateTimeParseException e) {
+            try {
+                date = LocalDate.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            } catch (DateTimeParseException e2) {
+                this.by = by;
+            }
+        }
     }
 
     public String getBy() {
-        return by;
+        if (dateAndTime != null) {
+            return dateAndTime.format(DateTimeFormatter.ofPattern("yyyy MM dd HH.mm"));
+        } else if (date != null) {
+            return date.format(DateTimeFormatter.ofPattern("yyyy MM dd"));
+        } else {
+            return by;
+        }
     }
 
     public void setBy(String by) {
         this.by = by;
+
+        try {
+            dateAndTime = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy MM dd HH.mm"));
+            date = null;
+        } catch (DateTimeParseException e) {
+            try {
+                date = LocalDate.parse(by, DateTimeFormatter.ofPattern("yyyy MM dd"));
+                dateAndTime = null;
+            } catch (DateTimeParseException e2) {
+                date = null;
+                dateAndTime = null;
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        String formattedTime;
+
+        if (dateAndTime != null) {
+            formattedTime = dateAndTime.format(DateTimeFormatter.ofPattern("dd/MM/yyyy, HH.mm"));
+        } else if (date != null) {
+            formattedTime = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        } else {
+            formattedTime = by;
+        }
+
+        return "[D]" + super.toString() + " (by: " + formattedTime + ")";
     }
 }
