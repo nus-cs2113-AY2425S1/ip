837a48a89fb8b2d9f163aeb14b20fa1c9d0c9241
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-09-23 19:36:18 +0800

    Add date-related attributes and methods to classes Deadline and Event

--- src/main/java/yapper/tasks/Deadline.java
+++ src/main/java/yapper/tasks/Deadline.java
@@ -2,36 +2,50 @@ package yapper.tasks;
 
 import yapper.io.StringStorage;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     // Additional Attributes
     protected String endDate;
+    protected LocalDateTime endDateTime;
 
-    // Constructor
-    public Deadline(String taskDesc,
-                    String endDate) {
+    // Constructors
+    public Deadline(String taskDesc, String endDate) {
         super(taskDesc);
-        this.endDate = endDate;
+        initializeEndDateTime(endDate);
     }
-    public Deadline(String taskDesc, boolean isDone,
-                    String endDate) {
+
+    public Deadline(String taskDesc, boolean isDone, String endDate) {
         super(taskDesc);
         this.isDone = isDone;
-        this.endDate = endDate;
+        initializeEndDateTime(endDate);
     }
-    // No Getters and Setters Yet
+
+    private void initializeEndDateTime(String endDateTime) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHHmm");
+        try {
+            this.endDateTime = LocalDateTime.parse(endDateTime, inputFormatter);
+        } catch (DateTimeParseException e) {
+            this.endDate = endDateTime;
+        }
+    }
 
     // Task Print Operations for Adding/Removing Data
     @Override
     public String taskToDisplay() {
-        return "[" + StringStorage.DEADLINE_SYMBOL + "]"
+        return "[" + StringStorage.DEADLINE_SYMBOL + "] "
                 + super.taskToDisplay() + ", by " + endDate;
     }
+
     // Task Conversion Operations for Saving/Loading Data
     @Override
     public String taskToString() {
         return StringStorage.DEADLINE_SYMBOL + " "
                 + StringStorage.COMBINE_USING_DELIMITER + " "
                 + super.taskToString() + " "
-                + StringStorage.COMBINE_USING_DELIMITER + " " + endDate;
+                + StringStorage.COMBINE_USING_DELIMITER + " "
+                + endDate;
     }
 }
--- src/main/java/yapper/tasks/Event.java
+++ src/main/java/yapper/tasks/Event.java
@@ -2,33 +2,56 @@ package yapper.tasks;
 
 import yapper.io.StringStorage;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
     // Additional Attributes
     protected String startDate;
     protected String endDate;
+    protected LocalDateTime startDateTime;
+    protected LocalDateTime endDateTime;
 
     // Constructor
-    public Event(String taskDesc,
-                 String startDate, String endDate) {
+    public Event(String taskDesc, String startDate, String endDate) {
         super(taskDesc);
-        this.startDate = startDate;
-        this.endDate = endDate;
+        initializeStartDateTime(startDate);
+        initializeEndDateTime(endDate);
     }
-    public Event(String taskDesc, boolean isDone,
-                 String startDate, String endDate) {
+
+    public Event(String taskDesc, boolean isDone, String startDate, String endDate) {
         super(taskDesc);
         this.isDone = isDone;
-        this.startDate = startDate;
-        this.endDate = endDate;
+        initializeStartDateTime(startDate);
+        initializeEndDateTime(endDate);
     }
-    // No Getters and Setters Yet
+
+    private void initializeStartDateTime(String startDateTime) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHHmm");
+        try {
+            this.startDateTime = LocalDateTime.parse(startDateTime, inputFormatter);
+        } catch (DateTimeParseException e) {
+            this.startDate = startDateTime;
+        }
+    }
+
+    private void initializeEndDateTime(String endDateTime) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHHmm");
+        try {
+            this.endDateTime = LocalDateTime.parse(endDateTime, inputFormatter);
+        } catch (DateTimeParseException e) {
+            this.endDate = endDateTime;
+        }
+    }
 
     // Task Print Operations for Adding/Removing Data
     @Override
     public String taskToDisplay() {
-        return "[" + StringStorage.EVENT_SYMBOL + "]"
+        return "[" + StringStorage.EVENT_SYMBOL + "] "
                 + super.taskToDisplay() + ", from " + startDate + " to " + endDate;
     }
+
     // Task Conversion Operations for Saving/Loading Data
     @Override
     public String taskToString() {
@@ -40,4 +63,5 @@ public class Event extends Task {
                 + StringStorage.COMBINE_USING_DELIMITER + " "
                 + endDate;
     }
+
 }
