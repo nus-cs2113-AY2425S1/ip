b8b95f874e6792991fd82acad2ac9122c8f3a61a
jemehgoh <jeremygoheh@gmail.com>
2024-09-23 11:47:41 +0800

    Add implementation for isOnDate to ToDo

--- src/main/java/nell/tasks/ToDo.java
+++ src/main/java/nell/tasks/ToDo.java
@@ -1,5 +1,7 @@
 package nell.tasks;
 
+import java.time.LocalDate;
+
 public class ToDo extends nell.tasks.Task {
 
     public ToDo(String description) {
@@ -13,5 +15,17 @@ public class ToDo extends nell.tasks.Task {
 
     public String toString() {
         return super.toString();
+    }
+
+    /**
+     * Returns true if the given task occurs on a given date.
+     * Always returns false as ToDo tasks are undated.
+     *
+     * @param date The specified date
+     * @return False in all cases
+     */
+    @Override
+    public boolean isOnDate(LocalDate date) {
+        return false;
     }
 }
