d197b4f58a38de0af2f54aadbc1878db3e6120a1
jemehgoh <jeremygoheh@gmail.com>
2024-09-24 08:37:24 +0800

    Add javadoc for methods in the ToDo class

--- src/main/java/nell/tasks/ToDo.java
+++ src/main/java/nell/tasks/ToDo.java
@@ -2,13 +2,27 @@ package nell.tasks;
 
 import java.time.LocalDate;
 
+/**
+ * Represents a ToDo task in the task list.
+ */
 public class ToDo extends Task {
     public static final String TASK_TYPE = "T";
 
+    /**
+     * Constructs a ToDo with the given description
+     *
+     * @param description The given description
+     */
     public ToDo(String description) {
         super(description, TASK_TYPE);
     }
 
+    /**
+     * Constructs a ToDo with the given data
+     *
+     * @param description The given description
+     * @param isDone Whether the task is done or not
+     */
     public ToDo(String description, boolean isDone) {
         super(description, isDone);
         this.type = TASK_TYPE;
