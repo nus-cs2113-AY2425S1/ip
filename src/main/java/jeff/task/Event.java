0fdd0b52e39f8df5de40d42cfc796e56dec40add
TheDinos <marcussvps2002@gmail.com>
2024-09-24 22:57:41 +0800

    Implemented dates and time for task

--- src/main/java/jeff/task/Event.java
+++ src/main/java/jeff/task/Event.java
@@ -1,22 +1,40 @@
 package jeff.task;
 
-public class Event extends Task{
-    protected String from;
-    protected String to;
+import jeff.exception.InvalidFormatException;
 
-    public Event(String description, String from, String to) {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Event extends Task{
+    protected LocalDate from;
+    protected LocalDate to;
+
+    public Event(String description, String from, String to) throws InvalidFormatException {
         super(description);
-        this.from = from;
-        this.to = to;
+        try {
+            this.from = LocalDate.parse(from);
+            this.to = LocalDate.parse(to);
+        } catch (DateTimeParseException e) {
+            throw new InvalidFormatException("Event date inputs are not formatted correctly...");
+        }
+    }
+
+    public String fileDate(LocalDate field) {
+        return field.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    public String userDate(LocalDate field) {
+        return field.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String fileContent() {
-        return "E" + super.fileContent() + " | " + from + " | " + to;
+        return "E" + super.fileContent() + " | " + fileDate(from) + " | " + fileDate(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + userDate(from) + " to: " + userDate(to) + ")";
     }
 }
