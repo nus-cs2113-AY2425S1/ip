13228d400039d778af51f0e3f7becf410fd413e2
ryan-txn <tanryan2002@gmail.com>
2024-09-25 00:44:36 +0800

    Add save and load functions

--- src/main/java/tommi/TaskList.java
+++ src/main/java/tommi/TaskList.java
@@ -6,6 +6,10 @@ public class TaskList {
 
     private static final ArrayList<Task> tasks = new ArrayList<>();  // Array to store tasks
 
+    public static ArrayList<Task> getTaskList() {
+        return tasks;
+    }
+
     public static void listTasks() {
         Tommi.printLine();
         System.out.println("Here are the tasks in your list:");
@@ -24,6 +28,10 @@ public class TaskList {
         Tommi.printLine();
     }
 
+    public static void addTaskWithoutMessage(Task task) {
+        tasks.add(task);
+    }
+
     public static void deleteTask(int index) {
         Tommi.printLine();
         System.out.println("I've removed the task: " + System.lineSeparator()
@@ -36,11 +44,9 @@ public class TaskList {
     public static void markTask(int index) {
         if (index >= 0 && index < tasks.size()) {
             Tommi.printLine();
+            Task updatedTask = tasks.get(index).updateIsDone(true);
+            tasks.set(index, updatedTask);
             System.out.println("Awesomesauce! I've marked this task as done:");
-            Task markedTask = tasks.get(index);
-            markedTask.updateIsDone(true);
-            deleteTask(index);
-            addTask(markedTask);
             System.out.println(tasks.get(index));
             Tommi.printLine();
         }
@@ -49,11 +55,9 @@ public class TaskList {
     public static void unmarkTask(int index) {
         if (index >= 0 && index < tasks.size()) {
             Tommi.printLine();
+            Task updatedTask = tasks.get(index).updateIsDone(false);
+            tasks.set(index, updatedTask);
             System.out.println("OK, I've marked this task as undone:");
-            Task markedTask = tasks.get(index);
-            markedTask.updateIsDone(false);
-            deleteTask(index);
-            addTask(markedTask);
             System.out.println(tasks.get(index));
             Tommi.printLine();
         }
