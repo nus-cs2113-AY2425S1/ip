d65ae50ca5de7deb1782a4830e4e73917f193dc0
CheeKiangg <ngchkiang@gmail.com>
2024-09-24 15:56:32 +0800

    feat: Add Parser class to handle command parsing
    - Introduced Parser class to encapsulate command parsing logic.
    - Added support for parsing commands like "todo", "deadline", "event", "delete", "mark", "unmark", and "bye".
    - This refactor follows SRP by isolating command parsing from the main application logic.

---
+++ src/main/java/codecatalyst/Parser.java
@@ -0,0 +1,62 @@
+package codecatalyst;
+
+import codecatalyst.command.*;
+import codecatalyst.exception.InvalidCommandException;
+
+public class Parser {
+    private static int TODO_LENGTH = 5;
+    private static int DEADLINE_LENGTH = 9;
+    private static int EVENT_LENGTH = 6;
+    private static int MARK_LENGTH = 5;
+    private static int UNMARK_LENGTH = 8;
+    private static int DELETE_LENGTH = 7;
+
+    public static Command parse(String fullCommand) throws Exception {
+        String commandWord = getCommand(fullCommand);
+
+        switch (commandWord) {
+        case "todo":
+            return new AddTodoCommand(fullCommand.substring(TODO_LENGTH));
+        case "deadline":
+            return new AddDeadlineCommand(fullCommand.substring(DEADLINE_LENGTH));
+        case "event":
+            return new AddEventCommand(fullCommand.substring(EVENT_LENGTH));
+        case "mark":
+            return new MarkCommand(fullCommand.substring(MARK_LENGTH));
+        case "unmark":
+            return new UnmarkCommand(fullCommand.substring(UNMARK_LENGTH));
+        case "delete":
+            return new deleteCommand(fullCommand.substring(DELETE_LENGTH));
+        case "bye":
+            return new ExitCommand();
+        case "list":
+            return new ListCommand();
+        default:
+            throw new InvalidCommandException("Invalid Command");
+        }
+    }
+
+    private static String getCommand(String fullCommand) {
+        if (fullCommand.equals("bye")){
+            return "bye";
+        } else if (fullCommand.equals("list")) {
+            return "list";
+        } else if (fullCommand.startsWith("mark ")) {
+            return "mark";
+        } else if (fullCommand.startsWith("unmark ")) {
+            return "unmark";
+        } else if (fullCommand.startsWith("todo ")) {
+            return "todo";
+        } else if (fullCommand.startsWith("deadline ")) {
+            return "deadline";
+        } else if (fullCommand.startsWith("event ")) {
+            return "event";
+        } else if (fullCommand.startsWith("delete")) {
+            return "delete";
+        } else {
+            return "invalid command";
+        }
+    }
+
+
+}
