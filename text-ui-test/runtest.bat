0240dfb5be55d791408ec262da14750da1140458
jemehgoh <jeremygoheh@gmail.com>
2024-09-24 08:04:52 +0800

    Update runtest.bat to account for list package

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -9,7 +9,7 @@ if exist ACTUAL.TXT del ACTUAL.TXT
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java\nell -Xlint:none -d ..\bin ..\src\main\java\nell\*.java ..\src\main\java\nell\tasks\*.java^
  ..\src\main\java\nell\storage\*.java ..\src\main\java\nell\common\*.java ..\src\main\java\nell\parser\*.java ^
- ..\src\main\java\nell\ui\*.java ..\src\main\java\nell\command\*.java
+ ..\src\main\java\nell\ui\*.java ..\src\main\java\nell\command\*.java ..\src\main\java\nell\list\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
