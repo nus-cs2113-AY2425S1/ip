4a04f7a0753fb643a0fed2d09dc710b145e2d760
CheeKiangg <ngchkiang@gmail.com>
2024-09-24 15:57:15 +0800

    feat: Add TaskList class to manage task operations
    - TaskList class introduced to manage the collection of tasks.
    - Includes methods for adding, removing, retrieving, and marking tasks as done/not done.
    - This refactor improves maintainability by encapsulating task-related operations in a dedicated class.

---
+++ src/main/java/codecatalyst/TaskList.java
@@ -0,0 +1,45 @@
+package codecatalyst;
+
+import codecatalyst.task.Task;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task removeTask(int index) {
+        return tasks.remove(index);
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    public void markTaskAsDone(int index) {
+        tasks.get(index).markAsDone();
+    }
+
+    public void markTaskAsNotDone(int index) {
+        tasks.get(index).markAsNotDone();
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
