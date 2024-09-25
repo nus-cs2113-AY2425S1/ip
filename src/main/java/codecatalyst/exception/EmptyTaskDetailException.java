31e61e7a702c8a76fe17e919c9462ef4ac5e6f2b
CheeKiangg <ngchkiang@gmail.com>
2024-09-25 10:24:03 +0800

    refactor: Rename InvalidTaskDescriptionException to InvalidTaskDetailException
    - better reflect its broader use case as it could be the Task description, start date or end date that is missing.

---
+++ src/main/java/codecatalyst/exception/EmptyTaskDetailException.java
@@ -0,0 +1,7 @@
+package codecatalyst.exception;
+
+public class EmptyTaskDetailException extends Exception {
+    public EmptyTaskDetailException(String message) {
+        super(message);
+    }
+}
