99f1eb63123b44414f0fa5c53fd91f91afd94f4f
hzxnancy <hzxnancy@gmail.com>
2024-09-25 17:27:44 +0800

    Update current and create new exceptions

---
+++ src/main/java/exceptions/IllegalIndexException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class IllegalIndexException extends Exception {
+    public IllegalIndexException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/exceptions/IllegalTaskException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class IllegalTaskException extends Exception {
+    public IllegalTaskException(String message) {
+        super(message);
+    }
+}
