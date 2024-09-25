31be5c5cc164a4dd25272f7faf5a4a24ec30a4dd
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-09-25 05:52:59 -0400

    samplw

--- src/main/java/AirBorder.java
+++ src/main/java/AirBorder.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import java.util.Scanner;
 
 
@@ -75,3 +76,15 @@ import java.util.Scanner;
             scanner.close();
         }
     }
+=======
+public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
+>>>>>>> Stashed changes
