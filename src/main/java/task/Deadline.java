9d15a9806ae509c21b2e2cbc46d86507b70f7e64
Toby-Yu <e1344677@u.nus.edu>
2024-09-26 21:36:33 +0800

    Add header comment for all class inside task package

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,17 +1,36 @@
 package task;
 
+/**
+ * Represents a task with a deadline.
+ */
 public class Deadline extends Task {
     protected String dueDate;
 
+    /**
+     * Constructor for a Deadline task.
+     *
+     * @param description The description of the task.
+     * @param dueDate     The due date of the task.
+     */
     public Deadline(String description, String dueDate) {
         super(description);
         this.dueDate = dueDate;
     }
 
+    /**
+     * Gets the due date of the deadline.
+     *
+     * @return The due date.
+     */
     public String getDueDate() {
         return dueDate;
     }
 
+    /**
+     * Returns a string representation of the deadline task.
+     *
+     * @return A string in the format "[D][X] description (by: dueDate)".
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + dueDate + ")";
--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,34 +1,68 @@
 package task;
 
+/**
+ * Represents a generic task with a description and a completion status.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor to create a Task with a given description.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
 
+    /**
+     * Gets the status icon for the task.
+     *
+     * @return "X" if the task is done, otherwise a space " ".
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Gets the description of the task.
+     *
+     * @return The task description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Checks if the task is done.
+     *
+     * @return true if the task is done, false otherwise.
+     */
     public boolean isDone() {
         return isDone;
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return A string in the format "[X] description" or "[ ] description".
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
