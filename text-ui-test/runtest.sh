18f2a12efdb480e1e1a0bcf01e1fea0a17265228
wx-03 <tngwenxi@gmail.com>
2024-09-27 16:14:43 +0800

    Update text UI test to delete data directory if it exists before running test

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,6 +12,11 @@ then
     rm ACTUAL.TXT
 fi
 
+if [ -e "./data" ]
+then
+    rm -rf data
+fi
+
 # compile the code into the bin folder, terminates if error occurred
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/luke/*.java ../src/main/java/luke/exceptions/*.java ../src/main/java/luke/tasks/*.java
 then
