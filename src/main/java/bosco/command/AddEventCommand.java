36de3da6caec8d53f3741aecdeb8ac29512de151
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-09-28 17:58:40 +0800

    Add JavaDoc comments to AddEventCommand

--- src/main/java/bosco/command/AddEventCommand.java
+++ src/main/java/bosco/command/AddEventCommand.java
@@ -3,13 +3,27 @@ package bosco.command;
 import bosco.task.Event;
 import bosco.ui.Ui;
 
+/**
+ * Represents the command to add a new Event.
+ */
 public class AddEventCommand extends Command {
     private final Event newEvent;
 
+    /**
+     * Class constructor.
+     *
+     * @param description Description of the new Event.
+     * @param from Start time of the new Event.
+     * @param to End time of the new Event.
+     */
     public AddEventCommand(String description, String from, String to) {
         this.newEvent = new Event(description, false, from, to);
     }
 
+    /**
+     * Overrides the default execute method to add the new Event.
+     * Prints message for the newly added Event.
+     */
     @Override
     public void execute() {
         tasks.addTask(newEvent);
