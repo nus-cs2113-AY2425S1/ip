a771dd7571fbc5a3ccd79d6986a51bef80a7c8e0
yeekian <53704991+yeekian@users.noreply.github.com>
2024-09-28 19:32:54 +0800

    Extract out closely related Storage code as a class

--- src/main/java/Mel.java
+++ src/main/java/Mel.java
@@ -15,12 +15,6 @@ public class Mel {
     private static final int DELETE_WORD_LEN = 6;
     private static final String LIST_FILE_PATH = ".\\data\\Mel.txt";
 
-    private static void writeToFile(String filePath, String textToAdd) throws IOException {
-        FileWriter fw = new FileWriter(filePath);
-        fw.write(textToAdd);
-        fw.close();
-    }
-
     public static void main(String[] args) throws IOException {
         Ui.printIntroMessage();
         
@@ -29,88 +23,13 @@ public class Mel {
         List userList = new List();
 
         try {
-            writerSetUp(LIST_FILE_PATH);
-            loadDataFromFile(LIST_FILE_PATH, userList); // Load saved tasks
+            Storage.writerSetUp(LIST_FILE_PATH);
+            Storage.loadDataFromFile(LIST_FILE_PATH, userList); // Load saved tasks
         } catch (IOException e) {
             System.out.println("An error occurred when setting up writer.");
         }
 
         getUserInput(in, userList);
-    }
-
-    private static void writerSetUp(String filePath) throws IOException {
-        File listFile = new File(filePath);
-        if (!listFile.exists()) {
-            File directory = listFile.getParentFile();
-            if (!directory.exists()) {
-                directory.mkdirs(); // Create directory if it doesn't exist
-            }
-            listFile.createNewFile(); // Create file if it doesn't exist
-        }
-    }
-
-    public static void loadDataFromFile(String filePath, List userList) {
-        File file = new File(filePath);
-        try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                parseAndAddItem(line, userList);
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + filePath);
-        }
-    }
-
-    private static void parseAndAddItem(String line, List userList) {
-        String[] parts = line.split(" \\| ");
-        String taskType = parts[0]; // T, D, E
-        boolean isDone = parts[1].equals("X");
-        String taskDescription = parts[2];
-
-
-        switch (taskType) {
-        case "T":
-            Todo todoTask = new Todo(taskDescription);
-            if (isDone) {
-                todoTask.markAsDone();
-            }
-            userList.itemArrayList.add(todoTask);
-            break;
-
-        case "D":
-            String deadlineDate = parts[3]; // Ignore remaining parts
-            Deadline deadlineTask = new Deadline(taskDescription, deadlineDate);
-            if (isDone) {
-                deadlineTask.markAsDone();
-            }
-            userList.itemArrayList.add(deadlineTask);
-            break;
-
-        case "E":
-            String eventStart = parts[3]; // Start date/time
-            String eventEnd = parts[4];   // End date/time
-            Event eventTask = new Event(taskDescription, eventStart, eventEnd);
-            if (isDone) {
-                eventTask.markAsDone();
-            }
-            userList.itemArrayList.add(eventTask);
-            break;
-
-        default:
-            System.out.println("Invalid task type in file: " + taskType);
-            break;
-        }
-    }
-
-
-    private static void saveListToFile(List userList) {
-        try {
-            writeToFile(LIST_FILE_PATH, userList.getFormattedTasks()); // getFormattedTasks returns a formatted String
-        } catch (IOException e) {
-            System.out.println("An error occurred while saving the list.");
-        }
     }
 
     private static void getUserInput(Scanner in, List userList) {
@@ -130,22 +49,22 @@ public class Mel {
             } else if (isMark(line)) {
                 userList.markItem(line);
                 Ui.printHorizontalLine();
-                saveListToFile(userList);
+                Storage.saveListToFile(LIST_FILE_PATH, userList);
 
             } else if (isUnmark(line)) {
                 userList.unmarkItem(line);
                 Ui.printHorizontalLine();
-                saveListToFile(userList);
+                Storage.saveListToFile(LIST_FILE_PATH, userList);
 
             } else if (isDelete(line)) {
                 userList.deleteItem(line);
                 Ui.printHorizontalLine();
-                saveListToFile(userList);
+                Storage.saveListToFile(LIST_FILE_PATH, userList);
 
             } else {
                 userList.addItem(line);
                 Ui.printHorizontalLine();
-                saveListToFile(userList);
+                Storage.saveListToFile(LIST_FILE_PATH, userList);
 
             }
         }
