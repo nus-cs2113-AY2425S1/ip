22c7af48b265d39df4e5aa92e34f6248a1467ae9
yeekian <53704991+yeekian@users.noreply.github.com>
2024-09-28 23:51:47 +0800

    Add JavaDoc comments to the code

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,19 +1,39 @@
 package task;
 
+/**
+ * Represents a deadline task with a description and a due date.
+ */
 public class Deadline extends Task {
 
     protected String by;
 
+    /**
+     * Constructs a Deadline with the specified description and due date.
+     *
+     * @param description The description of the deadline task.
+     * @param by The due date of the deadline task.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    /**
+     * Returns a string representation of the deadline task, including its completion status
+     * and description with the due date.
+     *
+     * @return A formatted string representation of the deadline task.
+     */
     @Override
     public String toString() {
         return ("[D][" + getDoneStatusIcon() + "] " + description + " (by: " + by + ")");
     }
 
+    /**
+     * Returns a formatted string representation of the deadline task for storage.
+     *
+     * @return A formatted string representing the deadline task suitable for saving to a file.
+     */
     @Override
     public String formattedTask() {
         return ("D | " + getDoneStatusIcon() + " | " + description + " | " + by);
--- src/main/java/task/Todo.java
+++ src/main/java/task/Todo.java
@@ -1,17 +1,38 @@
 package task;
 
+/**
+ * Represents a todo task with a description.
+ */
 public class Todo extends Task {
+
+    /**
+     * Constructs a Todo with the specified description.
+     *
+     * @param description The description of the todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of the todo task, including its completion status
+     * and description.
+     *
+     * @return A formatted string representation of the todo task.
+     */
     @Override
     public String toString() {
         return ("[T][" + getDoneStatusIcon() + "] " + description);
     }
 
+    /**
+     * Returns a formatted string representation of the todo task for storage.
+     *
+     * @return A formatted string representing the todo task suitable for saving to a file.
+     */
     @Override
     public String formattedTask() {
         return ("T | " + getDoneStatusIcon() + " | " + description);
     }
 }
+
