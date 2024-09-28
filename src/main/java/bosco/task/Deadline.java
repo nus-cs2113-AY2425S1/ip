4985c3b146bc84e4ca973b9bed485d779c3fa967
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-09-28 16:47:40 +0800

    Add JavaDoc comments to Todo, Deadline and Event

--- src/main/java/bosco/task/Deadline.java
+++ src/main/java/bosco/task/Deadline.java
@@ -1,17 +1,40 @@
 package bosco.task;
 
+/**
+ * Represents a Deadline, a subclass of Task.
+ * A <code>Deadline</code> object also contains a due time.
+ */
 public class Deadline extends Task {
     protected String by;
 
+    /**
+     * Constructs this class.
+     *
+     * @param description Description of this Deadline.
+     * @param isDone Whether this Deadline is done.
+     * @param by Time this Deadline is due.
+     */
     public Deadline(String description, boolean isDone, String by) {
         super(description, isDone);
         this.by = by;
     }
 
+    /**
+     * Gets the time this Deadline is due.
+     *
+     * @return String of when this Deadline is due.
+     */
     public String getBy() {
         return by;
     }
 
+    /**
+     * Overrides the <code>toString</code> method of Task
+     * to format the string representation of this Deadline.
+     *
+     * @return Formatted string of this Deadline,
+     * with Deadline indicator and due time.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
--- src/main/java/bosco/task/Event.java
+++ src/main/java/bosco/task/Event.java
@@ -1,23 +1,53 @@
 package bosco.task;
 
+/**
+ * Represents an Event, a subclass of Task.
+ * An <code>Event</code> object also contains
+ * a start time and end time.
+ */
 public class Event extends Task {
     protected String from;
     protected String to;
 
+    /**
+     * Constructs this class.
+     *
+     * @param description Description of this Event.
+     * @param isDone Whether this Event is done.
+     * @param from Start time of this Event.
+     * @param to End time of this Event.
+     */
     public Event(String description, boolean isDone, String from, String to) {
         super(description, isDone);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Gets the start time of this Event.
+     *
+     * @return String of the start time.
+     */
     public String getFrom() {
         return from;
     }
 
+    /**
+     * Gets the end time of this Event.
+     *
+     * @return String of the end time.
+     */
     public String getTo() {
         return to;
     }
 
+    /**
+     * Overrides the <code>toString</code> method of Task
+     * to format the string representation of this Event.
+     *
+     * @return Formatted string of this Event,
+     * with Event indicator, start time and end time.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
