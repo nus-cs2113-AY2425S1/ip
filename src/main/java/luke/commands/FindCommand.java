dfa2ddec45f62dcd11ab7c1fc89c211a8ce47c8a
wx-03 <tngwenxi@gmail.com>
2024-09-28 14:02:50 +0800

    Add FindCommand class

---
+++ src/main/java/luke/commands/FindCommand.java
@@ -0,0 +1,13 @@
+package luke.commands;
+
+import luke.TaskList;
+import luke.Ui;
+
+public class FindCommand extends Command{
+    public static final String COMMAND_WORD = "find";
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, String[] inputs) {
+
+    }
+}
