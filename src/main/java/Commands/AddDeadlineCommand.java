2f8644e41cc86aab42f925d95b4671f5a7d2b319
Atulteja <atulteja@hotmail.com>
2024-09-28 16:14:41 +0800

    branch-Level-9

--- src/main/java/Commands/AddDeadlineCommand.java
+++ src/main/java/Commands/AddDeadlineCommand.java
@@ -14,13 +14,13 @@ public class AddDeadlineCommand extends AddCommand {
     @Override
     public void execute(Storage storage) throws InvalidCommandException {
         String[] parts = userInput.split(" /by ");
-        if (parts[0].trim().isEmpty()) {
+        if (parts[0].substring(8).trim().isEmpty()) {
             throw new InvalidCommandException("The description of the deadline task cannot be empty");
         }
         if (parts.length < 2) {
             throw new InvalidCommandException("The deadline of the task cannot be empty");
         }
-        String taskContent = parts[0].trim();
+        String taskContent = parts[0].substring(8).trim();
         String deadline = parts[1].trim();
         Task task = new Deadline(taskContent, deadline);
         storage.storageInsert(task);
--- src/main/java/taskmanager/TaskLIst.java
+++
@@ -1,4 +0,0 @@
-package taskmanager;
-
-public class TaskLIst {
-}
