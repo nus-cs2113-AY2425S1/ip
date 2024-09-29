93010e5fb0ab96684e0b82ca0ed0964e36d2145f
louisjoety <kolouis002@gmail.com>
2024-09-29 15:23:36 +0800

    Add comments for TaskOperations

--- src/main/java/others/TaskOperations.java
+++ src/main/java/others/TaskOperations.java
@@ -1,7 +1,16 @@
 package others;
 
 public interface TaskOperations {
+    /**
+     * Marks task as done.
+     */
     void setAsDone();
+    /**
+     * Marks task as undone.
+     */
     void setAsUndone();
+    /**
+     * Returns a string version of the task.
+     */
     String toString();
 }
