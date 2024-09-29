f50a10ac2afa0631c2ed73fef7a39fea8aeb6867
louisjoety <kolouis002@gmail.com>
2024-09-29 15:31:10 +0800

    Add comments for UI

--- src/main/java/ui/EchoUI.java
+++ src/main/java/ui/EchoUI.java
@@ -6,16 +6,29 @@ import tasklist.TaskList;
 public class EchoUI {
     private static final String SEPARATOR = "_".repeat(30);
 
+    /**
+     * Displays the greeting message.
+     */
     public void displayGreeting() {
         String greetingMessage = "Hello! I'm Echo" + "\nWhat can I do for you?\n";
         printMessage(greetingMessage);
     }
 
+    /**
+     * Displays the exit message.
+     */
     public void displayExitMessage() {
         String exitMessage = "Bye. Hope to see you again soon!";
         printMessage(exitMessage);
     }
 
+    /**
+     * Processes user input until the user types "bye".
+     * Saves list of tasks upon exiting.
+     *
+     * @param parser The parser used to interpret the user input.
+     * @param taskList The task list that stores the tasks.
+     */
     public void runChat(Parser parser, TaskList taskList) {
         String userInput;
         do {
@@ -27,6 +40,11 @@ public class EchoUI {
         displayExitMessage();
     }
 
+    /**
+     * Prints input message.
+     *
+     * @param message The message to be printed.
+     */
     private void printMessage(String message) {
         System.out.println(SEPARATOR);
         System.out.println(message);
