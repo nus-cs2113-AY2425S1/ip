21f182827e5cb5655a1e528bea5e4deba796219c
Chiang-HuiXin <huixinchiang@gmail.com>
2024-09-29 14:05:00 +0800

    Simplify code in Eva.java
    Eva.java is simplified to only handling the starting of program and
    ending of program.
    This makes the main code shorter and improves readability

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
-
 public class Eva {
 
-    public static final String HORIZONTAL_LINE = "---------------------------------------------------------------";
     private final static TaskManager taskManager = new TaskManager();
     private final static Ui ui = new Ui();
+    private final static Parser parser = new Parser();
+    private final static CommandHandler commandHandler = new CommandHandler(taskManager);
 
     public static void main(String[] args) {
 
@@ -16,58 +15,18 @@ public class Eva {
             line = ui.readCommand();
 
             try {
-                switch (getCommand(line)) {
-                case "list":
-                    taskManager.printTaskList();
-                    break;
-                case "mark":
-                    taskManager.markTask(line);
-                    break;
-                case "unmark":
-                    taskManager.unmarkTask(line);
-                    break;
-                case "bye":
+                String command = parser.getCommand(line);
+                String[] parts = parser.parseArguments(line, command);
+
+                commandHandler.executeCommand(command, parts);
+
+                if (command.equals("bye")) {
                     ui.showGoodbyeMessage();
-                    return;
-                case "todo":
-                    taskManager.printTodo(line);
-                    break;
-                case "deadline":
-                    taskManager.printDeadline(line);
-                    break;
-                case "event":
-                    taskManager.printEvent(line);
-                    break;
-                case "delete":
-                    taskManager.deleteTask(line);
                     break;
                 }
             } catch (EvaException e) {
                 ui.showErrorMessage(e.getMessage());
             }
-        }
-    }
-
-    public static String getCommand(String line) throws EvaException {
-
-        if (line.equalsIgnoreCase("list")) {
-            return "list";
-        } else if (line.startsWith("mark")) {
-            return "mark";
-        } else if (line.startsWith("unmark")) {
-            return "unmark";
-        } else if (line.equalsIgnoreCase("bye")) {
-            return "bye";
-        } else if (line.startsWith("todo")) {
-            return "todo";
-        } else if (line.startsWith("deadline")) {
-            return "deadline";
-        } else if (line.startsWith("event")) {
-            return "event";
-        } else if (line.startsWith("delete")) {
-            return "delete";
-        } else {
-            throw new EvaException("Sorry!! I don't understand what does " + line + " means!");
         }
     }
 }
