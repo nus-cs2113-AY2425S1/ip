9b0a7916f30ccc3024c83b443136166cdd140138
3CCLY <cluyang22@hotmail.com>
2024-09-30 12:57:55 +0800

    Add JavaDoc to all Ran-specific exceptions

--- src/ran/exception/InvalidCommandException.java
+++ src/ran/exception/InvalidCommandException.java
@@ -1,5 +1,9 @@
 package ran.exception;
 
+/**
+ * Represents a exception for the Ran chatbot when user inputs an unrecognised command.
+ * Inherits from base <code>RanException</code> class.
+ */
 public class InvalidCommandException extends RanException {
 
 }
--- src/ran/exception/RanException.java
+++ src/ran/exception/RanException.java
@@ -1,5 +1,9 @@
 package ran.exception;
 
+/**
+ * Represents a generic exception for the Ran chatbot.
+ * To be used as base class for specific types of exceptions to inherit from.
+ */
 public class RanException extends Exception {
 
 }
