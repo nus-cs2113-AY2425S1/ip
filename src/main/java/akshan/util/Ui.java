fc629aaff93b5976e55a2eef0e311691a1f6b512
yijiano <zhangyijian2002@gmail.com>
2024-10-02 02:42:01 +0800

    Update JavaDocs, clean unused functions, fix minor bugs

--- src/main/java/akshan/util/Ui.java
+++ src/main/java/akshan/util/Ui.java
@@ -5,26 +5,35 @@ import java.util.Scanner;
 public final class Ui {
     private final Scanner sc = new Scanner(System.in);
 
+    /**
+     * Prints a line break.
+     */
     public void printLine() {
         Printer.printLine();
     }
 
+    /**
+     * Prints the welcome message and ASCII art upon starting Akshan.
+     */
     public void printInitMessage() {
         Printer.printInitMessage();
     }
 
+    /**
+     * Prints bye sequence upon exiting Akshan.
+     */
     public void printByeMessage() {
         Printer.printByeMessage();
     }
 
+    /**
+     * Scans for user input.
+     * @return The user input in string representation.
+     */
     public String getInput() {
         Printer.printLine();
         String input = this.sc.nextLine();
         Printer.printLine();
         return input;
-    }
-
-    public void closeScanner() {
-        this.sc.close();
     }
 }
