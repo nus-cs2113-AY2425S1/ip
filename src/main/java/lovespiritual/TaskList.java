893d63b3b0eae66e88cc494247e9c77b0a80bd4a
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-01 23:38:26 +0800

    Setup environment for functions in TaskList class
    Import classes and library
    Add seperator constant

--- src/main/java/lovespiritual/TaskList.java
+++ src/main/java/lovespiritual/TaskList.java
@@ -1,4 +1,13 @@
 package lovespiritual;
 
+import lovespiritual.exception.lovespiritualException;
+import lovespiritual.task.Deadline;
+import lovespiritual.task.Event;
+import lovespiritual.task.Task;
+import lovespiritual.task.Todo;
+import java.util.ArrayList;
+
 public class TaskList {
+    public static final String SEPARATOR = "_".repeat(30);
+
 }
