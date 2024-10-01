def006201689b699fa665850acf68f1b32533952
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-01 23:47:54 +0800

    Add filePath variable and constructor

--- src/main/java/lovespiritual/Storage.java
+++ src/main/java/lovespiritual/Storage.java
@@ -7,4 +7,10 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
 }
