fec4f2c29a60bb1a012000182a126f5910a544c3
ChunJia <chiew_chunjia@u.nus.edu>
2024-10-01 13:33:14 +0800

    Add JavaDoc comments for commands package

--- src/main/java/appal/commands/AddDeadlineCommand.java
+++ src/main/java/appal/commands/AddDeadlineCommand.java
@@ -12,22 +12,47 @@ import static appal.common.Utils.COMMAND_DEADLINE;
 import static appal.common.Utils.TASK_INDEX;
 import static appal.common.Utils.BY_INDEX;
 
+/**
+ * AddDeadlineCommand class adds a Deadline task,
+ * with the task description and deadline date, to the task list.
+ */
 public class AddDeadlineCommand extends Command {
     private String[] inputDetails;
     private boolean isFromUser;
 
+    /**
+     * Class constructor.
+     *
+     * @param inputDetails User's input that has been split into
+     *                     command type, task description and deadline date parameters
+     *                     by {@link appal.parser.Parser#extractInputDetails(String) extractInputDetails} method.
+     * @param isFromUser Indicates whether instruction to add task is from the user or from pre-saved tasks.
+     */
     public AddDeadlineCommand(String[] inputDetails, boolean isFromUser) {
         super(COMMAND_DEADLINE);
         this.inputDetails = inputDetails;
         this.isFromUser = isFromUser;
     }
 
+    /**
+     * Checks if task description is added as input by the user.
+     *
+     * @throws EmptyTaskException if task description is not specified.
+     */
     public void checkForTask() throws EmptyTaskException {
         if (inputDetails[TASK_INDEX] == null) {
             throw new EmptyTaskException();
         }
     }
 
+    /**
+     * Executes the command to add a new Deadline task to the task list.
+     *
+     * @param taskList Current task list tracked by Appal.
+     * @param ui Ui instance for Appal to show messages.
+     * @param storage Storage instance for Appal to handle task loading and storage.
+     * @throws AppalException if task description or deadline date is not specified.
+     */
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws AppalException {
         checkForTask();
--- src/main/java/appal/commands/DeleteCommand.java
+++ src/main/java/appal/commands/DeleteCommand.java
@@ -11,14 +11,32 @@ import static appal.common.Messages.DELETE_TASK_MESSAGE;
 import static appal.common.Utils.COMMAND_MARK;
 import static appal.common.Utils.TASK_INDEX;
 
+/**
+ * DeleteCommand class deletes a specified task from the task list.
+ */
 public class DeleteCommand extends Command {
     private String[] inputDetails;
 
+    /**
+     * Class constructor.
+     *
+     * @param inputDetails User's input that has been split into
+     *                     command type and index of task to be deleted parameters
+     *                     by {@link appal.parser.Parser#extractInputDetails(String) extractInputDetails} method.
+     */
     public DeleteCommand(String[] inputDetails) {
         super(COMMAND_MARK);
         this.inputDetails = inputDetails;
     }
 
+    /**
+     * Executes the command to delete a task, with a specified index, from the task list.
+     *
+     * @param taskList Current task list tracked by Appal.
+     * @param ui Ui instance for Appal to show messages.
+     * @param storage Storage instance for Appal to handle task loading and storage.
+     * @throws AppalException if an invalid task index is inputted by the user.
+     */
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws AppalException {
         try {
