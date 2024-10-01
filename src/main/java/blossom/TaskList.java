460a5255c696ca43e4d3b7f4d1fa119d8e14918e
coraleaf0602 <zhangco@tcd.ie>
2024-10-01 19:03:21 +0800

    Add TaskList class to manage with task list operations

---
+++ src/main/java/blossom/TaskList.java
@@ -0,0 +1,94 @@
+package blossom;
+
+import blossom.task.Deadline;
+import blossom.task.Event;
+import blossom.task.Task;
+import blossom.task.Todo;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    private final int LENGTH_OF_TODO = 5;
+    private final int LENGTH_OF_DEADLINE = 9;
+    private final int LENGTH_OF_EVENT = 6;
+    private ArrayList<Task> listOfTasks;
+    private final String HORIZONTAL_LINE = "____________________________________________________________";
+    private Ui ui;
+
+    TaskList(Ui ui) {
+        this.listOfTasks = new ArrayList<>();
+        this.ui = ui;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return listOfTasks;
+    }
+
+    public void markAndUnmarkItem(int itemIndex, String action) {
+        itemIndex--;
+        // If this item is marked - change boolean
+        Task item = this.listOfTasks.get(itemIndex);
+        if (item != null) {
+            System.out.println(HORIZONTAL_LINE);
+            if(action.equalsIgnoreCase("mark")) {
+                item.markAsDone();
+                System.out.println("Yayy~~ Good job in getting this done!");
+                System.out.println(item);
+            } else {
+                item.markAsUndone();
+                System.out.println("Hope you get this done soon! :D");
+                System.out.println(item);
+            }
+            System.out.println(HORIZONTAL_LINE);
+        }
+    }
+
+    public void addTask (String input) throws BlossomException {
+        System.out.println(HORIZONTAL_LINE);
+        catchInputErrors(input);
+        Task item = createTask(input);
+        System.out.println("Ok!! I've added this task *\\ (>o<) /*");
+        System.out.println(item.toString());
+        this.listOfTasks.add(item);
+        ui.printNumberOfTasks(this.listOfTasks);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public Task createTask(String input) throws BlossomException {
+        Task item;
+        if(input.contains("todo")) {
+            item = new Todo(input.substring(LENGTH_OF_TODO));
+        } else if (input.contains("deadline")) {
+            String[] parts = input.substring(LENGTH_OF_DEADLINE).split(" /by ");
+            item = new Deadline(parts[0], parts[1]);
+        } else if (input.contains("event")) {
+            String[] parts = input.substring(LENGTH_OF_EVENT).split(" /from | /to ");
+            if(parts.length < 3) throw new BlossomException("Event must include both start and end times.");
+            item = new Event(parts[0], parts[1], parts[2]);
+        } else {
+            throw new BlossomException("Sorry~~ idk what that means ( • ᴖ • ｡)");
+        }
+        return item;
+    }
+
+    public void catchInputErrors(String input) throws BlossomException {
+        if (input == null || input.trim().isEmpty()) {
+            throw new BlossomException("Oopsie!!! (;° ロ°) The description of a task cannot be empty~~ ");
+        } else if (input.trim().equals("todo")) {
+            throw new BlossomException("Oopsie!!! (;° ロ°) The description of a todo cannot be empty~~ ");
+        } else if (input.trim().equals("deadline")) {
+            throw new BlossomException("Oopsie!!! (;° ロ°) The description of a deadline cannot be empty~~ ");
+        } else if (input.trim().equals("event")) {
+            throw new BlossomException("Oopsie!!! (;° ロ°) The description of an event cannot be empty~~ ");
+        }
+    }
+
+    public void deleteTask(int indexOfTask) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Removing this task now! ᕙ(  •̀ ᗜ •́  )ᕗ ");
+        System.out.println(this.listOfTasks.get(indexOfTask-1).toString());
+        System.out.println(HORIZONTAL_LINE);
+        this.listOfTasks.remove(indexOfTask-1);
+        ui.printNumberOfTasks(this.listOfTasks);
+    }
+}
