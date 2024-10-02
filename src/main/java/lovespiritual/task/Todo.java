f1c8857c9b173cd4a77ffebb3e6b191927e25fc6
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-02 23:37:17 +0800

    Add Javadoc for Todo class

--- src/main/java/lovespiritual/task/Todo.java
+++ src/main/java/lovespiritual/task/Todo.java
@@ -1,10 +1,23 @@
 package lovespiritual.task;
 
+/**
+ * Represents a Todo task, which only has a description.
+ */
 public class Todo extends Task {
+    /**
+     * Constructs a new Todo task with the specified description.
+     *
+     * @param description Description of the task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns the string representation of the specific Todo task, including the type.
+     *
+     * @return String in the format "[T][status] description".
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
