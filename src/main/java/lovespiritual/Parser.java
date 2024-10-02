756410d26557da307632b43f34b036c053dd1e68
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-02 11:17:25 +0800

    Add parseCommand function
    Handle input parsing through normalising input by trimming and converting it to lowercase

--- src/main/java/lovespiritual/Parser.java
+++ src/main/java/lovespiritual/Parser.java
@@ -3,4 +3,26 @@ package lovespiritual;
 import lovespiritual.exception.lovespiritualException;
 
 public class Parser {
+    public static String parseCommand(String input) throws lovespiritualException {
+        String command = input.trim().toLowerCase();
+        if (command.equals("bye")) {
+            return "bye";
+        } else if (command.equals("list")) {
+            return "list";
+        } else if (command.startsWith("mark")) {
+            return "mark";
+        } else if (command.startsWith("unmark")) {
+            return "unmark";
+        } else if (command.startsWith("todo")) {
+            return "todo";
+        } else if (command.startsWith("deadline")) {
+            return "deadline";
+        } else if (command.startsWith("event")) {
+            return "event";
+        } else if (command.startsWith("delete")) {
+            return "delete";
+        } else {
+            throw new lovespiritualException("(^_^) Let's get started with a command!");
+        }
+    }
 }
