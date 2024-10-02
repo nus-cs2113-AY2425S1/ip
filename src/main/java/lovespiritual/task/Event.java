a955c13681b67f22660c450da6f43b123b06f671
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-02 23:37:42 +0800

    Add Javadoc for Event class

--- src/main/java/lovespiritual/task/Event.java
+++ src/main/java/lovespiritual/task/Event.java
@@ -1,15 +1,32 @@
 package lovespiritual.task;
 
+/**
+ * Represents an Event task, which has a description, start time, and end time.
+ */
 public class Event extends Task {
+    /** Start time of the event. */
     public String from;
+    /** End time of the event. */
     public String to;
 
+    /**
+     * Constructs a new Event task with the specified description, start time, and end time.
+     *
+     * @param description Description of the event.
+     * @param from Start time of the event.
+     * @param to End time of the event.
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Returns the string representation of the specific Event task, including the type, start time  and end time.
+     *
+     * @return String in the format "[E][status] description (from: start time, to: end time)".
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + ", to: " + to + ")";
