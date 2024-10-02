af7ff29a576f7c9f399f2877cfd61f79fd8e4a29
LuxLucky7 <e1451199@u.nus.edu>
2024-10-03 00:47:45 +0800

    Initial level 7

--- src/main/Edith/Main.java
+++ src/main/Edith/Main.java
@@ -1,28 +1,19 @@
-import java.io.IOException;
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import UserInteraction.ChatBot;
+import static File.FileFunctions.*;
 
-import static File.FileFunctions.printFileContents;
 
 public class Main {
-    Scanner sc = new Scanner(System.in);
-
-
-
-
     public static void main(String[] args) {
-        File f = new File("data/tasks.txt");
+        File f = new File("./data/tasks.txt");
+        getFileProperties(f);
         String absolutePath = f.getAbsolutePath();
         try {
             printFileContents(absolutePath, f);
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
         }
-        //System.out.println("file exists?: " + f.exists());
-        //System.out.println("is Directory?: " + f.isDirectory());
         ChatBot edith = new ChatBot("Edith");
         edith.giveIntroduction();
         edith.interactWithUser();
