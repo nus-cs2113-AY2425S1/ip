4c7f2de9fb6d8c3739db56faf4822615fa1b20b0
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-02 23:39:25 +0800

    Add Javadoc for lovespiritualException class

--- src/main/java/lovespiritual/exception/lovespiritualException.java
+++ src/main/java/lovespiritual/exception/lovespiritualException.java
@@ -1,6 +1,15 @@
 package lovespiritual.exception;
 
+/**
+ * Represents an exception specific to the application.
+ * Thrown when an error occurs in the logic or the user input.
+ */
 public class lovespiritualException extends Exception {
+    /**
+     * Constructs a new lovespiritualException with a specified error message.
+     *
+     * @param message Custom error message that explains the reason for the error.
+     */
     public lovespiritualException(String message) {
         super(message);
     }
