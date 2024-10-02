a0ec7da61bb9023613c7071b2c5e03875bbc5365
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-03 03:21:33 +0800

    Add new class "ListCommand" which inherits from Command class
    This command prints the contents of the ArrayList to the console terminal.

---
+++ src/main/java/atom/ListCommand.java
@@ -0,0 +1,14 @@
+package atom;
+
+public class ListCommand extends Command{
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.printList(tasks);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
