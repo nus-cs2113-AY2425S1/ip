52c71d8d8c3b770e2044bb8bdff3288877ba080b
Ridiculouswifi <thtan02@gmail.com>
2024-10-03 13:20:02 +0800

    Added Javadoc for Task.java

--- src/main/java/freedom/tasks/Task.java
+++ src/main/java/freedom/tasks/Task.java
@@ -2,17 +2,30 @@ package freedom.tasks;
 
 import freedom.ui.UiTask;
 
+/**
+ * Class for storing all related information to a specific task.
+ */
 public class Task {
     protected static final UiTask ui = new UiTask();
 
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor for <code>Task</code>.
+     *
+     * @param description Task description.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     *
+     * @throws Exception If task is already marked as done.
+     */
     public void markDone() throws Exception {
         try {
             if (isDone) {
@@ -25,6 +38,11 @@ public class Task {
         }
     }
 
+    /**
+     * Marks the task as undone.
+     *
+     * @throws Exception If task is already marked as not done.
+     */
     public void markUndone() throws Exception {
         try {
             if (!isDone) {
@@ -37,6 +55,11 @@ public class Task {
         }
     }
 
+    /**
+     * Generates the icon for task status.
+     *
+     * @return <code>String</code> format "X" or " ".
+     */
     public String getStatusIcon() {
         return isDone ? "X" : " ";
     }
@@ -49,14 +72,30 @@ public class Task {
         return description;
     }
 
+    /**
+     * Generates a <code>String</code> format of <code>Task</code> for printing in CLI.
+     *
+     * @return <code>String</code> with <code>Task</code> details.
+     */
     public String generateTaskLine() {
         return "[" + getStatusIcon() + "] " + getDescription();
     }
 
+    /**
+     * Generates a <code>String</code> format of <code>Task</code> for writing to storage text file.
+     *
+     * @return <code>String</code> with <code>Task</code> details.
+     */
     public String generateStorageLine() {
         return getStatusIcon() + " | " + getDescription();
     }
 
+    /**
+     * Checks if the <code>Task</code> contains a specified keyword.
+     *
+     * @param keyword user-defined keyword.
+     * @return <code>true</code> if keyword is found, <code>false</code> otherwise.
+     */
     public boolean containKeyword(String keyword) {
         return getDescription().contains(keyword);
     }
