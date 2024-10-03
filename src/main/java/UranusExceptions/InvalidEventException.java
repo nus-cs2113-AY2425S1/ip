b18d359f32eea8820acff2ad4723f6f1292026c5
C2linaung <c2linaung.main@gmail.com>
2024-10-03 15:16:47 +0800

    complete all comments

--- src/main/java/UranusExceptions/InvalidEventException.java
+++ src/main/java/UranusExceptions/InvalidEventException.java
@@ -1,10 +1,26 @@
 package UranusExceptions;
 
+/**
+ * Represents an exception that is thrown when the user
+ * provides an invalid event format.
+ * It extends the UranusExceptions class and provides
+ * a specific error message with the correct event format.
+ */
 public class InvalidEventException extends UranusExceptions {
+    /**
+     * Constructs a new InvalidEventException.
+     * This exception is triggered when the user provides an incorrect event format.
+     */
     public InvalidEventException() {
         super();
     }
 
+    /**
+     * Returns a specific error message indicating that the event format is invalid.
+     * Provides the correct format for the user to follow.
+     *
+     * @return A string message specifying the correct formats for events.
+     */
     @Override
     public String getMessage() {
         return "Event format is invalid! \n" +
