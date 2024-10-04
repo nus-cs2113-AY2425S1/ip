0c98a5cc1b90c1b64b39ca518141c95176a0d74d
fuyixuan <yixuanfu762@gmail.com>
2024-10-04 20:46:03 +0800

    convert the time

--- src/commands/AddCommand.java
+++ src/commands/AddCommand.java
@@ -5,8 +5,11 @@ import storage.Storage;
 import tasklist.TaskList;
 import ui.Ui;
 import task.*;
-
 import static main.Sirius.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 
 /**
  * AddCommand is responsible for adding a new task (Todo, Deadline, or Event) based on user input.
@@ -18,6 +21,36 @@ public class AddCommand extends Command {
     public AddCommand(String userInput) {
         this.userInput = userInput;
     }
+
+    /**
+     * Parses the given time string in two possible formats ("dd/MM/yyyy" or "yyyy-MM-dd").
+     * If the string matches one of these formats, it will be parsed into a {@link LocalDate} and
+     * formatted into the output format "MMM dd yyyy" (e.g., "Oct 15 2019").
+     * If the input string does not match either format, it will be returned as-is.
+     *
+     * @param time The time string to be parsed and formatted.
+     * @return The formatted date string in "MMM dd yyyy" format if parsing is successful,
+     *         or the original string if it does not match any of the expected formats.
+     */
+    public String dealWithTimeFormat(String time){
+        DateTimeFormatter inputFormat1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter inputFormat2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        LocalDate date = null;
+        try {
+            date = LocalDate.parse(time, inputFormat1);
+        } catch (DateTimeParseException e) {
+            // If the first format fails, try the second format (yyyy-MM-dd)
+            try {
+                date = LocalDate.parse(time, inputFormat2);
+            } catch (DateTimeParseException ex) {
+                // If both formats fail, just use the initial format
+                return time;
+            }
+        }
+        return date.format(outputFormat);
+    }
+
 
     /**
      * Executes the AddCommand by parsing the user input and adding the corresponding task to the TaskList.
@@ -48,8 +81,8 @@ public class AddCommand extends Command {
                     } else if (indexOfBy == -1) {  // cannot find "/by"
                         throw new InvalidTaskContentException("You should declare '/by' for deadline");
                     } else {
-                        String byTime = slashCommand[1].replace("by", EMPTY).trim();
-                        tasks.addTask(new Deadline(taskName, false, byTime));
+                        String byTime = userInput.substring(indexOfBy).replace("/by", EMPTY).trim();;
+                        tasks.addTask(new Deadline(taskName, false, dealWithTimeFormat(byTime)));
                     }
                     break;
                 case EVENT:
@@ -60,9 +93,9 @@ public class AddCommand extends Command {
                     } else if (indexOfFrom == -1 || indexOfTo == -1) {
                         throw new InvalidTaskContentException("You should declare '/from' and '/to' for event");
                     } else {
-                        String fromTime = slashCommand[1].replace("from", EMPTY).trim();
-                        String toTime = slashCommand[2].replace("to", EMPTY).trim();
-                        tasks.addTask(new Event(taskName, false, fromTime, toTime));
+                        String fromTime = userInput.substring(indexOfFrom, indexOfTo).replace("/from", EMPTY).trim();
+                        String toTime = userInput.substring(indexOfTo).replace("/to", EMPTY).trim();;
+                        tasks.addTask(new Event(taskName, false, dealWithTimeFormat(fromTime), dealWithTimeFormat(toTime)));
                     }
             }
             ui.showTaskAdded(tasks, tasks.getListSize());
