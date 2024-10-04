c35b40ffffff4f7427d2b4beb929d2dd4252851b
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-05 03:58:32 +0800

    Add method in "DateTimeParser" class to validate date and time entered by user
    This method first checks whether the date time format in the user command (DEADLINE and EVENT) is valid and throws the corresponding exception if it's not. Next, it checks for the 5 different parameters in the date time input. They are "day", "month" and "year" for DATE and "hour" and "min" for TIME. If any of the parameters are invalid (non-integers or contain extra non-integer characters), the parse method will throw an exception and hence the input is deemed invalid.

--- src/main/java/atom/parser/DateTimeParser.java
+++ src/main/java/atom/parser/DateTimeParser.java
@@ -1,4 +1,31 @@
 package atom.parser;
 
 public class DateTimeParser {
+    public static boolean isValidDateTime(String date, Ui ui) throws
+            ArrayIndexOutOfBoundsException, NumberFormatException{
+        //format example: "12/11/2024 17:30"
+        date = date.trim();
+        String[] dateTimeParams = date.split(" ");
+
+        String[] timeParams = dateTimeParams[1].split(":");
+        String hourString = timeParams[0].trim();
+        String minString = timeParams[1].trim();
+
+        String[] dateParams = dateTimeParams[0].split("/");
+        String dayString = dateParams[0].trim();
+        String monthString = dateParams[1].trim();
+        String yearString = dateParams[2].trim();
+
+        int hour = Integer.parseInt(hourString);
+        int min = Integer.parseInt(minString);
+
+        int day = Integer.parseInt(dayString);
+        int month = Integer.parseInt(monthString);
+        int year = Integer.parseInt(yearString);
+
+        boolean isValidDate = isValidDay(day, ui) && isValidMonth(month, ui) && isValidYear(year, ui);
+        boolean isValidTime = isValidHour(hour, ui) && isValidMinute(min, ui);
+
+        return isValidDate && isValidTime;
+    }
 }
