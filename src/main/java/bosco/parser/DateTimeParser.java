03455e7556976949e63f73dfd6e3cd965a480174
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-10-05 22:06:28 +0800

    Add JavaDoc comments to DateTimeParser

--- src/main/java/bosco/parser/DateTimeParser.java
+++ src/main/java/bosco/parser/DateTimeParser.java
@@ -10,6 +10,10 @@ import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Represents a date time parser that parses strings into LocalDateTime objects,
+ * and formats LocalDateTime objects into strings for UI output.
+ */
 public class DateTimeParser {
     // List of acceptable patterns for date-time input
     private static final List<DateTimeFormatter> dateTimeFormatters = Arrays.asList(
@@ -19,16 +23,25 @@ public class DateTimeParser {
             DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm"));
     // List of acceptable patterns for date only input
     private static final List<DateTimeFormatter> dateOnlyFormatters = Arrays.asList(
+            DateTimeFormatter.ofPattern("MMM dd yyyy"),
             DateTimeFormatter.ofPattern("yyyy-MM-dd"),
             DateTimeFormatter.ofPattern("dd/MM/yyyy"),
             DateTimeFormatter.ofPattern("MM-dd-yyyy"));
 
+    /**
+     * Parses input string into LocalDateTime object according to provided patterns.
+     * If hour and minute not provided, the method autofills with 23:59.
+     *
+     * @param input String of datetime to be parsed.
+     * @return LocalDateTime object.
+     * @throws IllegalArgumentException If input string doesn't match any of the provided patterns.
+     */
     public static LocalDateTime parseDateTime(String input) {
         for (DateTimeFormatter formatter: dateTimeFormatters) {
             try {
                 return LocalDateTime.parse(input, formatter);
             } catch (DateTimeException e) {
-                // Ignore and try next formatter
+                // If input doesn't match current formatter, ignore and try next formatter
             }
         }
         for (DateTimeFormatter formatter: dateOnlyFormatters) {
@@ -36,12 +49,18 @@ public class DateTimeParser {
                 LocalDate date = LocalDate.parse(input, formatter);
                 return date.atTime(LocalTime.MAX);
             } catch (DateTimeParseException e) {
-                // Ignore and try next formatter
+                // If input doesn't match current formatter, ignore and try next formatter
             }
         }
         throw new IllegalArgumentException();
     }
 
+    /**
+     * Formats the LocalDateTime object into a string.
+     *
+     * @param dateTime LocalDateTime object to be formatted.
+     * @return String representation with format "MMM dd yyyy h.mma".
+     */
     public static String formatDateTime(LocalDateTime dateTime) {
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy h.mma");
         return dateTime.format(outputFormatter);
