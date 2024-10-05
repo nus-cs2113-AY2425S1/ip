b57e9529422ad98f4f928f10e0d141e0badb3a6e
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-05 08:58:12 +0800

    Add date and time format to deadline task

--- src/main/java/lovespiritual/task/Deadline.java
+++ src/main/java/lovespiritual/task/Deadline.java
@@ -1,15 +1,18 @@
 package lovespiritual.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    public String by;
+    public LocalDateTime  by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma")) + ")";
     }
 }
