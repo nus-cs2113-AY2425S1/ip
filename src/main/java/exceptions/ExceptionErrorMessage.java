052c642262dfc39237311ca93e25c0b247679f99
TPH777 <pinghuitan80@gmail.com>
2024-10-08 00:22:05 +0800

    Add javadoc comments

--- src/main/java/exceptions/ExceptionErrorMessage.java
+++ src/main/java/exceptions/ExceptionErrorMessage.java
@@ -1,5 +1,11 @@
 package exceptions;
 
+/**
+ * The ExceptionErrorMessage class holds the possible exceptions to
+ * print to the user in Ui.
+ *
+ * @author Tan Ping Hui
+ */
 public class ExceptionErrorMessage {
     public static final String INVALID_DATE_FORMAT = "Invalid Date Format (Use YYYY-MM-DD)";
     public static final String EMPTY_COMMAND_MESSAGE = "No command given";
