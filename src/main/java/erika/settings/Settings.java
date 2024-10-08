c91a0f1dc6dc8f739e3ffb6c2aadf43e19e93a59
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-08 08:35:19 +0800

    Add more date formats

--- src/main/java/erika/settings/Settings.java
+++ src/main/java/erika/settings/Settings.java
@@ -7,15 +7,15 @@ import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 public class Settings {
+    public static final int TODO_LENGTH_OFFSET = 4;
+    public static final int DEADLINE_LENGTH_OFFSET = 8;
+    public static final int EVENT_LENGTH_OFFSET = 5;
     public static final int FROM_LENGTH_OFFSET = 5;
     public static final int TO_LENGTH_OFFSET = 3;
     public static final int BY_LENGTH_OFFSET = 3;
-    public static final int SPACE_OFFSET = 1;
     public static final int BY_REAR_OFFSET = 0;
     public static final int FROM_REAR_OFFSET = 0;
     public static final int TO_REAR_OFFSET = 0;
-    public static final int EVENT_OFFSET = 6;
-    public static final int DEADLINE_OFFSET = 9;
 
     public static final String DIRECTORY = "data";
     public static final String FILENAME = DIRECTORY + "/tasks.txt";
@@ -34,10 +34,18 @@ public class Settings {
                 DateTimeFormatter.ofPattern("MM/dd/yyyy"),
                 DateTimeFormatter.ofPattern("MM-dd-yyyy"),
                 DateTimeFormatter.ofPattern("dd-MM-yyyy"),
+                DateTimeFormatter.ofPattern("dd/MM/yy"),
+                DateTimeFormatter.ofPattern("MM/dd/yy"),
+                DateTimeFormatter.ofPattern("MM-dd-yy"),
+                DateTimeFormatter.ofPattern("dd-MM-yy"),
                 DateTimeFormatter.ofPattern("MMM dd yyyy", Locale.ENGLISH),
                 DateTimeFormatter.ofPattern("MMM dd yyyy", Locale.GERMAN),
                 DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH),
-                DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.GERMAN)
+                DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.GERMAN),
+                DateTimeFormatter.ofPattern("MMM dd yy", Locale.ENGLISH),
+                DateTimeFormatter.ofPattern("MMM dd yy", Locale.GERMAN),
+                DateTimeFormatter.ofPattern("dd MMM yy", Locale.ENGLISH),
+                DateTimeFormatter.ofPattern("dd MMM yy", Locale.GERMAN)
         };
         for (DateTimeFormatter formatter : formatters) {
             try {
@@ -57,10 +65,20 @@ public class Settings {
                 DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm"),
                 DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm"),
                 DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"),
+                DateTimeFormatter.ofPattern("yy-MM-dd HH:mm"),
+                DateTimeFormatter.ofPattern("dd/MM/yy HH:mm"),
+                DateTimeFormatter.ofPattern("MM-dd-yy HH:mm"),
+                DateTimeFormatter.ofPattern("MM/dd/yy HH:mm"),
+                DateTimeFormatter.ofPattern("MM-dd-yy HH:mm"),
+                DateTimeFormatter.ofPattern("dd-MM-yy HH:mm"),
                 DateTimeFormatter.ofPattern("MMM dd yyyy h.mma", Locale.ENGLISH),
                 DateTimeFormatter.ofPattern("MMM dd yyyy h.mma", Locale.GERMAN),
                 DateTimeFormatter.ofPattern("dd MMM yyyy h.mma", Locale.ENGLISH),
-                DateTimeFormatter.ofPattern("dd MMM yyyy h.mma", Locale.GERMAN)
+                DateTimeFormatter.ofPattern("dd MMM yyyy h.mma", Locale.GERMAN),
+                DateTimeFormatter.ofPattern("MMM dd yy h.mma", Locale.ENGLISH),
+                DateTimeFormatter.ofPattern("MMM dd yy h.mma", Locale.GERMAN),
+                DateTimeFormatter.ofPattern("dd MMM yy h.mma", Locale.ENGLISH),
+                DateTimeFormatter.ofPattern("dd MMM yy h.mma", Locale.GERMAN)
         };
         for (DateTimeFormatter formatter : formatters) {
             try {
