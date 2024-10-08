6254d1bc84f91dd1f96a80787c9e4eaac80975e9
shaunngoh <shaungoh682@gmail.com>
2024-10-08 14:09:56 +0800

    Add method for exception

--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -1,22 +1,33 @@
 package task;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Event extends Task {
     protected LocalDateTime from;
     protected LocalDateTime to;
-    protected DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = LocalDateTime.parse(from, formatter);
-        this.to = LocalDateTime.parse(to, formatter);
+        try {
+            this.from = LocalDateTime.parse(from, DEFAULT_FORMATTER);
+            this.to = LocalDateTime.parse(to, DEFAULT_FORMATTER);
+        } catch (DateTimeParseException e) {
+            try {
+                this.from = LocalDateTime.parse(from, ALTERNATE_FORMATTER);
+                this.to = LocalDateTime.parse(to, ALTERNATE_FORMATTER);
+            } catch (DateTimeParseException e2) {
+                LocalDateTime now = LocalDateTime.now();
+                this.from = now;
+                this.to = now.plusDays(5);
+                System.out.println("Using local date as format was wrong");
+            }
+        }
     }
 
     @Override
     public String toFileFormat() {
-        return super.toFileFormat() + " | " + from.format(formatter) + " | " + to.format(formatter);
+        return super.toFileFormat() + " | " + from.format(DEFAULT_FORMATTER) + " | " + to.format(DEFAULT_FORMATTER);
     }
 
     @Override
@@ -26,7 +37,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        DateTimeFormatter displayFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy HHmm");
-        return "[E]" + super.toString() + " (from: " + from.format(displayFormatter) + " to: " + to.format(displayFormatter) + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(DISPLAY_FORMATTER) + " to: " + to.format(DISPLAY_FORMATTER) + ")";
     }
 }
