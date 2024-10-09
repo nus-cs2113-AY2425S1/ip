42cbb2cb372c3e00c0e29d87ddb314d6a07f59f4
DarkDragoon2002 <deepankrishnaa2002@gmail.com>
2024-10-10 03:03:03 +0800

    Updated JavaDoc Comments

--- src/main/java/taskpackage/Task.java
+++ src/main/java/taskpackage/Task.java
@@ -1,33 +1,42 @@
 package taskpackage; // Package for Task-related classes
 
-// Parent class Task which serves as a base for ToDo, Deadline, and Event tasks
+/**
+ * Represents a general task that can be extended to specific types of tasks such as ToDo, Deadline, and Event.
+ */
 public class Task {
 
-    // Array to keep track of all tasks added
-    public String inputString;
-
-
-    // Object-specific variables
-    public String taskString; // String to hold the task description
+    public String inputString; // Input string for the task
+    public String taskString;  // Description of the task
     public boolean isDone = false; // Boolean to track if the task is marked as done
 
-    // Constructor Function: initializes a Task object and adds it to the tasks array
-    public Task(String inputString, TaskList tasks){
+    /**
+     * Constructor for creating a general task.
+     *
+     * @param inputString The description of the task.
+     * @param tasks       The task list to which the task is added.
+     */
+    public Task(String inputString, TaskList tasks) {
         this.taskString = inputString; // Set the task description
-        tasks.addTask(this); // Add the current task to the array
+        tasks.addTask(this);           // Add the task to the task list
     }
 
-    // Method to display a message when a task is successfully created
-    public void constructorMessage(){
+    /**
+     * Displays a message confirming the task was successfully created.
+     */
+    public void constructorMessage() {
         System.out.println("Muy Bien, work hard compadre!"); // Success message in Spanish
         System.out.println("I've Added the Task:");
         System.out.println(checkboxString()); // Display the task in checkbox format
     }
 
-    // Method to create and return a string with a checkbox (marked or unmarked) for the task
-    public String checkboxString(){
+    /**
+     * Returns the task's string with a checkbox indicating whether it's done.
+     *
+     * @return The formatted task string.
+     */
+    public String checkboxString() {
         String returnString = "["; // Start of the checkbox string
-        if (this.isDone){
+        if (this.isDone) {
             returnString += "X"; // Mark the checkbox as done if the task is completed
         } else {
             returnString += " "; // Leave the checkbox empty if the task is not completed
@@ -35,6 +44,4 @@ public class Task {
         returnString += "] " + this.taskString; // Append the task description
         return returnString; // Return the formatted string
     }
-
-
 }
