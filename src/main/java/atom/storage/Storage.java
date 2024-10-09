995622e55005b35dbc10985ee2955b5cad1fff4d
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-10 04:00:33 +0800

    Add JavaDoc comments for "Storage" class

--- src/main/java/atom/storage/Storage.java
+++ src/main/java/atom/storage/Storage.java
@@ -14,11 +14,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Deals with loading tasks from the txt file to the task list
+ * and storing tasks from the task list to the txt file.
+ */
 public class Storage {
 
+    /** File path of the txt file */
     private File file;
-
     //filepath = "./data/AtomList.txt"
+
     public Storage(String filePath) {
         File folder = new File("./data");
         file = new File(filePath);
@@ -42,7 +47,15 @@ public class Storage {
         }
     }
 
-
+    /**
+     * Loads all the tasks stored in the txt file to an <code>ArrayList</code>.
+     * <p>
+     * If the file cannot be located, the <code>FileNotFoundException</code> will be caught
+     * in the method.
+     *
+     * @return <code>ArrayList</code> containing all the tasks that needs to be added
+     * to the task list stored in <code>TaskList</code>.
+     */
     public ArrayList<Task> load() {
         TaskList tasks = new TaskList();
         //load data (if any) to the list
@@ -78,14 +91,29 @@ public class Storage {
         return tasks.getTasksList();
     }
 
-    public void loadTodoTaskToList(TaskList tasks, String taskName, boolean doneStatus) {
+    /**
+     * Loads a <code>todo</code> task to the task list in <code>TaskList</code>.
+     *
+     * @param tasks Task list containing tasks.
+     * @param taskName Name of <code>todo</code> task.
+     * @param doneStatus <code>true</code> if task is marked as done, <code>false</code> otherwise.
+     */
+    private void loadTodoTaskToList(TaskList tasks, String taskName, boolean doneStatus) {
         Todo todo = tasks.addTodoTask(taskName);
         if (doneStatus) {
             todo.markAsDone();
         }
     }
 
-    public void loadDeadlineTaskToList(TaskList tasks, String taskName, boolean doneStatus,
+    /**
+     * Loads a <code>deadline</code> task to the task list in <code>TaskList</code>.
+     *
+     * @param tasks Task list containing tasks.
+     * @param taskName Name of <code>deadline</code> task.
+     * @param doneStatus <code>true</code> if task is marked as done, <code>false</code> otherwise.
+     * @param taskDueDate Task due date.
+     */
+    private void loadDeadlineTaskToList(TaskList tasks, String taskName, boolean doneStatus,
                                        String taskDueDate) {
 
         Deadline deadline = tasks.addDeadlineTask(taskName, taskDueDate);
@@ -94,7 +122,15 @@ public class Storage {
         }
     }
 
-    public void loadEventTaskToList(TaskList tasks, String taskName, boolean doneStatus,
+    /**
+     * Loads an <code>event</code> task to the task list in <code>TaskList</code>.
+     *
+     * @param tasks Task list containing tasks.
+     * @param taskName Name of <code>event</code> task.
+     * @param doneStatus <code>true</code> if task is marked as done, <code>false</code> otherwise.
+     * @param taskDuration Duration of task, from start date to end date.
+     */
+    private void loadEventTaskToList(TaskList tasks, String taskName, boolean doneStatus,
                                     String taskDuration) {
 
         String[] eventDurationParams = taskDuration.split("-");
@@ -107,6 +143,14 @@ public class Storage {
         }
     }
 
+    /**
+     * Stores all tasks in the task list to the txt file.
+     * <p>
+     * If there are any errors regarding input/output operations,
+     * the <code>IOException</code> will be caught and handled in the method.
+     *
+     * @param tasks Task list containing tasks.
+     */
     public void store(TaskList tasks) {
 
         ArrayList<Task> list = tasks.getTasksList();
@@ -137,5 +181,4 @@ public class Storage {
             System.out.println("Oh no!! Something went wrong..");
         }
     }
-
 }
