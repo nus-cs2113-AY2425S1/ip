5643810eb5e9fedcd0535fb4b472f745274fc12c
Vũ Nhật Nguyên Thư <126085175+Metanyu@users.noreply.github.com>
2024-10-11 16:28:03 +0800

    Revert "Revert "Update package parser, storage, task, ui to follow oop principle and coding convention. added java doc for task""

--- src/main/java/amy/task/Task.java
+++ src/main/java/amy/task/Task.java
@@ -1,3 +1,4 @@
+package task;
 public class Task {
     protected String description;
     protected boolean isDone;
@@ -14,10 +15,13 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
+    public void markTask(boolean isDone){
+        this.isDone = isDone;
+    }
     public void markAsDone(){
         isDone = true;
     }
-    public void markAdUndone(){
+    public void markAsUndone(){
         isDone = false;
     }
     public String toString(){
@@ -25,5 +29,21 @@ public class Task {
     }
     public String saveAsString(){
         return(String.join(" | ", new String[]{description, String.valueOf(isDone)}));
+    }
+    public static Task fromStorage(String data){
+        String[] dataArr = data.split(" \\| ");
+        String type = dataArr[0];
+        String description = dataArr[1];
+        boolean isDone = Boolean.parseBoolean(dataArr[2]);
+        switch(type){
+            case "T":
+                return new Todo(description, isDone);
+            case "D":
+                return new Deadline(description, isDone, dataArr[3]);
+            case "E":
+                return new Event(description, isDone, dataArr[3], dataArr[4]);
+            default:
+                return null;
+        }
     }
 }
