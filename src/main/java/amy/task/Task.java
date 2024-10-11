1ffefb137db4420c4dc450bd683bf0ec3bad9ff1
Vũ Nhật Nguyên Thư <126085175+Metanyu@users.noreply.github.com>
2024-10-11 16:23:47 +0800

    Revert "Level-9. added find task function based on keyword. added javadoc for public classes and methods"

--- src/main/java/amy/task/Task.java
+++ src/main/java/amy/task/Task.java
@@ -1,7 +1,4 @@
 package task;
-/**
- * Represents a task object that is responsible for storing the description and status of a task.
- */
 public class Task {
     protected String description;
     protected boolean isDone;
@@ -21,38 +18,32 @@ public class Task {
     public void markTask(boolean isDone){
         this.isDone = isDone;
     }
-    @Override
+    public void markAsDone(){
+        isDone = true;
+    }
+    public void markAsUndone(){
+        isDone = false;
+    }
     public String toString(){
         return("[" + getStatusIcon() + "] " + description);
     }
-
-    /**
-     * Returns the task in a format that can be saved to storage.
-     */
     public String saveAsString(){
         return(String.join(" | ", new String[]{description, String.valueOf(isDone)}));
     }
-
-    /**
-     * Returns a Task object from the storage data.
-     * @param data The data to be converted to a Task object.
-     * @return The Task object converted from the storage data.
-     */
     public static Task fromStorage(String data){
         String[] dataArr = data.split(" \\| ");
         String type = dataArr[0];
         String description = dataArr[1];
         boolean isDone = Boolean.parseBoolean(dataArr[2]);
-        return switch (type) {
-            case "T" -> new Todo(description, isDone);
-            case "D" -> new Deadline(description, isDone, dataArr[3]);
-            case "E" -> new Event(description, isDone, dataArr[3], dataArr[4]);
-            default -> null;
-        };
+        switch(type){
+            case "T":
+                return new Todo(description, isDone);
+            case "D":
+                return new Deadline(description, isDone, dataArr[3]);
+            case "E":
+                return new Event(description, isDone, dataArr[3], dataArr[4]);
+            default:
+                return null;
+        }
     }
-
-    public String getDescription() {
-        return description;
-    }
-    
 }
--- src/main/java/amy/ui/Ui.java
+++ src/main/java/amy/ui/Ui.java
@@ -1,9 +1,6 @@
 package ui;
 import java.util.Scanner;
-/**
- * Represents the user interface of the chatbot.
- * The Ui class is responsible for displaying messages to the user and reading user input.
- */
+
 public class Ui {
     private static Scanner scanner = new Scanner(System.in);
 
@@ -13,12 +10,8 @@ public class Ui {
      */
     public static void print(String text){
         System.out.println(text);
-        printFiller();
     }
 
-    /**
-     * Print a line of filler to separate different sections of the chatbot.
-     */
     public static void printFiller(){
         System.out.println("____________________________________________________________");
     }
@@ -27,12 +20,6 @@ public class Ui {
      * Display the start screen of the chatbot.
      */
     public static void printGreeting(){
-        String logo =    "     /   | |  \\/  |\\ \\ / /\n"
-                    +    "    / /| | | \\  / | \\ V / \n"
-                    +    "   / /_| | | |\\/| |  \\ /\n"     
-                    +    "  /  __  | | |  | |  | |\n"
-                    +    " /_/   |_| |_|  |_|  |_|  ";
-        System.out.println(logo);
         String greet = """
                        Hello! I'm Amy ~ 
                        What can I do for you? (^v^) 
@@ -49,20 +36,10 @@ public class Ui {
         System.out.println(bye);
         printFiller();
     }
-
-    /**
-     * Read the command input by the user.
-     * @return The command input by the user.
-     */
     public static String readCommand(){
-        String command = scanner.nextLine();
-        System.out.println("");
-        return command;        
+        return scanner.nextLine();
     }
 
-    /**
-     * Display the error message when the chatbot is unable to load tasks.
-     */
     public static void showLoadingError(){
         String text = "Unable to load tasks due to some reason T_T";
         print(text);
