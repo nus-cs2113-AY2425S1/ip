955040e6b1af85a2c51fda9d8f6de0f7b0828e72
rahuljai-05 <e1186335@u.nus.edu>
2024-10-11 18:41:04 +0800

    Add JavaDoc comment for Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+/**
+ * The Main class serves as the entry point for the Fenix application.
+ * It creates an instance of Fenix and starts the application by calling the run() method.
+ */
 public class Main {
     public static void main(String[] args) {
         Fenix fenix = new Fenix();
