bc46775badf0d59f239746374068a4e3d7f809e8
rahuljai-05 <e1186335@u.nus.edu>
2024-10-11 18:28:31 +0800

    Add JavaDoc comments for TaskHandler class

--- src/main/java/TaskHandler.java
+++ src/main/java/TaskHandler.java
@@ -2,20 +2,42 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * The TaskHandler class manages the list of tasks, including adding, removing,
+ * marking as done, and unmarking tasks. It interacts with the Fenix class
+ * to validate and retrieve task indices based on user input.
+ */
 public class TaskHandler {
     private Fenix fenix;
     private ArrayList<Task> taskArrayList;
 
+    /**
+     * Constructs a TaskHandler associated with the given Fenix instance.
+     * Initializes an empty list of tasks.
+     *
+     * @param fenix The Fenix instance that this TaskHandler interacts with.
+     */
     public TaskHandler(Fenix fenix) {
         this.fenix = fenix;
         this.taskArrayList = new ArrayList<>();
     }
 
-    // Return an unmodifiable view of the taskArrayList
+    /**
+     * Returns an unmodifiable view of the task list to prevent external modification.
+     *
+     * @return an unmodifiable list of tasks.
+     */
     public List<Task> getTaskArrayList() {
         return Collections.unmodifiableList(taskArrayList);
     }
 
+    /**
+     * Marks the task specified by the task number as done.
+     * Retrieves the task index from Fenix and marks the corresponding task.
+     *
+     * @param taskNumber The task number provided by the user.
+     * @return The task that was marked as done, or null if the task number is invalid.
+     */
     public Task markAsDone(String taskNumber) {
         int taskIndex = fenix.getTaskIndexFromInput(taskNumber);
         if (taskIndex == -1) {
@@ -24,12 +46,25 @@ public class TaskHandler {
         return markTaskAsDone(taskIndex);
     }
 
+    /**
+     * Marks the task at the specified index as done.
+     *
+     * @param taskNumber The index of the task to mark as done.
+     * @return The task that was marked as done.
+     */
     private Task markTaskAsDone(int taskNumber) {
         Task task = taskArrayList.get(taskNumber);
         task.markAsDone();
         return task;
     }
 
+    /**
+     * Unmarks the task specified by the task number, setting it as not done.
+     * Retrieves the task index from Fenix and unmarks the corresponding task.
+     *
+     * @param taskNumber The task number provided by the user.
+     * @return The task that was unmarked, or null if the task number is invalid.
+     */
     public Task unmarkAsDone(String taskNumber) {
         int taskIndex = fenix.getTaskIndexFromInput(taskNumber);
         if (taskIndex == -1) {
@@ -38,16 +73,34 @@ public class TaskHandler {
         return unmarkTaskAsDone(taskIndex);
     }
 
+    /**
+     * Unmarks the task at the specified index, setting it as not done.
+     *
+     * @param taskNumber The index of the task to unmark.
+     * @return The task that was unmarked.
+     */
     private Task unmarkTaskAsDone(int taskNumber) {
         Task task = taskArrayList.get(taskNumber);
         task.unmarkAsDone();
         return task;
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void storeTask(Task task) {
         taskArrayList.add(task);
     }
 
+    /**
+     * Deletes the task specified by the task number from the task list.
+     * Retrieves the task index from Fenix and removes the corresponding task.
+     *
+     * @param taskNumber The task number provided by the user.
+     * @return The task that was deleted, or null if the task number is invalid.
+     */
     public Task deleteTask(String taskNumber) {
         int taskIndex = fenix.getTaskIndexFromInput(taskNumber);
         if (taskIndex == -1) {
