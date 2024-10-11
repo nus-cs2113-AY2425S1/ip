ac10ab9c20b46f5bfc8db5f4e96f926d092e5adc
rahuljai-05 <e1186335@u.nus.edu>
2024-10-11 17:55:15 +0800

    Define new constant in SampleStrings to make the previous commit work

--- src/main/java/SampleStrings.java
+++ src/main/java/SampleStrings.java
@@ -5,6 +5,7 @@ public interface SampleStrings {
     String HORIZONTAL_LINE_FENIX_MODIFICATION = "\t\t" + "*".repeat(HORIZONTAL_LINE_FENIX_MODIFICATION_LENGTH);
     String GREETING = "Greetings. I am Fenix, your digital assistant.";
     String SERVICE_PROMPT = "How may I be of service to you today?";
+    String MATCHED_TASKS = "Here are the tasks that align with your request";
     String FAREWELL = "It has been a pleasure assisting you. Farewell.";
     String[] ADD = {"The task has been duly noted and added to your list.", "added: "};
     String[] DELETE = {"The task has been removed from your list as per your request.", "deleted: "};
