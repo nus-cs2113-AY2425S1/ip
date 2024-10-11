f34e509a373f548330b4034c226c3449363b17bf
tkhahns <111437863+tkhahns@users.noreply.github.com>
2024-10-11 12:57:19 +0800

    A-JavaDoc

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,25 @@
+/**
+ * Represents a Deadline task in the KBot application.
+ * A Deadline task has a specific deadline by which it needs to be completed.
+ */
 public class Deadline extends Task {
+
+    /**
+     * Constructs a Deadline task with the specified description and time.
+     *
+     * @param description The description of the Deadline task.
+     * @param time       The time by which the task needs to be completed.
+     */
     public Deadline(String description, String time) {
         super(description, time); // Store time
     }
 
+    /**
+     * Converts the Deadline task to a string format for file storage.
+     * The format is: D | isDone | description | time
+     *
+     * @return A string representation of the Deadline task for saving to a file.
+     */
     @Override
     public String toFileString() {
         return "D | " + (isDone ? 1 : 0) + " | " + description + " | " + time;
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,30 @@
+/**
+ * Represents an Event task in the KBot application.
+ * An Event task has a start time and an end time, indicating when the event occurs.
+ */
 public class Event extends Task {
-    private String from;
-    private String to;
+    private String from; // The start time of the event
+    private String to;   // The end time of the event
 
+    /**
+     * Constructs an Event task with the specified description, start time, and end time.
+     *
+     * @param description The description of the Event task.
+     * @param from       The starting time of the event.
+     * @param to         The ending time of the event.
+     */
     public Event(String description, String from, String to) {
         super(description, null); // No direct time field; we'll use from and to
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Converts the Event task to a string format for file storage.
+     * The format is: E | isDone | description | from | to
+     *
+     * @return A string representation of the Event task for saving to a file.
+     */
     @Override
     public String toFileString() {
         return "E | " + (isDone ? 1 : 0) + " | " + description + " | " + from + " to: " + to;
