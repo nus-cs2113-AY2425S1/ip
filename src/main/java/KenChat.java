02fccb16e496cd22bb106db1979ab815b8ed4fda
SemiColonKen <kennethtanjunhao@gmail.com>
2024-10-13 13:52:19 +0800

    Level-0

---
+++ src/main/java/KenChat.java
@@ -0,0 +1,13 @@
+public class KenChat {
+
+    public static void main(String[] args) {
+        String chatBotName = "KenChat";
+        String line = "____________________________________" ;
+        System.out.println(line);
+        System.out.println("Hello! I'm " + chatBotName);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
