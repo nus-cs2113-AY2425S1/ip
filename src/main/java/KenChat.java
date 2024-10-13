98d29ab5b04f21628eb15b371f3e648cf9879cbd
SemiColonKen <kennethtanjunhao@gmail.com>
2024-10-13 14:09:37 +0800

    Level-2

--- src/main/java/KenChat.java
+++ src/main/java/KenChat.java
@@ -20,21 +20,43 @@ public class KenChat {
         printLine();
     }
 
-    public static void displayOutput(String display){
+    public static void displayList(String[] doList){
         printLine();
-        System.out.println(display);
+        for (int i=0; i< doList.length; i++){
+            if(doList[i] != null){
+                System.out.println((i+1)+". "+doList[i]);
+            }
+        }
         printLine();
     }
+
+    public static void addList(String[] doList, String item){
+        printLine();
+        System.out.println("added: "+item);
+        printLine();
+        for (int i=0; i< doList.length; i++){
+            if(doList[i] == null){
+                doList[i] = item;
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         boolean running = true;
+        int arraySize = 100;
+        String[] doList = new String[arraySize];
         startProgramme();
         while (running){
             System.out.println();
             String str= sc.nextLine();
-            if (str.equalsIgnoreCase("bye")  )
+            if (str.equalsIgnoreCase("bye"))
                 running = false;
-            displayOutput(str);
+            else if(str.equalsIgnoreCase("list"))
+                displayList(doList);
+            else
+                addList(doList, str);
         }
         endProgramme();
     }
