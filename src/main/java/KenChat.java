2fdeff946645d6bbec1e1a7b8e60a023158acc26
SemiColonKen <kennethtanjunhao@gmail.com>
2024-10-13 12:48:41 +0800

    Level-0

--- src/main/java/KenChat.java
+++ src/main/java/KenChat.java
@@ -1,13 +1,42 @@
-public class KenChat {
+import java.util.*;
 
-    public static void main(String[] args) {
-        String chatBotName = "KenChat";
+public class KenChat {
+    public static void printline() {
         String line = "____________________________________" ;
         System.out.println(line);
+    }
+
+    public static void startPrograme() {
+        String chatBotName = "KenChat";
+        printline();
         System.out.println("Hello! I'm " + chatBotName);
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        printline();
+    }
+
+    public static void endPrograme() {
+        printline();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        printline();
+    }
+
+    public static void displayOutput(String display){
+        printline();
+        System.out.println(display);
+        printline();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        boolean running = true;
+        startPrograme();
+        while (running){
+            System.out.println();
+            String str= sc.nextLine();
+            if (str.equalsIgnoreCase("bye")  )
+                running = false;
+            displayOutput(str);
+        }
+        endPrograme();
     }
 }
