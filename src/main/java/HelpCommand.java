b1675c01dc4040b5e39ecb5cb375d1ec5f927c24
SemiColonKen <e0811519@u.nus.edu>
2024-10-15 19:41:01 +0800

    A-JavaDoc

--- src/main/java/HelpCommand.java
+++ src/main/java/HelpCommand.java
@@ -1,7 +1,17 @@
 package main.java;
 
+/**
+ * Represents a command to display help information to the user.
+ */
 public class HelpCommand extends Command {
 
+    /**
+     * Performs the help command by displaying all the valid commands the user can enter.
+     *
+     * @param tasks The task list that the command will work on.
+     * @param ui The UI component of the chat bot.
+     * @param storage The storage component of the chat bot.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showHelp();
--- src/main/java/MarkCommand.java
+++ src/main/java/MarkCommand.java
@@ -1,5 +1,8 @@
 package main.java;
 
+/**
+ * Performs a command to mark a task as completed in the task list.
+ */
 public class MarkCommand extends Command {
     private final int index;
 
@@ -7,6 +10,15 @@ public class MarkCommand extends Command {
         this.index = index - 1;
     }
 
+    /**
+     * Performs the mark command, marking the specific task as done.
+     * Saves the updated task list to the storage and notifying the user through display via the UI
+     *
+     * @param tasks The task list that the command will work on.
+     * @param ui The UI component of the chat bot.
+     * @param storage The storage component of the chat bot.
+     * @throws KenChatException If the task list is empty or the index is invalid.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws KenChatException {
         if (tasks.getSize() == 0) {
