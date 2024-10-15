48f5dab2419e126f38d95d8d538b769884f13333
SemiColonKen <e0811519@u.nus.edu>
2024-10-15 19:39:57 +0800

    A-JavaDoc

--- src/main/java/AddCommand.java
+++ src/main/java/AddCommand.java
@@ -1,5 +1,8 @@
 package main.java;
 
+/**
+ * Represents the command to add a task to the task list.
+ */
 public class AddCommand extends Command {
     private final Task task;
 
@@ -7,6 +10,15 @@ public class AddCommand extends Command {
         this.task = task;
     }
 
+    /**
+     * Perform the add command by adding the task to the task list,
+     * saving the task list and notifying the user.
+     *
+     * @param tasks The task list.
+     * @param ui The UI component which the user will see.
+     * @param storage The storage component to save the updated task list.
+     * @throws KenChatException If there is an error when saving the task list.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws KenChatException {
         tasks.addTask(task);
--- src/main/java/DeleteCommand.java
+++ src/main/java/DeleteCommand.java
@@ -1,5 +1,8 @@
 package main.java;
 
+/**
+ * Represents a command to delete a task from the task list.
+ */
 public class DeleteCommand extends Command {
     private final int index;
 
@@ -7,6 +10,15 @@ public class DeleteCommand extends Command {
         this.index = index - 1;
     }
 
+    /**
+     * Performs the delete command, removing the specific task from the task list, using the task index.
+     * This is followed by saving to the storage and notifying the user through a display from the UI.
+     *
+     * @param tasks The task list that the command will work on.
+     * @param ui The UI component of the chat bot.
+     * @param storage The storage component of the chat bot.
+     * @throws KenChatException If the task list is empty, or if the task number is invalid.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws KenChatException {
         if (tasks.getSize() == 0) {
