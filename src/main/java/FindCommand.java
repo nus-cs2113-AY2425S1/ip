e193e2e2d1ce526c445c664e174c1e19aa2a7a80
SemiColonKen <e0811519@u.nus.edu>
2024-10-15 21:17:19 +0800

    Added JavaDoc for FindCommand

--- src/main/java/FindCommand.java
+++ src/main/java/FindCommand.java
@@ -2,6 +2,9 @@ package main.java;
 
 import java.util.Map;
 
+/**
+ * Represents a command to find tasks in the task list according to keyword(s).
+ */
 public class FindCommand extends Command {
     private final String keyword;
 
@@ -9,6 +12,15 @@ public class FindCommand extends Command {
         this.keyword = keyword;
     }
 
+    /**
+     * Performs the find command to search for tasks containing the specific keyword(s).
+     *
+     *
+     * @param tasks The task list that the command will work on.
+     * @param ui The UI component of the chat bot.
+     * @param storage The storage component of the chat bot.
+     * @throws KenChatException If an error occurs while performing the command.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws KenChatException {
         // Use the findTasksByKeywordWithIndex method that returns a Map of <index, task>
